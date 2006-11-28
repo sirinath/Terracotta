901dd77b96dc8a5745f44b2d44587d8d30801e58
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-11-28 01:31:26 +0000

    Creating lawton branch

---
+++ branches/lawton/external/QA/test-qa/SpringBasedBlogEngine/src/main/java/org/terracotta/spring/blog/BlogUser.java
@@ -0,0 +1,29 @@
+package org.terracotta.spring.blog;
+
+/**
+ * Blogger identity.
+ */
+public class BlogUser {
+
+  public static final BlogUser ANONYMOUS = new BlogUser("Anonymous Coward");
+
+  private String               username;
+
+  public BlogUser() {
+    username = null;
+  }
+
+  public BlogUser(final String username) {
+    this();
+    setUsername(username);
+  }
+
+  public synchronized void setUsername(final String username) {
+    this.username = username;
+  }
+
+  public synchronized String getUsername() {
+    return username;
+  }
+
+}
