102162a0b0302d86dc3c25feaace85efc201aec9
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-11-28 03:38:33 +0000

    Re-creating the lawton branch

---
+++ branches/lawton/enterprise/overwrite/open/code/base/dso-common/tests.unit/com/tc/net/protocol/transport/ConnectionPolicyTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2003-2006 Terracotta, Inc. All rights reserved.
+ */
+package com.tc.net.protocol.transport;
+
+import junit.framework.TestCase;
+
+public class ConnectionPolicyTest extends TestCase {
+  private ConnectionPolicy policy;
+  
+  public void tests() throws Exception {
+    int maxConnections = 2;
+    policy = new ConnectionPolicyImpl(maxConnections);
+
+    policy.clientConnected();
+    assertFalse(policy.maxConnectionsExceeded());
+    
+    policy.clientConnected();
+    assertFalse(policy.maxConnectionsExceeded());
+    
+    policy.clientConnected();
+    assertTrue(policy.maxConnectionsExceeded());
+    
+    policy.clientDisconnected();
+    assertFalse(policy.maxConnectionsExceeded());
+    
+    policy.clientConnected();
+    assertTrue(policy.maxConnectionsExceeded());
+    
+    assertEquals(maxConnections, policy.getMaxConnections());
+    
+    policy = new ConnectionPolicyImpl(-1);
+    for (int i=0; i<100; i++) {
+      policy.clientConnected();
+      assertFalse(policy.maxConnectionsExceeded());
+    }
+  }
+}
