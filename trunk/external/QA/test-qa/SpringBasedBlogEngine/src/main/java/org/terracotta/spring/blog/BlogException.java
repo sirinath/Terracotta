3bfb2aae092c023db7b3466cfdb01458892b39de
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-11-28 00:46:47 +0000

    Initial code import for the Terracotta Subversion repository

---
+++ trunk/external/QA/test-qa/SpringBasedBlogEngine/src/main/java/org/terracotta/spring/blog/BlogException.java
@@ -0,0 +1,21 @@
+package org.terracotta.spring.blog;
+
+public class BlogException extends Exception {
+
+  public BlogException() {
+    super();
+  }
+
+  public BlogException(String message) {
+    super(message);
+  }
+
+  public BlogException(Throwable cause) {
+    super(cause);
+  }
+
+  public BlogException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
