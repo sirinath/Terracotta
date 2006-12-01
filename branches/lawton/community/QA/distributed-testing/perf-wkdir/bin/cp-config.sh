94ad218c392dc51ba08812651cab4ca0c27709ed
cfisher <cfisher@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-12-01 06:07:25 +0000

    merging revisions 128,131,150,156-159 from private branch: updating copyright statement

--- branches/lawton/community/QA/distributed-testing/perf-wkdir/bin/cp-config.sh
+++ branches/lawton/community/QA/distributed-testing/perf-wkdir/bin/cp-config.sh
@@ -1,3 +1,9 @@
+#
+# All content copyright (c) 2003-2006 Terracotta, Inc.,
+# except as may otherwise be noted in a separate copyright notice.
+# All rights reserved
+#
+
 #!/bin/bash
 #-------------------------------------------------------------------------------
 # This script will copy the tc-config.xml to the L2
--- branches/lawton/community/QA/test-qa-pack/GUI/ListRoots.java
+++ branches/lawton/community/QA/test-qa-pack/GUI/ListRoots.java
@@ -1,3 +1,6 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
 import com.tc.admin.ConnectionListener;
 import com.tc.admin.ServerConnectionManager;
 import com.tc.admin.dso.DSOField;
