7c75304654f43d9c8f1e0f7285001582e17098fb
cfisher <cfisher@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-12-01 04:35:52 +0000

    more copyright update

--- branches/private/copyright-processing/lawton/community/open/code/base/services-tests/tests.base/com/tctest/util/Timer.java
+++ branches/private/copyright-processing/lawton/community/open/code/base/services-tests/tests.base/com/tctest/util/Timer.java
@@ -1,6 +1,6 @@
 package com.tctest.util;
 /*
- * Copyright (c) 2003-2006 Terracotta, Inc. All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
  */
 
 public class Timer
