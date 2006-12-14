d9580e2abe52ccfa0dd8b113c451e0b6cd9fc9d7
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-12-14 21:17:14 +0000

    Fixed mistake where a property name was evaluated instead of it's value

--- dso/branches/ivy-build-integration/code/base/common/tests.base/com/tc/test/TestConfigObject.java
+++ dso/branches/ivy-build-integration/code/base/common/tests.base/com/tc/test/TestConfigObject.java
@@ -1,5 +1,6 @@
 /**
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.test;
 
@@ -146,10 +147,11 @@ public class TestConfigObject {
       else break;
     }
 
-    if (baseDir == null) throw new RuntimeException("System Property: " + TC_BASE_DIR + " is not valid.\n"
-                                                    + System.getProperty(TC_BASE_DIR));
-
-    if (Directories.TC_INSTALL_ROOT_PROPERTY_NAME == null) {
+    if (baseDir == null || baseDir.trim().equals("")) throw new RuntimeException("System Property: " + TC_BASE_DIR
+                                                                                 + " is not valid.\n"
+                                                                                 + System.getProperty(TC_BASE_DIR));
+    
+    if (StringUtils.isBlank(System.getProperty(Directories.TC_INSTALL_ROOT_PROPERTY_NAME))) {
       System.setProperty(Directories.TC_INSTALL_ROOT_PROPERTY_NAME, baseDir);
       System.setProperty(Directories.TC_INSTALL_ROOT_IGNORE_CHECKS_PROPERTY_NAME, "true");
     }
