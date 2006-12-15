96c649d1aa1eaee10300e68b9d99ff11c445e2ff
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-12-15 21:47:48 +0000

    Merged revisions 385-457 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk
    ........
      r387 | jgalang | 2006-12-11 12:18:34 -0800 (Mon, 11 Dec 2006) | 2 lines
      
      Merged rev 385 from 2.2.0
    ........
      r389 | jgalang | 2006-12-11 14:08:30 -0800 (Mon, 11 Dec 2006) | 2 lines
      
      Merged rev 388 from 2.0.0
    ........
      r390 | nharward | 2006-12-11 14:32:31 -0800 (Mon, 11 Dec 2006) | 1 line
      
      LKC-2578: changing the name of the "node name" system property to match up with one we already use for JMX bean names.
    ........
      r398 | jgalang | 2006-12-12 11:27:13 -0800 (Tue, 12 Dec 2006) | 2 lines
      
      Merged rev 396 from 2.2.0
    ........
      r402 | ssubbiah | 2006-12-12 13:32:27 -0800 (Tue, 12 Dec 2006) | 2 lines
      
      Merging r375 into trunk from 2.2.1 (Disabling Aging and adding some debug info into LFU)
    ........
      r404 | jgalang | 2006-12-12 14:00:06 -0800 (Tue, 12 Dec 2006) | 2 lines
      
      Merged rev 403 from 2.2.0
    ........
      r406 | alex | 2006-12-12 14:12:27 -0800 (Tue, 12 Dec 2006) | 1 line
      
      405 from branches/2.2.1 into trunk
    ........
      r412 | teck | 2006-12-12 16:45:01 -0800 (Tue, 12 Dec 2006) | 2 lines
      
      merge r411 from 2.2.1 (remove Sun code)
    ........
      r414 | jgalang | 2006-12-12 16:58:53 -0800 (Tue, 12 Dec 2006) | 2 lines
      
      Merged rev 413 from 2.2.0
    ........
      r417 | jgalang | 2006-12-12 19:43:57 -0800 (Tue, 12 Dec 2006) | 2 lines
      
      Merged rev 415-416 from 2.2.0
    ........
      r422 | jgalang | 2006-12-13 11:25:53 -0800 (Wed, 13 Dec 2006) | 2 lines
      
      Merged rev 421 from 2.2.0
    ........
      r426 | ssubbiah | 2006-12-13 12:15:32 -0800 (Wed, 13 Dec 2006) | 2 lines
      
      Merged 424 from 2.2.1 ( disabling test)
    ........
      r429 | teck | 2006-12-13 14:07:46 -0800 (Wed, 13 Dec 2006) | 2 lines
      
      merge from 2.2.1 (rev 428) -- LKC-3240, don't print stack trace on bind exception
    ........
      r431 | jgalang | 2006-12-13 14:40:10 -0800 (Wed, 13 Dec 2006) | 2 lines
      
      Merged rev 430 from 2.2.0
    ........
      r435 | teck | 2006-12-13 16:18:46 -0800 (Wed, 13 Dec 2006) | 2 lines
      
      merge r434 from 2.2.1 branch -- Fix for LKC-3239 (don't release startup lock on server shutdown)
    ........
      r440 | nharward | 2006-12-13 17:26:50 -0800 (Wed, 13 Dec 2006) | 2 lines
      
      Merge from 2.2.1 for CDV-34: allows the Terracotta Server to be [experimentally] run with a 1.6 JDK (building still requires a 1.5 JDK).
    ........
      r449 | jvoegele | 2006-12-14 15:00:35 -0800 (Thu, 14 Dec 2006) | 2 lines
      
      Updated build system to write the TC_CONTAINER_HOME value to tests-prepared.properties
    ........
      r450 | jvoegele | 2006-12-14 16:01:17 -0800 (Thu, 14 Dec 2006) | 3 lines
      
      Updated build system to write the TC_CONTAINER_HOME *or* the build-config property
      tc.tests.configuration.appserver.home value to tests-prepared.properties
    ........
      r453 | jvoegele | 2006-12-14 18:17:35 -0800 (Thu, 14 Dec 2006) | 1 line
      
      LKC-3244: Removed dependency on Cygwin for building and testing.
    ........
      r457 | asi | 2006-12-15 10:54:56 -0800 (Fri, 15 Dec 2006) | 1 line
      
      Add more debug statements to the test.
    ........

--- dso/branches/ivy-build-integration/code/demos/dso/sharededitor/run.bat
+++ dso/branches/ivy-build-integration/code/demos/dso/sharededitor/run.bat
@@ -1,11 +1,16 @@
 @echo off
-rem @COPYRIGHT@
+
+rem
+rem  All content copyright (c) 2003-2006 Terracotta, Inc.,
+rem  except as may otherwise be noted in a separate copyright notice.
+rem  All rights reserved.
+rem
 
 setlocal
 set topdir=%~d0%~p0..\..
 if "x%tc_install_dir%"=="x" set tc_install_dir=%topdir%\..
 cd %~d0%~p0
 call "%topdir%\libexec\tc-functions.bat" tc_install_dir "%tc_install_dir%" true
-call "%topdir%\bin\dso-env.bat" tc-config.xml
+call "%topdir%\bin\dso-env.bat" -q tc-config.xml
 start call "%topdir%\libexec\tc-functions.bat" tc_java %tc_java_opts% -cp "classes" "-Djava.awt.Window.locationByPlatform=true" demo.sharededitor.Main %*
 endlocal
--- dso/branches/ivy-build-integration/kits/skeletons/shared-base/windows/bin/dso-env.bat
+++ dso/branches/ivy-build-integration/kits/skeletons/shared-base/windows/bin/dso-env.bat
@@ -1,13 +1,30 @@
 @echo off
-rem @COPYRIGHT@
+
+rem
+rem  All content copyright (c) 2003-2006 Terracotta, Inc.,
+rem  except as may otherwise be noted in a separate copyright notice.
+rem  All rights reserved.
+rem
 
 SET TOPDIR=%~d0%~p0..
 
-IF NOT "%1" == "-v" GOTO tc_dso_env_1
-SHIFT
+IF "%1" == "-q" GOTO tc_dso_env_0
 SETLOCAL
-SET __DSO_ENV_VERBOSE=true
+SET __DSO_ENV_QUIET=falsese
+GOTO tc_dso_env_1
 
+:tc_dso_env_0
+  IF NOT "%TOPDIR%" == "" GOTO tc_dso_env_0_0
+    ECHO Error: When the -q option is specified, I expect that
+    ECHO the environment variable TOPDIR is set so that I
+    ECHO can locate the libexec directory of your Terracotta
+    ECHO software installation.
+    GOTO tc_dso_env_3
+  
+  :tc_dso_env_0_0
+    SHIFT
+    SET __DSO_ENV_QUIET=false
+  
 :tc_dso_env_1
   SET __DSO_ENV_CONFIG=%~1
 
@@ -18,10 +35,12 @@ SET __DSO_ENV_VERBOSE=true
  
   SET TC_JAVA_OPTS=-Xbootclasspath/p:"%DSO_BOOT_JAR%" -Dtc.install-root="%TC_INSTALL_DIR%"
   IF NOT "%__DSO_ENV_CONFIG%" == "" SET TC_JAVA_OPTS=%TC_JAVA_OPTS% %D_TC_CONFIG%
-  IF "%__DSO_ENV_VERBOSE%" == "" GOTO tc_dso_env_2
+  IF NOT "%__DSO_ENV_QUIET%" == "" GOTO tc_dso_env_2
+  GOTO tc_dso_env_3
 
+:tc_dso_env_2
   ECHO %TC_JAVA_OPTS%
   ENDLOCAL
 
-:tc_dso_env_2
+:tc_dso_env_3
 
