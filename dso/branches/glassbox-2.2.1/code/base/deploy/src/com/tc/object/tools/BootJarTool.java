63ed3a662c0deb441e0544b157f93d16aebf443e
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2006-12-19 19:54:41 +0000

    Merged revisions 463-510 via svnmerge from https://svn.terracotta.org/repo/tc/dso/branches/2.2.1

--- dso/branches/glassbox-2.2.1/code/base/deploy/src/com/tc/object/tools/BootJarTool.java
+++ dso/branches/glassbox-2.2.1/code/base/deploy/src/com/tc/object/tools/BootJarTool.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.object.tools;
 
@@ -1129,7 +1130,7 @@ public class BootJarTool {
     // 1st pass
     ClassReader cr = new ClassReader(bytes);
     ClassWriter cw = new ClassWriter(true);
-    ClassVisitor cv = new StringBufferAdapter(cw, Vm.getMajorVersion());
+    ClassVisitor cv = new StringBufferAdapter(cw, Vm.VERSION);
     cr.accept(cv, false);
     bytes = cw.toByteArray();
 
@@ -1156,7 +1157,7 @@ public class BootJarTool {
     // 5th pass (fixups)
     cr = new ClassReader(bytes);
     cw = new ClassWriter(true);
-    cv = new StringBufferAdapter.FixUp(cw, Vm.getMajorVersion());
+    cv = new StringBufferAdapter.FixUp(cw, Vm.VERSION);
     cr.accept(cv, false);
     bytes = cw.toByteArray();
 
@@ -1236,7 +1237,7 @@ public class BootJarTool {
     ClassReader cr = new ClassReader(orig);
     ClassWriter cw = new ClassWriter(true);
 
-    ClassVisitor cv = new JavaLangStringAdapter(cw, Vm.getMajorVersion(), shouldIncludeStringBufferAndFriends());
+    ClassVisitor cv = new JavaLangStringAdapter(cw, Vm.VERSION, shouldIncludeStringBufferAndFriends());
     cr.accept(cv, false);
 
     bootJar.loadClassIntoJar("java.lang.String", cw.toByteArray(), false);
