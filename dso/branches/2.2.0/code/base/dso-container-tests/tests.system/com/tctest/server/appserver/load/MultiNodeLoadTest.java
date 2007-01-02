54bcb4ccc867c4c696f6caedd1c6432aa219f4a7
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-01-02 18:32:15 +0000

    Reverted last change

--- dso/branches/2.2.0/code/base/dso-container-tests/tests.system/com/tctest/server/appserver/load/MultiNodeLoadTest.java
+++ dso/branches/2.2.0/code/base/dso-container-tests/tests.system/com/tctest/server/appserver/load/MultiNodeLoadTest.java
@@ -25,7 +25,6 @@ public final class MultiNodeLoadTest extends AbstractAppServerTestCase {
   }
 
   public void testFourNodeLoad() throws Throwable {
-  	collectVmStats();
     startDsoServer();
     runNodes(4);
   }
