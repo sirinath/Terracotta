5627a3c67eed6c268e90a204da0ae9615c79cdd7
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-01-31 22:28:54 +0000

    Removing the word 'instrumentation' and replacing it with 'configuration' as that is more accurate.  Also changed the plugin version to move independently from the version of the package it supports (there may be many revisions to this plugin over time to support more of AWT)

--- dso/branches/config-bundles/code/plugins/clustered-awt-1.4/src/main/java/org/terracotta/plugins/configuration/awt/AWTTerracottaInstrumentation.java
+++ dso/branches/config-bundles/code/plugins/clustered-awt-1.4/src/main/java/org/terracotta/plugins/configuration/awt/AWTTerracottaInstrumentation.java
@@ -1,4 +1,4 @@
-package org.terracotta.plugins.instrumentation.awt;
+package org.terracotta.plugins.configuration.awt;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
