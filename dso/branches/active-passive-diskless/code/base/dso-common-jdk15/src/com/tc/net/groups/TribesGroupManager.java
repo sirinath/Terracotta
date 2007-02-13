b64660060a9005ad6bc0615cf2af2889cbb55148
ssubbiah <ssubbiah@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-02-13 06:24:52 +0000

    Tribes Group Manager

---
+++ dso/branches/active-passive-diskless/code/base/dso-common-jdk15/src/com/tc/net/groups/TribesGroupManager.java
@@ -0,0 +1,66 @@
+package com.tc.net.groups;
+
+
+import org.apache.catalina.tribes.Channel;
+import org.apache.catalina.tribes.ChannelException;
+import org.apache.catalina.tribes.ChannelListener;
+import org.apache.catalina.tribes.Member;
+import org.apache.catalina.tribes.MembershipListener;
+import org.apache.catalina.tribes.group.GroupChannel;
+
+import com.tc.exception.ImplementMe;
+import com.tc.logging.TCLogger;
+import com.tc.logging.TCLogging;
+import com.tc.net.groups.GroupException;
+import com.tc.net.groups.GroupManager;
+import com.tc.net.groups.NodeID;
+
+import java.io.Serializable;
+
+public class TribesGroupManager implements GroupManager, ChannelListener, MembershipListener {
+
+  private static final TCLogger logger = TCLogging.getLogger(TribesGroupManager.class);
+
+  private final GroupChannel    group;
+
+  private Member                thisMember;
+  private NodeID                thisNodeID;
+
+  public TribesGroupManager() {
+    group = new GroupChannel();
+    group.addChannelListener(this);
+    group.addMembershipListener(this);
+  }
+
+  public NodeID join() throws GroupException {
+    try {
+      group.start(Channel.DEFAULT);
+      this.thisMember = group.getLocalMember(false);
+      this.thisNodeID = new NodeID(this.thisMember.getName());
+      return this.thisNodeID;
+    } catch (ChannelException e) {
+      logger.error(e);
+      throw new GroupException(e);
+    }
+  }
+
+  public boolean accept(Serializable msg, Member sender) {
+    throw new ImplementMe();
+  }
+
+  public void messageReceived(Serializable msg, Member sender) {
+    throw new ImplementMe();
+
+  }
+
+  public void memberAdded(Member member) {
+    throw new ImplementMe();
+
+  }
+
+  public void memberDisappeared(Member member) {
+    throw new ImplementMe();
+
+  }
+
+}
