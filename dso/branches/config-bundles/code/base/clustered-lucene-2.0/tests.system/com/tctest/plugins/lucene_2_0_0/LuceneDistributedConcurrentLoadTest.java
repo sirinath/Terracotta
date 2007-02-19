16e36b2ae1cac3637f3d4de2cc74be6a3d229b7f
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-02-19 20:57:20 +0000

    Initial stages of official Lucene support - Simple clustered Lucene unit test

---
+++ dso/branches/config-bundles/code/base/clustered-lucene-2.0/tests.system/com/tctest/plugins/lucene_2_0_0/LuceneDistributedConcurrentLoadTest.java
@@ -0,0 +1,13 @@
+package com.tctest.plugins.lucene_2_0_0;
+
+import com.tctest.TransparentTestBase;
+
+public class LuceneDistributedConcurrentLoadTest extends TransparentTestBase {
+
+  @Override
+  protected Class getApplicationClass() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
