e88857c40625706ea4d3c12738aec2575feaf6ed
cfisher <cfisher@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-02-21 00:24:11 +0000

    creating a branch for synchronous commit related developement

---
+++ dso/branches/synch-commit/code/demos/spring/events/start-tomcat2.bat
@@ -0,0 +1,23 @@
+@echo off
+
+rem
+rem  All content copyright (c) 2003-2006 Terracotta, Inc.,
+rem  except as may otherwise be noted in a separate copyright notice.
+rem  All rights reserved.
+rem
+
+setlocal
+set topdir=%~d0%~p0..\..\..
+set catalina_home=%topdir%\vendors\tomcat5.5
+call "%topdir%\bin\dso-env.bat" -q tc-config.xml
+set java_opts=%tc_java_opts% -Dcom.sun.management.jmxremote
+set jpda_transport=dt_socket
+set jpda_address=localhost:8002
+
+set catalina_base=tomcat2
+set java_home=%tc_java_home%
+ 
+start "terracotta for spring: event sample: 8082" "%catalina_home%\bin\catalina.bat" jpda run
+
+:end
+endlocal
