aa15afe1bed4113e1e28d3623f1523f5172dcc08
ssubbiah <ssubbiah@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-03-02 08:55:23 +0000

    Missed this one out

---
+++ dso/branches/active-passive-diskless/code/base/dso-l2/src/com/tc/objectserver/tx/TransactionAccount.java
@@ -0,0 +1,34 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.objectserver.tx;
+
+import com.tc.net.protocol.tcm.ChannelID;
+import com.tc.object.tx.TransactionID;
+
+import java.util.Set;
+
+public interface TransactionAccount {
+
+  public abstract ChannelID getClientID();
+
+  /*
+   * returns true if completed, false if not completed or if the client has sent a duplicate ACK.
+   */
+  public abstract boolean removeWaitee(ChannelID waitee, TransactionID requestID);
+
+  public abstract void addWaitee(ChannelID waitee, TransactionID requestID);
+
+  public abstract boolean skipApplyAndCommit(TransactionID requestID);
+
+  public abstract void applyStarted(TransactionID requestID);
+
+  public abstract boolean applyCommitted(TransactionID requestID);
+
+  public abstract boolean broadcastCompleted(TransactionID requestID);
+
+  public abstract boolean hasWaitees(TransactionID requestID);
+
+  public abstract Set requestersWaitingFor(ChannelID waitee);
+
+}
