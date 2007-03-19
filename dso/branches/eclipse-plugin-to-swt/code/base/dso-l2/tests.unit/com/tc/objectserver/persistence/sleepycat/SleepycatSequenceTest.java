349a138bdc90732342d4a376cd092ddbed0873ab
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-03-19 23:41:22 +0000

    Merged revisions 1640,1644,1651,1658-1659,1661,1668,1682-1683,1685,1687-1689,1695-1696,1700,1708-1710,1716-1718,1722-1723,1726-1727,1739,1741-1742,1744-1745,1747-1748,1758,1760-1762,1764,1766-1767,1769-1770,1772,1777,1786,1788,1790,1801,1808-1811,1820,1822-1823,1830-1831,1833-1834,1848,1855-1856,1858-1859,1861-1891 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/eclipse-plugin-to-swt/code/base/dso-l2/tests.unit/com/tc/objectserver/persistence/sleepycat/SleepycatSequenceTest.java
+++ dso/branches/eclipse-plugin-to-swt/code/base/dso-l2/tests.unit/com/tc/objectserver/persistence/sleepycat/SleepycatSequenceTest.java
@@ -1,7 +1,10 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.objectserver.persistence.sleepycat;
+
+import org.apache.commons.io.FileUtils;
 
 import com.sleepycat.je.DatabaseConfig;
 import com.sleepycat.je.EnvironmentConfig;
@@ -20,8 +23,9 @@ public class SleepycatSequenceTest extends TCTestCase {
   private File              envHome;
   private EnvironmentConfig ecfg;
   private DatabaseConfig    dbcfg;
+  private DBEnvironment     env;
   private static int        count = 0;
-  
+
   protected void setUp() throws Exception {
     super.setUp();
     ecfg = new EnvironmentConfig();
@@ -37,9 +41,17 @@ public class SleepycatSequenceTest extends TCTestCase {
       //
     }
     System.out.println("DB home: " + envHome);
+    env = newEnv(true);
 
   }
-  
+
+  public void tearDown() throws Exception {
+    super.tearDown();
+    env.close();
+    envHome.delete();
+    FileUtils.cleanDirectory(envHome);
+  }
+
   private DBEnvironment newEnv(boolean paranoid) throws IOException {
     return newEnv(new HashMap(), new ArrayList(), paranoid);
   }
@@ -48,13 +60,14 @@ public class SleepycatSequenceTest extends TCTestCase {
     return new DBEnvironment(map, list, paranoid, envHome, ecfg, dbcfg);
   }
 
-  
-  public void testUID()  throws Exception {
-    DBEnvironment env = newEnv(true);
+  public void testUID() throws Exception {
     assertTrue(env.open().isClean());
-    SleepycatPersistenceTransactionProvider persistenceTransactionProvider = new SleepycatPersistenceTransactionProvider(env.getEnvironment());
+    SleepycatPersistenceTransactionProvider persistenceTransactionProvider = new SleepycatPersistenceTransactionProvider(
+                                                                                                                         env
+                                                                                                                             .getEnvironment());
     TCLogger logger = TCLogging.getLogger(SleepycatSequenceTest.class);
-    SleepycatSequence sequence = new SleepycatSequence(persistenceTransactionProvider, logger, 1, 1, env.getClientIDDatabase());
+    SleepycatSequence sequence = new SleepycatSequence(persistenceTransactionProvider, logger, 1, 1, env
+        .getClientIDDatabase());
     String uid1 = sequence.getUID();
     assertNotNull(uid1);
     System.err.println("UID is " + uid1);
@@ -66,6 +79,43 @@ public class SleepycatSequenceTest extends TCTestCase {
     String uid3 = sequence.getUID();
     System.err.println("UID is " + uid3);
     assertNotEquals(uid1, uid3);
+  }
+
+  public void testBasic() throws Exception {
+    assertTrue(env.open().isClean());
+    SleepycatPersistenceTransactionProvider persistenceTransactionProvider = new SleepycatPersistenceTransactionProvider(
+                                                                                                                         env
+                                                                                                                             .getEnvironment());
+    TCLogger logger = TCLogging.getLogger(SleepycatSequenceTest.class);
+    SleepycatSequence sequence = new SleepycatSequence(persistenceTransactionProvider, logger, 1, 1, env
+        .getClientIDDatabase());
+    long id = sequence.next();
+    assertEquals(1, id);
+    id = sequence.nextBatch(100);
+    assertEquals(2, id);
+    id = sequence.next();
+    assertEquals(102, id);
+    id = sequence.next();
+    assertEquals(103, id);
+    id = sequence.next();
+    assertEquals(104, id);
+    sequence.setNext(1000);
+    id = sequence.next();
+    assertEquals(1000, id);
+    id = sequence.nextBatch(100);
+    assertEquals(1001, id);
+    id = sequence.nextBatch(100);
+    assertEquals(1101, id);
+    boolean failed = false;
+    try {
+      sequence.setNext(100);
+      failed = true;
+    } catch (AssertionError er) {
+      // expected
+    }
+    id = sequence.next();
+    assertEquals(1201, id);
+    if (failed) { throw new AssertionError("Didn't fail"); }
   }
 
 }
