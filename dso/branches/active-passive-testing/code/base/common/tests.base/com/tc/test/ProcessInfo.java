a4515542c2c5947a000a27d20fd20c6935589032
cfisher <cfisher@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-04-04 22:44:20 +0000

    Merged revisions 1640,1644,1651,1658-1659,1661,1668,1682-1683,1685,1687-1689,1695-1696,1700,1708-1710,1716-1718,1722-1723,1726-1727,1739,1741-1742,1744-1745,1747-1748,1758,1760-1762,1764,1766-1767,1769-1770,1772,1777,1786,1788,1790,1801,1808-1811,1820,1822-1823,1830-1831,1833-1834,1848,1855-1856,1858-1859,1861,1866,1869,1872,1875-1876,1885,1888-1892,1895-1897,1900-1901,1903-1904,1907,1909-1910,1913,1919-1921,1926,1928-1938,1940,1942-1943,1945,1947-1948,1950,1952,1954-1955,1958-1959,1961-1964,1967,1970-1976,1978-1981,1983,1985-1986,1988,1990,1993-1994,1996,1998-1999,2003-2005,2009-2011,2014-2020,2027-2031,2033,2036-2037,2039-2046,2048-2053,2055,2057,2059-2063,2066,2068-2073,2077-2079,2081-2082,2084-2085,2088-2090,2092,2098-2100,2104-2107,2110,2112-2113,2115-2117,2119,2121,2124-2125,2128,2132,2135,2138,2141,2143,2145,2147-2154,2156-2157,2159-2160,2162-2163,2165-2167,2170-2177,2179-2181,2183,2185-2190,2196,2199-2200,2202-2204,2206,2208-2213,2219,2221-2224,2227,2231-2232,2234-2235,2238-2268 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk

---
+++ dso/branches/active-passive-testing/code/base/common/tests.base/com/tc/test/ProcessInfo.java
@@ -0,0 +1,68 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.test;
+
+import com.tc.util.runtime.Os;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ProcessInfo {
+  public static String ps_grep_java() {
+    try {
+      String[] args = new String[] { "sh", "-c", "ps auxwww | grep java | grep -v grep" };
+
+      if (Os.isWindows()) {
+        String execPath = TestConfigObject.getInstance().executableSearchPath();
+        args = new String[] { execPath + "\\pv.exe", "-l", "java.exe" };
+      } else if (Os.isSolaris()) {
+        args = new String[] { "sh", "-c", "/usr/ucb/ps auxwww | grep java | grep -v grep" };
+      }
+
+      Process p = Runtime.getRuntime().exec(args);
+      StreamGobbler out = new StreamGobbler(p.getInputStream(), "stdout");
+      StreamGobbler err = new StreamGobbler(p.getErrorStream(), "stderr");
+      
+      out.start();
+      err.start();
+      
+      System.out.println("ps_grep_java exit code: " + p.waitFor());
+      return out.getOutput() + "\n" + err.getOutput();
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
+
+class StreamGobbler extends Thread {
+  private InputStream  is;
+  private String       type;
+  private StringBuffer output = new StringBuffer(1024);
+
+  StreamGobbler(InputStream is, String type) {
+    this.is = is;
+    this.type = type;
+  }
+
+  public void run() {
+    try {
+      InputStreamReader isr = new InputStreamReader(is);
+      BufferedReader br = new BufferedReader(isr);
+      String line = null;
+      while ((line = br.readLine()) != null) {
+        output.append(line).append("\n");
+      }
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+  
+  public String getOutput() {
+    return output.toString();
+  }
+}
