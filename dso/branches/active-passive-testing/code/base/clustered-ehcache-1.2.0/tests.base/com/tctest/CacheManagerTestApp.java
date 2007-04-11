53b621e1c30d41216c43fbf39b724b9f68978e80
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-04-11 20:56:17 +0000

    Merged revisions 1640,1644,1651,1658-1659,1661,1668,1682-1683,1685,1687-1689,1695-1696,1700,1708-1710,1716-1718,1722-1723,1726-1727,1739,1741-1742,1744-1745,1747-1748,1758,1760-1762,1764,1766-1767,1769-1770,1772,1777,1786,1788,1790,1801,1808-1811,1820,1822-1823,1830-1831,1833-1834,1848,1855-1856,1858-1859,1861,1866,1869,1872,1875-1876,1885,1888-1892,1895-1897,1900-1901,1903-1904,1907,1909-1910,1913,1919-1921,1926,1928-1938,1940,1942-1943,1945,1947-1948,1950,1952,1954-1955,1958-1959,1961-1964,1967,1970-1976,1978-1981,1983,1985-1986,1988,1990,1993-1994,1996,1998-1999,2003-2005,2009-2011,2014-2020,2027-2031,2033,2036-2037,2039-2046,2048-2053,2055,2057,2059-2063,2066,2068-2073,2077-2079,2081-2082,2084-2085,2088-2090,2092,2098-2100,2104-2107,2110,2112-2113,2115-2117,2119,2121,2124-2125,2128,2132,2135,2138,2141,2143,2145,2147-2154,2156-2157,2159-2160,2162-2163,2165-2167,2170-2177,2179-2181,2183,2185-2190,2196,2199-2200,2202-2204,2206,2208-2213,2219,2221-2224,2227,2231-2232,2234-2235,2238-2244,2246-2247,2251-2252,2254,2256-2259,2261-2262,2264-2266,2268-2269,2271,2273-2281,2283-2285,2287-2289,2296-2297,2299-2301,2303-2306,2309-2310,2315-2316,2319-2323,2329,2336-2338,2342,2345,2347,2349-2351,2353-2354,2357-2359,2363,2365-2370,2373,2375-2376,2378,2380,2382-2383,2385-2402 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk
    ........
      r2387 | jgalang | 2007-04-10 16:31:24 -0700 (Tue, 10 Apr 2007) | 2 lines
      
      First cut for testing a clustered Ehcache CacheManger
    ........
      r2388 | jgalang | 2007-04-10 17:39:13 -0700 (Tue, 10 Apr 2007) | 2 lines
      
      Updates.
    ........
      r2389 | ssubbiah | 2007-04-10 18:05:12 -0700 (Tue, 10 Apr 2007) | 2 lines
      
      unit test failures fixes
    ........
      r2390 | siyer | 2007-04-10 18:12:19 -0700 (Tue, 10 Apr 2007) | 1 line
      
      ehcache config - needs to be spruced up to make it work with the latest ehcache version and tested against some more esoteric ehcache features.
    ........
      r2391 | jgalang | 2007-04-10 18:12:37 -0700 (Tue, 10 Apr 2007) | 2 lines
      
      updates
    ........
      r2392 | hhuynh | 2007-04-10 18:56:55 -0700 (Tue, 10 Apr 2007) | 1 line
      
      use buffer for faster copy
    ........
      r2393 | hhuynh | 2007-04-10 19:20:41 -0700 (Tue, 10 Apr 2007) | 1 line
      
      add checking for null pointer
    ........
      r2396 | hhuynh | 2007-04-11 10:19:11 -0700 (Wed, 11 Apr 2007) | 1 line
      
      disable till April 13
    ........
      r2397 | jgalang | 2007-04-11 11:05:36 -0700 (Wed, 11 Apr 2007) | 2 lines
      
      Updated EhCache Jar file
    ........
      r2398 | hhuynh | 2007-04-11 11:06:23 -0700 (Wed, 11 Apr 2007) | 1 line
      
      jar file updated, reenable
    ........
      r2399 | jgalang | 2007-04-11 11:56:07 -0700 (Wed, 11 Apr 2007) | 5 lines
      
      Additional asserts added into test:
      - Check that the cache's added in the CacheManager can be retrieved
      - Check that the contents of the cache can be retrieved
    ........
      r2400 | jgalang | 2007-04-11 12:08:30 -0700 (Wed, 11 Apr 2007) | 2 lines
      
      clustered-ehcache-1.3.0 -> clustered-ehcache-1.2.0
    ........
      r2401 | hhuynh | 2007-04-11 12:18:58 -0700 (Wed, 11 Apr 2007) | 1 line
      
      ignore ps grep in linux
    ........

---
+++ dso/branches/active-passive-testing/code/base/clustered-ehcache-1.2.0/tests.base/com/tctest/CacheManagerTestApp.java
@@ -0,0 +1,122 @@
+package com.tctest;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.Status;
+
+import com.tc.object.config.ConfigLockLevel;
+import com.tc.object.config.ConfigVisitor;
+import com.tc.object.config.DSOClientConfigHelper;
+import com.tc.object.config.TransparencyClassSpec;
+import com.tc.simulator.app.ApplicationConfig;
+import com.tc.simulator.listener.ListenerProvider;
+import com.tc.util.Assert;
+import com.tctest.runner.AbstractErrorCatchingTransparentApp;
+
+public class CacheManagerTestApp extends AbstractErrorCatchingTransparentApp {
+	static final int EXPECTED_THREAD_COUNT = 2;
+
+	private final CyclicBarrier barrier;
+
+    private final CacheManager clusteredCacheManager;
+
+	/**
+	 * Test that Ehcache's CacheManger and Cache objects can be clustered. 
+	 * @param appId
+	 * @param cfg
+	 * @param listenerProvider
+	 */
+	public CacheManagerTestApp(final String appId, final ApplicationConfig cfg,
+			final ListenerProvider listenerProvider) {
+		super(appId, cfg, listenerProvider);
+		barrier = new CyclicBarrier(getParticipantCount());
+		clusteredCacheManager = CacheManager.create();
+	}
+
+	/**
+	 * Inject Ehcache 1.2.0 configuration, and instrument this test class
+	 * @param visitor
+	 * @param config
+	 */
+	public static void visitL1DSOConfig(final ConfigVisitor visitor,
+			final DSOClientConfigHelper config) {
+	    config.addNewModule("clustered-ehcache-1.2.0", "1.0.0");
+		config.addAutolock("* *..*.*(..)", ConfigLockLevel.WRITE);
+
+	    final String testClass = CacheManagerTestApp.class.getName();
+		final TransparencyClassSpec spec = config.getOrCreateSpec(testClass);
+		spec.addRoot("barrier", "barrier");
+		spec.addRoot("clusteredCacheManager", "clusteredCacheManager");
+	}
+	
+	/**
+	 * Test that the data written in the clustered CacheManager
+	 * by one node, becomes available in the other.
+	 */
+	protected void runTest() throws Throwable {
+		if (barrier.await() == 0) {
+			addCache("CACHE1");
+			letOtherNodeProceed();
+			waitForPermissionToProceed();
+			verifyCache("CACHE2");
+		} else {
+			waitForPermissionToProceed();
+			verifyCache("CACHE1");
+			addCache("CACHE2");
+			letOtherNodeProceed();
+		}
+		barrier.await();
+	}
+
+	// This is lame but it makes runTest() slightly more readable
+	private void letOtherNodeProceed() throws InterruptedException,
+			BrokenBarrierException {
+		barrier.await();
+	}
+
+	// This is lame but it makes runTest() slightly more readable
+	private void waitForPermissionToProceed() throws InterruptedException,
+			BrokenBarrierException {
+		barrier.await();
+	}
+
+	/**
+	 * Add a cache into the CacheManager.
+	 * @param name The name of the cache to add
+	 * @throws Throwable
+	 */
+	private void addCache(final String name) throws Throwable {
+		synchronized(clusteredCacheManager) {
+	        Cache cache = new Cache(name, 1, true, true, 0, 0);
+			clusteredCacheManager.addCache(cache);
+	        cache.put(new Element(name + "key1", "value1"));
+	        cache.put(new Element(name + "key2", "value1"));
+		}
+	}
+
+	/**
+	 * Verify that the named cache exists and that it's contents
+	 * can be retrieved.
+	 * @param name The name of the cache to retrieve
+	 * @throws Exception
+	 */
+	private void verifyCache(final String name) throws Exception {
+		synchronized(clusteredCacheManager) {
+			Cache cache = clusteredCacheManager.getCache(name);
+			Assert.assertNotNull(cache);
+			Assert.assertEquals(name, cache.getName());
+			Assert.assertEquals(Status.STATUS_ALIVE, cache.getStatus());
+			
+	        int sizeFromGetSize = cache.getSize();
+	        int sizeFromKeys = cache.getKeys().size();
+	        Assert.assertEquals(sizeFromGetSize, sizeFromKeys);
+	        Assert.assertEquals(2, cache.getSize());
+	        Assert.assertNotNull(cache.get(name + "key1"));
+	        Assert.assertNotNull(cache.get(name + "key2"));
+		}
+	}
+}
