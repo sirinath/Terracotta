3daf00d775904cc38b1d85b1535c0d9ea96f5c16
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-04-16 21:39:23 +0000

    Merged revisions 2454-2455,2460,2463,2465-2475 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/ui-swing-to-swt/code/base/dso-common/src/com/tc/util/AdaptedClassDumper.java
+++ dso/branches/ui-swing-to-swt/code/base/dso-common/src/com/tc/util/AdaptedClassDumper.java
@@ -3,80 +3,22 @@
  */
 package com.tc.util;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
 /**
- * A little utility class that will write class files to disk.
+ * A little utility class that will write class files to disk for instrumented class files.
  */
-public class AdaptedClassDumper {
-
-  private final static File adaptedRoot = getFileRoot();
-
-  private AdaptedClassDumper() {
-    //
+public class AdaptedClassDumper extends AbstractClassDumper {
+  
+  public final static AdaptedClassDumper INSTANCE = new AdaptedClassDumper();
+  
+  private AdaptedClassDumper()  {
+    // make the default constructor private to turn this class into a singleton
+  }
+  
+  protected String getDumpDirectoryName() {
+    return "adapted";
   }
 
-  public synchronized static void write(String name, byte[] b) {
-    if (adaptedRoot == null) { return; }
-
-    name = name.replace('.', '/') + ".class";
-    FileOutputStream fos = null;
-
-    try {
-      try {
-        String pattern = File.separator.replaceAll("\\\\", "\\\\\\\\");
-        String[] strings = new File(adaptedRoot, name).getAbsolutePath().split(pattern);
-
-        final StringBuffer sb;
-        if (adaptedRoot.getAbsolutePath().startsWith("/")) {
-          sb = new StringBuffer("/");
-        } else {
-          sb = new StringBuffer();
-        }
-
-        for (int i = 0; i < strings.length - 1; i++) {
-          sb.append(strings[i]);
-          sb.append(File.separatorChar);
-        }
-
-        File dir = new File(sb.toString());
-        if (!dir.exists()) {
-          dir.mkdirs();
-        }
-
-        File outFile = new File(adaptedRoot, name);
-        System.out.println("Writing resource: " + outFile);
-        fos = new FileOutputStream(outFile);
-        fos.write(b);
-      } finally {
-        if (fos != null) {
-          fos.close();
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+  protected String getPropertyName() {
+    return "tc.classloader.writeToDisk";
   }
-
-  private static File getFileRoot() {
-    try {
-      boolean writeToDisk = (System.getProperty("tc.classloader.writeToDisk") != null);
-      if (!writeToDisk) { return null; }
-
-      String userHome = System.getProperty("user.home");
-
-      if (userHome != null) {
-        File homeDir = new File(userHome);
-        if (homeDir.isDirectory() && homeDir.canWrite()) { return new File(homeDir, "adapted"); }
-      }
-
-      return null;
-    } catch (Exception e) {
-      // you can get a SecurityException here, but we shouldn't blow up just b/c of that
-      e.printStackTrace();
-      return null;
-    }
-  }
-
 }
