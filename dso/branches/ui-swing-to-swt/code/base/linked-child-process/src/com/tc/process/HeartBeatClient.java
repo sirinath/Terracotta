9d6842968d222a21293729944c2b35d28ccdd764
eellis <eellis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-04-23 05:19:08 +0000

    Merged revisions 2600,2603-2604,2606,2616-2617,2629-2643 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/ui-swing-to-swt/code/base/linked-child-process/src/com/tc/process/HeartBeatClient.java
+++ dso/branches/ui-swing-to-swt/code/base/linked-child-process/src/com/tc/process/HeartBeatClient.java
@@ -8,16 +8,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HeartBeatClient extends Thread {
-  private Socket  socket;
-  private boolean isAppServer = false;
+  private Socket                  socket;
+  private boolean                 isAppServer       = false;
+  private static final DateFormat dateFormat        = new SimpleDateFormat("HH:mm:ss.SSS");
+  private static final int        HEARTBEAT_TIMEOUT = HeartBeatServer.PULSE_INTERVAL + 5000;
+
+  private static void log(String msg) {
+    System.out.println("Heartbeat: [" + dateFormat.format(new Date()) + "] " + msg);
+  }
 
   public HeartBeatClient(int listenPort, boolean isAppServer) {
     this.isAppServer = isAppServer;
     try {
       socket = new Socket("localhost", listenPort);
-      socket.setSoTimeout(HeartBeatServer.PULSE_INTERVAL + 1000);
+      socket.setSoTimeout(HEARTBEAT_TIMEOUT);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -29,28 +38,33 @@ public class HeartBeatClient extends Thread {
       PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
       while (true) {
+        long start = System.currentTimeMillis();
         // will time out if it didn't get any pulse from server
         String signal = in.readLine();
-
-        if (HeartBeatServer.PULSE.equals(signal)) {
-          System.out.println("Received pulse from heartbeat server.");
+        if (System.currentTimeMillis() - start > HEARTBEAT_TIMEOUT) { throw new Exception("Time expired for heartbeat."); }
+        if (signal == null) { 
+          throw new Exception("Null signal");
+        } else if (HeartBeatServer.PULSE.equals(signal)) {
+          log("Received pulse from heartbeat server.");
         } else if (HeartBeatServer.KILL.equals(signal)) {
-          System.out.println("Received KILL from heartbeat server. Killing self.");
+          log("Received KILL from heartbeat server. Killing self.");
           System.exit(1);
         } else if (HeartBeatServer.IS_APP_SERVER_ALIVE.equals(signal)) {
-          System.out.println("Received IS_APP_SERVER_ALIVE from heartbeat server. ");
+          log("Received IS_APP_SERVER_ALIVE from heartbeat server. ");
           if (isAppServer) {
             out.println(HeartBeatServer.IM_ALIVE);
-            System.out.println("  responded: IM_ALIVE");
+            log("  responded: IM_ALIVE");
           } else {
             out.println("NOT_AN_APP_SERVER");
-            System.out.println("  responded: NOT_AN_APP_SERVER");
+            log("  responded: NOT_AN_APP_SERVER");
           }
+        } else {
+          throw new Exception("Unknown signal");
         }
       }
 
     } catch (Throwable e) {
-      System.err.println("Caught exception in heartbeat client. Killing self. " + e.getMessage());
+      log("Caught exception in heartbeat client. Killing self. " + e.getMessage());
       System.exit(1);
     }
   }
