cc5bb6563898ac0b542ae7ad5d7a8baba5bb16e1
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-04-30 20:49:19 +0000

    increase tolerance for slow machines

--- dso/branches/2.3/code/base/dso-system-tests/tests.system/com/tctest/DistributedWaitTest.java
+++ dso/branches/2.3/code/base/dso-system-tests/tests.system/com/tctest/DistributedWaitTest.java
@@ -5,7 +5,7 @@ package com.tctest;
 
 public class DistributedWaitTest extends TransparentTestBase {
 
-  private static final int NODE_COUNT = 5;
+  private static final int NODE_COUNT = 2;
   
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
