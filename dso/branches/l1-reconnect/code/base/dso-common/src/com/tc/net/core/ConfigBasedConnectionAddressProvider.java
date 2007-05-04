43a2383dd315e42456ab71455b7fcee07a000922
alex <alex@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-05-04 18:16:28 +0000

    no more ConfigItemListener

--- dso/branches/l1-reconnect/code/base/dso-common/src/com/tc/net/core/ConfigBasedConnectionAddressProvider.java
+++ dso/branches/l1-reconnect/code/base/dso-common/src/com/tc/net/core/ConfigBasedConnectionAddressProvider.java
@@ -5,28 +5,17 @@
 package com.tc.net.core;
 
 import com.tc.config.schema.dynamic.ConfigItem;
-import com.tc.config.schema.dynamic.ConfigItemListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ConfigBasedConnectionAddressProvider implements ConnectionAddressProvider, ConfigItemListener {
+public class ConfigBasedConnectionAddressProvider implements ConnectionAddressProvider {
 
-  private final ConfigItem source;
   private List             currentAddresses = new ArrayList();
 
   public ConfigBasedConnectionAddressProvider(ConfigItem source) {
-    this.source = source;
-
-    this.source.addListener(this);
     this.currentAddresses.addAll(Arrays.asList((ConnectionInfo[]) source.getObject()));
-  }
-
-  public synchronized void valueChanged(Object oldValue, Object newValue) {
-    ConnectionInfo[] newAddresses = (ConnectionInfo[]) this.source.getObject();
-    this.currentAddresses.clear();
-    this.currentAddresses.addAll(Arrays.asList(newAddresses));
   }
 
   public synchronized String toString() {
