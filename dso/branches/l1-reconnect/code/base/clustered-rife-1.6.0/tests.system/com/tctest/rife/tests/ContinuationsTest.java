a2d1fa4ef04350becd30c0f20c00bfb351f664ef
alex <alex@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-05-11 17:36:59 +0000

    merge -r 2998:3019 https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/l1-reconnect/code/base/clustered-rife-1.6.0/tests.system/com/tctest/rife/tests/ContinuationsTest.java
+++ dso/branches/l1-reconnect/code/base/clustered-rife-1.6.0/tests.system/com/tctest/rife/tests/ContinuationsTest.java
@@ -21,6 +21,11 @@ import com.uwyn.rife.servlet.RifeFilter;
 import com.uwyn.rife.tools.StringUtils;
 
 public class ContinuationsTest extends AbstractTwoServerDeploymentTest {
+    public ContinuationsTest() {
+        disableTestUntil("testAllTypes", "2007-05-28");
+        disableTestUntil("testSimpleInterface", "2007-05-28");
+    }
+
 	public static Test suite() {
 		return new ContinuationsTestSetup();
 	}
