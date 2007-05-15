445d783cd0824026702adac81fe2e8218fd22621
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-05-15 00:13:38 +0000

    Change Configurator/WebSphere start script to use a specific profile when enabling DSO, rather than using the default profile (which is probably going to be different than the configurator environment

--- dso/branches/ibm-jdk-15/kits/skeletons/shared-base/common/tools/sessions/configurator-sandbox/was6.1/websphere-common.sh
+++ dso/branches/ibm-jdk-15/kits/skeletons/shared-base/common/tools/sessions/configurator-sandbox/was6.1/websphere-common.sh
@@ -72,14 +72,14 @@ function _runWsAdmin() {
 function _startWebSphere() {
     if test "${2}" != "nodso"; then
         # Instrument WebSphere for use with Terracotta
-        _runWsAdmin -connType NONE -javaoption -Denable.dso="true" -f "${WAS_SANDBOX}/toggle-dso.py"
+        _runWsAdmin -connType NONE -profileName "$tc-${1}" -javaoption -Denable.dso="true" -f "${WAS_SANDBOX}/toggle-dso.py"
         __retVal="$?"
         if test "${__retVal}" != "0"; then
             return "${__retVal}"
         fi
     else
         # Make sure DSO is not enabled in WebSphere
-        _runWsAdmin -connType NONE -javaoption -Denable.dso="false" -f "${WAS_SANDBOX}/toggle-dso.py"
+        _runWsAdmin -connType NONE -profileName "$tc-${1}" -javaoption -Denable.dso="false" -f "${WAS_SANDBOX}/toggle-dso.py"
         __retVal="$?"
         if test "${__retVal}" != "0"; then
             return "${__retVal}"
