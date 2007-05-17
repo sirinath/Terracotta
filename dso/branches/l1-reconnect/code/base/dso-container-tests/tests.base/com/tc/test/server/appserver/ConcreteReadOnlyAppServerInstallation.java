52d7793494cb8b3ca08015bf2944ab56ec5695cd
alex <alex@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-05-17 17:00:29 +0000

    merge -r 3110:3146 https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/l1-reconnect/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/ConcreteReadOnlyAppServerInstallation.java
+++ dso/branches/l1-reconnect/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/ConcreteReadOnlyAppServerInstallation.java
@@ -9,78 +9,35 @@ import com.tc.util.runtime.Os;
 
 import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.LineNumberReader;
-import java.io.PrintWriter;
 import java.net.URL;
-import java.net.URLConnection;
-import java.text.DateFormat;
-import java.util.Date;
 
 /**
- * This class manages the installation of the read-only portion of a running appserver. A timestamp is used to cache the
- * installation for future reference. This portion constitutes the majority of an appserver's filesize. This class
+ * This class manages the installation of the read-only portion of a running appserver. This class
  * should not be referenced by any class other than {@link AbstractAppServerInstallation}.
  */
 final class ConcreteReadOnlyAppServerInstallation {
-
-  private static final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG);
 
   static File create(URL host, File serverDir, String serverType, String majorVersion, String minorVersion)
       throws Exception {
     File serverInstallDir = new File(serverDir + File.separator + serverType + "-" + majorVersion + "." + minorVersion
                                      + "-install");
-    serverInstallDir.mkdir();
-    File timestampFile = new File(serverInstallDir + File.separator + "timestamp");
-    if (equalTimestamps(host, serverType, majorVersion, minorVersion, timestampFile)) { return serverInstallDir; }
-    File deleteServer = new File(serverInstallDir + File.separator + serverType + "-" + majorVersion + "."
-                                 + minorVersion);
-    if (deleteServer.exists()) {
-      deleteServer.delete();
+    if (!serverInstallDir.exists()) {
+      serverInstallDir.mkdir();
+      URL appUrl = appendPath(host, serverType, majorVersion, minorVersion);      
+      System.out.println("Downloading: " + appUrl);      
+      BufferedInputStream in = new BufferedInputStream(appUrl.openStream());
+      ZipBuilder.unzip(in, serverInstallDir);
+      in.close();
+      System.out.println("Unzip to: " + serverInstallDir);
+    } else {
+      System.out.println("Cached version of appserver found: " + serverInstallDir);
     }
-    BufferedInputStream in = new BufferedInputStream(appendPath(host, serverType, majorVersion, minorVersion)
-        .openStream());
-    ZipBuilder.unzip(in, serverInstallDir);
-    in.close();
     return serverInstallDir;
-  }
-
-  private static boolean equalTimestamps(URL host, String serverType, String majorVersion, String minorVersion,
-                                         File timestampFile) throws Exception {
-    URLConnection conn = appendPath(host, serverType, majorVersion, minorVersion).openConnection();
-    System.out.println("**Connection=" + host);
-    System.out.println("**Connection=" + conn);
-    long modified = conn.getLastModified();
-    if (!timestampFile.exists()) {
-      writeTimestamp(modified, timestampFile);
-      return false;
-    }
-    LineNumberReader in = new LineNumberReader(new FileReader(timestampFile));
-    String serverStamp = in.readLine();
-    in.close();
-    if (df.parse(serverStamp).getTime() != df.parse(df.format(new Date(modified))).getTime()) {
-      writeTimestamp(modified, timestampFile);
-      return false;
-    }
-    return true;
-  }
-
-  private static void writeTimestamp(long timestamp, File timestampFile) throws IOException {
-    PrintWriter out = new PrintWriter(new FileWriter(timestampFile));
-    out.println(df.format(new Date(timestamp)));
-    out.close();
-  }
-
-  // return platform the test running on
-  private static String resolvePlatform() {
-    return Os.platform();
   }
 
   private static URL appendPath(URL host, String serverType, String majorVersion, String minorVersion) throws Exception {
     String baseUrl = host.toExternalForm();
-    String appendedPath = serverType + "/" + resolvePlatform() + "/" + serverType.toLowerCase() + "-"
+    String appendedPath = serverType + "/" + Os.platform() + "/" + serverType.toLowerCase() + "-"
                           + majorVersion.toLowerCase() + "." + minorVersion.toLowerCase() + ".zip";
     return new URL(baseUrl + "/" + appendedPath);
   }
