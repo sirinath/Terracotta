1d2baebe49082b4c88f97d545176ed227ffd5544
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-05-24 01:29:42 +0000

    Refactoring SessionsHelper a little bit.

--- dso/branches/ibm-jdk-15/code/base/dso-l1/src/com/tc/object/bytecode/hook/impl/SessionsHelper.java
+++ dso/branches/ibm-jdk-15/code/base/dso-l1/src/com/tc/object/bytecode/hook/impl/SessionsHelper.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.object.bytecode.hook.impl;
 
@@ -13,7 +14,6 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class SessionsHelper {
@@ -24,26 +24,34 @@ public class SessionsHelper {
   private SessionsHelper() {
     //
   }
-  
+
   public static String[] injectClasses(String[] s) {
     List classPaths = new ArrayList(Arrays.asList(s));
 
     String tcSessionCP = System.getProperty(TC_SESSION_CLASSPATH);
     if (tcSessionCP != null) {
       tcSessionCP = tcSessionCP.replace('\\', '/'); // for windows
-
       String[] paths = tcSessionCP.split(File.pathSeparator);
       for (int i = 0; i < paths.length; i++) {
         String path = paths[i];
         if (!path.endsWith("/")) {
           path += "/";
         }
-
         if (!path.startsWith("/")) {
           path = "/" + path;
         }
-
         classPaths.add(path);
+      }
+    } else {
+      File installRoot = ClassProcessorHelper.getTCInstallDir(false);
+      File sessionsLib = new File(installRoot, "lib");
+      File tcSessionLib = new File(sessionsLib, "session");
+      File tcSessionJar = new File(tcSessionLib, "tc-session.jar");
+      if (tcSessionJar.exists() && tcSessionJar.isFile() && tcSessionJar.canRead()) {
+        classPaths.add(tcSessionJar.getAbsolutePath());
+      } else {
+        Banner.errorBanner(tcSessionJar.getAbsolutePath() + " does not exist, or is not an accessible directory");
+        Util.exit();
       }
     }
     s = new String[classPaths.size()];
@@ -52,58 +60,21 @@ public class SessionsHelper {
   }
 
   public static void injectClasses(ClassLoader loader) throws Exception {
-    List classPaths = new ArrayList();
-
-    String tcSessionCP = System.getProperty(TC_SESSION_CLASSPATH);
-    if (tcSessionCP != null) {
-      tcSessionCP = tcSessionCP.replace('\\', '/'); // for windows
-
-      String[] paths = tcSessionCP.split(File.pathSeparator);
-      for (int i = 0; i < paths.length; i++) {
-        String path = paths[i];
-        if (!path.endsWith("/")) {
-          path += "/";
-        }
-
-        if (!path.startsWith("/")) {
-          path = "/" + path;
-        }
-
-        classPaths.add(path);
-      }
-    } else {
-      File installRoot = ClassProcessorHelper.getTCInstallDir(false);
-      File sessionsLib = new File(installRoot, "lib");
-      File tcSessionLib = new File(sessionsLib, "session");
-
-      if (!tcSessionLib.exists() || !tcSessionLib.isDirectory() || !tcSessionLib.canRead()) {
-        Banner.errorBanner(tcSessionLib + " does not exist, or is not an accessible directory");
-        Util.exit();
-      }
-
-      classPaths.add(appendPath(tcSessionLib.getAbsolutePath(), "tc-session.jar"));
-    }
-
-//    Method m = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] { URL.class });
-//    m.setAccessible(true);
-//    for (Iterator iter = classPaths.iterator(); iter.hasNext();) {
-//      m.invoke(loader, new Object[] { new URL("file", "", (String) iter.next()) });
-//    }
-    
-    if (!invokeAddURLMethodIfPresent(loader, classPaths)) {
-      if (!invokeAddPathsMethodIfPresent(loader, classPaths)) {
-        throw new AssertionError("SessionsHelper.injectClasses cannot recognize classloader of type: " + loader.getClass().getName());
+    final String[] sessionsClasspaths = injectClasses(new String[0]);
+    if (!invokeAddURLMethodIfPresent(loader, sessionsClasspaths)) {
+      if (!invokeAddPathsMethodIfPresent(loader, sessionsClasspaths)) {
+        AssertionError ae = new AssertionError("SessionsHelper.injectClasses() cannot recognize classloader of type: "
+                                               + loader.getClass().getName());
+        throw ae;
       }
     }
   }
-  
-  private static boolean invokeAddPathsMethodIfPresent(ClassLoader loader, List classPaths) {
+
+  private static boolean invokeAddPathsMethodIfPresent(ClassLoader loader, String[] classPaths) {
     try {
-      Method m = loader.getClass().getDeclaredMethod("addPaths", new Class[]{String[].class});
+      Method m = loader.getClass().getDeclaredMethod("addPaths", new Class[] { String[].class });
       m.setAccessible(true);
-      String[] paths = new String[classPaths.size()];
-      classPaths.toArray(paths);
-      m.invoke(loader, new Object[] { paths });
+      m.invoke(loader, new Object[] { classPaths });
       return true;
     } catch (SecurityException e) {
       return false;
@@ -117,14 +88,14 @@ public class SessionsHelper {
       return false;
     }
   }
-  
-  private static boolean invokeAddURLMethodIfPresent(ClassLoader loader, List classPaths) {
+
+  private static boolean invokeAddURLMethodIfPresent(ClassLoader loader, String[] classPaths) {
     Method m;
     try {
       m = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] { URL.class });
       m.setAccessible(true);
-      for (Iterator iter = classPaths.iterator(); iter.hasNext();) {
-        m.invoke(loader, new Object[] { new URL("file", "", (String) iter.next()) });
+      for (int pos = 0; pos < classPaths.length; ++pos) {
+        m.invoke(loader, new Object[] { new URL("file", "", classPaths[0]) });
       }
       return true;
     } catch (SecurityException e) {
@@ -140,12 +111,6 @@ public class SessionsHelper {
     } catch (InvocationTargetException e) {
       return false;
     }
-  }
-
-  private static String appendPath(String value, String path) {
-    if (value == null) { return path; }
-    if (value.endsWith(File.separator)) { return value + path; }
-    return value + File.separator + path;
   }
 
 }
