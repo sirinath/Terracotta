82e99214eef7a8b050285e0880c3bdcebc6d5e4e
gbevin <gbevin@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-06-07 12:12:00 +0000

    Removed temporarily commented lines.

--- dso/branches/2.4/code/base/dso-l1/src/com/tc/object/config/StandardDSOClientConfigHelper.java
+++ dso/branches/2.4/code/base/dso-l1/src/com/tc/object/config/StandardDSOClientConfigHelper.java
@@ -1016,9 +1016,6 @@ public class StandardDSOClientConfigHelper implements DSOClientConfigHelper {
       spec = getOrCreateSpec("java.util.concurrent.CyclicBarrier$Generation");
       spec.setHonorJDKSubVersionSpecific(true);
 
-//      spec = getOrCreateSpec("java.util.concurrent.atomic.AtomicLong");
-//      spec.setHonorVolatile(true);
-
       spec = getOrCreateSpec("java.util.concurrent.TimeUnit");
 
       /*****************************************************************************************************************
