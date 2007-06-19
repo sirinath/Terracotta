a9887027d4f9a91bf3a5ab7e087f0d7c755f041c
alex <alex@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-06-19 21:09:42 +0000

    a simple script to assist with merging a single changeset from a branch into WC

---
+++ dev-tools/trunk/subversion/merge-from-branch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$#" -lt "2" ]; then
+  echo "Usage: $0 <branch> <revision> [commit]"
+  exit 1;
+fi
+branch=$1
+rev=$2
+commit=$3
+if [ "$commit" != "" ]; then
+  echo "========================================================================"
+  echo "Running: svn commit -m 'merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}'"
+  echo "========================================================================"
+  exec svn commit -m \'merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}\'
+else
+  echo "========================================================================"
+  echo "Running: svn merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}"
+  echo "========================================================================"
+  exec  svn merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}
+fi
+
