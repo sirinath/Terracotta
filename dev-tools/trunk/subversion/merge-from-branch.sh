cd68ee566210bc96be44b8fd7b6911544d1b1d82
nharward <nharward@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-06-22 18:18:30 +0000

    Adding support for an optional commit message argument; if specified it will be *prepended* to the original commit message, followed by a colon and a space.

--- dev-tools/trunk/subversion/merge-from-branch.sh
+++ dev-tools/trunk/subversion/merge-from-branch.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 if [ "$#" -lt "2" ]; then
-  echo "Usage: $0 <branch> <revision> [commit]"
+  echo "Usage: $0 <branch> <revision> [commit [message]]"
   exit 1;
 fi
-branch=$1
-rev=$2
-commit=$3
+branch="$1"
+rev="$2"
+commit="$3"
+optional_msg="$4"
 if [ "$commit" != "" ]; then
-  echo "========================================================================"
-  echo "Running: svn commit -m 'merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}'"
-  echo "========================================================================"
-  exec svn commit -m "merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}"
+  echo "================================================================================"
+  echo "Running: svn commit -m '${optional_msg+${optional_msg}: }merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}'"
+  echo "================================================================================"
+  exec svn commit -m "${optional_msg+${optional_msg}: }merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}"
 else
-  echo "========================================================================"
+  echo "================================================================================"
   echo "Running: svn merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}"
-  echo "========================================================================"
+  echo "================================================================================"
   exec  svn merge -c ${rev} https://svn.terracotta.org/repo/tc/dso/branches/${branch}
 fi
 
