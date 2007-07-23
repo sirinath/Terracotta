1e10a1f372e5e69cde41be7a919e0f2e050970a8
jgalang <jgalang@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-07-23 22:40:38 +0000

    branch for clustered-ehcache support

---
+++ dso/branches/ehcache-support/code/base/dso-l1-session/src/com/terracotta/session/util/DefaultSessionId.java
@@ -0,0 +1,65 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.terracotta.session.util;
+
+import com.terracotta.session.SessionId;
+
+public class DefaultSessionId implements SessionId {
+
+  private final String  key;
+  private final String  requestedId;
+  private final String  externalId;
+  private final Lock    lock;
+  private final boolean knownServerHop;
+
+  protected DefaultSessionId(final String internalKey, final String requestedId, final String externalId,
+                             final int lockType, boolean knownServerHop) {
+    Assert.pre(internalKey != null);
+    Assert.pre(externalId != null);
+    this.key = internalKey;
+    this.requestedId = requestedId;
+    this.externalId = externalId;
+    this.knownServerHop = knownServerHop;
+    this.lock = new Lock(this.key, lockType);
+  }
+
+  public String getRequestedId() {
+    return requestedId;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public boolean isNew() {
+    return requestedId == null;
+  }
+
+  public boolean isServerHop() {
+    return !isNew() && knownServerHop;
+  }
+
+  public String toString() {
+    return getClass().getName() + "{ " + "key=" + getKey() + ", requestedId=" + getRequestedId() + ", externalId="
+           + getExternalId() + "}";
+  }
+
+  public String getExternalId() {
+    return externalId;
+  }
+
+  public void commitLock() {
+    lock.commitLock();
+  }
+
+  public void getWriteLock() {
+    lock.getWriteLock();
+  }
+
+  public boolean tryWriteLock() {
+    return lock.tryWriteLock();
+  }
+
+}
