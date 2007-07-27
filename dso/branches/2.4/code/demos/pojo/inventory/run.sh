6144361cf6886502ec2235b5ee73f0069515c174
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-07-27 02:25:17 +0000

    DEV-814: changes to support /bin/sh broke argument passing to down-stream scripts.

--- dso/branches/2.4/code/demos/pojo/inventory/run.sh
+++ dso/branches/2.4/code/demos/pojo/inventory/run.sh
@@ -24,10 +24,10 @@
 
 CWD=`dirname "$0"`
 TC_INSTALL_DIR=${CWD}/../../..
-
+ARGS=$*
 TC_CONFIG_PATH="${CWD}/tc-config.xml"
 set -- -q "${TC_CONFIG}"
 . "${TC_INSTALL_DIR}/bin/dso-env.sh"
 
 exec "${JAVA_HOME}/bin/java" ${TC_JAVA_OPTS} ${JAVA_OPTS} \
-  -cp "${CWD}/classes" demo.inventory.Main "$@"
+  -cp "${CWD}/classes" demo.inventory.Main $ARGS
--- dso/branches/2.4/code/demos/pojo/sharedqueue/run.sh
+++ dso/branches/2.4/code/demos/pojo/sharedqueue/run.sh
@@ -24,7 +24,7 @@
 
 CWD=`dirname "$0"`
 TC_INSTALL_DIR=${CWD}/../../..
-
+ARGS=$*
 TC_CONFIG_PATH="${CWD}/tc-config.xml"
 set -- -q "${TC_CONFIG}"
 . "${TC_INSTALL_DIR}/bin/dso-env.sh"
@@ -36,4 +36,4 @@ CLASSPATH="${CLASSPATH}:${CWD}/lib/servlet-api-2.5-6.1.1.jar"
 
 exec "${JAVA_HOME}/bin/java" ${TC_JAVA_OPTS} ${JAVA_OPTS} \
   -Dcom.sun.management.jmxremote \
-  -cp "${CLASSPATH}" demo.sharedqueue.Main "$@"
+  -cp "${CLASSPATH}" demo.sharedqueue.Main $ARGS
