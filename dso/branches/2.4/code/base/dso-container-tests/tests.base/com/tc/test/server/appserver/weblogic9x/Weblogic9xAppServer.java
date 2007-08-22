14de9b594d8cfba4da7b01ec74a9413038702584
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-08-22 00:42:44 +0000

    merge 5172 5173 5175 5176 5178 from trunk: fix for wl92 in linux

--- dso/branches/2.4/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/weblogic9x/Weblogic9xAppServer.java
+++ dso/branches/2.4/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/weblogic9x/Weblogic9xAppServer.java
@@ -4,6 +4,7 @@
  */
 package com.tc.test.server.appserver.weblogic9x;
 
+import org.apache.commons.io.IOUtils;
 import org.codehaus.cargo.container.InstalledLocalContainer;
 import org.codehaus.cargo.container.State;
 import org.codehaus.cargo.container.configuration.LocalConfiguration;
@@ -12,9 +13,12 @@ import org.codehaus.cargo.container.weblogic.WebLogic9xInstalledLocalContainer;
 import com.tc.test.server.appserver.AppServerParameters;
 import com.tc.test.server.appserver.cargo.CargoAppServer;
 import com.tc.util.ReplaceLine;
+import com.tc.util.runtime.Os;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Weblogic9x AppServer implementation
@@ -52,10 +56,8 @@ public final class Weblogic9xAppServer extends CargoAppServer {
     protected void setState(State state) {
       if (state.equals(State.STARTING)) {
         adjustConfig();
-        File license = new File(getHome(), "license.bea");
-        if (license.exists()) {
-          this.setBeaHome(this.getHome());
-        }
+        setBeaHomeIfNeeded();
+        prepareSecurityFile();
       }
     }
 
@@ -70,6 +72,46 @@ public final class Weblogic9xAppServer extends CargoAppServer {
         ReplaceLine.parseFile(tokens, new File(getConfiguration().getHome(), "/config/config.xml"));
       } catch (IOException ioe) {
         throw new RuntimeException(ioe);
+      }
+    }
+
+    private void setBeaHomeIfNeeded() {
+      File license = new File(getHome(), "license.bea");
+      if (license.exists()) {
+        this.setBeaHome(this.getHome());
+      }
+    }
+
+    private void prepareSecurityFile() {
+      if (Os.isLinux()) {
+        try {
+          String[] resources = new String[] { "security/XACMLRoleMapperInit.ldift", "security/SerializedSystemIni.dat",
+              "security/DefaultRoleMapperInit.ldift", "security/DefaultAuthenticatorInit.ldift",
+              "init-info/tokenValue.properties", "init-info/startscript.xml", "init-info/security.xml",
+              "init-info/domain-info.xml", "config/nodemanager/nm_password.properties", "fileRealm.properties",
+              "bin/setDomainEnv.sh", "bin/startManagedWebLogic.sh", "bin/startPointBaseConsole.sh", 
+              "bin/startWebLogic.sh", "bin/stopManagedWebLogic.sh", "bin/stopWebLogic.sh"};
+          for (int i = 0; i < resources.length; i++) {
+            String resource = "linux/" + resources[i];
+            File dest = new File(getConfiguration().getHome(), resources[i]);
+            copyResource(resource, dest);
+          }
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+
+      }
+    }
+
+    private void copyResource(String name, File dest) throws IOException {
+      dest.getParentFile().mkdirs();
+      InputStream in = getClass().getResourceAsStream(name);
+      FileOutputStream out = new FileOutputStream(dest);
+      try {
+        IOUtils.copy(in, out);
+      } finally {
+        IOUtils.closeQuietly(in);
+        IOUtils.closeQuietly(out);
       }
     }
   }
