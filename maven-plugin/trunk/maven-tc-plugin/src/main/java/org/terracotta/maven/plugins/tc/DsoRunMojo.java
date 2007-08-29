7ef1a9d458cc160731d74c0a8bbcfc6eda9c1e38
ekulesho <ekulesho@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-08-29 21:38:21 +0000

    CDV-321: Developer Integration : Maven   support for running different main classes in tc:run

--- maven-plugin/trunk/maven-tc-plugin/src/main/java/org/terracotta/maven/plugins/tc/DsoRunMojo.java
+++ maven-plugin/trunk/maven-tc-plugin/src/main/java/org/terracotta/maven/plugins/tc/DsoRunMojo.java
@@ -4,8 +4,10 @@
 package org.terracotta.maven.plugins.tc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.maven.plugin.MojoExecutionException;
@@ -23,20 +25,21 @@ import EDU.oswego.cs.dl.util.concurrent.CyclicBarrier;
  * @execute phase="compile"
  * @requiresDependencyResolution runtime
  * @execute phase="compile"
+ * @configurator override
  */
 public class DsoRunMojo extends DsoLifecycleMojo {
 
-  //  /**
-  //   * Configuration for the DSO-enabled processes
-  //   * 
-  //   * @parameter expression="${processes}"
-  //   * @required
-  //   */
-  //  private ProcessConfiguration[] processes;
+  /**
+   * Configuration for the DSO-enabled processes
+   * 
+   * @parameter expression="${processes}"
+   * @optional
+   */
+  private ProcessConfiguration[] processes;
 
   /**
    * @parameter expression="${className}"
-   * @required
+   * @optional
    */
   private String className;
 
@@ -50,6 +53,7 @@ public class DsoRunMojo extends DsoLifecycleMojo {
    * @parameter expression="${numberOfNodes}" default-value="1"
    */
   private int numberOfNodes;
+  
     
   protected void onExecute() throws MojoExecutionException, MojoFailureException {
     getLog().info("------------------------------------------------------------------------");
@@ -57,25 +61,31 @@ public class DsoRunMojo extends DsoLifecycleMojo {
 
     getLog().info("Starting DSO processes");
 
-    ProcessConfiguration process = new ProcessConfiguration("node", className, arguments, Collections.EMPTY_MAP,
-        numberOfNodes);
+    List processes = new ArrayList();
+    if (className != null) {
+      processes.add(new ProcessConfiguration("node", className, arguments, Collections.EMPTY_MAP, numberOfNodes));
+    }
+    if(processes!=null) {
+      processes.addAll(Arrays.asList(this.processes));
+    }
 
     ArrayList names = new ArrayList();
     ArrayList cmds = new ArrayList();
 
-    for (int n = 0; n < process.getCount(); n++) {
-      String nodeName = process.getCount() > 0 ? process.getNodeName() + n : process.getNodeName();
-      names.add(nodeName);
-      cmds.add(createCommandLine(process, nodeName));
+    int totalNumberOfNodes = 0;
+    for (Iterator it = processes.iterator(); it.hasNext();) {
+      ProcessConfiguration process = (ProcessConfiguration) it.next();
+      totalNumberOfNodes += process.getCount();
     }
-
-    //    for (int i = 0; i < processes.length; i++) {
-    //      ProcessConfiguration process = processes[i];
-    //      getLog().info(process.getClassName());
-    //      for (int j = 0; j < process.getCount(); j++) {
-    //        createCommandLine(process, j, names, cmds);
-    //      }
-    //    }
+      
+    for (Iterator it = processes.iterator(); it.hasNext();) {
+      ProcessConfiguration process = (ProcessConfiguration) it.next();
+      for (int n = 0; n < process.getCount(); n++) {
+        String nodeName = process.getCount() > 1 ? process.getNodeName() + n : process.getNodeName();
+        names.add(nodeName);
+        cmds.add(createCommandLine(process, nodeName, totalNumberOfNodes));
+      }
+    }
 
     CyclicBarrier barrier = new CyclicBarrier(names.size() + 1);
 
@@ -96,7 +106,7 @@ public class DsoRunMojo extends DsoLifecycleMojo {
     getLog().info("DSO processes finished");
   }
 
-  private Commandline createCommandLine(ProcessConfiguration process, String nodeName) {
+  private Commandline createCommandLine(ProcessConfiguration process, String nodeName, int totalNumberOfNodes) {
     Commandline cmd = super.createCommandLine();
 
     if (workingDirectory != null) {
@@ -106,7 +116,7 @@ public class DsoRunMojo extends DsoLifecycleMojo {
     cmd.createArgument().setValue("-Xbootclasspath/p:" + bootJar.getAbsolutePath());
 
     cmd.createArgument().setValue("-Dtc.nodeName=" + nodeName);
-    cmd.createArgument().setValue("-Dtc.numberOfNodes=" + numberOfNodes);
+    cmd.createArgument().setValue("-Dtc.numberOfNodes=" + totalNumberOfNodes);
 
     // system properties      
     for (Iterator it = process.getProperties().entrySet().iterator(); it.hasNext();) {
@@ -131,12 +141,12 @@ public class DsoRunMojo extends DsoLifecycleMojo {
   private void fork(final String nodeName, final Commandline cmd, final CyclicBarrier barrier) {
     new Thread() {
       public void run() {
-        getLog().debug("Starting process: " + cmd.toString());
+        getLog().debug("Starting node " + nodeName + ": " + cmd.toString());
         try {
           StreamConsumer streamConsumer = new ForkedProcessStreamConsumer(nodeName);
           CommandLineUtils.executeCommandLine(cmd, streamConsumer, streamConsumer);
         } catch (CommandLineException e) {
-          getLog().error("Failed to start process", e);
+          getLog().error("Failed to start node " + nodeName, e);
         } finally {
           try {
             barrier.barrier();
@@ -151,5 +161,5 @@ public class DsoRunMojo extends DsoLifecycleMojo {
 
     // Thread.yield();
   }
-
+  
 }
