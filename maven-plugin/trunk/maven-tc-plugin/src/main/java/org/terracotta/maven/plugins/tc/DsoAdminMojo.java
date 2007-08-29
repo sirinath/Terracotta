a422a012faf3580af32b50b0929363c834ff67a0
ekulesho <ekulesho@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-08-29 22:19:19 +0000

    CDV-321: Developer Integration : Maven   tc:admin goal

---
+++ maven-plugin/trunk/maven-tc-plugin/src/main/java/org/terracotta/maven/plugins/tc/DsoAdminMojo.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.terracotta.maven.plugins.tc;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.terracotta.maven.plugins.tc.cl.CommandLineUtils;
+import org.terracotta.maven.plugins.tc.cl.Commandline;
+
+import com.tc.admin.AdminClient;
+
+/**
+ * @author Eugene Kuleshov
+ * 
+ * @goal admin
+ */
+public class DsoAdminMojo extends AbstractDsoMojo {
+
+  public DsoAdminMojo() {
+  }
+  
+  /**
+   * @see org.apache.maven.plugin.Mojo#execute()
+   */
+  public void execute() throws MojoExecutionException {
+    Commandline cmd = createCommandLine();
+
+    cmd.createArgument().setValue("-cp");
+    cmd.createArgument().setValue(createPluginClasspath());
+
+    cmd.createArgument().setValue(AdminClient.class.getName());
+    
+    getLog().info(cmd.toString());
+
+    try {
+      ForkedProcessStreamConsumer streamConsumer = new ForkedProcessStreamConsumer("admin");
+
+      getLog().info("------------------------------------------------------------------------");
+      getLog().info("Starting Terracotta Admin");
+      CommandLineUtils.executeCommandLine(cmd, null, streamConsumer, streamConsumer, true);
+      getLog().info("OK");
+    } catch (Exception e) {
+      getLog().error("Failed to execute bootjar tool", e);
+    }
+  }
+
+}
