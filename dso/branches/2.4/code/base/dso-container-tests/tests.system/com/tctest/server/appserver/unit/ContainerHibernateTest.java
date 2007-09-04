8d19729142d720302a29494e8b3570ba4fe57a3d
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-09-04 15:42:25 +0000

    timebombed

--- dso/branches/2.4/code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/ContainerHibernateTest.java
+++ dso/branches/2.4/code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/ContainerHibernateTest.java
@@ -31,6 +31,8 @@ public class ContainerHibernateTest extends AbstractTwoServerDeploymentTest {
   }
 
   public ContainerHibernateTest() {
+    disableAllUntil("2007-09-07");
+    
     if (shouldDisable()) {
       disableAllUntil(new Date(Long.MAX_VALUE));
     }
