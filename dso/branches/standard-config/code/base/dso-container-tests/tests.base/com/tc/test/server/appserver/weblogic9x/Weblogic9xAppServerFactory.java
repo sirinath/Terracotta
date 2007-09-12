458acb254ea7eb1828fe51a04e5f323dd0e82d01
jgalang <jgalang@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-09-12 18:27:37 +0000

    Merged revisions 5532-5554 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/standard-config/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/weblogic9x/Weblogic9xAppServerFactory.java
+++ dso/branches/standard-config/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/weblogic9x/Weblogic9xAppServerFactory.java
@@ -1,20 +1,17 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.test.server.appserver.weblogic9x;
 
 import com.tc.test.TestConfigObject;
 import com.tc.test.server.appserver.AppServer;
+import com.tc.test.server.appserver.AppServerFactory;
 import com.tc.test.server.appserver.AppServerInstallation;
 import com.tc.test.server.appserver.AppServerParameters;
-import com.tc.test.server.appserver.AppServerFactory;
-import com.tc.test.server.appserver.war.DescriptorXml;
-import com.tc.test.server.appserver.war.DtdWar;
-import com.tc.test.server.appserver.war.War;
-import com.tc.test.server.tcconfig.StandardTerracottaAppServerConfig;
+import com.tc.test.server.appserver.StandardAppServerParameters;
 
 import java.io.File;
-import java.net.URL;
 import java.util.Properties;
 
 /**
@@ -29,31 +26,15 @@ public final class Weblogic9xAppServerFactory extends AppServerFactory {
   }
 
   public AppServerParameters createParameters(String instanceName, Properties props) {
-    return new Weblogic9xAppServerParameters(instanceName, props, config.sessionClasspath());
+    return new StandardAppServerParameters(instanceName, props);
   }
 
   public AppServer createAppServer(AppServerInstallation installation) {
     return new Weblogic9xAppServer((Weblogic9xAppServerInstallation) installation);
   }
 
-  public AppServerInstallation createInstallation(URL host, File serverDir, File workingDir) throws Exception {
-    return new Weblogic9xAppServerInstallation(host, serverDir, workingDir, config.appserverMajorVersion(), config
-        .appserverMinorVersion());
-  }
-
   public AppServerInstallation createInstallation(File home, File workingDir) throws Exception {
     return new Weblogic9xAppServerInstallation(home, workingDir, config.appserverMajorVersion(), config
         .appserverMinorVersion());
-  }
-
-  public War createWar(String appName) {
-    War war = new DtdWar(appName);
-    DescriptorXml weblogicWar = new Weblogic9xDescriptorXml();
-    war.addContainerSpecificXml(weblogicWar.getFileName(), weblogicWar.getBytes());
-    return war;
-  }
-
-  public StandardTerracottaAppServerConfig createTcConfig(File baseDir) {
-    return new Weblogic9xAppServerConfig(baseDir);
   }
 }
