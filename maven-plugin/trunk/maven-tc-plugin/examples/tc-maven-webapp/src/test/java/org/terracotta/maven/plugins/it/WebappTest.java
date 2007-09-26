eadd1559d4ad6a5aa3daa649a8c51a86edc413bf
ekulesho <ekulesho@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-09-26 18:52:01 +0000

    web application example

---
+++ maven-plugin/trunk/maven-tc-plugin/examples/tc-maven-webapp/src/test/java/org/terracotta/maven/plugins/it/WebappTest.java
@@ -0,0 +1,17 @@
+package org.terracotta.maven.plugins.it;
+
+import junit.framework.TestCase;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+public class WebappTest extends TestCase
+{
+    public void testCallIndexPage() throws Exception
+    {
+        URL url = new URL("http://localhost:8080/tc-maven-webapp");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        assertEquals(200, connection.getResponseCode());
+    }
+}
