983d5d64cf0938920ec43a1d73728a03a14bd6a9
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-10-22 23:58:19 +0000

    default test run to jdk 1.5

--- dev-tools/trunk/tcdev/src/launch/actions/WorkbenchOptionAction.java
+++ dev-tools/trunk/tcdev/src/launch/actions/WorkbenchOptionAction.java
@@ -2,7 +2,7 @@
  * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
  */
 package launch.actions;
-import org.eclipse.core.runtime.Preferences;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -10,24 +10,19 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import refreshall.Activator;
 
-
 public class WorkbenchOptionAction implements IWorkbenchWindowActionDelegate {
 
-	public static final String TEST_WITH_14_KEY = "testWith14"; 
-	
-	public void dispose() {
-	}
+  public void dispose() {
+  }
 
-	public void init(IWorkbenchWindow window) {
-	}
+  public void init(IWorkbenchWindow window) {
+  }
 
-	public void run(IAction action) {
-		Preferences prefs = Activator.getDefault().getPluginPreferences();
-		prefs.setValue(TEST_WITH_14_KEY, action.isChecked());
-		Activator.getDefault().savePluginPreferences();
-	}
+  public void run(IAction action) {
+    Activator.getDefault().savePluginPreferences();
+  }
 
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
+  public void selectionChanged(IAction action, ISelection selection) {
+  }
 
 }
