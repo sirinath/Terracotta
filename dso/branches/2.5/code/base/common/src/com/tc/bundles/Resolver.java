003382d0b4e8c2be573dec6466884bb979180197
jgalang <jgalang@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-10-25 22:36:12 +0000

    merged rev 6073:6077 from trunk, this will unbreak maven-tc-plugin's usage of BundleSpec

--- dso/branches/2.5/code/base/common/src/com/tc/bundles/Resolver.java
+++ dso/branches/2.5/code/base/common/src/com/tc/bundles/Resolver.java
@@ -223,10 +223,10 @@ public class Resolver {
       return;
     }
 
-    String[] defaultModulesSpec = BundleSpecImpl.getRequirements(defaultModulesProp);
+    final String[] defaultModulesSpec = BundleSpec.getRequirements(defaultModulesProp);
     if (defaultModulesSpec.length > 0) {
       for (int i = 0; i < defaultModulesSpec.length; i++) {
-        BundleSpec spec = new BundleSpecImpl(defaultModulesSpec[i]);
+        BundleSpec spec = BundleSpec.newInstance(defaultModulesSpec[i]);
         ensureBundle(spec);
       }
     } else {
@@ -249,22 +249,22 @@ public class Resolver {
     final TCProperties props = TCPropertiesImpl.getProperties().getPropertiesFor(TC_PROPERTIES_SECTION);
     final String additionalModulesProp = props != null ? props.getProperty("additional") : null;
     if (additionalModulesProp != null) {
-      String[] additionalModulesSpec = BundleSpecImpl.getRequirements(additionalModulesProp);
+      String[] additionalModulesSpec = BundleSpec.getRequirements(additionalModulesProp);
       if (additionalModulesSpec.length > 0) {
         for (int i = 0; i < additionalModulesSpec.length; i++) {
-          BundleSpec spec = new BundleSpecImpl(additionalModulesSpec[i]);
+          BundleSpec spec = BundleSpec.newInstance(additionalModulesSpec[i]);
           ensureBundle(spec);
         }
       }
     }
   }
 
-  private BundleSpec[] getRequirements(Manifest manifest) throws BundleException {
+  private BundleSpec[] getRequirements(Manifest manifest) {
     List requirementList = new ArrayList();
-    String[] manifestRequirements = BundleSpecImpl.getRequirements(manifest);
+    String[] manifestRequirements = BundleSpec.getRequirements(manifest);
     if (manifestRequirements.length > 0) {
       for (int i = 0; i < manifestRequirements.length; i++) {
-        requirementList.add(new BundleSpecImpl(manifestRequirements[i]));
+        requirementList.add(BundleSpec.newInstance(manifestRequirements[i]));
       }
     }
     return (BundleSpec[]) requirementList.toArray(new BundleSpec[0]);
