bfb41f53a34b49a16bfe6e45d064576ab815e2a7
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-10-25 13:41:12 +0000

    Created 2.5 branch from trunk.

---
+++ dso/branches/2.5/code/base/common/src/com/tc/bundles/BundleSpecImpl.java
@@ -0,0 +1,147 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.bundles;
+
+import org.knopflerfish.framework.VersionRange;
+import org.osgi.framework.BundleException;
+
+import com.tc.bundles.exception.InvalidBundleManifestException;
+import com.tc.bundles.Version;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.jar.Manifest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Specification for the Require-Bundle attribute
+ * 
+ * <pre>
+ * SYNTAX:
+ * Require-Bundle ::= bundle {, bundle...}
+ * bundle ::= symbolic-name{;bundle-version:=&quot;constraint&quot;{;resolution:=optional}}
+ * constraint ::= [range] || (range)
+ * range ::= min, {max}
+ * 
+ * EXAMPLES:
+ * Require-Bundle: foo.bar.baz.widget - require widget bundle from group foo.bar.baz
+ * Require-Bundle: foo.bar.baz.widget, foo.bar.baz.gadget - require widget and gadget bundles
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;1.0.0&quot; - widget bundle must be version 1.0.0
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;[1.0.0, 2.0.0]&quot; - bundle version must &gt; 1.0.0 and &lt; 2.0.0
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;[1.0.0, 2.0.0)&quot; - bundle version must &gt; 1.0.0 and &lt;= 2.0.0
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;(1.0.0, 2.0.0)&quot; - bundle version must &gt;= 1.0.0 and &lt;= 2.0.0
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;(1.0.0, 2.0.0]&quot; - bundle version must &gt;= 1.0.0 and &lt; 2.0.0
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;[1.0.0,]&quot; - bundle version must &gt; 1.0.0
+ * Require-Bundle: foo.bar.baz.widget;bundle-version:=&quot;(1.0.0,)&quot; - bundle version must &gt;= 1.0.0
+ * Require-Bundle: foo.bar.baz.widget;resolution:=optional - bundle is optional (recognized but not supported)
+ * </pre>
+ */
+public final class BundleSpecImpl extends BundleSpec {
+  private static final String PROP_KEY_RESOLUTION         = "resolution";
+  private static final String PROP_KEY_BUNDLE_VERSION     = "bundle-version";
+  private static final String BUNDLE_SYMBOLIC_NAME_REGEX  = "[a-zA-Z][A-Za-z0-9._\\-]+";
+  private static final String REQUIRE_BUNDLE_EXPR_MATCHER = "(" + BUNDLE_SYMBOLIC_NAME_REGEX
+                                                              + "(;resolution:=\"?optional\"?)?" + //
+                                                              "(;bundle-version:=(\"?[A-Za-z0-9.]+\"?|" + //
+                                                              "\"?[\\[\\(][A-Za-z0-9.]+,[A-Za-z0-9.]*[\\]\\)]\"?))?)";
+
+  private final String        symbolicName;
+  private final Map           attributes                  = new HashMap();
+
+  public static final String[] getRequirements(final Manifest manifest) throws BundleException {
+    return getRequirements(manifest.getMainAttributes().getValue("Require-Bundle"));
+  }
+
+  public static final String[] getRequirements(final String source) throws BundleException {
+    if (source == null) return new String[0];
+
+    final List list = new ArrayList();
+    final String spec = source.replaceAll(" ", "");
+    final Pattern pattern = Pattern.compile(REQUIRE_BUNDLE_EXPR_MATCHER);
+    final Matcher matcher = pattern.matcher(spec);
+    final StringBuffer check = new StringBuffer();
+
+    while (matcher.find()) {
+      final String group = matcher.group();
+      check.append("," + group);
+      list.add(group);
+    }
+
+    if (!spec.equals(check.toString().replaceFirst(",", ""))) { //
+      throw new InvalidBundleManifestException("Syntax error specifying Require-Bundle: " + source + " found " + check);
+    }
+
+    return (String[]) list.toArray(new String[list.size()]);
+  }
+
+  public BundleSpecImpl(final String spec) {
+    final String[] data = spec.split(";");
+    this.symbolicName = data[0];
+    for (int i = 1; i < data.length; i++) {
+      final String[] pairs = data[i].replaceAll(" ", "").split(":=");
+      attributes.put(pairs[0], pairs[1].replaceAll("\\\"", ""));
+    }
+  }
+
+  public String getSymbolicName() {
+    return this.symbolicName;
+  }
+
+  public String getName() {
+    return extractInfo("name");
+  }
+
+  public String getGroupId() {
+    return extractInfo("group-id");
+  }
+
+  private String extractInfo(final String n) {
+    final String[] pieces = this.symbolicName.split("\\.");
+    int k = 0;
+    for (int i = pieces.length - 1; i >= 0; i--) {
+      if (pieces[i].matches("^" + BUNDLE_SYMBOLIC_NAME_REGEX)) {
+        k = i;
+        break;
+      }
+    }
+    final int start = "name".equals(n) ? k : 0;
+    final int end = "name".equals(n) ? pieces.length : k;
+    final StringBuffer result = new StringBuffer();
+    for (int j = start; j < end; j++) {
+      result.append(pieces[j]).append(".");
+    }
+    return result.toString().replaceFirst("\\.$", "");
+  }
+
+  public String getVersion() {
+    final String bundleversion = (String) attributes.get(PROP_KEY_BUNDLE_VERSION);
+    return (bundleversion == null) ? "(any-version)" : bundleversion;
+  }
+
+  public boolean isOptional() {
+    final String resolution = (String) attributes.get(PROP_KEY_RESOLUTION);
+    return (resolution != null) && resolution.equals("optional");
+  }
+
+  public boolean isCompatible(final String symname, final String version) {
+    // symbolic-names must match
+    if (!BundleSpec.isMatchingSymbolicName(this.symbolicName, symname)) return false;
+
+    // if symbolic-names are matching, then check for version compatibility
+    String spec = (String) attributes.get(PROP_KEY_BUNDLE_VERSION);
+
+    // no specific bundle-version required/specified
+    // so it must be compatible with the version
+    if (spec == null) return true;
+
+    final Version target = new Version(version);
+    VersionRange range = new VersionRange(spec);
+
+    return range.withinRange(target);
+  }
+}
---
+++ dso/branches/2.5/code/base/dso-l1-tomcat55/src/org/apache/catalina/connector/SessionResponse55.java
@@ -0,0 +1,54 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package org.apache.catalina.connector;
+
+import com.tc.object.util.OverrideCheck;
+import com.terracotta.session.SessionResponse;
+
+// NOTE: A class adapter adds methods that delegate all override'able methods of tomcat's
+// Response class to the "valveRes" instance
+public class SessionResponse55 extends Response {
+  static {
+    OverrideCheck.check(Response.class, SessionResponse55.class);
+  }
+
+  private final Response         valveRes;
+  private final SessionRequest55 sessReq;
+  private final SessionResponse  sessRes;
+
+  public SessionResponse55(Response valveRes, SessionRequest55 sessReq, SessionResponse sessRes) {
+    this.valveRes = valveRes;
+    this.sessRes = sessRes;
+    this.sessReq = sessReq;
+
+    // silence compiler warning
+    if (false && this.valveRes != this.valveRes) { throw new AssertionError(); }
+  }
+
+  public String encodeRedirectUrl(String url) {
+    return sessRes.encodeRedirectUrl(url);
+  }
+
+  public String encodeRedirectURL(String url) {
+    return sessRes.encodeRedirectUrl(url);
+  }
+
+  public String encodeUrl(String url) {
+    return sessRes.encodeUrl(url);
+  }
+
+  public String encodeURL(String url) {
+    return sessRes.encodeURL(url);
+  }
+
+  public Request getRequest() {
+    return this.sessReq;
+  }
+
+  public javax.servlet.http.HttpServletResponse getResponse() {
+    return this.sessRes;
+  }
+
+}
