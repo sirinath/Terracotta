c8c5f0e3cdcd0dd0fa0670fd48c2a3401971f409
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-11-28 23:36:01 +0000

    Branch for TC Group Comm

---
+++ dso/branches/TcGroupComm/dso-container-tests/tests.base/com/tc/test/server/appserver/deployment/ServerTestSetup.java
@@ -0,0 +1,71 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.test.server.appserver.deployment;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import junit.extensions.TestSetup;
+import junit.framework.TestSuite;
+
+public class ServerTestSetup extends TestSetup {
+
+  private final Class   testClass;  
+  private ServerManager sm;
+  protected boolean     persistentMode   = false;
+  
+
+  public ServerTestSetup(Class testClass) {
+    super(new TestSuite(testClass));
+    this.testClass = testClass;
+  }
+
+  public ServerTestSetup(Class testClass, boolean persistentMode) {
+    this(testClass);
+    this.persistentMode = persistentMode;
+  }
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    getServerManager();
+  }
+
+  protected void tearDown() throws Exception {
+    if (sm != null) {
+      ServerManagerUtil.stopAndRelease(sm);
+    }
+  }
+
+  protected ServerManager getServerManager() {
+    if (sm == null) {
+      try {
+        sm = ServerManagerUtil.startAndBind(testClass, isWithPersistentStore());
+      } catch (Exception e) {
+        throw new RuntimeException("Unable to create server manager", e);
+      }
+    }
+    return sm;
+  }
+
+  public DeploymentBuilder makeDeploymentBuilder() throws IOException {
+    return getServerManager().makeDeploymentBuilder();
+  }
+
+  public DeploymentBuilder makeDeploymentBuilder(String warFileName) {
+    return getServerManager().makeDeploymentBuilder(warFileName);
+  }
+
+  public boolean isWithPersistentStore() {
+    return persistentMode;
+  }
+
+  public boolean shouldDisable() {
+    for (Enumeration e = ((TestSuite) fTest).tests(); e.hasMoreElements();) {
+      Object o = e.nextElement();
+      if (o instanceof AbstractDeploymentTest && ((AbstractDeploymentTest) o).shouldDisable()) { return true; }
+    }
+    return false;
+  }
+}
---
+++ dso/branches/TcGroupComm/ui-eclipse/src/org/terracotta/dso/JavaModelClassInfo.java
@@ -0,0 +1,139 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package org.terracotta.dso;
+
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.IAnnotationBinding;
+
+import com.tc.aspectwerkz.reflect.ClassInfo;
+import com.tc.aspectwerkz.reflect.FieldInfo;
+import com.tc.aspectwerkz.reflect.MethodInfo;
+import com.tc.backport175.bytecode.AnnotationElement;
+import com.tc.object.bytecode.aspectwerkz.SimpleClassInfo;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JavaModelClassInfo extends SimpleClassInfo {
+  private IType fType;
+  private String fSuperClassSig;
+  private ClassInfo fSuperClass;
+  private String[] fInterfaceSigs;
+  private ClassInfo[] fInterfaces;
+  private List<AnnotationElement.Annotation> fAnnotations = new ArrayList<AnnotationElement.Annotation>();
+  private final Map<IMethod, SoftReference> fMethodInfoCache = new HashMap<IMethod, SoftReference>();
+  private final Map<IField, SoftReference> fFieldInfoCache = new HashMap<IField, SoftReference>();
+
+  private static final String[] NO_INTERFACE_SIGS = new String[0];
+  private static final ClassInfo[] NO_INTERFACES = new ClassInfo[0];
+  
+  public JavaModelClassInfo(String classname) {
+    super(classname);
+    fInterfaceSigs = NO_INTERFACE_SIGS;
+    fInterfaces = NO_INTERFACES;
+  }
+  
+  public JavaModelClassInfo(IType type) {
+    super(type.getFullyQualifiedName('$'));
+    fType = type;
+    try {
+      fSuperClassSig = type.getSuperclassTypeSignature();
+      if(fSuperClassSig != null) {
+        fSuperClass = new JavaModelClassInfo(JdtUtils.getResolvedTypeName(fSuperClassSig, type));
+      }
+      fInterfaceSigs = NO_INTERFACE_SIGS;
+      fInterfaces = NO_INTERFACES;
+      if(type.isClass()) {
+        fInterfaceSigs = type.getSuperInterfaceTypeSignatures();
+        fInterfaces = new JavaModelClassInfo[fInterfaceSigs.length];
+        for(int i = 0; i < fInterfaceSigs.length; i++) {
+          fInterfaces[i] = new JavaModelClassInfo(JdtUtils.getResolvedTypeName(fInterfaceSigs[i], type));
+        }
+      }
+    } catch(JavaModelException jme) {
+      fInterfaceSigs = NO_INTERFACE_SIGS;
+      fInterfaces = NO_INTERFACES;
+    }
+  }
+
+  public IType getType() {
+    return fType;
+  }
+  
+  public ClassInfo getSuperclass() {
+    return fSuperClass;
+  }
+  
+  public ClassInfo[] getInterfaces() {
+    return fInterfaces.clone();
+  }
+
+  public MethodInfo getMethod(ClassInfoFactory classInfoFactory, IMethod method) throws JavaModelException {
+    MethodInfo info = null;
+    synchronized (fMethodInfoCache) {
+      SoftReference ref = fMethodInfoCache.get(method);
+      if(ref != null) {
+        info = (MethodInfo) ref.get();
+      }
+      if (info == null) {
+        info = new JavaModelMethodInfo(classInfoFactory, method);
+        fMethodInfoCache.put(method, new SoftReference(info));
+      }
+    }
+    return info;
+  }
+  
+  public FieldInfo getField(ClassInfoFactory classInfoFactory, IField field) {
+    FieldInfo info = null;
+    synchronized (fFieldInfoCache) {
+      SoftReference ref = fFieldInfoCache.get(field);
+      if(ref != null) {
+        info = (FieldInfo) ref.get();
+      }
+      if (info == null) {
+        info = new JavaModelFieldInfo(classInfoFactory, field);
+        fFieldInfoCache.put(field, new SoftReference(info));
+      }
+    }
+    return info;
+  }
+  
+  public void clearAnnotations() {
+    fAnnotations.clear();
+  }
+  
+  public void addAnnotation(Annotation annotation) {
+    IAnnotationBinding binding = annotation.resolveAnnotationBinding();
+    String name = binding.getAnnotationType().getQualifiedName();
+    fAnnotations.add(new AnnotationElement.Annotation(name));
+  }
+  
+  public AnnotationElement.Annotation[] getAnnotations() {
+    return fAnnotations.toArray(new AnnotationElement.Annotation[0]);
+  }
+  
+  public boolean isStale() {
+    try {
+      if(fType == null) return true;
+      String superClassSig = fType.getSuperclassTypeSignature();
+      if(superClassSig == null && fSuperClassSig != null ||
+          superClassSig != null && fSuperClassSig == null ||
+          superClassSig != null && !superClassSig.equals(fSuperClassSig)) { return true; }
+
+      String[] interfaceSigs = fType.getSuperInterfaceTypeSignatures();
+      if(interfaceSigs.length != fInterfaceSigs.length) return true;
+      for(int i = 0; i < interfaceSigs.length; i++) {
+        if(!interfaceSigs[i].equals(fInterfaceSigs[i])) return true;
+      }
+    } catch(JavaModelException jme) {/**/}
+    return false;
+  }
+}
