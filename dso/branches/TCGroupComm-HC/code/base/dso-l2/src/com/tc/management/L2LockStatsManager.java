5fd00d4997a56911d3123f82b6f517d3edad0e09
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2007-12-19 17:31:35 +0000

    TCGroupComm module : HealthCheck

---
+++ dso/branches/TCGroupComm-HC/code/base/dso-l2/src/com/tc/management/L2LockStatsManager.java
@@ -0,0 +1,228 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.management;
+
+import com.tc.async.api.Sink;
+import com.tc.net.groups.NodeID;
+import com.tc.object.lockmanager.api.LockID;
+import com.tc.object.lockmanager.api.ThreadID;
+import com.tc.object.net.DSOChannelManager;
+import com.tc.objectserver.lockmanager.api.LockHolder;
+import com.tc.objectserver.lockmanager.api.LockManager;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public interface L2LockStatsManager {
+  public final static L2LockStatsManager NULL_LOCK_STATS_MANAGER = new L2LockStatsManager() {
+    public void start(DSOChannelManager channelManager, LockManager lockManager, Sink sink) {
+      // do nothing
+    }
+    
+    public void enableClientStackTrace(LockID lockID) {
+      // do nothing
+    }
+    
+    public void enableClientStackTrace(LockID lockID, int stackTraceDepth, int statCollectFrequency) {
+      // do nothing
+    }
+    
+    public void disableClientStackTrace(LockID lockID) {
+      // do nothing
+    }
+    
+    public boolean isClientLockStackTraceEnable(LockID lockID) {
+      return false;
+    }
+    
+    public void lockRequested(LockID lockID, NodeID nodeID, ThreadID threadID, int lockLevel) {
+      // do nothing
+    }
+    
+    public void lockAwarded(LockID lockID, NodeID nodeID, ThreadID threadID, boolean isGreedy, long lockAwardTimestamp) {
+      // do nothing
+    }
+    
+    public void lockReleased(LockID lockID, NodeID nodeID, ThreadID threadID) {
+      // do nothing
+    }
+    
+    public void lockRejected(LockID lockID, NodeID nodeID, ThreadID threadID) {
+      // do nothing
+    }
+    
+    public void lockWait(LockID lockID) {
+      // do nothing
+    }
+    
+    public void lockNotified(LockID lockID, int n) {
+      // do nothing
+    }
+    
+    public long getNumberOfLockRequested(LockID lockID) {
+      return 0;
+    }
+    
+    public long getNumberOfLockReleased(LockID lockID) {
+      return 0;
+    }
+    
+    public long getNumberOfPendingRequests(LockID lockID) {
+      return 0;
+    }
+    
+    public long getNumberOfLockHopRequests(LockID lockID) {
+      return 0;
+    }
+    
+    public LockHolder getLockHolder(LockID lockID, NodeID nodeID, ThreadID threadID) {
+      return null;
+    }
+    
+    public Collection getTopLockStats(int n) {
+      return Collections.EMPTY_LIST;
+    }
+    
+    public Collection getTopLockHoldersStats(int n) {
+      return Collections.EMPTY_LIST;
+    }
+    
+    public Collection getTopWaitingLocks(int n) {
+      return Collections.EMPTY_LIST;
+    }
+    
+    public Collection getTopContendedLocks(int n) {
+      return Collections.EMPTY_LIST;
+    }
+    
+    public Collection getTopLockHops(int n) {
+      return Collections.EMPTY_LIST;
+    }
+
+    public void recordStackTraces(LockID lockID, NodeID nodeID, List stackTraces) {
+      // do nothing
+    }
+    
+    public Collection getStackTraces(LockID lockID) {
+      return Collections.EMPTY_LIST;
+    }
+
+    public boolean isLockStackTraceEnabledInClient(LockID lockID, NodeID nodeID) {
+      return false;
+    }
+
+    public void recordClientStackTraceEnabled(LockID lockID, NodeID nodeID) {
+      // do nothing
+    }
+
+    public int getLockStackTraceDepth(LockID lockID) {
+      return 0;
+    }
+
+    public int getLockStatCollectFrequency(LockID lockID) {
+      return 0;
+    }
+    
+    public void setLockStatisticsEnabled(boolean lockStatsEnabled) {
+      // do nothing
+    }
+
+    public boolean isLockStatisticsEnabled() {
+      return false;
+    }
+    
+    public void enableLockStatistics() {
+      // do nothing
+    }
+    
+    public void disableLockStatistics() {
+      // do nothing
+    }
+
+    public Collection getTopAggregateLockHolderStats(int n) {
+      return Collections.EMPTY_LIST;
+    }
+
+    public Collection getTopAggregateWaitingLocks(int n) {
+      return Collections.EMPTY_LIST;
+    }
+
+    public void lockHopped(LockID lockID) {
+      // do nothing
+    }
+
+    public void clearAllStatsFor(NodeID nodeID) {
+      // do nothing
+    }
+  };
+  
+  public void start(DSOChannelManager channelManager, LockManager lockManager, Sink sink);
+  
+  public void enableClientStackTrace(LockID lockID);
+  
+  public void enableClientStackTrace(LockID lockID, int stackTraceDepth, int statCollectFrequency);
+  
+  public void disableClientStackTrace(LockID lockID);
+  
+  public boolean isClientLockStackTraceEnable(LockID lockID);
+  
+  public boolean isLockStackTraceEnabledInClient(LockID lockID, NodeID nodeID);
+  
+  public void recordClientStackTraceEnabled(LockID lockID, NodeID nodeID);
+  
+  public void lockHopped(LockID lockID);
+  
+  public void lockRequested(LockID lockID, NodeID nodeID, ThreadID threadID, int lockLevel);
+
+  public void lockAwarded(LockID lockID, NodeID nodeID, ThreadID threadID, boolean isGreedy, long lockAwardTimestamp);
+  
+  public void lockReleased(LockID lockID, NodeID nodeID, ThreadID threadID);
+  
+  public void lockRejected(LockID lockID, NodeID nodeID, ThreadID threadID);
+  
+  public void lockWait(LockID lockID);
+  
+  public void lockNotified(LockID lockID, int n);
+  
+  public void recordStackTraces(LockID lockID, NodeID nodeID, List stackTraces);
+  
+  public long getNumberOfLockRequested(LockID lockID);
+  
+  public long getNumberOfLockReleased(LockID lockID);
+  
+  public long getNumberOfPendingRequests(LockID lockID);
+  
+  public long getNumberOfLockHopRequests(LockID lockID);
+  
+  public LockHolder getLockHolder(LockID lockID, NodeID nodeID, ThreadID threadID);
+  
+  public Collection getTopLockStats(int n);
+  
+  public Collection getTopAggregateLockHolderStats(int n);
+  
+  public Collection getTopLockHoldersStats(int n);
+  
+  public Collection getTopAggregateWaitingLocks(int n);
+  
+  public Collection getTopWaitingLocks(int n);
+  
+  public Collection getTopContendedLocks(int n);
+  
+  public Collection getTopLockHops(int n);
+  
+  public Collection getStackTraces(LockID lockID);
+  
+  public int getLockStackTraceDepth(LockID lockID);
+  
+  public int getLockStatCollectFrequency(LockID lockID);
+  
+  public void setLockStatisticsEnabled(boolean lockStatsEnabled);
+
+  public boolean isLockStatisticsEnabled();
+  
+  public void clearAllStatsFor(NodeID nodeID);
+  
+}
---
+++ dso/branches/TCGroupComm-HC/code/base/dso-spring/src/com/tcspring/ApplicationHelper.java
@@ -0,0 +1,87 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tcspring;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.tc.object.bytecode.hook.DSOContext;
+import com.tc.object.bytecode.hook.impl.ClassProcessorHelper;
+import com.tc.object.config.DSOSpringConfigHelper;
+import com.tc.object.loaders.NamedClassLoader;
+
+import java.util.Iterator;
+
+/**
+ * ApplicationHelper
+ *
+ * @author Eugene Kuleshov
+ */
+public class ApplicationHelper {
+  private final transient Log logger          = LogFactory.getLog(getClass());
+
+  private static final String STANDALONE_APP  = "";
+
+  // Those prefixes defined in com.tc.object.loaders.Namespace which is not visible from this class
+  private static final String TOMCAT_PREFIX   = "Tomcat.";
+  private static final String WEBLOGIC_PREFIX = "Weblogic.";
+
+  private String              appName;
+  private DSOContext          dsoContext;
+
+  public ApplicationHelper(Class c) {
+    ClassLoader cl = c.getClassLoader();
+
+    try {
+      this.dsoContext = ClassProcessorHelper.getContext(cl);
+
+      if (cl instanceof NamedClassLoader) {
+        String name = ((NamedClassLoader) cl).__tc_getClassLoaderName();
+        logger.info("Application name " + name);
+        if (name != null) {
+          if (name.startsWith(TOMCAT_PREFIX)) {
+            name = name.substring(name.lastIndexOf('/') + 1);
+          } else if (name.startsWith(WEBLOGIC_PREFIX)) {
+            int n = name.lastIndexOf('@');
+            if (n > -1) {
+              name = name.substring(n + 1);
+            }
+
+            // for weblogic 9.2
+            if (name.endsWith(".war")) {
+              name = name.substring(0, name.length() - 4);
+            }
+          }
+        }
+        this.appName = name;
+      } else {
+        this.appName = STANDALONE_APP;
+      }
+
+    } catch (Exception e) {
+      // TODO find a better way
+    }
+  }
+
+  public boolean isDSOApplication() {
+    return this.dsoContext != null && this.appName != null;
+  }
+
+  public String getAppName() {
+    return appName;
+  }
+
+  public DSOContext getDsoContext() {
+    return dsoContext;
+  }
+
+  public boolean isFastProxyEnabled() {
+    for (Iterator it = this.dsoContext.getDSOSpringConfigHelpers().iterator(); it.hasNext();) {
+      DSOSpringConfigHelper springConfigHelper = (DSOSpringConfigHelper) it.next();
+      if (springConfigHelper.isMatchingApplication(this.appName)) { return springConfigHelper.isFastProxyEnabled(); }
+    }
+    return false;
+  }
+}
