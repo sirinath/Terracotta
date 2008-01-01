30f0db5c0ba5d50d41e19abcf3676332a9fb62cf
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-01 21:17:27 +0000

    TC-Group-Comm: test case for TC-Group birectional channel opening

--- dso/branches/TCGroupComm/code/base/dso-common/tests.unit/com/tc/net/protocol/tcm/TCGroupCommunicationsManagerImplTest.java
+++ dso/branches/TCGroupComm/code/base/dso-common/tests.unit/com/tc/net/protocol/tcm/TCGroupCommunicationsManagerImplTest.java
@@ -69,13 +69,12 @@ public class TCGroupCommunicationsManagerImplTest extends TestCase {
       assertTrue(channel.getChannelID().getNodeID().equals(nodeID1));
 
       assertEquals(1, channelManager.getChannels().length);
-      
+
       MessageChannel serverChannel = channelManager.getChannels()[0];
-      System.out.println("Group server node name:"+((NodeIDImpl)serverChannel.getChannelID().getNodeID()).getName());
-      System.out.println("Group client node name:"+((NodeIDImpl)channel.getChannelID().getNodeID()).getName());
+      System.out.println("Group server node name:" + ((NodeIDImpl) serverChannel.getChannelID().getNodeID()).getName());
+      System.out.println("Group client node name:" + ((NodeIDImpl) channel.getChannelID().getNodeID()).getName());
       assertTrue(channel.getChannelID().getNodeID().equals(serverChannel.getChannelID().getNodeID()));
-      
-      
+
       clientComms.getConnectionManager().closeAllConnections(5000);
       assertFalse(channel.isConnected());
 
@@ -92,6 +91,112 @@ public class TCGroupCommunicationsManagerImplTest extends TestCase {
         clientComms.shutdown();
       } finally {
         serverComms.shutdown();
+      }
+    }
+  }
+
+  public void testTwoWayChannelOpenClose() throws Exception {
+    NodeID nodeID1 = new NodeIdUuidImpl("node1");
+    NodeID nodeID2 = new NodeIdUuidImpl("node2");
+    
+    CommunicationsManager clientComms1 = new TCGroupCommunicationsManagerImpl(monitor,
+                                                                        new TCGroupNetworkStackHarnessFactory(), null,
+                                                                        new NullConnectionPolicy(), 0, nodeID1);
+    CommunicationsManager serverComms1 = clientComms1;
+    CommunicationsManager clientComms2 = new TCGroupCommunicationsManagerImpl(monitor,
+                                                                        new TCGroupNetworkStackHarnessFactory(), null,
+                                                                        new NullConnectionPolicy(), 0, nodeID2);
+    CommunicationsManager serverComms2 = clientComms2;
+    try {
+      NetworkListener lsnr1 = serverComms1.createListener(sessionManager, new TCSocketAddress(TCSocketAddress.LOOPBACK_ADDR,
+                                                                                        0), true,
+                                                    new DefaultConnectionIdFactory());
+      lsnr1.start(new HashSet());
+      ChannelManager channelManager1 = lsnr1.getChannelManager();
+      assertEquals(0, channelManager1.getChannels().length);
+
+      NetworkListener lsnr2 = serverComms2.createListener(sessionManager, new TCSocketAddress(TCSocketAddress.LOOPBACK_ADDR,
+                                                                                        0), true,
+                                                    new DefaultConnectionIdFactory());
+      lsnr2.start(new HashSet());
+      ChannelManager channelManager2 = lsnr2.getChannelManager();
+      assertEquals(0, channelManager2.getChannels().length);
+
+      ClientMessageChannel channel1;
+      channel1 = clientComms1
+          .createClientChannel(
+                               sessionManager,
+                               0,
+                               TCSocketAddress.LOOPBACK_IP,
+                               lsnr2.getBindPort(),
+                               3000,
+                               new ConnectionAddressProvider(
+                                                             new ConnectionInfo[] { new ConnectionInfo(
+                                                                                                       "localhost",
+                                                                                                       lsnr2
+                                                                                                           .getBindPort()) }));
+      ClientMessageChannel channel2;
+      channel2 = clientComms2
+          .createClientChannel(
+                               sessionManager,
+                               0,
+                               TCSocketAddress.LOOPBACK_IP,
+                               lsnr1.getBindPort(),
+                               3000,
+                               new ConnectionAddressProvider(
+                                                             new ConnectionInfo[] { new ConnectionInfo(
+                                                                                                       "localhost",
+                                                                                                       lsnr1
+                                                                                                           .getBindPort()) }));
+
+      channel1.open();
+      assertTrue(channel1.isConnected());
+      assertTrue(channel1.getChannelID().getNodeID().equals(nodeID1));
+      assertEquals(1, channelManager2.getChannels().length);
+
+      channel2.open();
+      assertTrue(channel2.isConnected());
+      assertTrue(channel2.getChannelID().getNodeID().equals(nodeID2));
+      assertEquals(1, channelManager1.getChannels().length);
+
+      MessageChannel serverChannel1 = channelManager1.getChannels()[0];
+      MessageChannel serverChannel2 = channelManager2.getChannels()[0];
+
+      System.out
+          .println("Group server node name:" + ((NodeIDImpl) serverChannel1.getChannelID().getNodeID()).getName());
+      System.out.println("Group client node name:" + ((NodeIDImpl) channel1.getChannelID().getNodeID()).getName());
+      assertTrue(channel1.getChannelID().getNodeID().equals(serverChannel2.getChannelID().getNodeID()));
+
+      System.out
+          .println("Group server node name:" + ((NodeIDImpl) serverChannel2.getChannelID().getNodeID()).getName());
+      System.out.println("Group client node name:" + ((NodeIDImpl) channel2.getChannelID().getNodeID()).getName());
+      assertTrue(channel2.getChannelID().getNodeID().equals(serverChannel1.getChannelID().getNodeID()));
+
+      clientComms1.getConnectionManager().closeAllConnections(5000);
+      assertFalse(channel1.isConnected());
+
+      clientComms2.getConnectionManager().closeAllConnections(5000);
+      assertFalse(channel2.isConnected());
+
+      for (int i = 0; i < 30; i++) {
+        if (channelManager1.getChannels().length == 0) {
+          if (channelManager2.getChannels().length == 0) {
+            break;
+          }
+        }
+        Thread.sleep(100);
+      }
+
+      assertEquals(0, channelManager1.getChannels().length);
+      assertEquals(0, channelManager2.getChannels().length);
+
+    } finally {
+      try {
+        clientComms1.shutdown();
+        clientComms2.shutdown();
+      } finally {
+        serverComms1.shutdown();
+        serverComms2.shutdown();
       }
     }
   }
