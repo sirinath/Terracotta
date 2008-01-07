30d40e0dfc853f19b67d9d2a4cb8278ce9dd09a3
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-07 05:01:52 +0000

    TC-Group-Comm: more test for sending GroupMessage between members

--- dso/branches/TCGroupComm/code/base/dso-l2/tests.unit/com/tc/net/groups/TCGroupMembershipImplTest.java
+++ dso/branches/TCGroupComm/code/base/dso-l2/tests.unit/com/tc/net/groups/TCGroupMembershipImplTest.java
@@ -4,6 +4,7 @@
  */
 package com.tc.net.groups;
 
+import com.tc.l2.ha.L2HAZapNodeRequestProcessor;
 import com.tc.lang.TCThreadGroup;
 import com.tc.lang.ThrowableHandler;
 import com.tc.logging.TCLogging;
@@ -18,6 +19,8 @@ import com.tc.object.session.NullSessionManager;
 import com.tc.util.PortChooser;
 
 import java.util.HashSet;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import junit.framework.TestCase;
 
@@ -28,24 +31,40 @@ public class TCGroupMembershipImplTest extends TestCase {
   final TCMessageFactory   msgFactory     = new TCMessageFactoryImpl(sessionManager, monitor);
   final TCMessageRouter    msgRouter      = new TCMessageRouterImpl();
 
-  public void testBasicChannelOpenClose() throws Exception {
-    PortChooser pc = new PortChooser();
-    final int groupPort1 = pc.chooseRandomPort();
-    final int groupPort2 = pc.chooseRandomPort();
-    Node[] nodes = new Node[] { new Node("localhost", groupPort1), new Node("localhost", groupPort2) };
-    final TCGroupMemberDiscovery discover1 = new TCGroupMemberDiscoveryStatic(nodes);
-    final TCGroupMemberDiscovery discover2 = new TCGroupMemberDiscoveryStatic(nodes);
+  private int              groupPort1, groupPort2;
+  private TCGroupMemberDiscovery discover1, discover2;
+  private TCGroupMembership      group1, group2;
 
-    TCGroupMembership group1 = new TCGroupMembershipImpl(new NullConnectionPolicy(), "localhost", groupPort1, 0,
-                                                         new TCThreadGroup(new ThrowableHandler(TCLogging
-                                                             .getLogger(TCGroupMembershipImplTest.class))));
+  protected void setUp() throws Exception {
+    super.setUp();
+    PortChooser pc = new PortChooser();
+    groupPort1 = pc.chooseRandomPort();
+    groupPort2 = pc.chooseRandomPort();
+    Node[] nodes = new Node[] { new Node("localhost", groupPort1), new Node("localhost", groupPort2) };
+    discover1 = new TCGroupMemberDiscoveryStatic(nodes);
+    discover2 = new TCGroupMemberDiscoveryStatic(nodes);
+
+    group1 = new TCGroupMembershipImpl(new NullConnectionPolicy(), "localhost", groupPort1, 0,
+                                       new TCThreadGroup(new ThrowableHandler(TCLogging
+                                           .getLogger(TCGroupMembershipImplTest.class))));
     group1.setDiscover(discover1);
     group1.start(new HashSet());
-    TCGroupMembership group2 = new TCGroupMembershipImpl(new NullConnectionPolicy(), "localhost", groupPort2, 0,
-                                                         new TCThreadGroup(new ThrowableHandler(TCLogging
-                                                             .getLogger(TCGroupMembershipImplTest.class))));
+    group2 = new TCGroupMembershipImpl(new NullConnectionPolicy(), "localhost", groupPort2, 0,
+                                       new TCThreadGroup(new ThrowableHandler(TCLogging
+                                           .getLogger(TCGroupMembershipImplTest.class))));
     group2.setDiscover(discover2);
     group2.start(new HashSet());
+
+  }
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+
+    group1.shutdown();
+    group2.shutdown();
+  }
+
+  public void testBasicChannelOpenClose() throws Exception {
 
     // open test
     TCGroupMember member1 = group1.openChannel("localhost", groupPort2);
@@ -71,31 +90,12 @@ public class TCGroupMembershipImplTest extends TestCase {
     }
     assertEquals(0, group2.size());
 
-    group1.shutdown();
-    group2.shutdown();
   }
 
   /*
    * Both open channel to each other, only one direction to keep
    */
   public void testResolveTwoWayConnection() throws Exception {
-    PortChooser pc = new PortChooser();
-    final int groupPort1 = pc.chooseRandomPort();
-    final int groupPort2 = pc.chooseRandomPort();
-    Node[] nodes = new Node[] { new Node("localhost", groupPort1), new Node("localhost", groupPort2) };
-    final TCGroupMemberDiscovery discover1 = new TCGroupMemberDiscoveryStatic(nodes);
-    final TCGroupMemberDiscovery discover2 = new TCGroupMemberDiscoveryStatic(nodes);
-
-    TCGroupMembership group1 = new TCGroupMembershipImpl(new NullConnectionPolicy(), "localhost", groupPort1, 0,
-                                                         new TCThreadGroup(new ThrowableHandler(TCLogging
-                                                             .getLogger(TCGroupMembershipImplTest.class))));
-    group1.setDiscover(discover1);
-    group1.start(new HashSet());
-    TCGroupMembership group2 = new TCGroupMembershipImpl(new NullConnectionPolicy(), "localhost", groupPort2, 0,
-                                                         new TCThreadGroup(new ThrowableHandler(TCLogging
-                                                             .getLogger(TCGroupMembershipImplTest.class))));
-    group2.setDiscover(discover2);
-    group2.start(new HashSet());
 
     TCGroupMember member1 = group1.openChannel("localhost", groupPort2);
     TCGroupMember member2 = group2.openChannel("localhost", groupPort1);
@@ -112,8 +112,73 @@ public class TCGroupMembershipImplTest extends TestCase {
     assertTrue("Expected  " + m1.getDstNodeID() + " but got " + m2.getDstNodeID(), m1.getDstNodeID()
         .equals(m2.getDstNodeID()));
 
-    group1.shutdown();
-    group2.shutdown();
+  }
+
+  public void testSendTo() throws Exception {
+    TestGroupMessageListener listener1 = new TestGroupMessageListener(100);
+    TestGroupMessageListener listener2 = new TestGroupMessageListener(100);
+    group1.registerForMessages(GroupZapNodeMessage.class, listener1);
+    group2.registerForMessages(GroupZapNodeMessage.class, listener2);
+
+    TCGroupMember member1 = group1.openChannel("localhost", groupPort2);
+    Thread.sleep(200);
+    TCGroupMember member2 = group2.getMembers().get(0);
+
+    long weights[] = new long[] { 1, 23, 44, 78 };
+    GroupMessage sMesg = new GroupZapNodeMessage(GroupZapNodeMessage.ZAP_NODE_REQUEST,
+                                                 L2HAZapNodeRequestProcessor.SPLIT_BRAIN, "Zapping node", weights);
+
+    group1.sendTo(member1.getNodeID(), sMesg);
+    GroupMessage rMesg = listener2.getNextMessageFrom(group1.getNodeID());
+    assertTrue(sMesg.toString().equals(rMesg.toString()));
+
+    group2.sendTo(member2.getNodeID(), sMesg);
+    rMesg = listener1.getNextMessageFrom(group2.getNodeID());
+    assertTrue(sMesg.toString().equals(rMesg.toString()));
+
+  }
+
+  private class MessagePackage {
+    private final GroupMessage message;
+    private final NodeID       nodeID;
+
+    MessagePackage(NodeID nodeID, GroupMessage message) {
+      this.message = message;
+      this.nodeID = nodeID;
+    }
+
+    GroupMessage getMessage() {
+      return this.message;
+    }
+
+    NodeID getNodeID() {
+      return this.nodeID;
+    }
+  }
+
+  private class TestGroupMessageListener implements GroupMessageListener {
+    private long                                timeout;
+    private LinkedBlockingQueue<MessagePackage> queue = new LinkedBlockingQueue(10);
+
+    TestGroupMessageListener(long timeout) {
+      this.timeout = timeout;
+    }
+
+    public void messageReceived(NodeID fromNode, GroupMessage msg) {
+      queue.add(new MessagePackage(fromNode, msg));
+    }
+
+    public MessagePackage poll() throws InterruptedException {
+      return (queue.poll(timeout, TimeUnit.MILLISECONDS));
+    }
+
+    public GroupMessage getNextMessageFrom(NodeID nodeID) throws InterruptedException {
+      MessagePackage pkg = poll();
+      System.out.println("XXX expected:"+nodeID+ " got:"+pkg.getNodeID());
+      assertTrue(nodeID.equals(pkg.getNodeID()));
+      return (pkg.getMessage());
+    }
+
   }
 
 }
