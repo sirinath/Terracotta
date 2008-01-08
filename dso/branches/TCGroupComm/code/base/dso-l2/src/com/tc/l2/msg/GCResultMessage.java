2b06d9332b4815bfd26309737435003d50d4025c
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-08 06:18:24 +0000

    TC-Group-Comm: use ObjectOutputStream, instead of TCObjectOutputStream, to do GroupMessage serialization to TCMessage

--- dso/branches/TCGroupComm/code/base/dso-l2/src/com/tc/l2/msg/GCResultMessage.java
+++ dso/branches/TCGroupComm/code/base/dso-l2/src/com/tc/l2/msg/GCResultMessage.java
@@ -7,7 +7,6 @@ package com.tc.l2.msg;
 import com.tc.async.api.EventContext;
 import com.tc.net.groups.AbstractGroupMessage;
 import com.tc.util.Assert;
-import com.tc.util.ObjectIDSet2;
 
 import java.io.IOException;
 import java.io.ObjectInput;
@@ -30,18 +29,14 @@ public class GCResultMessage extends AbstractGroupMessage implements EventContex
 
   protected void basicReadExternal(int msgType, ObjectInput in) throws IOException, ClassNotFoundException {
     Assert.assertEquals(GC_RESULT, msgType);
-    // Instead of in.readObject(), using externalizable api for DNADecoding
-    gcedOids = new ObjectIDSet2();
-    ((ObjectIDSet2)gcedOids).readExternal(in);
+    gcedOids = (Set) in.readObject();
   }
 
   protected void basicWriteExternal(int msgType, ObjectOutput out) throws IOException {
     Assert.assertEquals(GC_RESULT, msgType);
     // XXX::Directly serializing instead of using writeObjectIDs() to avoid HUGE messages. Since the (wrapped) set is
     // ObjectIDSet2 and since it has optimized externalization methods, this should result in far less data written out.
-    // out.writeObject(gcedOids);
-    // Instead of out.writeObject, using externalizable api for DNAEncoding
-    ((ObjectIDSet2)gcedOids).writeExternal(out);
+    out.writeObject(gcedOids);
   }
 
   public Set getGCedObjectIDs() {
