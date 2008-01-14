d0f2accbdb91df3bf46672a482084a288204d1eb
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-14 19:21:23 +0000

    fix broken tribes tests

--- dso/branches/2.5/code/base/dso-common/src/com/tc/net/groups/Node.java
+++ dso/branches/2.5/code/base/dso-common/src/com/tc/net/groups/Node.java
@@ -4,16 +4,13 @@
  */
 package com.tc.net.groups;
 
+
 public class Node {
 
   private final String host;
   private final int    port;
   private final int    hashCode;
   private final String bind;
-
-  public Node(final String host, final int port) {
-    this(host, port, null);
-  }
 
   public Node(final String host, final int port, final String bind) {
     checkArgs(host, port);
