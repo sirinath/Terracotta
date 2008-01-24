f07aee4e0b725cfa9306ae767ac947e0950cf2e9
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-24 20:00:59 +0000

    tag 2.5.1 final at rev 6823 from 2.5 branch

---
+++ dso/tags/2.5.1/code/base/common/src/com/tc/io/serializer/impl/CharacterSerializer.java
@@ -0,0 +1,29 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.io.serializer.impl;
+
+import com.tc.io.serializer.api.Serializer;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/**
+ * Character
+ */
+public final class CharacterSerializer implements Serializer {
+
+  public void serializeTo(Object o, ObjectOutput out) throws IOException {
+    out.writeChar(((Character)o).charValue());
+  }
+
+  public Object deserializeFrom(ObjectInput in) throws IOException {
+    return new Character(in.readChar());
+  }
+
+  public byte getSerializerID() {
+    return CHARACTER;
+  }
+  
+}
