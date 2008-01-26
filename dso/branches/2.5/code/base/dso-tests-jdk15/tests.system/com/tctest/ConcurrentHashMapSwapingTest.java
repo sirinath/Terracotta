8381dcbdd091810bd540bb7d65c92332d70ae4e0
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-26 09:30:34 +0000

    disabled ConcurrentHashMapSwapingTest in 2.5 branch

--- dso/branches/2.5/code/base/dso-tests-jdk15/tests.system/com/tctest/ConcurrentHashMapSwapingTest.java
+++ dso/branches/2.5/code/base/dso-tests-jdk15/tests.system/com/tctest/ConcurrentHashMapSwapingTest.java
@@ -3,9 +3,19 @@
  */
 package com.tctest;
 
+import com.tc.util.runtime.Vm;
+
+import java.util.Date;
+
 public class ConcurrentHashMapSwapingTest extends TransparentTestBase {
 
   private static final int NODE_COUNT = 3;
+
+  public ConcurrentHashMapSwapingTest() {
+    if (Vm.isIBM()) {
+      disableAllUntil(new Date(Long.MAX_VALUE));
+    }
+  }
 
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
