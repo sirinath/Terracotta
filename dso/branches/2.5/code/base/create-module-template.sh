76570a57b18803af7186d3a9abf5bda530eb99b7
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-28 23:48:38 +0000

    revert back 2.5.1 version to rebuild final 2.5.1

--- dso/branches/2.5/code/base/create-module-template.sh
+++ dso/branches/2.5/code/base/create-module-template.sh
@@ -101,7 +101,7 @@ EOF
   <parent>
     <artifactId>parent</artifactId>
     <groupId>org.terracotta</groupId>
-    <version>2.5.2-SNAPSHOT</version>
+    <version>2.5.1</version>
   </parent>
   <modelVersion>4.0.0</modelVersion>
   <artifactId>*** MODULE NAME ***</artifactId>
