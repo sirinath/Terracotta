a459ecfeb3470605af557a8e48f985f104f1e192
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-01-29 01:08:10 +0000

    tag for 2.5.1 final release from 2.5 branch at rev 6850

---
+++ dso/tags/2.5.1/code/base/common/src/com/tc/config/schema/builder/SpringApplicationContextConfigBuilder.java
@@ -0,0 +1,13 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.config.schema.builder;
+
+
+public interface SpringApplicationContextConfigBuilder {
+
+  public void setPaths(String[] paths);
+
+  public SpringBeanConfigBuilder addBean(String beanName);
+
+}
