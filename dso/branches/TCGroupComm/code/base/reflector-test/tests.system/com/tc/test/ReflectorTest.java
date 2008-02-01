89795f60cb66314fd4b2f91a8e1f42fa4e5c68e5
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-02-01 21:48:19 +0000

    Merged revisions 6879-6899 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk
    ........
      r6879 | hhuynh | 2008-01-31 11:15:42 -0800 (Thu, 31 Jan 2008) | 1 line
      
      remove jdk restriction, monkey script will handle it
    ........
      r6880 | hhuynh | 2008-01-31 11:24:45 -0800 (Thu, 31 Jan 2008) | 1 line
      
      fixed import
    ........
      r6881 | hhuynh | 2008-01-31 11:59:26 -0800 (Thu, 31 Jan 2008) | 1 line
      
      separate module for reflector test
    ........
      r6883 | hhuynh | 2008-01-31 12:02:34 -0800 (Thu, 31 Jan 2008) | 1 line
      
      add reflector-test module
    ........
      r6884 | hhuynh | 2008-01-31 12:05:28 -0800 (Thu, 31 Jan 2008) | 1 line
      
      remove bin
    ........
      r6886 | gkeim | 2008-01-31 13:13:23 -0800 (Thu, 31 Jan 2008) | 2 lines
      
      Merge of 6885 (fix for DEV-1251) from the 2.5 branch.
    ........
      r6889 | gkeim | 2008-01-31 15:16:52 -0800 (Thu, 31 Jan 2008) | 1 line
      
      Merge of 6888 (fix for DEV-1256) from 2.5 branch.
    ........
      r6893 | hhuynh | 2008-01-31 20:37:08 -0800 (Thu, 31 Jan 2008) | 1 line
      
      fixed dependecies for reflector-test module
    ........
      r6894 | hhuynh | 2008-01-31 20:38:03 -0800 (Thu, 31 Jan 2008) | 1 line
      
      missing
    ........
      r6895 | hhuynh | 2008-01-31 20:39:03 -0800 (Thu, 31 Jan 2008) | 1 line
      
      fixed typo
    ........

--- dso/branches/TCGroupComm/code/base/reflector-test/tests.system/com/tc/test/ReflectorTest.java
+++ dso/branches/TCGroupComm/code/base/reflector-test/tests.system/com/tc/test/ReflectorTest.java
@@ -8,7 +8,6 @@ import com.meterware.httpunit.HeadMethodWebRequest;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
-import com.tc.util.runtime.Vm;
 
 import java.net.URL;
 import java.text.SimpleDateFormat;
@@ -47,10 +46,6 @@ public class ReflectorTest extends TestCase {
   }
 
   public void testBrokenLinks() throws Exception {
-    // only run this test with jdk1.4 to reduce the amount
-    // of unnecessary runs
-    if (!Vm.isJDK14()) return;
-
     for (Iterator it = reflector.entrySet().iterator(); it.hasNext();) {
       Map.Entry e = (Map.Entry) it.next();
       if (excludedLinks.contains(e.getKey())) continue;
