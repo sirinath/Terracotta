046518d90af4f0351365063224c38548c94957bf
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-02-01 15:40:12 +0000

    Added SigarUtil class that provides a sigarInit static method to ensure that the Sigar native libraries are in java.library.path.

---
+++ sigarstats/trunk/src/main/java/com/tc/statistics/retrieval/SigarUtil.java
@@ -0,0 +1,143 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.statistics.retrieval;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class SigarUtil {
+  private static final String PATH_SEPARATOR = System.getProperty("path.separator");
+  private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+
+  /**
+   * Attempts to ensure that Sigar native libraries are in java.library.path.
+   */
+  public static void sigarInit() {
+    // If it's already there, nothing to do (this would be the case with tests run from tcbuild)
+    if (isSigarInJavaLibraryPath())
+      return;
+
+    // Next try the kit installation directory
+    String installRoot = System.getProperty("tc.install-root");
+    if (installRoot != null && isSigarInDirectory(new File(installRoot)))
+      return;
+
+    // Otherwise, add the appropriate Maven repository directories to java.library.path
+    addMavenRepositoryToJavaLibraryPath();
+  }
+
+  private static boolean isSigarInJavaLibraryPath() {
+    String[] parts = getJavaLibraryPath().split(PATH_SEPARATOR);
+    for (int i = 0; i < parts.length; ++i) {
+      File part = new File(parts[i]);
+      if (isSigarInDirectory(part))
+        return true;
+    }
+    return false;
+  }
+
+  private static class SigarFilenameFilter implements FilenameFilter {
+    public boolean accept(File dir, String name) {
+      return name.toLowerCase().indexOf("sigar") > -1;
+    }
+  }
+  private static final FilenameFilter sigarFilenameFilter = new SigarFilenameFilter();
+  private static boolean isSigarInDirectory(File dir) {
+    return dir.isDirectory() && dir.list(sigarFilenameFilter).length > 0;
+  }
+
+  private static String getJavaLibraryPath() {
+    return System.getProperty("java.library.path");
+  }
+
+  private static void addMavenRepositoryToJavaLibraryPath() {
+    String baseLibraryName = baseLibraryName();
+    if (baseLibraryName != null) {
+      File m2Repository = new File(System.getProperty("user.home") +
+                                   FILE_SEPARATOR + ".m2" +
+                                   FILE_SEPARATOR + "repository");
+      StringBuffer buf = new StringBuffer(getJavaLibraryPath());
+      buf.append(PATH_SEPARATOR).append(m2Repository);
+      buf.append(FILE_SEPARATOR).append("org");
+      buf.append(FILE_SEPARATOR).append("hyperic");
+      buf.append(FILE_SEPARATOR).append(baseLibraryName);
+
+      // FIXME: Do not hardcode version.  Instead, figure out how to retrieve it from the POM.
+      buf.append(FILE_SEPARATOR).append("1.5.0.0");
+
+      System.setProperty("java.library.path", buf.toString());
+    }
+  }
+
+  // Adapted from Sigar's ArchName.java
+  private static String baseLibraryName() {
+      String name    = System.getProperty("os.name");
+      String arch    = System.getProperty("os.arch");
+      String version = System.getProperty("os.version");
+      String majorVersion = version.substring(0, 1); //4.x, 5.x, etc.
+
+      StringBuffer buf = new StringBuffer();
+
+      if (arch.endsWith("86")) {
+          arch = "x86";
+      }
+
+      if (name.equals("Linux")) {
+          buf.append(arch).append("-linux");
+      }
+      else if (name.indexOf("Windows") > -1) {
+          buf.append(arch).append("-winnt");
+      }
+      else if (name.equals("SunOS")) {
+          if (arch.startsWith("sparcv") && "64".equals(System.getProperty("sun.arch.data.model"))) {
+              arch = "sparc64";
+          }
+          buf.append(arch).append("-solaris");
+      }
+      else if (name.equals("HP-UX")) {
+          if (arch.startsWith("IA64")) {
+              arch = "ia64";
+          }
+          else {
+              arch = "pa";
+          }
+          if (version.indexOf("11") > -1) {
+              buf.append(arch).append("-hpux-11");
+          }
+      }
+      else if (name.equals("AIX")) {
+          buf.append("ppc-aix-").append(majorVersion);
+      }
+      else if (name.equals("Mac OS X")) {
+          buf.append("universal-macosx");
+      }
+      else if (name.equals("FreeBSD")) {
+          //none of the 4,5,6 major versions are binary compatible
+          buf.append(arch).append("-freebsd-").append(majorVersion);
+      }
+      else if (name.equals("OpenBSD")) {
+          buf.append(arch).append("-openbsd-").append(majorVersion);
+      }
+      else if (name.equals("NetBSD")) {
+          buf.append(arch).append("-netbsd-").append(majorVersion);
+      }
+      else if (name.equals("OSF1")) {
+          buf.append("alpha-osf1-").append(majorVersion);
+      }
+      else if (name.equals("NetWare")) {
+          buf.append("x86-netware-").append(majorVersion);
+      }
+
+      if (buf.length() == 0) {
+        return null;
+      }
+      else {
+        String prefix = "libsigar-";
+        if (name.startsWith("Windows")) {
+          prefix = "sigar-";
+        }
+        return prefix + buf.toString();
+      }
+  }
+}
