6b3e0cb01dd69a54a2f5e7894c755042d58c56c9
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-02-07 02:44:46 +0000

    HealthChecker : incorporated round2 review comments.

---
+++ dso/branches/HealthChecker/code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerContext.java
@@ -0,0 +1,26 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.net.protocol.transport;
+
+/**
+ * A Context per Transport takes care of sending and receiving the health checking probe signals to peer nodes. Also the
+ * extra checks like socker connect to detect Long GC.
+ * 
+ * @author Manoj
+ */
+public interface ConnectionHealthCheckerContext {
+
+  /* Transport is lively */
+  public void refresh();
+
+  /* Probe Message send and receive */
+  public boolean sendProbe();
+
+  public boolean receiveProbe(HealthCheckerProbeMessage message);
+
+  /* Do Extra Health Checks other than above Probes */
+  public boolean doSocketConnect();
+
+}
---
+++ dso/branches/HealthChecker/code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerContextEchoImpl.java
@@ -0,0 +1,47 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.net.protocol.transport;
+
+/**
+ * ECHO HealthChecker Context. On receiving a PING probe, it sends back the PING_REPLY.
+ * 
+ * @author Manoj
+ */
+public class ConnectionHealthCheckerContextEchoImpl implements ConnectionHealthCheckerContext {
+  private final MessageTransportBase             transport;
+  private final HealthCheckerProbeMessageFactory messageFactory;
+
+  public ConnectionHealthCheckerContextEchoImpl(MessageTransportBase mtb) {
+    this.transport = mtb;
+    this.messageFactory = new TransportMessageFactoryImpl();
+  }
+
+  public boolean doSocketConnect() {
+    throw new AssertionError("Echo HealthChecker");
+  }
+
+  public int getExtraCheckSuccessCount() {
+    throw new AssertionError("Echo HealthChecker");
+  }
+
+  public boolean receiveProbe(HealthCheckerProbeMessage message) {
+    if (message.isPing()) {
+      HealthCheckerProbeMessage pingReplyMessage = this.messageFactory.createPingReply(transport.getConnectionId(),
+                                                                                       transport.getConnection());
+      this.transport.send(pingReplyMessage);
+      return true;
+    }
+    throw new AssertionError("Echo HealthChecker");
+  }
+
+  public boolean sendProbe() {
+    throw new AssertionError("Echo HealthChecker");
+  }
+
+  public void refresh() {
+    throw new AssertionError("Echo HealthChecker");
+  }
+
+}
