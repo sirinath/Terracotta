d3ddd5fc4c7a6856fef1aac24973707d13fef188
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-02-09 00:13:07 +0000

    HealthChecker: minor fixes

--- dso/branches/HealthChecker/code/base/dso-common/src/com/tc/net/protocol/transport/HealthCheckerSocketConnect.java
+++ dso/branches/HealthChecker/code/base/dso-common/src/com/tc/net/protocol/transport/HealthCheckerSocketConnect.java
@@ -4,8 +4,6 @@
  */
 package com.tc.net.protocol.transport;
 
-import EDU.oswego.cs.dl.util.concurrent.SynchronizedShort;
-
 import com.tc.net.TCSocketAddress;
 import com.tc.net.core.TCConnection;
 import com.tc.net.core.TCConnectionManager;
@@ -14,6 +12,7 @@ import com.tc.net.core.event.TCConnectionEvent;
 import com.tc.net.core.event.TCConnectionEventListener;
 import com.tc.net.protocol.NullProtocolAdaptor;
 import com.tc.util.Assert;
+import com.tc.util.State;
 
 import java.io.IOException;
 
@@ -28,17 +27,17 @@ public class HealthCheckerSocketConnect implements TCConnectionEventListener {
 
   private final TCSocketAddress     peerNodeAddr;
   private final TCConnectionManager connectionManager;
-  private final SynchronizedShort   state                        = new SynchronizedShort((short) 0);
   private TCConnection              conn                         = null;
+  private State                     currentState;
 
-  private static final short        STATE_ASYNC_CONNECT_PREINIT  = 0x01;
-  private static final short        STATE_ASYNC_CONNECT_INITED   = 0x02;
-  private static final short        STATE_ASYNC_CONNECT_FAILED   = 0x03;
-  private static final short        STATE_ASYNC_CONNECT_PASSED   = 0x04;
+  private static final State        ASYNC_CONNECT_PREINIT        = new State("ASYNC_CONNECT_PREINIT");
+  private static final State        ASYNC_CONNECT_INITED         = new State("ASYNC_CONNECT_INITED");
+  private static final State        ASYNC_CONNECT_FAILED         = new State("ASYNC_CONNECT_FAILED");
+  private static final State        ASYNC_CONNECT_PASSED         = new State("ASYNC_CONNECT_PASSED");
 
-  public static final short         SOCKET_CONNECT_PASS          = 0x11;
-  public static final short         SOCKET_CONNECT_FAIL          = 0x12;
-  public static final short         SOCKET_CONNECT_RETRY         = 0x13;
+  public static final State         SOCKET_CONNECT_PASS          = new State("SOCKET_CONNECT_PASS");
+  public static final State         SOCKET_CONNECT_FAIL          = new State("SOCKET_CONNECT_FAIL");
+  public static final State         SOCKET_CONNECT_RETRY         = new State("SOCKET_CONNECT_RETRY");
 
   public static final int           MAX_ASYNC_CONNECT_WAIT_CYCLE = 2;
   private short                     asyncConnectWaitCycleCount   = 0;
@@ -50,22 +49,19 @@ public class HealthCheckerSocketConnect implements TCConnectionEventListener {
     this.asyncConnectWaitCycleCount = 0;
   }
 
-  private void changeState(short changeState) {
-    this.state.set(changeState);
+  /* the callers of this method are synchronized */
+  private void changeState(State newState) {
+    currentState = newState;
   }
 
-  private boolean isState(short checkState) {
-    return (this.state.compareTo(checkState) == 0);
-  }
-
-  public short detect() {
+  public synchronized State detect() {
 
     /*
      * Heuristic 1 : Try connecting to some listener port on the peer machine. Even for a LongGC App, new connection
      * should go thru. Note: we are doing an async connect
      */
 
-    if (isState(STATE_ASYNC_CONNECT_PREINIT)) {
+    if (currentState.equals(ASYNC_CONNECT_PREINIT)) {
       this.conn = connectionManager.createConnection(new NullProtocolAdaptor());
       conn.addListener(this);
 
@@ -81,20 +77,20 @@ public class HealthCheckerSocketConnect implements TCConnectionEventListener {
         reset();
         return SOCKET_CONNECT_FAIL;
       }
-      changeState(STATE_ASYNC_CONNECT_INITED);
+      changeState(ASYNC_CONNECT_INITED);
     } else {
       // prev async connect is still in progress
-      if (isState(STATE_ASYNC_CONNECT_FAILED)) {
+      if (currentState.equals(ASYNC_CONNECT_FAILED)) {
         reset();
         return SOCKET_CONNECT_FAIL;
-      } else if (isState(STATE_ASYNC_CONNECT_PASSED)) {
+      } else if (currentState.equals(ASYNC_CONNECT_PASSED)) {
         reset();
         return SOCKET_CONNECT_PASS;
       }
     }
-    
+
     // asyncConnect didnt return us any answer... increment the cycle and return true
-    Assert.eval(isState(STATE_ASYNC_CONNECT_INITED));
+    Assert.eval(currentState.equals(ASYNC_CONNECT_INITED));
     asyncConnectWaitCycleCount++;
     if (asyncConnectWaitCycleCount >= MAX_ASYNC_CONNECT_WAIT_CYCLE) {
       reset();
@@ -105,28 +101,28 @@ public class HealthCheckerSocketConnect implements TCConnectionEventListener {
 
   private void reset() {
     asyncConnectWaitCycleCount = 0;
-    changeState(STATE_ASYNC_CONNECT_PREINIT);
+    changeState(ASYNC_CONNECT_PREINIT);
   }
 
-  public void closeEvent(TCConnectionEvent event) {
+  public synchronized void closeEvent(TCConnectionEvent event) {
     //
   }
 
-  public void connectEvent(TCConnectionEvent event) {
-    changeState(STATE_ASYNC_CONNECT_PASSED);
+  public synchronized void connectEvent(TCConnectionEvent event) {
+    changeState(ASYNC_CONNECT_PASSED);
     closeConn();
   }
 
-  public void endOfFileEvent(TCConnectionEvent event) {
-    changeState(STATE_ASYNC_CONNECT_FAILED);
+  public synchronized void endOfFileEvent(TCConnectionEvent event) {
+    changeState(ASYNC_CONNECT_FAILED);
   }
 
-  public void errorEvent(TCConnectionErrorEvent errorEvent) {
-    changeState(STATE_ASYNC_CONNECT_FAILED);
+  public synchronized void errorEvent(TCConnectionErrorEvent errorEvent) {
+    changeState(ASYNC_CONNECT_FAILED);
   }
 
   private void closeConn() {
-    conn.close(500);
+    conn.asynchClose();
     conn.removeListener(this);
   }
 }
