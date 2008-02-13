80d7ea4648a5eb2970a1c111357d82cea8d2a8ba
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-02-13 23:38:38 +0000

    branch for active passive testing

---
+++ dso/branches/active-passive-test/code/base/dso-l2/src/com/tc/l2/api/ReplicatedClusterStateManager.java
@@ -0,0 +1,22 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.l2.api;
+
+import com.tc.net.groups.GroupException;
+import com.tc.net.groups.NodeID;
+
+public interface ReplicatedClusterStateManager {
+
+  public void publishNextAvailableObjectID(long l);
+
+  public void publishNextAvailableGlobalTransactionID(long l);
+
+  public void goActiveAndSyncState();
+
+  public void publishClusterState(NodeID nodeID) throws GroupException;
+
+  public void fireNodeLeftEvent(NodeID nodeID);
+
+}
