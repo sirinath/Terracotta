cf95b6becf34b8eae50bd4586c7c47370d135509
amiller <amiller@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-02-29 20:48:18 +0000

    Create branch for work on string compression and interned strings

---
+++ dso/branches/strings/code/base/dso-common/src/com/tc/net/protocol/transport/MessageTransportBase.java
@@ -0,0 +1,356 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.net.protocol.transport;
+
+import EDU.oswego.cs.dl.util.concurrent.SynchronizedBoolean;
+import EDU.oswego.cs.dl.util.concurrent.SynchronizedRef;
+
+import com.tc.bytes.TCByteBuffer;
+import com.tc.logging.ConnectionIDProvider;
+import com.tc.logging.TCLogger;
+import com.tc.net.MaxConnectionsExceededException;
+import com.tc.net.TCSocketAddress;
+import com.tc.net.core.TCConnection;
+import com.tc.net.core.event.TCConnectionErrorEvent;
+import com.tc.net.core.event.TCConnectionEvent;
+import com.tc.net.core.event.TCConnectionEventListener;
+import com.tc.net.protocol.IllegalReconnectException;
+import com.tc.net.protocol.NetworkLayer;
+import com.tc.net.protocol.NetworkStackID;
+import com.tc.net.protocol.TCNetworkMessage;
+import com.tc.util.Assert;
+import com.tc.util.TCTimeoutException;
+
+import java.io.IOException;
+
+/**
+ * Implementation of MessaageTransport
+ */
+abstract class MessageTransportBase extends AbstractMessageTransport implements NetworkLayer,
+    TCConnectionEventListener, ConnectionIDProvider {
+  private TCConnection                             connection;
+
+  protected ConnectionID                           connectionId           = ConnectionID.NULL_ID;
+  protected final MessageTransportStatus           status;
+  protected final SynchronizedBoolean              isOpen;
+  protected final TransportHandshakeMessageFactory messageFactory;
+  private final TransportHandshakeErrorHandler     handshakeErrorHandler;
+  private NetworkLayer                             receiveLayer;
+
+  private final Object                             attachingNewConnection = new Object();
+  private final SynchronizedRef                    connectionCloseEvent   = new SynchronizedRef(null);
+  private byte[]                                   sourceAddress;
+  private int                                      sourcePort;
+  private byte[]                                   destinationAddress;
+  private int                                      destinationPort;
+  private boolean                                  allowConnectionReplace = false;
+  private ConnectionHealthCheckerContext           healthCheckerContext   = null;
+
+  protected MessageTransportBase(MessageTransportState initialState,
+                                 TransportHandshakeErrorHandler handshakeErrorHandler,
+                                 TransportHandshakeMessageFactory messageFactory, boolean isOpen, TCLogger logger) {
+
+    super(logger);
+    this.handshakeErrorHandler = handshakeErrorHandler;
+    this.messageFactory = messageFactory;
+    this.isOpen = new SynchronizedBoolean(isOpen);
+    this.status = new MessageTransportStatus(initialState, logger);
+  }
+
+  public void setAllowConnectionReplace(boolean allow) {
+    this.allowConnectionReplace = allow;
+  }
+
+  public synchronized void setHealthCheckerContext(ConnectionHealthCheckerContext context) {
+    healthCheckerContext = context;
+  }
+
+  public synchronized ConnectionHealthCheckerContext getHealthCheckerContext() {
+    return healthCheckerContext;
+  }
+
+  public final ConnectionID getConnectionId() {
+    return this.connectionId;
+  }
+
+  public final void setReceiveLayer(NetworkLayer layer) {
+    this.receiveLayer = layer;
+  }
+
+  public final void setSendLayer(NetworkLayer layer) {
+    throw new UnsupportedOperationException("Transport layer has no send layer.");
+  }
+
+  public final void receiveTransportMessage(WireProtocolMessage message) {
+    synchronized (attachingNewConnection) {
+      if (message.getSource() == this.connection) {
+        receiveTransportMessageImpl(message);
+      } else {
+        logger.warn("Received message from an old connection: " + message);
+      }
+    }
+  }
+
+  public abstract NetworkStackID open() throws MaxConnectionsExceededException, TCTimeoutException, IOException;
+
+  protected abstract void receiveTransportMessageImpl(WireProtocolMessage message);
+
+  protected final void receiveToReceiveLayer(WireProtocolMessage message) {
+    Assert.assertNotNull(receiveLayer);
+    if (message.getMessageProtocol() == WireProtocolHeader.PROTOCOL_TRANSPORT_HANDSHAKE) {
+      //message is printed for debugging
+      logger.info(message.toString());
+      throw new AssertionError("Wrong handshake message from: " + message.getSource());
+    } else if (message.getMessageProtocol() == WireProtocolHeader.PROTOCOL_HEALTHCHECK_PROBES) {
+      Assert.assertNotNull(healthCheckerContext);
+      if (this.healthCheckerContext.receiveProbe((HealthCheckerProbeMessage) message)) {
+        // Proper health checker probe message. The context would have taken care of processing.
+        return;
+      }
+      throw new AssertionError("Wrong HealthChecker Probe message from: " + message.getSource());
+    }
+    this.receiveLayer.receive(message.getPayload());
+    message.getWireProtocolHeader().recycle();
+  }
+
+  public final void receive(TCByteBuffer[] msgData) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Moves the MessageTransport state to closed and closes the underlying connection, if any.
+   */
+  public void close() {
+    terminate(false);
+  }
+
+  public void disconnect() {
+    terminate(true);
+  }
+
+  private void terminate(boolean disconnect) {
+    synchronized (isOpen) {
+      if (!isOpen.get()) {
+        // see DEV-659: we used to throw an assertion error here if already closed
+        logger.warn("Can only close an open connection");
+        return;
+      }
+      if (disconnect) {
+        if (!this.status.isEnd()) this.status.disconnect();
+        // Dont fire any events here. Anyway asynchClose is triggered below and we are expected to receive a closeEvent
+        // and upon which we open up the OOO Reconnect window
+      } else {
+        if (!this.status.isEnd()) this.status.closed();
+        isOpen.set(false);
+        fireTransportClosedEvent();
+      }
+    }
+
+    synchronized (status) {
+      if (connection != null && !this.connection.isClosed()) {
+        this.connection.asynchClose();
+      }
+    }
+  }
+
+  public final void send(TCNetworkMessage message) {
+    // synchronized (isOpen) {
+    // Assert.eval("Can't send on an unopen transport [" +
+    // Thread.currentThread().getName() + "]", isOpen.get());
+    // }
+
+    synchronized (status) {
+      if (!status.isEstablished()) {
+        if (!message.isSealed()) message.seal();
+        logger.warn("Ignoring message sent to non-established transport: " + message);
+        return;
+      }
+
+      sendToConnection(message);
+    }
+  }
+
+  public final void sendToConnection(TCNetworkMessage message) {
+    if (message == null) throw new AssertionError("Attempt to send a null message.");
+    if (!(message instanceof WireProtocolMessage)) {
+      final TCNetworkMessage payload = message;
+
+      message = WireProtocolMessageImpl.wrapMessage(message, connection);
+      Assert.eval(message.getSentCallback() == null);
+
+      final Runnable callback = payload.getSentCallback();
+      if (callback != null) {
+        message.setSentCallback(new Runnable() {
+          public void run() {
+            callback.run();
+          }
+        });
+      }
+    }
+
+    WireProtocolHeader hdr = (WireProtocolHeader) message.getHeader();
+
+    hdr.setSourceAddress(getSourceAddress());
+    hdr.setSourcePort(getSourcePort());
+    hdr.setDestinationAddress(getDestinationAddress());
+    hdr.setDestinationPort(getDestinationPort());
+    hdr.computeChecksum();
+
+    connection.putMessage(message);
+  }
+
+  /**
+   * Returns true if the underlying connection is open.
+   */
+  public final boolean isConnected() {
+    synchronized (status) {
+      return ((getConnection() != null) && getConnection().isConnected() && this.status.isEstablished());
+    }
+  }
+
+  public final void attachNewConnection(TCConnection newConnection) throws IllegalReconnectException {
+    synchronized (attachingNewConnection) {
+      if ((this.connection != null) && !allowConnectionReplace) { throw new IllegalReconnectException(); }
+
+      getConnectionAttacher().attachNewConnection((TCConnectionEvent) this.connectionCloseEvent.get(), this.connection,
+                                                  newConnection);
+    }
+  }
+
+  protected ConnectionAttacher getConnectionAttacher() {
+    return new DefaultConnectionAttacher(this);
+  }
+
+  protected interface ConnectionAttacher {
+    public void attachNewConnection(TCConnectionEvent closeEvent, TCConnection oldConnection, TCConnection newConnection);
+  }
+
+  private static final class DefaultConnectionAttacher implements ConnectionAttacher {
+
+    private final MessageTransportBase transport;
+
+    private DefaultConnectionAttacher(MessageTransportBase transport) {
+      this.transport = transport;
+    }
+
+    public void attachNewConnection(TCConnectionEvent closeEvent, TCConnection oldConnection, TCConnection newConnection) {
+      Assert.assertNotNull(oldConnection);
+      if (closeEvent == null || closeEvent.getSource() != oldConnection) {
+        // We either didn't receive a close event or we received a close event
+        // from a connection that isn't our current connection.
+        this.transport.fireTransportDisconnectedEvent();
+      }
+      // remove the transport as a listener for the old connection
+      if (oldConnection != null && oldConnection != transport.connection) {
+        oldConnection.removeListener(transport);
+      }
+      // set the new connection to the current connection.
+      transport.wireNewConnection(newConnection);
+    }
+  }
+
+  /*********************************************************************************************************************
+   * TCConnection listener interface
+   */
+
+  public void connectEvent(TCConnectionEvent event) {
+    return;
+  }
+
+  public void closeEvent(TCConnectionEvent event) {
+    boolean isSameConnection = false;
+
+    synchronized (attachingNewConnection) {
+      TCConnection src = event.getSource();
+      isSameConnection = (src == this.connection);
+      if (isSameConnection) {
+        this.connectionCloseEvent.set(event);
+      }
+    }
+
+    if (isSameConnection) {
+      fireTransportDisconnectedEvent();
+    }
+  }
+
+  public void errorEvent(TCConnectionErrorEvent errorEvent) {
+    return;
+  }
+
+  public void endOfFileEvent(TCConnectionEvent event) {
+    return;
+  }
+
+  protected void handleHandshakeError(TransportHandshakeErrorContext e) {
+    this.handshakeErrorHandler.handleHandshakeError(e);
+  }
+
+  protected void handleHandshakeError(TransportHandshakeErrorContext e, TransportHandshakeMessage m) {
+    this.handshakeErrorHandler.handleHandshakeError(e, m);
+  }
+
+  protected TCConnection getConnection() {
+    return connection;
+  }
+
+  public TCSocketAddress getRemoteAddress() {
+    return this.connection.getRemoteAddress();
+  }
+
+  public TCSocketAddress getLocalAddress() {
+    return this.connection.getLocalAddress();
+  }
+
+  protected void setConnection(TCConnection conn) {
+    TCConnection old = this.connection;
+    this.connection = conn;
+    clearAddressCache();
+    this.connection.addListener(this);
+    if (old != null) {
+      old.removeListener(this);
+    }
+  }
+
+  protected void clearConnection() {
+    getConnection().close(10000);
+    this.connectionId = ConnectionID.NULL_ID;
+    this.connection.removeListener(this);
+    clearAddressCache();
+    this.connection = null;
+  }
+
+  private void clearAddressCache() {
+    this.sourceAddress = null;
+    this.sourcePort = -1;
+    this.destinationAddress = null;
+    this.destinationPort = -1;
+  }
+
+  private byte[] getSourceAddress() {
+    if (sourceAddress == null) { return sourceAddress = connection.getLocalAddress().getAddressBytes(); }
+    return sourceAddress;
+  }
+
+  private byte[] getDestinationAddress() {
+    if (destinationAddress == null) { return destinationAddress = connection.getRemoteAddress().getAddressBytes(); }
+    return destinationAddress;
+  }
+
+  private int getSourcePort() {
+    if (sourcePort == -1) { return this.sourcePort = connection.getLocalAddress().getPort(); }
+    return sourcePort;
+  }
+
+  private int getDestinationPort() {
+    if (destinationPort == -1) { return this.destinationPort = connection.getRemoteAddress().getPort(); }
+    return sourcePort;
+  }
+
+  protected void wireNewConnection(TCConnection conn) {
+    logger.info("Attaching new connection: " + conn);
+    setConnection(conn);
+    this.status.reset();
+  }
+}
---
+++ dso/branches/strings/code/base/dso-common/src/com/tc/object/msg/CommitTransactionMessageImpl.java
@@ -0,0 +1,100 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object.msg;
+
+import com.tc.async.api.EventContext;
+import com.tc.bytes.TCByteBuffer;
+import com.tc.io.TCByteBufferOutputStream;
+import com.tc.lang.Recyclable;
+import com.tc.net.protocol.tcm.MessageChannel;
+import com.tc.net.protocol.tcm.MessageMonitor;
+import com.tc.net.protocol.tcm.TCMessageHeader;
+import com.tc.net.protocol.tcm.TCMessageType;
+import com.tc.object.dna.impl.ObjectStringSerializer;
+import com.tc.object.session.SessionID;
+import com.tc.object.tx.TransactionBatch;
+
+import java.io.IOException;
+
+/**
+ * @author steve
+ */
+public class CommitTransactionMessageImpl extends DSOMessageBase implements EventContext, CommitTransactionMessage {
+  private static final byte      BATCH_TRANSACTION_DATA_ID = 1;
+  private static final byte      SERIALIZER_ID             = 2;
+  private ObjectStringSerializer serializer;
+
+  private Recyclable             batch;                        // This is used to recycle buffers on
+  // the write side
+  private TCByteBuffer[]         batchData;
+
+  public CommitTransactionMessageImpl(SessionID sessionID, MessageMonitor monitor, TCByteBufferOutputStream out,
+                                      MessageChannel channel, TCMessageType type) {
+    super(sessionID, monitor, out, channel, type);
+  }
+
+  public CommitTransactionMessageImpl(SessionID sessionID, MessageMonitor monitor, MessageChannel channel,
+                                      TCMessageHeader header, TCByteBuffer[] data) {
+    super(sessionID, monitor, channel, header, data);
+  }
+
+  public ObjectStringSerializer getSerializer() {
+    return serializer;
+  }
+
+  protected void dehydrateValues() {
+    putNVPair(SERIALIZER_ID, serializer);
+    putNVPair(BATCH_TRANSACTION_DATA_ID, batchData);
+    batchData = null;
+  }
+
+  protected boolean hydrateValue(byte name) throws IOException {
+    switch (name) {
+      case BATCH_TRANSACTION_DATA_ID: {
+        this.batchData = getInputStream().toArray();
+        return true;
+      }
+      case SERIALIZER_ID:
+        this.serializer = (ObjectStringSerializer) getObject(new ObjectStringSerializer());
+        return true;
+
+      default: {
+        return false;
+      }
+    }
+  }
+
+  public void setBatch(TransactionBatch batch, ObjectStringSerializer serializer) {
+    this.batch = batch;
+    setBatchData(batch.getData(), serializer);
+  }
+
+  // This is here for a test
+  synchronized void setBatchData(TCByteBuffer[] batchData, ObjectStringSerializer serializer) {
+    if (this.batchData != null) throw new AssertionError("Attempt to set TransactionBatch more than once.");
+    this.batchData = batchData;
+    this.serializer = serializer;
+  }
+
+  public synchronized TCByteBuffer[] getBatchData() {
+    return batchData;
+  }
+
+  public void doRecycleOnRead() {
+    // recycle will be called later
+  }
+
+  protected boolean isOutputStreamRecycled() {
+    return true;
+  }
+
+  public void doRecycleOnWrite() {
+    // recycle only those buffers created for this message
+    recycleOutputStream();
+    if (batch != null) {
+      batch.recycle();
+    }
+  }
+}
