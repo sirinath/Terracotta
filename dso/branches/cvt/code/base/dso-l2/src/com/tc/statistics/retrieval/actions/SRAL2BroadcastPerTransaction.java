d1cb6c63be2ea56f4a025708e5643d565675f61b
asingh <asingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-03-03 17:34:26 +0000

    Added statistics for L2 broadcast counts and changes.

--- dso/branches/cvt/code/base/dso-l2/src/com/tc/statistics/retrieval/actions/SRAL2BroadcastPerTransaction.java
+++ dso/branches/cvt/code/base/dso-l2/src/com/tc/statistics/retrieval/actions/SRAL2BroadcastPerTransaction.java
@@ -3,44 +3,49 @@
  */
 package com.tc.statistics.retrieval.actions;
 
-import com.tc.objectserver.core.api.DSOGlobalServerStats;
-import com.tc.statistics.StatisticData;
 import com.tc.statistics.StatisticRetrievalAction;
 import com.tc.statistics.StatisticType;
+import com.tc.statistics.StatisticData;
 import com.tc.stats.counter.sampled.SampledCounter;
 import com.tc.stats.counter.sampled.TimeStampedCounterValue;
+import com.tc.objectserver.core.api.DSOGlobalServerStats;
 import com.tc.util.Assert;
 
 import java.util.Date;
+import java.math.BigDecimal;
 
-public class SRAL2ToL1FaultRate implements StatisticRetrievalAction {
+public class SRAL2BroadcastPerTransaction implements StatisticRetrievalAction {
 
-  public final static String ACTION_NAME = "l2 l1 fault";
+  public static final String ACTION_NAME = "l2 broadcast per transaction";
 
-  private final SampledCounter counter;
-  
-  public SRAL2ToL1FaultRate(DSOGlobalServerStats serverStats) {
+  private final SampledCounter txnCounter;
+  private final SampledCounter broadcastCounter;
+
+  public SRAL2BroadcastPerTransaction(DSOGlobalServerStats serverStats) {
     Assert.assertNotNull("serverStats", serverStats);
-    counter = serverStats.getObjectFaultCounter();
-    Assert.assertNotNull("counter", counter);
+    this.txnCounter = serverStats.getTransactionCounter();
+    this.broadcastCounter = serverStats.getBroadcastCounter();
   }
 
   public String getName() {
     return ACTION_NAME;
   }
-  
+
   public StatisticType getType() {
     return StatisticType.SNAPSHOT;
   }
 
   public StatisticData[] retrieveStatisticData() {
-    TimeStampedCounterValue value = counter.getMostRecentSample();
+    TimeStampedCounterValue numTxn = txnCounter.getMostRecentSample();
+    TimeStampedCounterValue numBroadcasts = broadcastCounter.getMostRecentSample();
     // todo: this might have to be changed into new Date(value.getTimestamp()),
     // which is when the actual sampling occurred, we use the 'now' timestamp at
     // the moment to make sure that the statistic data retrieval arrives in order.
     // Otherwise, this data entry could be timed before the startup data event of
     // the capture session.
     Date moment = new Date();
-    return new StatisticData[] {new StatisticData(ACTION_NAME, moment, value.getCounterValue())};
+    BigDecimal value = numTxn.getCounterValue() != 0 ? new BigDecimal((double)numBroadcasts.getCounterValue() /
+                                                                      numTxn.getCounterValue()) : new BigDecimal(0);
+    return new StatisticData[] { new StatisticData(ACTION_NAME, moment, value) };
   }
 }
