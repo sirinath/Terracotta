453d633b42923ecdf91f818ba9b06afdda51fbf0
sbale <sbale@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-03-12 03:40:36 +0000

    fix converting between byte[] and char[] and vice versa, for String compression

--- dso/branches/stringopt/code/base/dso-l1/src/com/tc/object/ByteToChar.java
+++ dso/branches/stringopt/code/base/dso-l1/src/com/tc/object/ByteToChar.java
@@ -5,19 +5,36 @@ package com.tc.object;
 
 public class ByteToChar {
 
-  public char[] convert(byte[] bytes) {
+  public char[] toCharArray(byte[] bytes) {
     
     int remainder = bytes.length%2;
     char[] result = new char[bytes.length/2 + remainder];
     for (int i=0; i< bytes.length; i=i+2){
-      short aShort = bytes[i];
-      char aChar = (char) (aShort << 4);
+      int anInt = bytes[i];
+      anInt = anInt << 8; //shift first byte up 8 bits, making room for the second byte
       if (i+1 < bytes.length){
-        aChar += bytes[i+1];
+        int bitmask = 0x000000FF & bytes[i+1]; //zero out all bits in this bitmask to the left of the 8 bits coming from the byte
+        anInt = anInt | bitmask; //now paste those 8 bits into the low bits of "anInt" (whose low 8 bits should be zeroes due to the previous shift)
       }
-      result[i/2] = aChar;
+      //System.out.println("an Int " +anInt + ", hex " + Integer.toHexString(anInt)+ ", binary " + Integer.toBinaryString(anInt));
+      result[i/2] = (char) anInt; //cast to char (drop all but low 16 bits)
+      
     }
     return result;
+  }
+
+  public byte[] toByteArray(char[] chars, int originalLength) {
+    byte[] bytes = new byte[originalLength];
+    
+    int byteIndex = 0;
+    for(int i=0; i<chars.length; i++) {
+      int anInt = chars[i];
+      bytes[byteIndex++] = (byte)(anInt>>8);
+      if (byteIndex < bytes.length){
+        bytes[byteIndex++] = (byte)(anInt);
+      }
+    }
+    return bytes;
   }
 
 }
