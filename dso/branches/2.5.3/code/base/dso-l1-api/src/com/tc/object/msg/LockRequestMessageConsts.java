09771600b17f1cfde39649f4590e24e1853ffd46
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-03-14 20:25:13 +0000

    Created 2.5.3 branch from 2.5.2 tag.

---
+++ dso/branches/2.5.3/code/base/dso-l1-api/src/com/tc/object/msg/LockRequestMessageConsts.java
@@ -0,0 +1,16 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.object.msg;
+
+/**
+ * Constants for lock requests
+ */
+public interface LockRequestMessageConsts {
+
+  /**
+   * Uninitialized time to wait (value=-1)
+   */
+  public final static int UNITIALIZED_WAIT_TIME = -1;
+
+}
