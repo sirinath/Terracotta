475fab66f4432cd5480a70fdb950773e62798423
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-04-26 01:13:30 +0000

    Fixed L2InfoTest to use local host info and not rely on fully-qualified hostname if it can't be determined.

--- dso/branches/2.6/code/base/common/tests.unit/com/tc/config/schema/L2InfoTest.java
+++ dso/branches/2.6/code/base/common/tests.unit/com/tc/config/schema/L2InfoTest.java
@@ -5,52 +5,71 @@ package com.tc.config.schema;
 
 import com.tc.test.TCTestCase;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class L2InfoTest extends TCTestCase {
+  private String  canonicalHostName;
+  private String  hostAddress;
+  private boolean haveFQHostName;
+
+  public L2InfoTest() {
+    try {
+      canonicalHostName = InetAddress.getLocalHost().getCanonicalHostName();
+      hostAddress = InetAddress.getLocalHost().getHostAddress();
+      haveFQHostName = !canonicalHostName.equals(hostAddress);
+    } catch (UnknownHostException uhe) {
+      disableAllUntil("2010-01-01");
+    }
+  }
+
   public void testEquals() {
     L2Info inst1 = new L2Info("primary", "localhost", 9520);
     L2Info inst2 = new L2Info("primary", "localhost", 9520);
     assertEquals(inst1, inst2);
-    
+
     inst1 = new L2Info("primary", "localhost", 9520);
     inst2 = new L2Info("secondary", "localhost", 9520);
     assertNotEquals(inst1, inst2);
-    
+
     inst1 = new L2Info("primary", "localhost", 9520);
     inst2 = new L2Info("primary", "localhost", 9521);
     assertNotEquals(inst1, inst2);
-    
+
     inst1 = new L2Info("primary", "localhost", 9520);
     inst2 = new L2Info("primary", "127.0.0.1", 9520);
     assertNotEquals(inst1, inst2);
   }
-  
+
   public void testMatch() {
     L2Info inst1 = new L2Info("primary", "localhost", 9520);
     L2Info inst2 = new L2Info("primary", "127.0.0.1", 9520);
     assertTrue("hosts don't match", inst1.matches(inst2));
-    
+
     inst1 = new L2Info("primary", "localhost", 9520);
     inst2 = new L2Info("secondary", "localhost", 9520);
     assertFalse("names match", inst1.matches(inst2));
-    
+
     inst1 = new L2Info("primary", "localhost", 9520);
     inst2 = new L2Info("primary", "localhost", 9521);
     assertFalse("jmxPorts match", inst1.matches(inst2));
-    
+
     inst1 = new L2Info("primary", "localhost", 9520);
     inst2 = new L2Info("primary", "127.0.0.1", 9520);
     assertTrue("hosts don't match", inst1.matches(inst2));
-    
-    inst1 = new L2Info("primary", "localhost", 9520);
-    inst2 = new L2Info("primary", "rascal.terracotta.lan", 9520);
-    assertTrue("hosts don't match", inst1.matches(inst2));
 
-    inst1 = new L2Info("primary", "localhost", 9520);
-    inst2 = new L2Info("primary", "10.0.0.123", 9520);
-    assertTrue("hosts don't match", inst1.matches(inst2));
+    if (haveFQHostName) {
+      inst1 = new L2Info("primary", "localhost", 9520);
+      inst2 = new L2Info("primary", canonicalHostName, 9520);
+      assertTrue("hosts don't match", inst1.matches(inst2));
 
-    inst1 = new L2Info("primary", "127.0.0.1", 9520);
-    inst2 = new L2Info("primary", "10.0.0.123", 9520);
-    assertTrue("hosts don't match", inst1.matches(inst2));
+      inst1 = new L2Info("primary", "localhost", 9520);
+      inst2 = new L2Info("primary", hostAddress, 9520);
+      assertTrue("hosts don't match", inst1.matches(inst2));
+
+      inst1 = new L2Info("primary", "127.0.0.1", 9520);
+      inst2 = new L2Info("primary", hostAddress, 9520);
+      assertTrue("hosts don't match", inst1.matches(inst2));
+    }
   }
 }
