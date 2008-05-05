ad76448dde2facdb2b75fb25324b69ea523a1946
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-05-05 23:06:50 +0000

    tag release 2.6-practice from 2.6 at r8465

---
+++ dso/tags/2.6-practice/code/base/dso-system-tests/tests.system/com/tctest/ShadowVariableTest.java
@@ -0,0 +1,24 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tctest;
+
+
+public class ShadowVariableTest extends TransparentTestBase {
+
+  private static final int NODE_COUNT = 2;
+
+  public ShadowVariableTest() {
+    this.disableAllUntil("2008-08-01");
+  }
+  
+  public void setUp() throws Exception {
+    super.setUp();
+    getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
+    initializeTestRunner();
+  }
+
+  protected Class getApplicationClass() {
+    return ShadowVariableTestApp.class;
+  }
+}
