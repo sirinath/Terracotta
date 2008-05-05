b4a32ae163033574fca8a42eb1137176b95122a6
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-05-05 23:11:38 +0000

    tag release 2.6-practice2 from 2.6 at r8465

---
+++ dso/tags/2.6-practice2/code/base/dso-l1/src/com/tc/object/Portability.java
@@ -0,0 +1,19 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.object;
+
+import com.tc.util.NonPortableReason;
+
+public interface Portability {
+
+  public NonPortableReason getNonPortableReason(Class topLevelClass);
+
+  public boolean isPortableClass(Class clazz);
+
+  public boolean isClassPhysicallyInstrumented(Class clazz);
+
+  public boolean isInstrumentationNotNeeded(String name);
+
+  public boolean isPortableInstance(Object obj);
+}
