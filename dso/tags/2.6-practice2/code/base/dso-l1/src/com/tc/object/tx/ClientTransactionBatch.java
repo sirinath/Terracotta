192bda432bba239697c138c49686e28aa9b28db9
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-05-05 23:11:38 +0000

    tag release 2.6-practice2 from 2.6 at r8465

---
+++ dso/tags/2.6-practice2/code/base/dso-l1/src/com/tc/object/tx/ClientTransactionBatch.java
@@ -0,0 +1,48 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.object.tx;
+
+import com.tc.util.SequenceGenerator;
+import com.tc.util.SequenceID;
+
+import java.util.Collection;
+
+/**
+ * Client representation of a batch of transactions.  Has methods that are only useful in a client context.
+ */
+public interface ClientTransactionBatch extends TransactionBatch {
+
+  public TxnBatchID getTransactionBatchID();
+
+  /**
+   * Adds the collection of transaction ids in this batch to the given collection and returns it.
+   */
+  public Collection addTransactionIDsTo(Collection c);
+
+  /**
+   * Add the given transaction to this batch.
+   * @return true if the transaction was folded
+   */
+  public boolean addTransaction(ClientTransaction txn, SequenceGenerator sequenceGenerator);
+
+  public void removeTransaction(TransactionID txID);
+
+  /**
+   * Send the transaction to the server.
+   */
+  public void send();
+
+  public int numberOfTxnsBeforeFolding();
+
+  public int byteSize();
+
+  public boolean isNull();
+
+  public SequenceID getMinTransactionSequence();
+
+  public Collection addTransactionSequenceIDsTo(Collection sequenceIDs);
+
+  // For testing
+  public String dump();
+}
