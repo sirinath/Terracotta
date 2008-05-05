eb8a798a3218500d211414105c374f2ac7032c21
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-05-05 22:32:30 +0000

    tag release 2.6-practice from 2.6 at r8465

---
+++ dso/tags/2.6-practice/code/base/dso-tests-jdk15/tests.unit/com/tctest/ManagedObjectStateSerialization15Test.java
@@ -0,0 +1,46 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+import com.tc.objectserver.core.api.ManagedObjectState;
+import com.tc.objectserver.managedobject.ManagedObjectStateSerializationTestBase;
+
+public class ManagedObjectStateSerialization15Test extends ManagedObjectStateSerializationTestBase {
+
+  public void testEnum() throws Exception {
+    String className = "java.lang.Enum";
+    State state = State.RUN;
+    TestDNACursor cursor = new TestDNACursor();
+
+    cursor.addLiteralAction(state);
+    ManagedObjectState managedObjectState = applyValidation(className, cursor);
+    serializationValidation(managedObjectState, cursor, ManagedObjectState.LITERAL_TYPE);
+  }
+
+  public interface EnumIntf {
+    public int getStateNum();
+
+    public void setStateNum(int stateNum);
+  }
+
+  public enum State implements EnumIntf {
+    START(0), RUN(1), STOP(2);
+
+    private int stateNum;
+
+    State(int stateNum) {
+      this.stateNum = stateNum;
+    }
+
+    public int getStateNum() {
+      return this.stateNum;
+    }
+
+    public void setStateNum(int stateNum) {
+      this.stateNum = stateNum;
+    }
+  }
+
+}
