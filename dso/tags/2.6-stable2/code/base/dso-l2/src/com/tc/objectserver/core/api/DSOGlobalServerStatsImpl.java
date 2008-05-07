f93edf2c18fb9fa04f3a61e0bca4dae20db98c9e
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-05-07 06:02:30 +0000

    tag OS release 2.6-stable2 from 2.6 at r8254

---
+++ dso/tags/2.6-stable2/code/base/dso-l2/src/com/tc/objectserver/core/api/DSOGlobalServerStatsImpl.java
@@ -0,0 +1,54 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.objectserver.core.api;
+
+import com.tc.objectserver.api.ObjectManagerStats;
+import com.tc.objectserver.impl.ObjectManagerStatsImpl;
+import com.tc.stats.counter.sampled.SampledCounter;
+
+public class DSOGlobalServerStatsImpl implements DSOGlobalServerStats {
+
+  private final SampledCounter faultCounter;
+  private final SampledCounter flushCounter;
+  private final SampledCounter txnCounter;
+  private final ObjectManagerStatsImpl objMgrStats;
+
+  private final SampledCounter broadcastCounter;
+  private final SampledCounter changesCounter;
+
+  public DSOGlobalServerStatsImpl(SampledCounter flushCounter, SampledCounter faultCounter, SampledCounter txnCounter,
+                                  ObjectManagerStatsImpl objMgrStats, SampledCounter broadcastCounter,
+                                  SampledCounter changesCounter) {
+    this.flushCounter = flushCounter;
+    this.faultCounter = faultCounter;
+    this.txnCounter = txnCounter;
+    this.objMgrStats = objMgrStats;
+    this.broadcastCounter = broadcastCounter;
+    this.changesCounter = changesCounter;
+  }
+
+  public SampledCounter getObjectFlushCounter() {
+    return this.flushCounter;
+  }
+
+  public SampledCounter getObjectFaultCounter() {
+    return this.faultCounter;
+  }
+
+  public ObjectManagerStats getObjectManagerStats() {
+    return this.objMgrStats;
+  }
+
+  public SampledCounter getTransactionCounter() {
+    return this.txnCounter;
+  }
+
+  public SampledCounter getBroadcastCounter() {
+    return broadcastCounter;
+  }
+
+  public SampledCounter getChangesCounter() {
+    return changesCounter;
+  }
+}
---
+++ dso/tags/2.6-stable2/code/base/dso-spring-tests/tests.system/com/tctest/spring/bean/RedeploymentBean.java
@@ -0,0 +1,15 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tctest.spring.bean;
+
+
+public interface RedeploymentBean {
+
+  void setValue(int value);
+  
+  int getValue();
+  
+  boolean hasResource(String resource);
+  
+}
