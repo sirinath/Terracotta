05f3c92f936fbed5c5f4ab1b8023e7086f3921f6
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-05-20 00:31:39 +0000

    merge 8580 and 8584 from 2.6 branch (DEV-1605)

--- dso/branches/2.6.0/code/base/dso-common/src/com/tc/object/msg/ClientHandshakeMessage.java
+++ dso/branches/2.6.0/code/base/dso-common/src/com/tc/object/msg/ClientHandshakeMessage.java
@@ -6,8 +6,8 @@ package com.tc.object.msg;
 
 import com.tc.net.groups.ClientID;
 import com.tc.net.protocol.tcm.MessageChannel;
-import com.tc.object.ObjectID;
 import com.tc.object.lockmanager.api.LockContext;
+import com.tc.object.lockmanager.api.TryLockContext;
 import com.tc.object.lockmanager.api.WaitContext;
 
 import java.util.Collection;
@@ -16,8 +16,6 @@ import java.util.Set;
 public interface ClientHandshakeMessage {
 
   public Collection getTransactionSequenceIDs();
-
-  public void addObjectID(ObjectID object);
 
   public Set getObjectIDs();
 
@@ -31,7 +29,7 @@ public interface ClientHandshakeMessage {
 
   public void addPendingLockContext(LockContext ctxt);
 
-  public void addPendingTryLockContext(LockContext ctxt);
+  public void addPendingTryLockContext(TryLockContext ctxt);
 
   public void setClientVersion(String v);
 
@@ -45,9 +43,9 @@ public interface ClientHandshakeMessage {
 
   public void send();
 
-  public void setTransactionSequenceIDs(Collection transactionSequenceIDs);
+  public void addTransactionSequenceIDs(Collection transactionSequenceIDs);
 
-  public void setResentTransactionIDs(Collection resentTransactionIDs);
+  public void addResentTransactionIDs(Collection resentTransactionIDs);
 
   public Collection getResentTransactionIDs();
 
