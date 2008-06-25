dfcf022ba5f57d256b360112c57a1b0eccd59046
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-06-25 23:40:55 +0000

    dist command will now create a build-data.txt under the lib/resources directory of the resulting kit.

--- dso/branches/patching/code/base/buildscripts/modules_compile.rb
+++ dso/branches/patching/code/base/buildscripts/modules_compile.rb
@@ -8,6 +8,8 @@
 # ability to compile themselves.
 
 class BuildSubtree
+  include BuildData
+  
   # Compiles the given subtree. jvm_set is the set of JVMs available; the
   # subtree will look for one named 'compile-<version>', where <version> is
   # the value passed as :compiler_version in the options hash of the BuildSubtree
@@ -114,13 +116,6 @@ class BuildSubtree
     end
   end
 
-  protected
-
-  # Where should we put our build-data file?
-  def build_data_file(build_results)
-    FilePath.new(build_results.classes_directory(self), "build-data.txt")
-  end
-
   private
 
   def prepare_emma_dir(build_results)      
@@ -136,27 +131,6 @@ class BuildSubtree
       Registry[:jvm_set][name]
     else
       build_module.jdk
-    end
-  end
-
-  # Creates a 'build data' file at the given location, putting into it a number
-  # of properties that specify when, where, and how the code in it was compiled.
-  def create_build_data(config_source, build_results, build_environment)
-    File.open(build_data_file(build_results).to_s, "w") do |file|
-      file.puts("terracotta.build.productname=terracotta")
-      file.puts("terracotta.build.version=#{build_environment.version}")
-      file.puts("terracotta.build.maven.artifacts.version=#{build_environment.maven_version}")
-      file.puts("terracotta.build.host=#{build_environment.build_hostname}")
-      file.puts("terracotta.build.user=#{build_environment.build_username}")
-      file.puts("terracotta.build.timestamp=#{build_environment.build_timestamp.strftime('%Y%m%d-%H%m%S')}")
-      file.puts("terracotta.build.revision=#{build_environment.current_revision}")
-      file.puts("terracotta.build.branch=#{build_environment.current_branch}")
-      file.puts("terracotta.build.edition=#{build_environment.edition}")
-        
-      # extra info if built under EE branch
-      if build_environment.ee_svninfo
-        file.puts("terracotta.build.ee.revision=#{build_environment.ee_svninfo.current_revision}")
-      end
     end
   end
 end
