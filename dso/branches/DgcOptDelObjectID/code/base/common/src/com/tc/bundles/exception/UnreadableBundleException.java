a2199cb8d5d5692b437b4dbbded6bcdfd8afd71f
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-06-27 18:27:30 +0000

    DGC optimization for deleting objects by ID

---
+++ dso/branches/DgcOptDelObjectID/code/base/common/src/com/tc/bundles/exception/UnreadableBundleException.java
@@ -0,0 +1,43 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.bundles.exception;
+
+import org.osgi.framework.BundleException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UnreadableBundleException extends BundleException implements BundleExceptionSummary {
+
+  private File bundle;
+  
+  public UnreadableBundleException(final String msg) {
+    super(msg);
+  }
+
+  public UnreadableBundleException(final String msg, final Throwable cause) {
+    super(msg, cause);
+  }
+  
+  public UnreadableBundleException(final String msg, final File bundle) {
+    super(msg);
+    this.bundle = bundle;
+  }
+
+  private String expectedLocation() {
+    try {
+      return bundle.getParentFile().getCanonicalPath();
+    } catch (IOException e) {
+      return bundle.getParentFile().toString();
+    }
+  }
+
+  public String getSummary() {
+    String msg = getMessage();
+    msg += "\n\n" + INDENT + "TIM jar filename: " + bundle.getName();
+    msg += "\n\n" + INDENT + "Path to jar file: " + expectedLocation();
+    return msg;
+  }
+  
+}
