0c14a3e8a03af5d41688953d2742c3334b8fe41e
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-08 20:45:34 +0000

    Added String[] args to Command#execute method signature.

--- dso/branches/tim-update-center/code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/ListCommandTest.java
+++ dso/branches/tim-update-center/code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/ListCommandTest.java
@@ -21,7 +21,7 @@ public class ListCommandTest extends AbstractCommandTestCase {
    * Test method for {@link org.terracotta.modules.tool.commands.ListCommand#execute()}.
    */
   public final void testExecute() {
-    command.execute();
+    command.execute(new String[] {});
     assertOutMatches("TIM packages for Terracotta " + ProductInfo.getInstance().version());
     assertEquals(0, commandErr.toString().length());
   }
