040cccb30994412fb2ac998eddbe0594a4ac78f3
ssubbiah <ssubbiah@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-09 01:06:39 +0000

    Merged revisions 9001-9100 via svnmerge from https://svn.terracotta.org/repo/tc/dso/trunk

--- dso/branches/dgc/kits/skeletons/shared-base/common/bin/start-tc-server.sh
+++ dso/branches/dgc/kits/skeletons/shared-base/common/bin/start-tc-server.sh
@@ -30,13 +30,6 @@ if $cygwin; then
   [ -n "$TC_INSTALL_DIR" ] && TC_INSTALL_DIR=`cygpath --windows "$TC_INSTALL_DIR"`
 fi
 
-trap 'signalHandler' SIGHUP SIGINT SIGABRT SIGTERM
-
-function signalHandler()
-{
-	kill "-s" KILL $PID;
-}
-
 start=true
 while "$start"
 do
@@ -45,14 +38,12 @@ do
    -Dtc.install-root="${TC_INSTALL_DIR}" \
    ${JAVA_OPTS} \
    -cp "${TC_INSTALL_DIR}/lib/tc.jar" \
-   com.tc.server.TCServerMain "$@" &
- PID=$!
- wait $PID
+   com.tc.server.TCServerMain "$@"
  exitValue=$?
  start=false;
  if [ "$exitValue" == 11 ]
  then
    start=true;
-   echo "Restarting the Server ...";
+   echo "start-tc-server: Restarting the server..."
  fi
 done
