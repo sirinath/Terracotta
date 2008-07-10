b2b317c81968e0ffb7aca710f97980e727d9cfa2
jgalang <jgalang@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-10 20:57:54 +0000

    new commands

---
+++ dso/branches/tim-update-center/code/base/tim-update-center/src/org/terracotta/modules/tool/commands/InfoCommand.java
@@ -0,0 +1,18 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package org.terracotta.modules.tool.commands;
+
+import org.apache.commons.cli.CommandLine;
+
+public class InfoCommand extends AbstractCommand {
+
+  public void execute(CommandLine cli) {
+    //
+  }
+
+  public String help() {
+    return null;
+  }
+
+}
