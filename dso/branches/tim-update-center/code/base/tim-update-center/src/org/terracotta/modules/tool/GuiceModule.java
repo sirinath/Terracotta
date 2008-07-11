077cd41346f3b77fdf1ccf0dd7e9156941fbe4d1
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-11 22:10:28 +0000

    Tell Guice how to create our class.

--- dso/branches/tim-update-center/code/base/tim-update-center/src/org/terracotta/modules/tool/GuiceModule.java
+++ dso/branches/tim-update-center/code/base/tim-update-center/src/org/terracotta/modules/tool/GuiceModule.java
@@ -3,11 +3,16 @@
  */
 package org.terracotta.modules.tool;
 
+import org.jdom.JDOMException;
 import org.terracotta.modules.tool.commands.CommandRegistry;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.google.inject.Provider;
 import com.google.inject.Scopes;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Module definition for Guice dependency injection.
@@ -15,6 +20,17 @@ import com.google.inject.Scopes;
 public class GuiceModule implements Module {
 
   public void configure(Binder binder) {
+    binder.bind(Modules.class).toProvider(new Provider<CachedXmlModules>() {
+      public CachedXmlModules get() {
+        try {
+          return new CachedXmlModules(new File("modules.xml"));
+        } catch (JDOMException e) {
+          throw new RuntimeException(e);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });
     binder.bind(Modules.class).to(CachedXmlModules.class).in(Scopes.SINGLETON);
     binder.bind(CommandRegistry.class).in(Scopes.SINGLETON);
   }
