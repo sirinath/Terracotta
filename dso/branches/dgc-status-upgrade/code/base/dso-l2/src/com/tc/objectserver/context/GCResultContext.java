b066fe7c36542531be1f0df190ebd9ede8c44d67
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-11 20:46:11 +0000

    include GarbageDisposerHandler deleting as deleteStageTime

--- dso/branches/dgc-status-upgrade/code/base/dso-l2/src/com/tc/objectserver/context/GCResultContext.java
+++ dso/branches/dgc-status-upgrade/code/base/dso-l2/src/com/tc/objectserver/context/GCResultContext.java
@@ -12,10 +12,12 @@ public class GCResultContext implements EventContext {
 
   private final int       gcIteration;
   private final SortedSet gcedOids;
+  private long  deleteStartMillis;
 
-  public GCResultContext(int gcIteration, SortedSet gcedOids) {
+  public GCResultContext(int gcIteration, SortedSet gcedOids, long deleteStartMillis) {
     this.gcIteration = gcIteration;
     this.gcedOids = gcedOids;
+    this.deleteStartMillis = deleteStartMillis;
   }
 
   public int getGCIterationCount() {
@@ -24,6 +26,10 @@ public class GCResultContext implements EventContext {
 
   public SortedSet getGCedObjectIDs() {
     return gcedOids;
+  }
+  
+  public long getDeleteStartMillis() {
+    return deleteStartMillis;
   }
 
   public String toString() {
