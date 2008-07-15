24f1200983938b36499954e7dbcb7eb0dfcc11ad
amiller <amiller@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-15 22:11:40 +0000

    Switch to JPA

--- lassen/prototypes/persistence/lassen/src/main/java/org/terracotta/reference/exam/domain/User.java
+++ lassen/prototypes/persistence/lassen/src/main/java/org/terracotta/reference/exam/domain/User.java
@@ -1,10 +1,29 @@
 package org.terracotta.reference.exam.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+  
+  @Id @GeneratedValue
+  @Column(name = "USER_ID")
   private Long id;
+  
+  @Column(name = "FIRST_NAME")
   private String firstName;
+  
+  @Column(name = "LAST_NAME")
   private String lastName;
+  
+  @Column(name = "EMAIL")
   private String email;
+  
+  @Column(name = "PASSWORD")
   private String password;
   
   public User() {
