cdb20ed006f970bdff5dca733204616686c825be
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-07-28 13:53:39 +0000

    Created 2.7 branch from revision 9504 of trunk.

---
+++ dso/branches/2.7/code/base/deploy/src/com/tc/admin/dso/ClientNode.java
@@ -0,0 +1,78 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.admin.dso;
+
+import com.tc.admin.common.ComponentNode;
+import com.tc.admin.model.IClient;
+
+import java.awt.Component;
+
+public class ClientNode extends ComponentNode {
+  protected ClientsNode            m_clientsNode;
+  protected IClient                m_client;
+  protected ClientPanel            m_clientPanel;
+  protected ClientThreadDumpsNode  m_threadDumpsNode;
+  protected ClientRuntimeStatsNode m_runtimeStatsNode;
+  protected ClientRootsNode        m_rootsNode;
+
+  public ClientNode(ClientsNode clientsNode, IClient client) {
+    super(client.getRemoteAddress());
+    m_clientsNode = clientsNode;
+    m_client = client;
+    addChildren();
+  }
+
+  ClientsNode getClientsNode() {
+    return m_clientsNode;
+  }
+
+  protected ClientPanel createClientPanel() {
+    return new ClientPanel(this);
+  }
+
+  public Component getComponent() {
+    if (m_clientPanel == null) {
+      m_clientPanel = createClientPanel();
+    }
+    return m_clientPanel;
+  }
+
+  protected void addChildren() {
+    add(m_rootsNode = createClientRootsNode());
+    add(m_runtimeStatsNode = createRuntimeStatsNode());
+    add(m_threadDumpsNode = createThreadDumpsNode());
+  }
+
+  protected ClientRuntimeStatsNode createRuntimeStatsNode() {
+    return new ClientRuntimeStatsNode(this);
+  }
+
+  protected ClientThreadDumpsNode createThreadDumpsNode() {
+    return new ClientThreadDumpsNode(this);
+  }
+
+  protected ClientRootsNode createClientRootsNode() {
+    return new ClientRootsNode(this);
+  }
+
+  public IClient getClient() {
+    return m_client;
+  }
+
+  public void tearDown() {
+    if (m_clientPanel != null) {
+      m_clientPanel.tearDown();
+      m_clientPanel = null;
+    }
+
+    super.tearDown();
+
+    m_clientsNode = null;
+    m_client = null;
+    m_rootsNode = null;
+    m_threadDumpsNode = null;
+    m_runtimeStatsNode = null;
+  }
+}
---
+++ dso/branches/2.7/code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/AbstractCommandTestCase.java
@@ -0,0 +1,53 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package org.terracotta.modules.tool.commands;
+
+import com.tc.test.TCTestCase;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Abstract base class for command test cases.
+ */
+public abstract class AbstractCommandTestCase extends TCTestCase {
+  protected AbstractCommand command;
+  protected StringWriter    commandOut;
+  protected StringWriter    commandErr;
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    command = createCommand();
+    commandOut = new StringWriter();
+    commandErr = new StringWriter();
+    command.out = new PrintWriter(commandOut);
+    command.err = new PrintWriter(commandErr);
+  }
+
+  public void testHelpNotNull() {
+    assertNotNull(command.description());
+    assertNotEquals(0, command.description().length());
+    assertNotNull(command.help());
+    assertNotEquals(0, command.help().length());
+  }
+
+  protected abstract AbstractCommand createCommand();
+
+  protected void assertOutMatches(String regex) {
+    assertMatches(commandOut, regex);
+  }
+
+  protected void assertErrMatches(String regex) {
+    assertMatches(commandErr, regex);
+  }
+
+  private void assertMatches(StringWriter out, String regex) {
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(out.toString());
+    assertTrue(matcher.find());
+  }
+}
