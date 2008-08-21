27c576de5d7495999c74d97e9f534d2c9076cafd
rsingh <rsingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-08-21 09:08:46 +0000

    reformating

--- dso/branches/active-active-test-framework2/code/base/legacy-test-tree/tests.base/com/tc/test/activeactive/ActiveActiveTestSetupManager.java
+++ dso/branches/active-active-test-framework2/code/base/legacy-test-tree/tests.base/com/tc/test/activeactive/ActiveActiveTestSetupManager.java
@@ -4,61 +4,16 @@
  */
 package com.tc.test.activeactive;
 
-import com.tc.test.activepassive.ActivePassiveTestSetupManager;
+import com.tc.test.TestSetupManagerBase;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class ActiveActiveTestSetupManager {
+public class ActiveActiveTestSetupManager extends TestSetupManagerBase{
 
-  private ActivePassiveTestSetupManager apTestSetupMgr     = new ActivePassiveTestSetupManager();
   private int                           electionTime       = 5;
   private List                          activeServerGroups = new ArrayList();
-
-  public void setServerCount(int count) {
-    this.apTestSetupMgr.setServerCount(count);
-  }
-
-  public int getServerCount() {
-    return this.apTestSetupMgr.getServerCount();
-  }
-
-  public void setServerCrashMode(String mode) {
-    this.apTestSetupMgr.setServerCrashMode(mode);
-  }
-
-  public void setMaxCrashCount(int count) {
-    this.apTestSetupMgr.setMaxCrashCount(count);
-  }
-
-  public int getMaxCrashCount() {
-    return this.getMaxCrashCount();
-  }
-
-  public String getServerCrashMode() {
-    return this.apTestSetupMgr.getServerCrashMode();
-  }
-
-  public void setServerShareDataMode(String mode) {
-    this.apTestSetupMgr.setServerShareDataMode(mode);
-  }
-
-  public boolean isNetworkShare() {
-    return this.apTestSetupMgr.isNetworkShare();
-  }
-
-  public void setServerPersistenceMode(String mode) {
-    this.apTestSetupMgr.setServerPersistenceMode(mode);
-  }
-
-  public String getServerPersistenceMode() {
-    return this.apTestSetupMgr.getServerPersistenceMode();
-  }
-
-  public void setServerCrashWaitTimeInSec(long time) {
-    this.apTestSetupMgr.setServerCrashWaitTimeInSec(time);
-  }
 
   public long getServerCrashWaitTimeInSec() {
     return this.getServerCrashWaitTimeInSec();
@@ -105,9 +60,9 @@ public class ActiveActiveTestSetupManager {
   }
 
   private void checkServerCount() {
-    int serverCount = this.apTestSetupMgr.getServerCount();
+    int serverCount = this.getServerCount();
     if (this.activeServerGroups.size() == 0) {
-      addActiveServerGroup(serverCount, this.apTestSetupMgr.getServerSharedDataMode(), this.electionTime);
+      addActiveServerGroup(serverCount, this.getServerSharedDataMode(), this.electionTime);
     }
 
     int totalMemberCount = 0;
