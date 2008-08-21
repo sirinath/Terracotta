f035c6c11c5b37d60c028204b33e90fdfb4172a2
npurwar <npurwar@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-08-21 12:46:03 +0000

    check in private branch

--- dso/branches/active-active-test-framework2/code/base/legacy-test-tree/tests.base/com/tc/test/activepassive/ActivePassiveCrashMode.java
+++ dso/branches/active-active-test-framework2/code/base/legacy-test-tree/tests.base/com/tc/test/activepassive/ActivePassiveCrashMode.java
@@ -6,16 +6,20 @@ package com.tc.test.activepassive;
 
 import com.tc.test.MultipleServersCrashMode;
 
-public class ActivePassiveCrashMode extends MultipleServersCrashMode {
+public class ActivePassiveCrashMode implements MultipleServersCrashMode {
 
-  public static final String CRASH_AFTER_MUTATE      = "crash-after-mutate";
-  public static final String CONTINUOUS_ACTIVE_CRASH = "continuous-active-crash";
-  public static final String RANDOM_SERVER_CRASH     = "random-server-crash";
-  public static final String AP_CUSTOMIZED_CRASH     = "active-passive-customized-crash";
+  public static final String AP_CUSTOMIZED_CRASH = "active-passive-customized-crash";
+
+  protected String           mode;
 
   public ActivePassiveCrashMode(String mode) {
-    super(mode);
+    this.mode = mode;
     if (!mode.equals(CRASH_AFTER_MUTATE) && !mode.equals(CONTINUOUS_ACTIVE_CRASH) && !mode.equals(RANDOM_SERVER_CRASH)
         && !mode.equals(AP_CUSTOMIZED_CRASH)) { throw new AssertionError("Unrecognized crash mode [" + mode + "]"); }
+  }
+
+  public String getMode() {
+    if (mode == null) { throw new AssertionError("Mode was not set"); }
+    return mode;
   }
 }
