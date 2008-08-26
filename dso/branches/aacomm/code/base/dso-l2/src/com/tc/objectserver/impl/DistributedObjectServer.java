cd00090274c90311f9c10d8331510d7b5824f9fd
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-08-26 00:40:20 +0000

    GroupID implements NodeID, instead of extends NodeIDImpl

--- dso/branches/aacomm/code/base/dso-l2/src/com/tc/objectserver/impl/DistributedObjectServer.java
+++ dso/branches/aacomm/code/base/dso-l2/src/com/tc/objectserver/impl/DistributedObjectServer.java
@@ -7,9 +7,6 @@ package com.tc.objectserver.impl;
 import bsh.EvalError;
 import bsh.Interpreter;
 
-import bsh.EvalError;
-import bsh.Interpreter;
-
 import com.tc.async.api.SEDA;
 import com.tc.async.api.Sink;
 import com.tc.async.api.Stage;
