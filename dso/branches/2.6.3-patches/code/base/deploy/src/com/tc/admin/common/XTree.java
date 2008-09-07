1e109b74d3a55bb0665a05a84ebc429b8230091c
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-09-07 01:05:14 +0000

    Merge of changes 9997 and 10004 from 2.6 branch improving AdminConsole Lock Profiler.

--- dso/branches/2.6.3-patches/code/base/deploy/src/com/tc/admin/common/XTree.java
+++ dso/branches/2.6.3-patches/code/base/deploy/src/com/tc/admin/common/XTree.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.admin.common;
 
@@ -14,13 +15,15 @@ import javax.swing.InputMap;
 import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
 public class XTree extends org.dijon.Tree {
   protected JPopupMenu m_popupMenu;
-  
+
   public XTree() {
     super();
 
@@ -38,76 +41,95 @@ public class XTree extends org.dijon.Tree {
       }
 
       public void testPopup(MouseEvent e) {
-        if(e.isPopupTrigger()) {
-          TreePath   path = getPathForLocation(e.getX(), e.getY());
-          Object     comp = path != null ? path.getLastPathComponent() : null;
-          
-          XTreeNode  node =  comp instanceof XTreeNode ? (XTreeNode)comp : null;
+        if (e.isPopupTrigger()) {
+          TreePath path = getPathForLocation(e.getX(), e.getY());
+          Object comp = path != null ? path.getLastPathComponent() : null;
+          XTreeNode node = comp instanceof XTreeNode ? (XTreeNode) comp : null;
           JPopupMenu menu = node != null ? node.getPopupMenu() : XTree.this.getPopupMenu();
 
-          if(menu != null) {
+          if (menu != null) {
             menu.show(XTree.this, e.getX(), e.getY());
+          }
+        }
+      }
+
+      public void mouseClicked(MouseEvent me) {
+        TreePath path = getPathForLocation(me.getX(), me.getY());
+        if (path != null) {
+          requestFocus();
+          XTreeNode node = (XTreeNode) path.getLastPathComponent();
+          if (node != null) {
+            node.nodeClicked(me);
           }
         }
       }
     };
     addMouseListener(ml);
+
+    addTreeSelectionListener(new TreeSelectionListener() {
+      public void valueChanged(TreeSelectionEvent tse) {
+        TreePath path = tse.getNewLeadSelectionPath();
+        if (path != null) {
+          requestFocus();
+          XTreeNode node = (XTreeNode) path.getLastPathComponent();
+          if (node != null) {
+            node.nodeSelected(tse);
+          }
+        }
+      }
+    });
   }
 
   public XTree(TreeModel model) {
     this();
     setModel(model);
   }
+
   public void setPopupMenu(JPopupMenu popupMenu) {
-    if(popupMenu != null) {
+    if (popupMenu != null) {
       add(m_popupMenu = popupMenu);
     }
   }
-  
+
   public JPopupMenu getPopupMenu() {
     return m_popupMenu;
   }
-  
+
   private Action getAction(KeyStroke ks) {
     Action result = null;
     Object object = getLastSelectedPathComponent();
-    
-    if(object instanceof XTreeNode) {
-      XTreeNode node = (XTreeNode)object;
-  
-      while(node != null) {
+
+    if (object instanceof XTreeNode) {
+      XTreeNode node = (XTreeNode) object;
+
+      while (node != null) {
         ActionMap actionMap = node.getActionMap();
-  
-        if(actionMap != null) {
+
+        if (actionMap != null) {
           InputMap inputMap = node.getInputMap();
-  
-          if(inputMap != null) {
+
+          if (inputMap != null) {
             Object binding = inputMap.get(ks);
-  
-            if(binding != null) {
-              if((result = actionMap.get(binding)) != null) {
-                return result;
-              }
+
+            if (binding != null) {
+              if ((result = actionMap.get(binding)) != null) { return result; }
             }
           }
         }
-  
-        node = (XTreeNode)node.getParent();
+
+        node = (XTreeNode) node.getParent();
       }
     }
-    
+
     return null;
   }
 
-  protected boolean processKeyBinding(KeyStroke ks, KeyEvent e,
-                                      int condition, boolean pressed)
-  {
+  protected boolean processKeyBinding(KeyStroke ks, KeyEvent e, int condition, boolean pressed) {
     Action action = getAction(ks);
 
-    if(action != null) {
+    if (action != null) {
       return SwingUtilities.notifyAction(action, ks, e, this, e.getModifiers());
-    }
-    else {
+    } else {
       return super.processKeyBinding(ks, e, condition, pressed);
     }
   }
@@ -118,7 +140,7 @@ public class XTree extends org.dijon.Tree {
   }
 
   public void selectTop() {
-    if(getRowCount() > 0) {
+    if (getRowCount() > 0) {
       setSelectionRow(0);
       expandTop();
     }
@@ -129,14 +151,14 @@ public class XTree extends org.dijon.Tree {
   }
 
   public void expandAll() {
-    DefaultMutableTreeNode root = (DefaultMutableTreeNode)getModel().getRoot();
+    DefaultMutableTreeNode root = (DefaultMutableTreeNode) getModel().getRoot();
     DefaultMutableTreeNode node = root.getFirstLeaf();
     DefaultMutableTreeNode parent;
 
-    while(node != null) {
-      parent = (DefaultMutableTreeNode)node.getParent();
-      
-      if(parent != null) {
+    while (node != null) {
+      parent = (DefaultMutableTreeNode) node.getParent();
+
+      if (parent != null) {
         expandPath(new TreePath(parent.getPath()));
       }
 
@@ -146,6 +168,7 @@ public class XTree extends org.dijon.Tree {
     revalidate();
     repaint();
   }
-  
-  public void createOverlayListener() {/**/}
+
+  public void createOverlayListener() {/**/
+  }
 }
