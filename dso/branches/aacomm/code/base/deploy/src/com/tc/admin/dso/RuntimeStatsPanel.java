1b8a0ac4f7f94910ab16a95a6ff0b0021bf47b49
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-09-22 22:46:49 +0000

    re-branch aacomm from trunk

---
+++ dso/branches/aacomm/code/base/deploy/src/com/tc/admin/dso/RuntimeStatsPanel.java
@@ -0,0 +1,451 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.admin.dso;
+
+import org.dijon.Button;
+import org.dijon.Container;
+import org.dijon.ContainerResource;
+import org.dijon.Spinner;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.AxisSpace;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.Range;
+import org.jfree.data.time.Second;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.ui.RectangleInsets;
+
+import com.tc.admin.AdminClient;
+import com.tc.admin.AdminClientContext;
+import com.tc.admin.common.BrowserLauncher;
+import com.tc.admin.common.DemoChartFactory;
+import com.tc.admin.common.XContainer;
+import com.tc.admin.common.XTextPane;
+import com.tc.management.RuntimeStatisticConstants;
+import com.tc.stats.statistics.CountStatistic;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.prefs.Preferences;
+
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Element;
+import javax.swing.text.html.HTML;
+
+public class RuntimeStatsPanel extends XContainer implements RuntimeStatisticConstants {
+  protected AdminClientContext     m_acc;
+  protected Timer                  m_statsGathererTimer;
+  protected Container              m_chartsPanel;
+  private Button                   m_startMonitoringButton;
+  private Button                   m_stopMonitoringButton;
+  private Button                   m_clearSamplesButton;
+  private Spinner                  m_samplePeriodSpinner;
+  private Spinner                  m_sampleHistorySpinner;
+  protected AxisSpace              m_rangeAxisSpace;
+
+  private boolean                  m_shouldAutoStart;
+
+  protected static final Dimension fDefaultGraphSize                       = new Dimension(0,0);
+
+  private static final int         DEFAULT_POLL_PERIOD_SECS                = 3;
+  private static final int         DEFAULT_SAMPLE_HISTORY_MINUTES          = 5;
+  private static final int         SAMPLE_SAMPLE_HISTORY_STEP_SIZE         = 1;
+
+  private static final String      DEFAULT_POLL_PERIOD_SECONDS_PREF_KEY    = "poll-periods-seconds";
+  private static final String      DEFAULT_SAMPLE_HISTORY_MINUTES_PREF_KEY = "sample-history-minutes";
+
+  private ArrayList<TimeSeries>    m_allSeries;
+  private ArrayList<JFreeChart>    m_allCharts;
+
+  private static final String      HYPERIC_INSTRUCTIONS_URI                = "/com/tc/admin/HypericInstructions.html";
+
+  public RuntimeStatsPanel() {
+    super();
+    m_acc = AdminClient.getContext();
+    m_allSeries = new ArrayList<TimeSeries>();
+    m_allCharts = new ArrayList<JFreeChart>();
+    m_shouldAutoStart = true;
+    load((ContainerResource) m_acc.childResource("RuntimeStatsPanel"));
+  }
+
+  public void load(ContainerResource res) {
+    super.load(res);
+
+    m_chartsPanel = (Container) findComponent("ChartsPanel");
+
+    m_startMonitoringButton = (Button) findComponent("StartMonitoringButton");
+    m_startMonitoringButton.addActionListener(new StartMonitoringAction());
+
+    m_stopMonitoringButton = (Button) findComponent("StopMonitoringButton");
+    m_stopMonitoringButton.addActionListener(new StopMonitoringAction());
+
+    m_clearSamplesButton = (Button) findComponent("ClearSamplesButton");
+    m_clearSamplesButton.addActionListener(new ClearSamplesAction());
+
+    m_samplePeriodSpinner = (Spinner) findComponent("SamplePeriodSpinner");
+    m_samplePeriodSpinner.setModel(new SpinnerNumberModel(Integer.valueOf(getDefaultPollPeriodSeconds()), Integer
+        .valueOf(1), null, Integer.valueOf(1)));
+    m_samplePeriodSpinner.addChangeListener(new SamplePeriodChangeHandler());
+
+    m_sampleHistorySpinner = (Spinner) findComponent("SampleHistorySpinner");
+    m_sampleHistorySpinner.setModel(new SpinnerNumberModel(Integer.valueOf(getDefaultSampleHistoryMinutes()), Integer
+        .valueOf(1), null, Integer.valueOf(SAMPLE_SAMPLE_HISTORY_STEP_SIZE)));
+    m_sampleHistorySpinner.addChangeListener(new SampleHistoryChangeHandler());
+  }
+
+  protected ChartPanel createChartPanel(JFreeChart chart) {
+    boolean useBuffer = false;
+    boolean properties = false;
+    boolean save = false;
+    boolean print = false;
+    boolean zoom = false;
+    boolean tooltips = true;
+
+    ChartPanel chartPanel = new ChartPanel(chart, ChartPanel.DEFAULT_WIDTH, ChartPanel.DEFAULT_HEIGHT,
+                                           ChartPanel.DEFAULT_MINIMUM_DRAW_WIDTH,
+                                           ChartPanel.DEFAULT_MINIMUM_DRAW_HEIGHT,
+                                           ChartPanel.DEFAULT_MAXIMUM_DRAW_WIDTH,
+                                           ChartPanel.DEFAULT_MAXIMUM_DRAW_HEIGHT, useBuffer, properties, save, print,
+                                           zoom, tooltips);
+    chartPanel.setRangeZoomable(false);
+    chartPanel.setDomainZoomable(false);
+
+    return chartPanel;
+  }
+
+  private int getDefaultPollPeriodSeconds() {
+    return getIntPref(DEFAULT_POLL_PERIOD_SECONDS_PREF_KEY, DEFAULT_POLL_PERIOD_SECS);
+  }
+
+  private int getDefaultSampleHistoryMinutes() {
+    return getIntPref(DEFAULT_SAMPLE_HISTORY_MINUTES_PREF_KEY, DEFAULT_SAMPLE_HISTORY_MINUTES);
+  }
+
+  private class StartMonitoringAction implements ActionListener {
+    public void actionPerformed(ActionEvent ae) {
+      startMonitoringRuntimeStats();
+    }
+  }
+
+  private class StopMonitoringAction implements ActionListener {
+    public void actionPerformed(ActionEvent ae) {
+      stopMonitoringRuntimeStats();
+    }
+  }
+
+  private class ClearSamplesAction implements ActionListener {
+    public void actionPerformed(ActionEvent ae) {
+      clearAllRuntimeStatsSamples();
+    }
+  }
+
+  private class SamplePeriodChangeHandler implements ChangeListener {
+    public void stateChanged(ChangeEvent e) {
+      SpinnerNumberModel model = (SpinnerNumberModel) m_samplePeriodSpinner.getModel();
+      Integer i = (Integer) model.getNumber();
+      setRuntimeStatsPollPeriodSeconds(i.intValue());
+    }
+  }
+
+  private class SampleHistoryChangeHandler implements ChangeListener {
+    public void stateChanged(ChangeEvent e) {
+      SpinnerNumberModel model = (SpinnerNumberModel) m_sampleHistorySpinner.getModel();
+      Integer i = (Integer) model.getNumber();
+      setRuntimeStatsSampleHistoryMinutes(i.intValue());
+    }
+  }
+
+  protected TimeSeries createTimeSeries(String name) {
+    TimeSeries ts = new TimeSeries(name, Second.class);
+    ts.setMaximumItemCount(50);
+    m_allSeries.add(ts);
+    return ts;
+  }
+
+  protected JFreeChart createChart(TimeSeries series) {
+    return createChart(new TimeSeries[] { series });
+  }
+
+  protected JFreeChart createChart(TimeSeries series, boolean createLegend) {
+    return createChart(new TimeSeries[] { series }, createLegend);
+  }
+
+  protected JFreeChart createChart(TimeSeries[] seriesArray) {
+    return createChart(seriesArray, true);
+  }
+
+  protected JFreeChart createChart(TimeSeries[] seriesArray, boolean createLegend) {
+    JFreeChart chart = DemoChartFactory.getXYLineChart("", "", "", seriesArray, createLegend);
+    int sampleHistoryMinutes = getRuntimeStatsSampleHistoryMinutes();
+    int sampleHistoryMillis = sampleHistoryMinutes * 60 * 1000;
+
+    XYPlot plot = (XYPlot) chart.getPlot();
+    plot.getDomainAxis().setFixedAutoRange(sampleHistoryMillis);
+    ((NumberAxis) plot.getRangeAxis()).setAutoRangeIncludesZero(true);
+
+    int maxSampleCount = (sampleHistoryMinutes * 60) / getRuntimeStatsPollPeriodSeconds();
+    for (TimeSeries series : seriesArray) {
+      series.setMaximumItemCount(maxSampleCount);
+    }
+
+    m_allCharts.add(chart);
+    return chart;
+  }
+
+  public void addNotify() {
+    super.addNotify();
+
+    double fixedRangeAxisSpace = 0;
+    List<XYPlot> plotList = new ArrayList<XYPlot>();
+    java.awt.Component[] chartPanels = m_chartsPanel.getComponents();
+    for (java.awt.Component comp : chartPanels) {
+      if (!(comp instanceof ChartPanel)) continue;
+      ChartPanel chartPanel = (ChartPanel) comp;
+      JFreeChart chart = chartPanel.getChart();
+      if (chart == null) continue;
+      XYPlot plot = ((XYPlot) chart.getPlot());
+      plotList.add(plot);
+      double rangeAxisSpace = getRangeAxisTickWidth(chartPanel.getGraphics(), plot);
+      fixedRangeAxisSpace = Math.max(fixedRangeAxisSpace, rangeAxisSpace);
+    }
+
+    m_rangeAxisSpace = new AxisSpace();
+    m_rangeAxisSpace.setLeft(fixedRangeAxisSpace);
+    m_rangeAxisSpace.setRight(5);
+
+    if (plotList.size() > 0) {
+      Iterator<XYPlot> plotIter = plotList.iterator();
+      while (plotIter.hasNext()) {
+        XYPlot plot = plotIter.next();
+        plot.setFixedRangeAxisSpace(m_rangeAxisSpace);
+      }
+    }
+
+    if (m_shouldAutoStart) {
+      startMonitoringRuntimeStats();
+      m_shouldAutoStart = false;
+    }
+  }
+
+  private double getRangeAxisTickWidth(Graphics graphics, XYPlot plot) {
+    NumberAxis numberAxis = (NumberAxis) plot.getRangeAxis();
+    RectangleInsets tickLabelInsets = numberAxis.getTickLabelInsets();
+    NumberTickUnit unit = numberAxis.getTickUnit();
+
+    // look at lower and upper bounds...
+    FontMetrics fm = graphics.getFontMetrics(numberAxis.getTickLabelFont());
+    Range range = numberAxis.getRange();
+    double lower = range.getLowerBound();
+    double upper = range.getUpperBound();
+    String lowerStr = "";
+    String upperStr = "";
+    NumberFormat formatter = numberAxis.getNumberFormatOverride();
+    if (formatter != null) {
+      lowerStr = formatter.format(lower);
+      upperStr = formatter.format(upper);
+    } else {
+      lowerStr = unit.valueToString(lower);
+      upperStr = unit.valueToString(upper);
+    }
+    double w1 = fm.stringWidth(lowerStr);
+    double w2 = fm.stringWidth(upperStr);
+    return Math.max(w1, w2) + tickLabelInsets.getLeft() + tickLabelInsets.getRight();
+  }
+
+  protected void setup(Container runtimeStatsPanel) {
+    /* override this */
+  }
+
+  public void startMonitoringRuntimeStats() {
+    testStartStatsGatherer();
+    m_chartsPanel.setVisible(true);
+    m_startMonitoringButton.setEnabled(false);
+    m_stopMonitoringButton.setEnabled(true);
+  }
+
+  public void stopMonitoringRuntimeStats() {
+    if (m_statsGathererTimer != null) {
+      m_statsGathererTimer.stop();
+      m_startMonitoringButton.setEnabled(true);
+      m_stopMonitoringButton.setEnabled(false);
+    }
+  }
+
+  private void clearAllRuntimeStatsSamples() {
+    boolean monitoring = false;
+    if (m_statsGathererTimer != null && m_statsGathererTimer.isRunning()) {
+      monitoring = true;
+      m_statsGathererTimer.stop();
+    }
+
+    Iterator<TimeSeries> iter = m_allSeries.iterator();
+    while (iter.hasNext()) {
+      iter.next().clear();
+    }
+
+    if (monitoring) {
+      m_statsGathererTimer.start();
+    }
+  }
+
+  private int getIntPref(String key, int defaultValue) {
+    return m_acc.getPrefs().node("RuntimeStats").getInt(key, defaultValue);
+  }
+
+  private void putIntPref(String key, int value) {
+    Preferences prefs = m_acc.getPrefs().node("RuntimeStats");
+    prefs.putInt(key, value);
+    m_acc.storePrefs();
+    try {
+      prefs.flush();
+    } catch (Exception e) {/**/
+    }
+  }
+
+  private void setRuntimeStatsPollPeriodSeconds(int seconds) {
+    putIntPref(DEFAULT_POLL_PERIOD_SECONDS_PREF_KEY, seconds);
+
+    if (m_statsGathererTimer != null) {
+      int pollMillis = seconds * 1000;
+      m_statsGathererTimer.setInitialDelay(pollMillis);
+      m_statsGathererTimer.restart();
+
+      Iterator<JFreeChart> chartIter = m_allCharts.iterator();
+      int sampleHistoryMinutes = getRuntimeStatsSampleHistoryMinutes();
+      int sampleHistoryMillis = sampleHistoryMinutes * 60 * 1000;
+      while (chartIter.hasNext()) {
+        ((XYPlot) chartIter.next().getPlot()).getDomainAxis().setFixedAutoRange(sampleHistoryMillis);
+      }
+    }
+  }
+
+  protected int getRuntimeStatsPollPeriodSeconds() {
+    SpinnerNumberModel model = (SpinnerNumberModel) m_samplePeriodSpinner.getModel();
+    Integer i = (Integer) model.getNumber();
+    return i.intValue();
+  }
+
+  private int getRuntimeStatsPollPeriodMillis() {
+    return getRuntimeStatsPollPeriodSeconds() * 1000;
+  }
+
+  private int getRuntimeStatsSampleHistoryMinutes() {
+    SpinnerNumberModel model = (SpinnerNumberModel) m_sampleHistorySpinner.getModel();
+    Integer i = (Integer) model.getNumber();
+    return i.intValue();
+  }
+
+  private void setRuntimeStatsSampleHistoryMinutes(int sampleHistoryMinutes) {
+    putIntPref(DEFAULT_SAMPLE_HISTORY_MINUTES_PREF_KEY, sampleHistoryMinutes);
+
+    int sampleHistoryMillis = sampleHistoryMinutes * 60 * 1000;
+    int maxSampleCount = (sampleHistoryMinutes * 60) / getRuntimeStatsPollPeriodSeconds();
+
+    Iterator<TimeSeries> seriesIter = m_allSeries.iterator();
+    while (seriesIter.hasNext()) {
+      seriesIter.next().setMaximumItemCount(maxSampleCount);
+    }
+
+    Iterator<JFreeChart> chartIter = m_allCharts.iterator();
+    while (chartIter.hasNext()) {
+      ((XYPlot) chartIter.next().getPlot()).getDomainAxis().setFixedAutoRange(sampleHistoryMillis);
+    }
+  }
+
+  protected void testStartStatsGatherer() {
+    if (m_statsGathererTimer == null) {
+      int pollMillis = getRuntimeStatsPollPeriodMillis();
+      m_statsGathererTimer = new Timer(pollMillis, new StatisticsRetrievalAction());
+      m_statsGathererTimer.setRepeats(false);
+    }
+    if (!m_statsGathererTimer.isRunning()) {
+      m_statsGathererTimer.start();
+    }
+  }
+
+  private Date m_tmpDate = new Date();
+
+  protected void updateSeries(TimeSeries series, CountStatistic value) {
+    m_tmpDate.setTime(value.getLastSampleTime());
+    series.addOrUpdate(new Second(m_tmpDate), value.getCount());
+  }
+
+  protected void retrieveStatistics() {
+    /* override this */
+  }
+
+  private class StatisticsRetrievalAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      retrieveStatistics();
+    }
+  }
+
+  protected void setupHypericInstructions(JComponent comp) {
+    comp.removeAll();
+    XTextPane textPane = new XTextPane();
+    textPane.setEditable(false);
+    textPane.setBackground(Color.WHITE);
+    textPane.addHyperlinkListener(new HypericInstructionsLinkListener());
+    try {
+      textPane.setPage(getClass().getResource(HYPERIC_INSTRUCTIONS_URI));
+    } catch (IOException ioe) {
+      textPane.setText(ioe.getMessage());
+    }
+    comp.setLayout(new BorderLayout());
+    comp.add(new JScrollPane(textPane));
+    comp.revalidate();
+    comp.repaint();
+  }
+
+  private static class HypericInstructionsLinkListener implements HyperlinkListener {
+    public void hyperlinkUpdate(HyperlinkEvent e) {
+      HyperlinkEvent.EventType type = e.getEventType();
+      Element elem = e.getSourceElement();
+
+      if (elem == null || type == HyperlinkEvent.EventType.ENTERED || type == HyperlinkEvent.EventType.EXITED) { return; }
+
+      AttributeSet a = elem.getAttributes();
+      AttributeSet anchor = (AttributeSet) a.getAttribute(HTML.Tag.A);
+      String href = (String) anchor.getAttribute(HTML.Attribute.HREF);
+      BrowserLauncher.openURL(href);
+    }
+  }
+
+  public synchronized void tearDown() {
+    stopMonitoringRuntimeStats();
+
+    super.tearDown();
+
+    m_acc = null;
+    m_statsGathererTimer = null;
+    m_chartsPanel = null;
+    m_startMonitoringButton = null;
+    m_stopMonitoringButton = null;
+    m_clearSamplesButton = null;
+    m_samplePeriodSpinner = null;
+    m_sampleHistorySpinner = null;
+  }
+}
---
+++ dso/branches/aacomm/code/base/dso-common/src/com/tc/object/msg/ClientHandshakeAckMessage.java
@@ -0,0 +1,32 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object.msg;
+
+import com.tc.net.ClientID;
+import com.tc.net.ServerID;
+import com.tc.net.protocol.tcm.MessageChannel;
+
+import java.util.Set;
+
+public interface ClientHandshakeAckMessage {
+  public void send();
+
+  public boolean getPersistentServer();
+
+  public void initialize(boolean persistent, Set allNodes, String thisNodeID, String serverVersion,
+                         ServerID serverNodeID);
+
+  public MessageChannel getChannel();
+
+  public String[] getAllNodes();
+
+  public String getThisNodeId();
+
+  public String getServerVersion();
+
+  public ClientID getClientID();
+
+  public ServerID getServerNodeID();
+}
