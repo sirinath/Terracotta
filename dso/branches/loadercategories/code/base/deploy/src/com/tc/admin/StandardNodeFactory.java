df0865d0b74e38772e6aa5956b062f6e6d83d4d7
wharley <wharley@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-10-01 23:15:18 +0000

    Branch for researching classloader substitution categories

---
+++ dso/branches/loadercategories/code/base/deploy/src/com/tc/admin/StandardNodeFactory.java
@@ -0,0 +1,24 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.admin;
+
+import com.tc.admin.model.IServer;
+
+public class StandardNodeFactory extends AbstractNodeFactory {
+  public ClusterNode createClusterNode() {
+    return new ClusterNode();
+  }
+
+  @Override
+  public ClusterNode createClusterNode(String host, int jmxPort, boolean autoConnect) {
+    return new ClusterNode(host, jmxPort, autoConnect);
+  }
+
+  @Override
+  public ServerNode createServerNode(ServersNode serversNode, IServer server) {
+    return new ServerNode(serversNode, server);
+  }
+
+}
