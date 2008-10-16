cd5c5ed273550bb6c795cabdfabed7cb0e0d9d91
jgalang <jgalang@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-10-16 20:36:54 +0000

    updated scripts, so it's easy to run from kit

--- dso/branches/thunderchicken/code/demos/competition/perftune/bin/reader.sh
+++ dso/branches/thunderchicken/code/demos/competition/perftune/bin/reader.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-. ./env.sh
-
+BASEDIR=`dirname $0`/..
+cd $BASEDIR
+. $BASEDIR/bin/env.sh
 
 if test \! -d "${JAVA_HOME}"; then
    echo "$0: the JAVA_HOME environment variable is not defined
-correctly"
+   correctly"
    exit 2
 fi
 
-#TC_INSTALL_DIR=`dirname "$0"`/..
 if test \! -d "${TC_INSTALL_DIR}"; then
    echo "$0: the TC_INSTALL_DIR environment variable is not defined
-correctly"
+   correctly"
    exit 2
 fi
 
-for i in ../lib/*.jar ; do
-  if [ "$CLASSPATH" != "" ]; then
-       CLASSPATH=${CLASSPATH}:$i
-  else
-    CLASSPATH=$i
-  fi
+for i in $BASEDIR/lib/*.jar ; do
+   if test ! -z $CLASSPATH; then
+      CLASSPATH=${CLASSPATH}:$i
+   else
+      CLASSPATH=$i
+   fi
 done
- 
-export TC_CONFIG_PATH=../tc-config.xml
+
+export TC_CONFIG_PATH=$BASEDIR/tc-config.xml
 . $TC_INSTALL_DIR/bin/dso-env.sh -q
 
 $JAVA_HOME/bin/java -Xmx1536m -Xms1536m  $TC_JAVA_OPTS -Dtc.node-name=stress_reader -cp $CLASSPATH  com.tctest.perf.dashboard.stats.app.test.Reader
