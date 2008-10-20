1584527a9bdce09d549053203bb1eff8146bc927
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-10-20 23:45:38 +0000

    placeholder objectmanager for garbage collector

--- dso/branches/aa-dgc/code/base/dso-l2/tests.base/com/tc/objectserver/dgc/impl/ObjectManagerArray.java
+++ dso/branches/aa-dgc/code/base/dso-l2/tests.base/com/tc/objectserver/dgc/impl/ObjectManagerArray.java
@@ -7,30 +7,44 @@ package com.tc.objectserver.dgc.impl;
 import com.tc.lang.TCThreadGroup;
 import com.tc.lang.ThrowableHandler;
 import com.tc.logging.TCLogging;
+import com.tc.net.NodeID;
 import com.tc.object.ObjectID;
 import com.tc.object.cache.EvictionPolicy;
 import com.tc.object.cache.NullCache;
+import com.tc.objectserver.api.ObjectManager;
+import com.tc.objectserver.api.ObjectManagerStatsListener;
 import com.tc.objectserver.api.TestSink;
+import com.tc.objectserver.context.GCResultContext;
+import com.tc.objectserver.context.ObjectManagerResultsContext;
+import com.tc.objectserver.core.api.ManagedObject;
 import com.tc.objectserver.core.impl.TestManagedObject;
+import com.tc.objectserver.dgc.api.GarbageCollector;
 import com.tc.objectserver.impl.InMemoryManagedObjectStore;
 import com.tc.objectserver.impl.ObjectManagerConfig;
 import com.tc.objectserver.impl.ObjectManagerImpl;
 import com.tc.objectserver.l1.api.ClientStateManager;
 import com.tc.objectserver.l1.impl.ClientStateManagerImpl;
 import com.tc.objectserver.persistence.api.ManagedObjectStore;
+import com.tc.objectserver.persistence.api.PersistenceTransaction;
 import com.tc.objectserver.persistence.api.PersistenceTransactionProvider;
 import com.tc.objectserver.persistence.impl.TestPersistenceTransactionProvider;
+import com.tc.util.ObjectIDSet;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-public class ObjectManagerArray {
+public class ObjectManagerArray implements ObjectManager {
 
   private final int                 arrayCount;
   private final ObjectManagerImpl[] objectManagers;
   private final ClientStateManager  clientStateManager;
+  private GarbageCollector          collector;
 
   public ObjectManagerArray(int arrayCount) {
     this.arrayCount = arrayCount;
@@ -44,19 +58,23 @@ public class ObjectManagerArray {
       objectManagers[i] = createObjectManager();
     }
   }
-  
+
   public ObjectManagerImpl[] getObjectManagers() {
     return objectManagers;
   }
 
   public void createObjects(Set<ObjectID> ids) {
+    createObjects(ids, new HashSet<ObjectID>());
+  }
+
+  public void createObjects(Set<ObjectID> ids, Set<ObjectID> children) {
     for (Iterator<ObjectID> iter = ids.iterator(); iter.hasNext();) {
       ObjectID id = iter.next();
-      TestManagedObject mo = new TestManagedObject(id, new ObjectID[] {});
+      TestManagedObject mo = new TestManagedObject(id, children.toArray(new ObjectID[children.size()]));
       Random rand = new Random();
       int index = rand.nextInt(arrayCount);
       objectManagers[index].createObject(mo);
-      objectManagers[index].getObjectStore().addNewObject(mo);   
+      objectManagers[index].getObjectStore().addNewObject(mo);
     }
   }
 
@@ -96,6 +114,122 @@ public class ObjectManagerArray {
     public boolean paranoid() {
       return paranoid;
     }
+  }
+
+  public void addFaultedObject(ObjectID oid, ManagedObject mo, boolean removeOnRelease) {
+    //
+  }
+
+  public void createNewObjects(Set<ObjectID> ids) {
+    //
+  }
+
+  public void createRoot(String name, ObjectID id) {
+    //
+  }
+
+  public void flushAndEvict(List objects2Flush) {
+    //
+  }
+
+  public ObjectIDSet getAllObjectIDs() {
+    return null;
+  }
+
+  public int getCheckedOutCount() {
+    return 0;
+  }
+
+  public GarbageCollector getGarbageCollector() {
+    return collector;
+  }
+
+  public ManagedObject getObjectByIDOrNull(ObjectID id) {
+   return null;
+  }
+
+  public ManagedObject getObjectFromCacheByIDOrNull(ObjectID id) {
+    return null;
+  }
+
+  public ObjectIDSet getObjectIDsInCache() {
+    return null;
+  }
+
+  public Set getRootIDs() {
+    return null;
+  }
+
+  public Map getRootNamesToIDsMap() {
+    return null;
+  }
+
+  public Iterator getRoots() {
+     return null;
+  }
+
+  public boolean lookupObjectsAndSubObjectsFor(NodeID nodeID, ObjectManagerResultsContext responseContext, int maxCount) {
+     return false;
+  }
+
+  public boolean lookupObjectsFor(NodeID nodeID, ObjectManagerResultsContext context) {
+    return false;
+  }
+
+  public ObjectID lookupRootID(String name) {
+    return null;
+  }
+
+  public void notifyGCComplete(GCResultContext resultContext) {
+    for(int i = 0; i < objectManagers.length; i++) {
+      objectManagers[i].notifyGCComplete(resultContext);
+    }
+  }
+
+  public void preFetchObjectsAndCreate(Set<ObjectID> oids, Set<ObjectID> newOids) {
+  //
+  }
+
+  public void release(PersistenceTransaction tx, ManagedObject object) {
+  //
+  }
+
+  public void releaseAll(PersistenceTransaction tx, Collection<ManagedObject> collection) {
+  //
+  }
+
+  public void releaseAllReadOnly(Collection<ManagedObject> objects) {
+   //
+  }
+
+  public void releaseReadOnly(ManagedObject object) {
+   //
+  }
+
+  public void setGarbageCollector(GarbageCollector gc) {
+    this.collector = gc;
+  }
+
+  public void setStatsListener(ObjectManagerStatsListener listener) {
+    //
+  }
+
+  public void start() {
+  //
+  }
+
+  public void stop() {
+   //
+  }
+
+  public void waitUntilReadyToGC() {
+    for(int i = 0; i < objectManagers.length; i++) {
+      objectManagers[i].waitUntilReadyToGC();
+    }
+  }
+
+  public ManagedObject getObjectByID(ObjectID id) {
+    return null;
   }
 
 }
