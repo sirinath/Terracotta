6f94472490e8a988aae5c5193c5f09e0ec8eaaa9
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-10-31 07:18:16 +0000

    Making activer and passive start with different stats dir. Otherwise c10704 will kill the server

--- dso/branches/2.7/code/base/dso-tests-jdk15/tests.system/com/tctest/PassiveSmoothStartTest.java
+++ dso/branches/2.7/code/base/dso-tests-jdk15/tests.system/com/tctest/PassiveSmoothStartTest.java
@@ -37,7 +37,7 @@ public class PassiveSmoothStartTest extends TransparentTestBase {
   String[]                 serverDataPath  = new String[SERVERS];
 
   public PassiveSmoothStartTest() {
-    disableAllUntil("2008-11-05");
+    //
   }
 
   protected Class getApplicationClass() {
@@ -90,6 +90,7 @@ public class PassiveSmoothStartTest extends TransparentTestBase {
         L2ConfigBuilder l2 = new L2ConfigBuilder();
         serverDataPath[i] = getTempFile("data-" + i).getAbsolutePath();
         l2.setData(serverDataPath[i]);
+        l2.setStatistics(serverDataPath[i]);
         l2.setLogs(getTempFile("logs-" + i).getAbsolutePath());
         serverNames[i] = "server-" + i;
         l2.setName(serverNames[i]);
