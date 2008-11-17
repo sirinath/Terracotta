3d24dc78b2144b2dbc16c2f57f78069e486a3770
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-11-17 12:29:06 +0000

    private branch: listener util for client

--- dso/branches/L2SocketConnectL1/code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerUtil.java
+++ dso/branches/L2SocketConnectL1/code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerUtil.java
@@ -4,7 +4,43 @@
  */
 package com.tc.net.protocol.transport;
 
+import com.tc.exception.TCRuntimeException;
+import com.tc.net.AddressChecker;
+import com.tc.net.TCSocketAddress;
+import com.tc.net.protocol.tcm.CommunicationsManager;
+import com.tc.net.protocol.tcm.NetworkListener;
+import com.tc.object.session.NullSessionManager;
+import com.tc.properties.TCProperties;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class ConnectionHealthCheckerUtil {
+
+  public static NetworkListener createHealthCheckListener(CommunicationsManager communicationsManager,
+                                                          TCProperties healthCheckListenerProperties) {
+    InetAddress bindAddr;
+    String bindAddress = healthCheckListenerProperties.getProperty("healthcheck.bindAddress");
+    if (bindAddress == null) {
+      bindAddress = TCSocketAddress.WILDCARD_IP;
+    }
+
+    try {
+      bindAddr = InetAddress.getByName(bindAddress);
+    } catch (UnknownHostException e) {
+      throw new TCRuntimeException("Cannot create InetAddress instance for " + TCSocketAddress.WILDCARD_IP);
+    }
+    AddressChecker addressChecker = new AddressChecker();
+    if (!addressChecker.isLegalBindAddress(bindAddr)) { throw new TCRuntimeException("Invalid bind address ["
+                                                                                     + bindAddr
+                                                                                     + "]. Local addresses are "
+                                                                                     + addressChecker
+                                                                                         .getAllLocalAddresses()); }
+
+    int bindPort = healthCheckListenerProperties.getInt("healthcheck.bindPort", 0);
+    return communicationsManager.createListener(new NullSessionManager(), new TCSocketAddress(bindAddr, bindPort),
+                                                true, new DefaultConnectionIdFactory());
+  }
 
   public static long getMaxIdleTimeForAlive(HealthCheckerConfig config, boolean considerPeerNodeLongGC) {
     // XXX should we check for disabled hc config ?
@@ -14,6 +50,5 @@ public class ConnectionHealthCheckerUtil {
     } else {
       return (config.getPingIdleTimeMillis() + (config.getPingIntervalMillis() * config.getPingProbes()));
     }
-
   }
 }
