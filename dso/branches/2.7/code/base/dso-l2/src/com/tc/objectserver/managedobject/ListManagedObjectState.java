fc109c2823a96d0eae6dd0e31542db736fb0831f
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-12-08 23:54:23 +0000

    rollback 10999 commit

--- dso/branches/2.7/code/base/dso-l2/src/com/tc/objectserver/managedobject/ListManagedObjectState.java
+++ dso/branches/2.7/code/base/dso-l2/src/com/tc/objectserver/managedobject/ListManagedObjectState.java
@@ -1,6 +1,5 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
- * notice. All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
  */
 package com.tc.objectserver.managedobject;
 
@@ -17,18 +16,16 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 /**
  * Server representation of a list
  */
 public class ListManagedObjectState extends LogicalManagedObjectState {
-  protected List references;
+  private ArrayList references;
 
   ListManagedObjectState(ObjectInput in) throws IOException {
     super(in);
-    references = new ArrayList(1);
   }
 
   protected ListManagedObjectState(long classID) {
@@ -41,100 +38,96 @@ public class ListManagedObjectState extends LogicalManagedObjectState {
       LogicalAction action = cursor.getLogicalAction();
       int method = action.getMethod();
       Object[] params = action.getParameters();
-      applyOperation(method, objectID, includeIDs, params );
-    }
-  }
 
-  protected void applyOperation(int method, ObjectID objectID, BackReferences includeIDs, Object[] params)
-      throws AssertionError {
-    switch (method) {
-      case SerializationUtil.ADD:
-      case SerializationUtil.ADD_LAST:
-        addChangeToCollector(objectID, params[0], includeIDs);
-        references.add(params[0]);
-        break;
-      case SerializationUtil.ADD_FIRST:
-        addChangeToCollector(objectID, params[0], includeIDs);
-        references.add(0, params[0]);
-        break;
-      case SerializationUtil.INSERT_AT:
-      case SerializationUtil.ADD_AT:
-        addChangeToCollector(objectID, params[1], includeIDs);
-        int ai = Math.min(((Integer) params[0]).intValue(), references.size());
-        if (references.size() < ai) {
-          references.add(params[1]);
-        } else {
-          references.add(ai, params[1]);
-        }
-        break;
-      case SerializationUtil.REMOVE:
-        references.remove(params[0]);
-        break;
-      case SerializationUtil.REMOVE_AT:
-        int index = ((Integer) params[0]).intValue();
-        if (references.size() > index) {
-          references.remove(index);
-        }
-        break;
-      case SerializationUtil.REMOVE_RANGE: {
-        int size = references.size();
-        int fromIndex = ((Integer) params[0]).intValue();
-        int toIndex = ((Integer) params[1]).intValue();
-        int removeIndex = fromIndex;
-        if (size > fromIndex && size >= toIndex) {
-          while (fromIndex++ < toIndex) {
-            references.remove(removeIndex);
+      switch (method) {
+        case SerializationUtil.ADD:
+        case SerializationUtil.ADD_LAST:
+          addChangeToCollector(objectID, params[0], includeIDs);
+          references.add(params[0]);
+          break;
+        case SerializationUtil.ADD_FIRST:
+          addChangeToCollector(objectID, params[0], includeIDs);
+          references.add(0, params[0]);
+          break;
+        case SerializationUtil.INSERT_AT:
+        case SerializationUtil.ADD_AT:
+          addChangeToCollector(objectID, params[1], includeIDs);
+          int ai = Math.min(((Integer) params[0]).intValue(), references.size());
+          if (references.size() < ai) {
+            references.add(params[1]);
+          } else {
+            references.add(ai, params[1]);
+          }
+          break;
+        case SerializationUtil.REMOVE:
+          references.remove(params[0]);
+          break;
+        case SerializationUtil.REMOVE_AT:
+          int index = ((Integer) params[0]).intValue();
+          if (references.size() > index) {
+            references.remove(index);
+          }
+          break;
+        case SerializationUtil.REMOVE_RANGE: {
+          int size = references.size();
+          int fromIndex = ((Integer) params[0]).intValue();
+          int toIndex = ((Integer) params[1]).intValue();
+          int removeIndex = fromIndex;
+          if (size > fromIndex && size >= toIndex) {
+            while (fromIndex++ < toIndex) {
+              references.remove(removeIndex);
+            }
           }
         }
+          break;
+        case SerializationUtil.CLEAR:
+          references.clear();
+          break;
+        case SerializationUtil.SET_ELEMENT:
+        case SerializationUtil.SET:
+          addChangeToCollector(objectID, params[1], includeIDs);
+          int si = Math.min(((Integer) params[0]).intValue(), references.size());
+          if (references.size() <= si) {
+            references.add(params[1]);
+          } else {
+            references.set(si, params[1]);
+          }
+          break;
+        case SerializationUtil.REMOVE_FIRST:
+          if (references.size() > 0) {
+            references.remove(0);
+          }
+          break;
+        case SerializationUtil.REMOVE_LAST:
+          int size = references.size();
+          if (size > 0) {
+            references.remove(size - 1);
+          }
+          break;
+        case SerializationUtil.SET_SIZE:
+          int setSize = ((Integer) params[0]).intValue();
+          int listSize = references.size();
+
+          if (listSize < setSize) {
+            for (int i = listSize; i < setSize; i++) {
+              references.add(ObjectID.NULL_ID);
+            }
+          } else if (listSize > setSize) {
+            for (int i = listSize; i != setSize; i--) {
+              references.remove(i - 1);
+            }
+          }
+          break;
+        case SerializationUtil.TRIM_TO_SIZE:
+          // do nothing for now
+          break;
+        default:
+          throw new AssertionError("Invalid method:" + method + " state:" + this);
       }
-        break;
-      case SerializationUtil.CLEAR:
-        references.clear();
-        break;
-      case SerializationUtil.SET_ELEMENT:
-      case SerializationUtil.SET:
-        addChangeToCollector(objectID, params[1], includeIDs);
-        int si = Math.min(((Integer) params[0]).intValue(), references.size());
-        if (references.size() <= si) {
-          references.add(params[1]);
-        } else {
-          references.set(si, params[1]);
-        }
-        break;
-      case SerializationUtil.REMOVE_FIRST:
-        if (references.size() > 0) {
-          references.remove(0);
-        }
-        break;
-      case SerializationUtil.REMOVE_LAST:
-        int size = references.size();
-        if (size > 0) {
-          references.remove(size - 1);
-        }
-        break;
-      case SerializationUtil.SET_SIZE:
-        int setSize = ((Integer) params[0]).intValue();
-        int listSize = references.size();
-
-        if (listSize < setSize) {
-          for (int i = listSize; i < setSize; i++) {
-            references.add(ObjectID.NULL_ID);
-          }
-        } else if (listSize > setSize) {
-          for (int i = listSize; i != setSize; i--) {
-            references.remove(i - 1);
-          }
-        }
-        break;
-      case SerializationUtil.TRIM_TO_SIZE:
-        // do nothing for now
-        break;
-      default:
-        throw new AssertionError("Invalid method:" + method + " state:" + this);
     }
   }
 
-  protected void addChangeToCollector(ObjectID objectID, Object newValue, BackReferences includeIDs) {
+  private void addChangeToCollector(ObjectID objectID, Object newValue, BackReferences includeIDs) {
     if (newValue instanceof ObjectID) {
       getListener().changed(objectID, null, (ObjectID) newValue);
       includeIDs.addBackReference((ObjectID) newValue, objectID);
@@ -185,9 +178,9 @@ public class ListManagedObjectState extends LogicalManagedObjectState {
       out.writeObject(i.next());
     }
   }
-
+  
   protected boolean basicEquals(LogicalManagedObjectState o) {
-    ListManagedObjectState mo = (ListManagedObjectState) o;
+    ListManagedObjectState mo = (ListManagedObjectState) o; 
     return references.equals(mo.references);
   }
 
