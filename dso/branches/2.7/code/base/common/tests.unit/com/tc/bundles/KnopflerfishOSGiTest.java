f1a97b6fca88c668966c87efed25e2cdbec18278
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-12-08 21:59:32 +0000

    Set version number to 2.7.2 on 2.7 branch.

--- dso/branches/2.7/code/base/common/tests.unit/com/tc/bundles/KnopflerfishOSGiTest.java
+++ dso/branches/2.7/code/base/common/tests.unit/com/tc/bundles/KnopflerfishOSGiTest.java
@@ -24,7 +24,7 @@ import junit.framework.TestCase;
 
 public class KnopflerfishOSGiTest extends TestCase {
 
-  private static final String PRODUCT_VERSION_DASH_QUALIFIER = "2.7.2-SNAPSHOT";
+  private static final String PRODUCT_VERSION_DASH_QUALIFIER = "2.7.2";
   private static final String PRODUCT_VERSION_DOT_QUALIFIER  = PRODUCT_VERSION_DASH_QUALIFIER.replace('-', '.');
   private KnopflerfishOSGi    osgiRuntime                    = null;
 
