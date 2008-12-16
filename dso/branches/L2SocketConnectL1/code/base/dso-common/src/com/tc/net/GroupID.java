046c5fb89e10676661ed8587520a508d7a098de7
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-12-16 10:19:12 +0000

    merge from trunk

--- dso/branches/L2SocketConnectL1/code/base/dso-common/src/com/tc/net/GroupID.java
+++ dso/branches/L2SocketConnectL1/code/base/dso-common/src/com/tc/net/GroupID.java
@@ -8,8 +8,14 @@ import com.tc.io.TCByteBufferInput;
 import com.tc.io.TCByteBufferOutput;
 
 import java.io.IOException;
+import java.io.Serializable;
 
-public class GroupID implements NodeID {
+/**
+ * In active-active, a GroupID identifies a group of servers with one
+ * active server and some number of passive servers.  An object resides
+ * on a particular group, hence the method ObjectID#getGroupID().
+ */
+public class GroupID implements NodeID, Serializable {
   private static final int    NULL_NUMBER       = -1;
   private static final int    ALL_GROUPS_NUMBER = Integer.MIN_VALUE;
 
