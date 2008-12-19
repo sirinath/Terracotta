c64e446d57c7438957415735f8428f2d67325c87
ssubbiah <ssubbiah@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-12-19 02:47:33 +0000

    Branch for fixing Object lookup problem, 2 phase commit, loose transaction ordering across cluster and world hunger.

---
+++ dso/branches/aa-2pc/code/base/dso-common/src/com/tc/object/msg/CommitTransactionMessage.java
@@ -0,0 +1,20 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object.msg;
+
+import com.tc.bytes.TCByteBuffer;
+import com.tc.net.protocol.tcm.TCMessage;
+import com.tc.object.dna.impl.ObjectStringSerializer;
+import com.tc.object.tx.TransactionBatch;
+
+public interface CommitTransactionMessage extends TCMessage {
+
+  public ObjectStringSerializer getSerializer();
+
+  public void setBatch(TransactionBatch batch, ObjectStringSerializer serializer);
+
+  public TCByteBuffer[] getBatchData();
+
+}
