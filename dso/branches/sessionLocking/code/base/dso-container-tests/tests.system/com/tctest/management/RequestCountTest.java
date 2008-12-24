35dd8e954f1c8a54274128b6a6779341b188c12c
asingh <asingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2008-12-24 09:47:41 +0000

    Revert unwanted commit

--- dso/branches/sessionLocking/code/base/dso-container-tests/tests.system/com/tctest/management/RequestCountTest.java
+++ dso/branches/sessionLocking/code/base/dso-container-tests/tests.system/com/tctest/management/RequestCountTest.java
@@ -8,13 +8,7 @@ import com.tc.logging.TCLogger;
 import com.tc.logging.TCLogging;
 import com.tc.management.JMXConnectorProxy;
 import com.tc.management.TerracottaManagement;
-import com.tc.management.TerracottaManagement.MBeanKeys;
-import com.tc.management.beans.L1MBeanNames;
-import com.tc.management.beans.sessions.SessionStatisticsMBean;
-import com.tc.statistics.StatisticData;
-import com.tc.statistics.beans.StatisticsGatewayMBean;
-import com.tc.statistics.beans.StatisticsMBeanNames;
-import com.tc.statistics.retrieval.actions.SRAHttpSessions;
+import com.tc.management.beans.sessions.SessionMonitorMBean;
 import com.tc.test.server.appserver.deployment.AbstractDeploymentTest;
 import com.tc.test.server.appserver.deployment.Deployment;
 import com.tc.test.server.appserver.deployment.DeploymentBuilder;
@@ -24,14 +18,11 @@ import com.tc.test.server.appserver.load.Node;
 import com.tc.test.server.util.TcConfigBuilder;
 import com.tctest.webapp.servlets.CounterServlet;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.Set;
 
 import javax.management.MBeanServerConnection;
-import javax.management.MBeanServerInvocationHandler;
-import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 
@@ -40,7 +31,6 @@ import junit.framework.Test;
 public final class RequestCountTest extends AbstractDeploymentTest {
   private static final TCLogger logger             = TCLogging.getTestingLogger(RequestCountTest.class);
 
-  private static final int      NODE_COUNT         = 2;
   private static final int      SESSIONS_PER_NODE  = 10;
   private static final long     TEST_DURATION      = 60 * 1000;
   private static final String   CLIENT_NAME_PREFIX = "client-";
@@ -70,7 +60,7 @@ public final class RequestCountTest extends AbstractDeploymentTest {
 
   public void testRequestCount() throws Throwable {
     assertTimeDirection();
-    runNodes(NODE_COUNT);
+    runNodes(2);
   }
 
   private Deployment makeDeployment() throws Exception {
@@ -93,33 +83,23 @@ public final class RequestCountTest extends AbstractDeploymentTest {
     return url;
   }
 
-  private static StatisticsGatewayMBean getStatisticGatewayMBean(MBeanServerConnection mbs) {
-    final long timeout = 60 * 1000;
-    final long start = System.currentTimeMillis();
-    final long beanCutoffTime = start + timeout;
+  private void runNodes(int nodeCount) throws Throwable {
+    Thread[] nodeRunners = new Thread[nodeCount];
+    NodeWithJMX[] nodes = new NodeWithJMX[nodeCount];
+    int[] ports = new int[nodeCount];
 
-    StatisticsGatewayMBean statsGateway;
-    while((statsGateway = (StatisticsGatewayMBean)MBeanServerInvocationHandler
-        .newProxyInstance(mbs, StatisticsMBeanNames.STATISTICS_GATEWAY, StatisticsGatewayMBean.class, false)) == null) {
-      logger.info("Statistics Gateway bean not found yet...");
-      if (System.currentTimeMillis() > beanCutoffTime) {
-        final String errMsg = "Unable to find Statistics Gateway MBean within " + timeout + "ms";
-        logger.error(errMsg);
-        fail(errMsg);        
-      }
+    for (int i = 0; i < nodeCount; i++) {
+      ports[i] = createAndStartServer("-Dtc.node-name=" + CLIENT_NAME_PREFIX + i);
     }
 
-    return statsGateway;
-  }
-  
-  private static SessionStatisticsMBean[] getSessionStatisticsMBeans(MBeanServerConnection mbs, int nodeCount) throws IOException, MalformedObjectNameException, NullPointerException {
+    final JMXConnector jmxConnector = getJMXConnector();
+    final MBeanServerConnection mbs = jmxConnector.getMBeanServerConnection();
     final long timeout = 60 * 1000;
     final long start = System.currentTimeMillis();
     final long beanCutoffTime = start + timeout;
-    final ObjectName beanName = new ObjectName(L1MBeanNames.HTTP_SESSIONS_PUBLIC.getCanonicalName() + ",*");
     Set beanSet = null;
-    for (beanSet = mbs.queryNames(beanName, null); beanSet.size() != nodeCount;
-         beanSet = mbs.queryNames(beanName, null)) {
+    for (beanSet = TerracottaManagement.getAllSessionMonitorMBeans(mbs); beanSet.size() != nodeCount; beanSet = TerracottaManagement
+        .getAllSessionMonitorMBeans(mbs)) {
       if (!beanSet.isEmpty()) {
         logger.info("Found some session beans but not all:");
         for (final Iterator pos = beanSet.iterator(); pos.hasNext();) {
@@ -146,9 +126,10 @@ public final class RequestCountTest extends AbstractDeploymentTest {
     for (final Iterator pos = beanSet.iterator(); pos.hasNext();) {
       logger.info("Session bean[" + ((ObjectName) pos.next()).getCanonicalName() + "]");
     }
-    final SessionStatisticsMBean[] mbeans = new SessionStatisticsMBean[nodeCount];
+    final SessionMonitorMBean[] mbeans = new SessionMonitorMBean[nodeCount];
     for (int i = 0; i < nodeCount; i++) {
-      SessionStatisticsMBean clientMBean = getClientSessionStatisticsMBean(mbs, beanSet, CLIENT_NAME_PREFIX + i);
+      SessionMonitorMBean clientMBean = TerracottaManagement.getClientSessionMonitorMBean(mbs, beanSet,
+                                                                                          CLIENT_NAME_PREFIX + i);
       if (clientMBean == null) {
         final String errMsg = "We thought we had all of our DSO client MBeans, turns out we don't have one for client["
                               + CLIENT_NAME_PREFIX + i + "]";
@@ -157,37 +138,7 @@ public final class RequestCountTest extends AbstractDeploymentTest {
       }
       mbeans[i] = clientMBean;
     }
-    
-    return mbeans;
-  }
-  
-  private static final SessionStatisticsMBean getClientSessionStatisticsMBean(MBeanServerConnection mbs,
-                                                                              Set sessionStatisticsMBeans, String nodeName)
-  throws IOException {
-    for (Iterator iter = sessionStatisticsMBeans.iterator(); iter.hasNext();) {
-      ObjectName smObjectName = (ObjectName) iter.next();
-      if (nodeName.equals(smObjectName.getKeyProperty(MBeanKeys.MBEAN_NODE_NAME))) {
-        return (SessionStatisticsMBean) TerracottaManagement.findMBean(smObjectName, SessionStatisticsMBean.class, mbs);
-      }
-    }
-    throw new AssertionError("No SessionStatisticsMBean found for " + nodeName);
-  }
 
-  private void runNodes(int nodeCount) throws Throwable {
-    Thread[] nodeRunners = new Thread[nodeCount];
-    NodeWithJMX[] nodes = new NodeWithJMX[nodeCount];
-    int[] ports = new int[nodeCount];
-
-    for (int i = 0; i < nodeCount; i++) {
-      ports[i] = createAndStartServer("-Dtc.node-name=" + CLIENT_NAME_PREFIX + i);
-    }
-
-    final JMXConnector jmxConnector = getJMXConnector();
-    final MBeanServerConnection mbs = jmxConnector.getMBeanServerConnection();
-
-    StatisticsGatewayMBean statsGateway = getStatisticGatewayMBean(mbs);
-    SessionStatisticsMBean[] sessionMBeans = getSessionStatisticsMBeans(mbs, nodeCount);
-        
     for (int i = 0; i < nodeCount; i++) {
       URL mutateUrl = createUrl(ports[i], "");
       URL validateUrl = createUrl(ports[(i + 1) % nodeCount], "read=true");
@@ -208,11 +159,7 @@ public final class RequestCountTest extends AbstractDeploymentTest {
     }
 
     for (int i = 0; i < nodeCount; i++) {
-      nodes[i].validateMBean(sessionMBeans[i]);
-    }
-    
-    for (int i = 0; i < nodeCount; i++) {
-      nodes[i].validateSRA(statsGateway, i);
+      nodes[i].validateMBean(mbeans[i]);
     }
 
     jmxConnector.close();
@@ -229,49 +176,18 @@ public final class RequestCountTest extends AbstractDeploymentTest {
       super(mutateUrl, validateUrl, numSessions, duration);
     }
 
-    public void validateMBean(SessionStatisticsMBean sessionStatisticsMBean) {
+    public void validateMBean(SessionMonitorMBean sessionMonitorMBean) {
       int totalRequests = 0;
       for (int i = 0; i < numRequests.length; i++) {
         totalRequests += numRequests[i];
       }
 
-      assertEquals(totalRequests, sessionStatisticsMBean.getRequestCount());
+      assertEquals(totalRequests, sessionMonitorMBean.getRequestCount());
 
       assertEquals(1, validateUrls.length);
       final URL validateUrl = validateUrls[0];
 
-      System.err.println("MBean : validated value of " + totalRequests + " for client " + validateUrl);
-    }
-    
-    public void validateSRA(StatisticsGatewayMBean statsGateway, int node) {
-
-      StatisticData[] data = statsGateway.retrieveStatisticData(SRAHttpSessions.ACTION_NAME);
-      
-      assertNotNull("StatisticsData array for HTTP Sessions SRA", data);
-
-      String agentDifferentiator = "L1/" + node;
-      
-      int totalRequests = 0;
-      for (int i = 0; i < numRequests.length; i++) {
-        totalRequests += numRequests[i];
-      }
-
-      int sraRequestCount = -1;
-      for (int i = 0; i < data.length; i++) {
-        if (agentDifferentiator.equals(data[i].getAgentDifferentiator()) && SRAHttpSessions.REQUEST.equals(data[i].getName())) {
-          sraRequestCount = ((Long) data[i].getData()).intValue();
-          break;
-        }
-      }
-
-      assertTrue("Request count statistic not found", sraRequestCount >= 0);
-
-      assertEquals("Request count reported by SRA is incorrect", totalRequests, sraRequestCount);
-      
-      assertEquals(1, validateUrls.length);
-      final URL validateUrl = validateUrls[0];
-
-      System.err.println("SRA   : validated value of " + totalRequests + " for client " + validateUrl);
+      System.err.println("validated value of " + totalRequests + " for client " + validateUrl);
     }
   }
 }
