65bc53f9127a8b51f0b05bc9fa75cf9f549a4261
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-01-26 22:51:23 +0000

    main tree branch for session2forge

---
+++ dso/branches/session2forge/code/base/dso-l1-session/src/com/terracotta/session/util/ContextMgr.java
@@ -0,0 +1,19 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.terracotta.session.util;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSessionContext;
+
+public interface ContextMgr {
+  String getAppName();
+
+  HttpSessionContext getSessionContext();
+
+  ServletContext getServletContext();
+
+  String getHostName();
+
+}
