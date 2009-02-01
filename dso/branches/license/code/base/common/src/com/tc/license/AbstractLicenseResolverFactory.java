11e0820d8aa20efa118e686ced465f8d6bb1b986
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-02-01 19:45:57 +0000

    refactored

--- dso/branches/license/code/base/common/src/com/tc/license/AbstractLicenseResolverFactory.java
+++ dso/branches/license/code/base/common/src/com/tc/license/AbstractLicenseResolverFactory.java
@@ -7,8 +7,10 @@ package com.tc.license;
 import com.tc.util.factory.AbstractFactory;
 
 public abstract class AbstractLicenseResolverFactory extends AbstractFactory {
-  private static String FACTORY_SERVICE_ID                = "com.tc.license.LicenseResolverFactory";
-  private static Class  OPENSOURCE_RESOLVER_FACTORY_CLASS = OpenSourceLicenseResolverFactory.class;
+  private static String  FACTORY_SERVICE_ID                = "com.tc.license.LicenseResolverFactory";
+  private static Class   OPENSOURCE_RESOLVER_FACTORY_CLASS = OpenSourceLicenseResolverFactory.class;
+
+  private static License license;
 
   public static AbstractLicenseResolverFactory getFactory() {
     return (AbstractLicenseResolverFactory) getFactory(FACTORY_SERVICE_ID, OPENSOURCE_RESOLVER_FACTORY_CLASS);
@@ -16,5 +18,14 @@ public abstract class AbstractLicenseResolverFactory extends AbstractFactory {
 
   public abstract License resolveLicense();
 
-  public abstract Capabilities resolveCapabilities();
+  public static synchronized License getLicense() {
+    if (license == null) {
+      license = getFactory().resolveLicense();
+    }
+    return license;
+  }
+
+  public static Capabilities getCapabilities() {
+    return getLicense().capabilities();
+  }
 }
