c0e58fcce6204fff5ddccca91bb73d70b1d285d6
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-02-02 22:09:35 +0000

    tag OS release 2.7.3 from 2.7 at r11424

---
+++ dso/tags/2.7.3/code/base/ui-eclipse/src/org/terracotta/dso/ModuleInfo.java
@@ -0,0 +1,60 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package org.terracotta.dso;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+
+import com.terracottatech.config.DsoApplication;
+import com.terracottatech.config.Module;
+
+import java.io.File;
+
+public class ModuleInfo {
+  private Module fModule;
+  private File fLocation;
+  private Bundle fBundle;
+  private BundleException fBundleException;
+  private DsoApplication fApplication;
+  
+  public ModuleInfo(Module module) {
+    fModule = module;
+  }
+
+  public Module getModule() {
+    return fModule;
+  }
+  
+  public void setLocation(File location) {
+    fLocation = location;
+  }
+  
+  public File getLocation() {
+    return fLocation;
+  }
+  
+  public void setBundle(Bundle bundle) {
+    fBundle = bundle;
+  }
+  
+  public Bundle getBundle() {
+    return fBundle;
+  }
+  
+  public void setError(BundleException error) {
+    fBundleException = error;
+  }
+  
+  public BundleException getError() {
+    return fBundleException;
+  }
+  
+  public void setApplication(DsoApplication application) {
+    fApplication = application;
+  }
+  
+  public DsoApplication getApplication() {
+    return fApplication;
+  }
+}
