80f0ccc0aadd49260b1cddefe59e3b105d1ff6e9
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-02-13 05:33:15 +0000

    oops, removed comment character on first line

--- dso/branches/session2forge/buildsystems/maven/jruby-tcbuild-lib/src/main/resources/buildscripts/modules_test.rb
+++ dso/branches/session2forge/buildsystems/maven/jruby-tcbuild-lib/src/main/resources/buildscripts/modules_test.rb
@@ -1,4 +1,4 @@
- content copyright (c) 2003-2006 Terracotta, Inc.,
+# content copyright (c) 2003-2006 Terracotta, Inc.,
 # except as may otherwise be noted in a separate copyright notice.
 # All rights reserved
 #
