ee80950e162f4a43df3618caacbff08954cc06bf
etsai <etsai@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-03-11 02:57:37 +0000

    MNK-974, adapted from trunk 12071

--- dso/branches/2.7/code/base/dso-system-tests/tests.system/com/tctest/objectserver/CreateRescueCandidatesYoungGCTest.java
+++ dso/branches/2.7/code/base/dso-system-tests/tests.system/com/tctest/objectserver/CreateRescueCandidatesYoungGCTest.java
@@ -10,24 +10,44 @@ import com.tc.test.activepassive.ActivePassiveSharedDataMode;
 import com.tc.test.activepassive.ActivePassiveTestSetupManager;
 import com.tctest.YoungGCTestBase;
 
-
+import java.util.ArrayList;
 
 public class CreateRescueCandidatesYoungGCTest extends YoungGCTestBase {
 
- 
+  private final long LOW_FREE_MEMORY    = 20 * 1024 * 1024;
+  private final long MIDDLE_FREE_MEMORY = 40 * 1024 * 1024;
+  private final int  LOW_APP_NODES      = 1;
+  private final int  MIDDLE_APP_NODES   = 2;
+  private final int  HIGH_APP_NODES     = 3;
+
   public CreateRescueCandidatesYoungGCTest() {
-   //
+    //
   }
-  
+
+  protected int getNodeCount() {
+    // set client instance according to available free memory
+    long freemem = Runtime.getRuntime().freeMemory();
+    int app_count;
+    if (freemem < LOW_FREE_MEMORY) {
+      app_count = LOW_APP_NODES;
+    } else if (freemem < MIDDLE_FREE_MEMORY) {
+      app_count = MIDDLE_APP_NODES;
+    } else {
+      app_count = HIGH_APP_NODES;
+    }
+    return app_count;
+  }
+
   protected boolean canRunActivePassive() {
     return true;
   }
-  
-  // start only 1 L1
-  protected int getNodeCount() {
-    return 4;
+
+  protected void setExtraJvmArgs(final ArrayList jvmArgs) {
+    super.setExtraJvmArgs(jvmArgs);
+    jvmArgs.add("-verbose:gc");
+    jvmArgs.add("-XX:+PrintGCTimeStamps");
   }
-  
+
   public void setupActivePassiveTest(ActivePassiveTestSetupManager setupManager) {
     setupManager.setServerCount(2);
     setupManager.setServerCrashMode(ActivePassiveCrashMode.CONTINUOUS_ACTIVE_CRASH);
@@ -43,6 +63,5 @@ public class CreateRescueCandidatesYoungGCTest extends YoungGCTestBase {
   protected int getGarbageCollectionInterval() {
     return 180;
   }
-  
-  
+
 }
