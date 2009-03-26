c9b5292462af32f6e5a3f26c000559f3984047bc
npurwar <npurwar@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-03-26 09:54:58 +0000

    creating a new branch for removing dso-tests-jdk15

---
+++ dso/branches/jdk-15-tests/code/base/buildscripts/utils.rb
@@ -0,0 +1,137 @@
+#
+# All content copyright (c) 2003-2008 Terracotta, Inc.,
+# except as may otherwise be noted in a separate copyright notice.
+# All rights reserved
+#
+require 'rexml/document'
+require 'rexml/xpath'
+
+unless defined?(JavaSystem)
+  include_class('java.lang.System') { |p, name| "Java" + name }
+end
+
+unless defined?(JavaFile)
+  include_class('java.io.File') { |p, name| "Java" + name }
+end
+
+# A basic assertion facility for Ruby. Takes a block, and fails if
+# the block returns false.
+def assert(message="Assertion Failed")
+  raise RuntimeError, message unless yield
+end
+
+# Prints a "loud", very visible message to the console.  This is used for
+# warning messages that, even while the scroll by quickly on the console,
+# should call out for the user's attention.
+def loud_message(message)
+  banner_char = "*"
+  puts(banner_char * 80)
+  puts(banner_char)
+  puts("#{banner_char} #{message}")
+  puts(banner_char)
+  puts(banner_char * 80)
+end
+
+def time
+  start = Time.now
+  yield
+  Time.now - start
+end
+
+def get_version(file, xpath)
+  version = nil
+  File.open(file) do |f|
+    doc = REXML::Document.new(f)
+    version = REXML::XPath.first(doc, xpath).text 
+  end
+  version
+end
+
+def compare_maven_version(maven_version, pom, xpath)
+  version = get_version(pom, xpath)
+  printf("%-50s: %s\n", pom, version)
+  if version != maven_version
+    raise "version #{version} in #{pom} " +
+      "doesn't match maven.version #{maven_version}"
+  end
+end
+
+def delete_deep_folder(folder)
+  target = File.expand_path(folder)
+
+  if File.file?(target)
+    File.delete(target)
+    return
+  end
+
+  queue = []
+  queue << target
+
+  while (! queue.empty?)  
+    dir = queue.delete_at(0)
+  
+    Dir.chdir(dir) do 
+      Dir.glob("*") do | f |
+        num = 0
+        next if (File.file?(f))
+      
+        begin
+          File.rename(f, num.to_s)				
+        rescue
+          num += 1				
+          retry                
+        end
+      
+        newDir = File.expand_path(num.to_s)
+        queue << newDir
+        num += 1  
+      end
+    end
+
+  end
+
+  FileUtils.rm_rf target
+end
+
+def load_yaml_erb(file, _binding)
+  YAML.load(ERB.new(File.read(file), 0, "%<>").result(_binding))
+end
+
+class Hash
+  def boolean(key, default = false)
+    if has_key?(key)
+      value = self[key]
+      if value.is_a?(String)
+        case value.downcase.strip
+        when /^true$/: true
+        when /^false$/: false
+        when "": default
+        else
+          raise("Cannot convert value for key '#{key}' to boolean: #{value}")
+        end
+      else
+        value ? true : false
+      end
+    else
+      default
+    end
+  end
+end
+
+module CallWithVariableArguments
+  # A method to call a procedure that may take variable arguments, but
+  # which issues much nicer error messages when something fails than
+  # the default messages issued by JRuby.
+  def call_proc_variable(proc, args)
+    if proc.arity == 0
+      proc.call
+    elsif proc.arity > 0
+      raise RuntimeError, "Insufficient arguments: proc takes %d, but we only have %d." % [ proc.arity, args.length ] if proc.arity > args.length
+      proc.call(*(args[0..(proc.arity - 1)]))
+    else
+      required = proc.arity.abs - 1
+      raise RuntimeError, "Insufficient arguments: proc takes %d, but we only have %d." % [ required, args.length ] if required > args.length
+      proc.call(*args)
+    end
+  end
+end
---
+++ dso/branches/jdk-15-tests/code/base/dso-l1-api/src/com/tc/object/bytecode/hook/DSOContext.java
@@ -0,0 +1,74 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object.bytecode.hook;
+
+import com.tc.aspectwerkz.reflect.ClassInfo;
+import com.tc.object.bytecode.Manager;
+
+import java.net.URL;
+import java.util.Collection;
+
+/**
+ * The idea behind DSOContext is to encapsulate a DSO "world" in a client VM. But this idea has not been fully realized.
+ */
+public interface DSOContext extends ClassProcessor {
+
+  public static final String CLASS = "com/tc/object/bytecode/hook/DSOContext";
+  public static final String TYPE  = "L" + CLASS + ";";
+
+  /**
+   * @return The Manager instance
+   */
+  public Manager getManager();
+
+  /**
+   * The DSOSpringConfigHelpers in the DSO
+   *
+   * @return Collection of DSOSpringConfigHelper
+   */
+  public Collection getDSOSpringConfigHelpers();
+
+  /**
+   * Add include and lock
+   *
+   * @param expression Class expression
+   * @param callConstructorOnLoad True to call constructor on load
+   * @param lockExpression Lock expression
+   * @param classInfo Class information
+   */
+  public void addInclude(String expression, boolean callConstructorOnLoad, String lockExpression, ClassInfo classInfo);
+
+  /**
+   * Add transient field
+   *
+   * @param beanClassName Bean class name
+   * @param fieldName
+   */
+  public void addTransient(String beanClassName, String fieldName);
+
+  /**
+   * Get type of locks used by sessions
+   *
+   * @param appName Web app anem
+   * @return Lock type
+   */
+  public int getSessionLockType(String appName);
+
+  /**
+   * Get url to class file
+   *
+   * @param className Class name
+   * @param loader the calling classloader
+   * @param hideSystemResources true if resources destined only for the system class loader should be hidden
+   * @return URL to class itself
+   */
+  public URL getClassResource(String className, ClassLoader loader, boolean hideSystemResources);
+
+  /**
+   * Returns true is web-application is configured for session-locking
+   */
+  public boolean isApplicationSessionLocked(String appName);
+
+}
