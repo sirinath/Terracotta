55ab1e51c5c5748bf05ccadf33af8f01659c5954
rsingh <rsingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-04-20 08:26:23 +0000

    reducing number of loops for MNK-1095

--- dso/branches/3.0/code/base/dso-system-tests/tests.base/com/tctest/restart/system/ObjectDataTestApp.java
+++ dso/branches/3.0/code/base/dso-system-tests/tests.base/com/tctest/restart/system/ObjectDataTestApp.java
@@ -33,8 +33,8 @@ public class ObjectDataTestApp extends AbstractTransparentApp {
   public static final String SYNCHRONOUS_WRITE = "synch-write";
 
   private final int                threadCount       = 10;
-  private final int                workSize          = 1 * 100;
-  private final int                testObjectDepth   = 1 * 50;
+  private final int                workSize          = 1 * 50;
+  private final int                testObjectDepth   = 1 * 25;
   // Beware when tuning down the iteration count, it might not run long enough to actually do a useful crash test
   private final int                iterationCount    = 10;
   private final List               workQueue         = new ArrayList();
