7a5c1ae499e3b597863df8aab38d8e108162218d
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-05-01 17:34:14 +0000

    OSS nospring

---
+++ dso/branches/nospring/code/base/dso-l2/src/com/tc/objectserver/lockmanager/impl/ServerThreadContextFactory.java
@@ -0,0 +1,18 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.objectserver.lockmanager.impl;
+
+import com.tc.net.NodeID;
+import com.tc.object.lockmanager.api.ServerThreadID;
+import com.tc.object.lockmanager.api.ThreadID;
+
+class ServerThreadContextFactory {
+  public static final ServerThreadContextFactory DEFAULT_FACTORY       = new ServerThreadContextFactory();
+  public static final String                     ACTIVITY_MONITOR_NAME = "ServerThreadContextFactoryActivityMonitor";
+
+  ServerThreadContext getOrCreate(NodeID nid, ThreadID threadID) {
+    return new ServerThreadContext(new ServerThreadID(nid, threadID));
+  }
+}
