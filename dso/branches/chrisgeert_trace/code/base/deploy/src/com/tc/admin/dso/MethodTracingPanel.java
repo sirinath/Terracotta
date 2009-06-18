9635fb25f4ebd5ba1c4b705e79f45cebf63f3b9f
cdennis <cdennis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-06-18 03:45:37 +0000

    added a skeleton gui to the admin console

---
+++ dso/branches/chrisgeert_trace/code/base/deploy/src/com/tc/admin/dso/MethodTracingPanel.java
@@ -0,0 +1,211 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.admin.dso;
+
+import com.tc.admin.AbstractClusterListener;
+import com.tc.admin.ClusterElementChooser;
+import com.tc.admin.IAdminClientContext;
+import com.tc.admin.common.PagedView;
+import com.tc.admin.common.XContainer;
+import com.tc.admin.common.XLabel;
+import com.tc.admin.common.XTreeNode;
+import com.tc.admin.model.ClientConnectionListener;
+import com.tc.admin.model.IClient;
+import com.tc.admin.model.IClusterModel;
+import com.tc.admin.model.IServer;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.SwingUtilities;
+import javax.swing.tree.TreePath;
+
+public class MethodTracingPanel extends XContainer implements ActionListener, ClientConnectionListener,
+    PropertyChangeListener {
+
+  private IAdminClientContext adminClientContext;
+  private IClusterModel       clusterModel;
+  private ClusterListener     clusterListener;
+  private XLabel              currentViewLabel;
+  private ElementChooser      elementChooser;
+  private PagedView           pagedView;
+  private boolean             inited;
+
+  public MethodTracingPanel(IAdminClientContext adminClientContext, IClusterModel clusterModel) {
+    super(new BorderLayout());
+
+    this.adminClientContext = adminClientContext;
+    this.clusterModel = clusterModel;
+
+    add(pagedView = new PagedView(), BorderLayout.CENTER);
+
+    XContainer topPanel = new XContainer(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.gridx = gbc.gridy = 0;
+    gbc.insets = new Insets(3, 3, 3, 3);
+    gbc.anchor = GridBagConstraints.EAST;
+
+    Font headerFont = (Font) adminClientContext.getObject("header.label.font");
+    XLabel headerLabel = new XLabel(adminClientContext.getString("current.view.type"));
+    topPanel.add(headerLabel, gbc);
+    headerLabel.setFont(headerFont);
+    gbc.gridx++;
+
+    topPanel.add(currentViewLabel = new XLabel(), gbc);
+    gbc.gridx++;
+
+    // filler
+    gbc.weightx = 1.0;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    topPanel.add(new XLabel(), gbc);
+    gbc.gridx++;
+
+    gbc.weightx = 0.0;
+    gbc.fill = GridBagConstraints.NONE;
+
+    headerLabel = new XLabel(adminClientContext.getString("select.view"));
+    topPanel.add(headerLabel, gbc);
+    headerLabel.setFont(headerFont);
+    gbc.gridx++;
+
+    topPanel.add(elementChooser = new ElementChooser(), gbc);
+    elementChooser.addActionListener(this);
+
+    topPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
+    add(topPanel, BorderLayout.NORTH);
+
+    clusterModel.addPropertyChangeListener(clusterListener = new ClusterListener(clusterModel));
+    if (clusterModel.isConnected()) {
+      addNodePanels();
+    }
+  }
+
+  private void addNodePanels() {
+    pagedView.removeAll();
+    pagedView.addPropertyChangeListener(this);
+    inited = true;
+  }
+
+  private class ElementChooser extends ClusterElementChooser {
+    ElementChooser() {
+      super(clusterModel, MethodTracingPanel.this);
+    }
+
+    @Override
+    protected XTreeNode[] createTopLevelNodes() {
+      ClientsNode clientsNode = new ClientsNode(adminClientContext, clusterModel) {
+        @Override
+        protected void updateLabel() {/**/
+        }
+      };
+      clientsNode.setLabel(adminClientContext.getString("method.tracing.per.client.view"));
+      return new XTreeNode[] { clientsNode };
+    }
+
+    @Override
+    protected boolean acceptPath(TreePath path) {
+      Object o = path.getLastPathComponent();
+      if (o instanceof XTreeNode) {
+        XTreeNode node = (XTreeNode) o;
+        return node instanceof ClientNode;
+      }
+      return false;
+    }
+  }
+
+  public void actionPerformed(ActionEvent e) {
+    ElementChooser chsr = (ElementChooser) e.getSource();
+    XTreeNode node = (XTreeNode) chsr.getSelectedObject();
+    String name = node.getName();
+    if (pagedView.hasPage(name)) {
+      pagedView.setPage(name);
+    }
+    TreePath path = elementChooser.getSelectedPath();
+    Object type = path.getPathComponent(1);
+    currentViewLabel.setText(type.toString());
+  }
+
+  private class ClusterListener extends AbstractClusterListener {
+    private ClusterListener(IClusterModel clusterModel) {
+      super(clusterModel);
+    }
+
+    @Override
+    protected void handleConnected() {
+      if (!inited && clusterModel.isConnected()) {
+        addNodePanels();
+      }
+    }
+
+    @Override
+    protected void handleActiveCoordinator(IServer oldActive, IServer newActive) {
+      if (oldActive != null) {
+        oldActive.removeClientConnectionListener(MethodTracingPanel.this);
+      }
+      if (newActive != null) {
+        newActive.removeClientConnectionListener(MethodTracingPanel.this);
+      }
+    }
+  }
+
+  public void clientConnected(final IClient client) {
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        pagedView.addPage(createClientViewPanel(client));
+      }
+    });
+  }
+
+  protected Component createClientViewPanel(IClient client) {
+    ClientMethodTracingPanel panel = new ClientMethodTracingPanel(client);
+    panel.setName(client.toString());
+    return panel;
+  }
+
+  public void clientDisconnected(final IClient client) {
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        pagedView.remove(pagedView.getPage(client.toString()));
+      }
+    });
+  }
+
+  public void propertyChange(PropertyChangeEvent evt) {
+    String prop = evt.getPropertyName();
+    if (PagedView.PROP_CURRENT_PAGE.equals(prop)) {
+      String newPage = (String) evt.getNewValue();
+      elementChooser.setSelectedPath(newPage);
+    }
+  }
+
+  @Override
+  public void tearDown() {
+    clusterModel.removePropertyChangeListener(clusterListener);
+    clusterListener.tearDown();
+
+    pagedView.removePropertyChangeListener(this);
+    elementChooser.removeActionListener(this);
+
+    synchronized (this) {
+      adminClientContext = null;
+      clusterModel = null;
+      clusterListener = null;
+      elementChooser = null;
+      pagedView = null;
+      currentViewLabel = null;
+    }
+
+    super.tearDown();
+  }
+}
