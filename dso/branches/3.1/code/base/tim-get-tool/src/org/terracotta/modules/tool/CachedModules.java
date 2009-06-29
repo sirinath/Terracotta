02fd4a229a31236ad18cb0b5dd5b51d82db7d351
asingh <asingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-06-29 13:16:02 +0000

    CDV-1303: merge from trunk

--- dso/branches/3.1/code/base/tim-get-tool/src/org/terracotta/modules/tool/CachedModules.java
+++ dso/branches/3.1/code/base/tim-get-tool/src/org/terracotta/modules/tool/CachedModules.java
@@ -147,9 +147,11 @@ public class CachedModules implements Modules {
           throw new RuntimeException("Error parsing index file: " + e.getMessage(), e);
         }
       } catch (FileNotFoundException e) {
-        throw new RemoteIndexIOException("Remote index file not found: " + e.getMessage(), e);
+        throw new RemoteIndexIOException("Remote index file not found: " + e.getMessage(), e, dataLoader
+            .getLocalDataFile(), dataLoader.getRemoteDataUrl());
       } catch (IOException e) {
-        throw new RemoteIndexIOException("Error reading remote index file: " + e.getMessage(), e);
+        throw new RemoteIndexIOException("Error reading remote index file: " + e.getMessage(), e, dataLoader
+            .getLocalDataFile(), dataLoader.getRemoteDataUrl());
       } finally {
         IOUtils.closeQuietly(dataStream);
       }
@@ -296,7 +298,7 @@ public class CachedModules implements Modules {
   public String apiVersion() {
     return apiVersion;
   }
-  
+
   public String indexTimeStamp() {
     return timeStamp;
   }
