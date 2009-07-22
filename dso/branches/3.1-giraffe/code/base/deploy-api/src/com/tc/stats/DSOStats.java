ee6902d539f4382902ec0fd1b0bbac578c6ccafe
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-07-22 16:09:21 +0000

    create tc-branch for performance testing (authinator)

---
+++ dso/branches/3.1-giraffe/code/base/deploy-api/src/com/tc/stats/DSOStats.java
@@ -0,0 +1,27 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.stats;
+
+public interface DSOStats {
+
+  Number[] getStatistics(String[] names);
+
+  double getCacheHitRatio();
+
+  long getCacheMissRate();
+
+  long getTransactionRate();
+
+  long getObjectFaultRate();
+
+  long getObjectFlushRate();
+
+  long getGlobalLockRecallRate();
+
+  long getTransactionSizeRate();
+
+  long getBroadcastRate();
+
+}
