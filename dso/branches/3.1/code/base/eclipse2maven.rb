29b748f09fe1960d1f741e258fdfbbe3275054fe
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-08-21 23:07:52 +0000

    3.1.0 to 3.1.1-SNAPSHOT for next release

--- dso/branches/3.1/code/base/eclipse2maven.rb
+++ dso/branches/3.1/code/base/eclipse2maven.rb
@@ -21,7 +21,7 @@ POM_DEF = <<POM
   <parent>
     <groupId>org.terracotta</groupId>
     <artifactId>parent</artifactId>
-    <version>3.1.0</version>
+    <version>3.1.1-SNAPSHOT</version>
   </parent>
   <artifactId>${artifactId}</artifactId>
   ${dependencies}
