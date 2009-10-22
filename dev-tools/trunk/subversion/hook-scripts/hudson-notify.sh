c85d90767262b7ccdb057309955bc3401c2bf626
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-10-22 19:34:55 +0000

    Added hudson-notify.sh hook script for notifying Hudson of changes.

---
+++ dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
@@ -0,0 +1,12 @@
+# Notify Hudson of changes
+
+REPOS="$1"
+REV="$2"
+HUDSON_MASTER_HOST=su10vmo3
+
+UUID=`svnlook uuid $REPOS`
+/usr/bin/wget \
+  --header "Content-Type:text/plain;charset=UTF-8" \
+  --post-data "`svnlook changed --revision $REV $REPOS`" \
+  --output-document "-" \
+  http://$HUDSON_MASTER_HOST/hudson/subversion/${UUID}/notifyCommit?rev=$REV
