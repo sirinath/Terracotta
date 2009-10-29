9479f41c2e228c201f75c0cbcfc492402eb1242c
cdennis <cdennis@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-10-29 15:52:06 +0000

    feature branch : clear out old classes, mucho refactoring

--- dso/branches/santiago_lock_rewrite/code/base/dso-l2/tests.base/com/tc/objectserver/locks/NullChannelManager.java
+++ dso/branches/santiago_lock_rewrite/code/base/dso-l2/tests.base/com/tc/objectserver/locks/NullChannelManager.java
@@ -2,7 +2,7 @@
  * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
  * notice. All rights reserved.
  */
-package com.tc.objectserver.lockmanager.api;
+package com.tc.objectserver.locks;
 
 import com.tc.net.ClientID;
 import com.tc.net.NodeID;
