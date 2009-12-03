fdaf55f741eaaa4b8ba7d62c07591927dc5afec6
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-12-03 01:20:50 +0000

    set relative path

--- dso/branches/3.2/deploy-artifacts.sh
+++ dso/branches/3.2/deploy-artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd code/base
+root=`dirname $0`
+cd $root/code/base
 
 ./tcbuild clean deploy_artifacts api_dir=/shares/monkeyoutput/api maven.repo=file:///shares/maven2 maven.repositoryId=kong maven.snapshot=true 
 
