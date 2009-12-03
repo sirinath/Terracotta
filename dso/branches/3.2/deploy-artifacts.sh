2f658ee20aa41f5bc82fb277f4f3f810eb72c923
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-12-03 02:17:31 +0000

    not needed

--- dso/branches/3.2/deploy-artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-root=`dirname $0`
-cd $root/code/base
-
-./tcbuild clean deploy_artifacts api_dir=/shares/monkeyoutput/api maven.repo=file:///shares/maven2 maven.repositoryId=kong maven.snapshot=true 
-
