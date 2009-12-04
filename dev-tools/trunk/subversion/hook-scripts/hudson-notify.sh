f43485c0f065d44f6a42ae12e021bae9032b6eb4
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-12-04 16:26:02 +0000

    Disable Hudson notification.

--- dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
+++ dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
@@ -5,8 +5,9 @@ REV="$2"
 HUDSON_MASTER_HOST="rh5fm0.terracotta.lan:9000"
 
 UUID=`svnlook uuid $REPOS`
-/usr/bin/wget \
-  --header "Content-Type:text/plain;charset=UTF-8" \
-  --post-data "`svnlook changed --revision $REV $REPOS`" \
-  --output-document "-" \
-  http://$HUDSON_MASTER_HOST/hudson/subversion/${UUID}/notifyCommit?rev=$REV
+#/usr/bin/wget \
+#  --header "Content-Type:text/plain;charset=UTF-8" \
+#  --post-data "`svnlook changed --revision $REV $REPOS`" \
+#  --output-document "-" \
+#  http://$HUDSON_MASTER_HOST/hudson/subversion/${UUID}/notifyCommit?rev=$REV
+
