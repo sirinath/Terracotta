336bf83ecf74ae3a326e67b26a12c25384363890
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2009-12-08 23:25:58 +0000

    merged 14165, fix some unit tests

--- dso/branches/3.2/code/base/common-api/src/com/tc/object/ObjectID.java
+++ dso/branches/3.2/code/base/common-api/src/com/tc/object/ObjectID.java
@@ -62,7 +62,7 @@ public class ObjectID extends AbstractIdentifier implements Serializable {
     long oid = toLong();
     long gid = oid & 0xFF00000000000000L;
     gid = gid >>> 56;
-    if((gid < 0 && gid > 254)) {
+    if((gid < 0 || gid > 254)) {
       throw new AssertionError("gid is not between 0 and 254, the value was = " + gid);
     }
     return (int) gid;
