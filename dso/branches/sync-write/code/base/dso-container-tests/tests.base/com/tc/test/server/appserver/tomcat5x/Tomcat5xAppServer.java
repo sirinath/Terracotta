8ef3b5171b98199fc5ac7a8056b42a73f7d21fe0
npurwar <npurwar@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-01-13 06:18:13 +0000

    merging trunk changes to sync write branch

--- dso/branches/sync-write/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/tomcat5x/Tomcat5xAppServer.java
+++ dso/branches/sync-write/code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/tomcat5x/Tomcat5xAppServer.java
@@ -5,12 +5,15 @@
 package com.tc.test.server.appserver.tomcat5x;
 
 import org.codehaus.cargo.container.InstalledLocalContainer;
+import org.codehaus.cargo.container.State;
 import org.codehaus.cargo.container.configuration.LocalConfiguration;
 import org.codehaus.cargo.container.property.GeneralPropertySet;
 import org.codehaus.cargo.container.tomcat.Tomcat5xInstalledLocalContainer;
 
+import com.tc.test.AppServerInfo;
 import com.tc.test.server.appserver.AppServerParameters;
 import com.tc.test.server.appserver.cargo.CargoAppServer;
+import com.tc.test.server.appserver.tomcat.TomcatStartupActions;
 import com.tc.test.server.util.AppServerUtil;
 
 /**
@@ -18,19 +21,49 @@ import com.tc.test.server.util.AppServerUtil;
  */
 public final class Tomcat5xAppServer extends CargoAppServer {
 
+  private final AppServerInfo appServerInfo;
+
   public Tomcat5xAppServer(Tomcat5xAppServerInstallation installation) {
     super(installation);
+    appServerInfo = installation.appServerInfo();
   }
 
+  @Override
   protected String cargoServerKey() {
     return "tomcat5x";
   }
 
+  @Override
   protected InstalledLocalContainer container(LocalConfiguration config, AppServerParameters params) {
-    return new Tomcat5xInstalledLocalContainer(config);
+    return new TCTomcat5xInstalledLocalContainer(config, params, appServerInfo);
   }
 
+  @Override
   protected void setConfigProperties(LocalConfiguration config) throws Exception {
     config.setProperty(GeneralPropertySet.RMI_PORT, Integer.toString(AppServerUtil.getPort()));
   }
+
+  private static class TCTomcat5xInstalledLocalContainer extends Tomcat5xInstalledLocalContainer {
+
+    private final AppServerParameters params;
+    private final AppServerInfo       appServerInfo;
+
+    public TCTomcat5xInstalledLocalContainer(LocalConfiguration config, AppServerParameters params,
+                                             AppServerInfo appServerInfo) {
+      super(config);
+      this.params = params;
+      this.appServerInfo = appServerInfo;
+    }
+
+    @Override
+    protected void setState(State state) {
+      if (state.isStarting()) {
+        int line = appServerInfo.getMinor().startsWith("0.") ? 45 : 60;
+        TomcatStartupActions.modifyConfig(params, this, line);
+      }
+
+      super.setState(state);
+    }
+  }
+
 }
