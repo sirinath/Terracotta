46bdf085baaf0bc2bebfd80013ba930290a619dd
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-05-13 14:37:24 +0000

    private branch: better message recycling for grouped messages

--- dso/branches/messageBatching/code/base/dso-common/src/com/tc/net/protocol/transport/WireProtocolGroupMessage.java
+++ dso/branches/messageBatching/code/base/dso-common/src/com/tc/net/protocol/transport/WireProtocolGroupMessage.java
@@ -3,11 +3,13 @@
  */
 package com.tc.net.protocol.transport;
 
+import com.tc.net.protocol.TCNetworkMessage;
+
 import java.util.Iterator;
 
 public interface WireProtocolGroupMessage extends WireProtocolMessage {
 
-  public Iterator<WireProtocolMessage> getMessageIterator();
+  public Iterator<TCNetworkMessage> getMessageIterator();
 
   public int getTotalMessageCount();
 
