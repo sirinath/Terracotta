faad20c397432284dc3feb461d69ca82a362cb44
nelrahma <nelrahma@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-07-02 01:40:52 +0000

    merged in 15781 and 15782 from trunk, property to enable/disable periodic eviction for servermap

--- dso/branches/3.3/code/base/dso-l2/src/com/tc/objectserver/impl/ServerMapEvictionManagerImpl.java
+++ dso/branches/3.3/code/base/dso-l2/src/com/tc/objectserver/impl/ServerMapEvictionManagerImpl.java
@@ -58,6 +58,11 @@ public class ServerMapEvictionManagerImpl implements ServerMapEvictionManager {
   private static final TCLogger                logger                        = TCLogging
                                                                                  .getLogger(ServerMapEvictionManagerImpl.class);
 
+  private final static boolean                 PERIODIC_EVICTOR_ENABLED      = TCPropertiesImpl
+                                                                                 .getProperties()
+                                                                                 .getBoolean(
+                                                                                             TCPropertiesConsts.EHCACHE_STORAGESTRATEGY_DCV2_PERIODICEVICTION_ENABLED);
+
   // 15 Minutes
   public static final long                     DEFAULT_SLEEP_TIME            = 15 * 60000;
 
@@ -89,13 +94,15 @@ public class ServerMapEvictionManagerImpl implements ServerMapEvictionManager {
   }
 
   public void startEvictor() {
-    if (!this.isStarted.getAndSet(true)) {
+    if (PERIODIC_EVICTOR_ENABLED && !this.isStarted.getAndSet(true)) {
       logger.info("Server Map Eviction : Evictor will run every " + this.evictionSleepTime + " ms");
       this.evictor.schedule(new EvictorTask(this), this.evictionSleepTime, this.evictionSleepTime);
-      logger.info(TCPropertiesConsts.EHCAHCE_EVICTOR_LOGGING_ENABLED + " : " + EVICTOR_LOGGING);
-      logger.info(TCPropertiesConsts.EHCACHE_STORAGESTRATEGY_DCV2_PERELEMENT_TTI_TTL_ENABLED + " : "
-                  + ELEMENT_BASED_TTI_TTL_ENABLED);
-    }
+    } 
+    logger.info(TCPropertiesConsts.EHCAHCE_EVICTOR_LOGGING_ENABLED + " : " + EVICTOR_LOGGING);
+    logger.info(TCPropertiesConsts.EHCACHE_STORAGESTRATEGY_DCV2_PERIODICEVICTION_ENABLED + " : " + PERIODIC_EVICTOR_ENABLED);
+    logger.info(TCPropertiesConsts.EHCACHE_STORAGESTRATEGY_DCV2_PERELEMENT_TTI_TTL_ENABLED + " : "
+                + ELEMENT_BASED_TTI_TTL_ENABLED);
+
   }
 
   public void runEvictor() {
