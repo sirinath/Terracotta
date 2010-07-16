1c07cfff82f21c166b7650b967e62c21f28a2ecf
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-07-16 17:10:23 +0000

    Fixed notify URL.

--- dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
+++ dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
@@ -6,18 +6,20 @@ repo_name=`basename $repos`
 hudson_master_host="rh5fm0.terracotta.lan:9000"
 
 uuid=`svnlook uuid $repos`
+notify_url=http://$hudson_master_host/subversion/${uuid}/notifyCommit?rev=$rev
 timeout=10
 tries=3
+
 /usr/bin/wget \
   --header "Content-Type:text/plain;charset=UTF-8" \
   --timeout $timeout --tries $tries \
   --post-data "`svnlook changed --revision $rev $repos`" \
   --output-document "-" \
-  http://$hudson_master_host/hudson/subversion/${uuid}/notifyCommit?rev=$rev
+  $notify_url
 
 timestamp=`/bin/date`
 log=$HOME/logs/$repo_name-post-commit.log
-echo "$timestamp: http://rh5fm0.terracotta.lan:9000/subversion/${uuid}/notifyCommit?rev=$rev" > $log
+echo "$timestamp: $notify_url" > $log
 echo "post data $repos: `svnlook changed --revision $rev $repos`" >> $log
 echo "">> $log
 
