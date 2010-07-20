fea02901060924a69ef4b6f9048fc2148d328e07
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-07-20 22:34:07 +0000

    add a big hack to let anything that looks like our toolkit bundle to impersonate as other versions

--- knopflerfish-tc/trunk/osgi/framework/src/org/knopflerfish/framework/Bundles.java
+++ knopflerfish-tc/trunk/osgi/framework/src/org/knopflerfish/framework/Bundles.java
@@ -38,7 +38,6 @@ import java.io.*;
 import java.net.*;
 import java.security.*;
 
-
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -57,310 +56,308 @@ import org.osgi.framework.*;
  */
 public class Bundles {
 
-  /**
+    private static final String TC_TOOLKIT_PREFIX = "org.terracotta.toolkit.terracotta-toolkit-";
+
+    /**
    * Table of all installed bundles in this framework.
    * Key is bundle location.
-   */
+     */
   private Hashtable /* location String -> BundleImpl */ bundles = new Hashtable();
 
-  /**
-   * Link to framework object.
-   */
+    /**
+     * Link to framework object.
+     */
   private Framework framework;
 
 
-  /**
-   * Create a container for all bundles in this framework.
-   */
-  Bundles(Framework fw) {
-    framework = fw;
-    bundles.put(fw.systemBundle.location, fw.systemBundle);
-  }
-
-
-  /**
-   * Install a new bundle.
-   *
-   * @param location The location to be installed
-   */
-  BundleImpl install(final String location, final InputStream in) throws BundleException {
-    BundleImpl b;
-    synchronized (this) {
-      b = (BundleImpl)bundles.get(location);
-      if (b != null) {
-        return b;
-      }
-      b = framework.perm.callInstall0(this, location, in);
+    /**
+     * Create a container for all bundles in this framework.
+     */
+    Bundles(Framework fw) {
+        framework = fw;
+        bundles.put(fw.systemBundle.location, fw.systemBundle);
     }
+
+    /**
+     * Install a new bundle.
+     *
+   * @param location The location to be installed
+     */
+  BundleImpl install(final String location, final InputStream in) throws BundleException {
+        BundleImpl b;
+        synchronized (this) {
+            b = (BundleImpl) bundles.get(location);
+            if (b != null) {
+                return b;
+            }
+            b = framework.perm.callInstall0(this, location, in);
+        }
     framework.listeners.bundleChanged(new BundleEvent(BundleEvent.INSTALLED, b));
-    return b;
-  }
+        return b;
+    }
 
+    BundleImpl install0(String location, InputStream in, Object checkContext)
+            throws BundleException {
+        InputStream bin;
+        BundleArchive ba = null;
+        try {
+            if (in == null) {
+                // Do it the manual way to have a chance to
+                // set request properties
+                URL url = new URL(location);
+                URLConnection conn = url.openConnection();
 
-  BundleImpl install0(String location, InputStream in, Object checkContext)
-    throws BundleException {
-    InputStream bin;
-    BundleArchive ba = null;
-    try {
-      if (in == null) {
-        // Do it the manual way to have a chance to 
-        // set request properties
-        URL url  = new URL(location);
-        URLConnection conn = url.openConnection(); 
-
-        // Support for http proxy authentication
-        //TODO put in update as well
-        String auth = System.getProperty("http.proxyAuth");
-        if (auth != null && !"".equals(auth)) {
+                // Support for http proxy authentication
+                // TODO put in update as well
+                String auth = System.getProperty("http.proxyAuth");
+                if (auth != null && !"".equals(auth)) {
           if ("http".equals(url.getProtocol()) ||
               "https".equals(url.getProtocol())) {
-            String base64 = Util.base64Encode(auth);
+                        String base64 = Util.base64Encode(auth);
             conn.setRequestProperty("Proxy-Authorization", 
                                     "Basic " + base64);
-          }
-        }
-        bin = conn.getInputStream();
-      } else {
-        bin = in;
-      }
-      BundleImpl res = null;
-      try {
-        ba = framework.storage.insertBundleJar(location, bin);
-      } finally {
-        bin.close();
-      }
+                    }
+                }
+                bin = conn.getInputStream();
+            } else {
+                bin = in;
+            }
+            BundleImpl res = null;
+            try {
+                ba = framework.storage.insertBundleJar(location, bin);
+            } finally {
+                bin.close();
+            }
 
       String ee = ba.getAttribute(Constants.BUNDLE_REQUIREDEXECUTIONENVIRONMENT);
-      if (ee != null) {
-        if (Debug.packages) {
+            if (ee != null) {
+                if (Debug.packages) {
           Debug.println("bundle #" + ba.getBundleId() + " has EE=" + ee);
-        }
-        if (!framework.isValidEE(ee)) {
+                }
+                if (!framework.isValidEE(ee)) {
           throw new RuntimeException("Execution environment '" + ee +
                                      "' is not supported");
-        }
-      }
-      res = new BundleImpl(framework, ba);
+                }
+            }
+            res = new BundleImpl(framework, ba);
 
-      framework.perm.checkLifecycleAdminPerm(res, checkContext);
-      if (res.isExtension()) {
+            framework.perm.checkLifecycleAdminPerm(res, checkContext);
+            if (res.isExtension()) {
         framework.perm.checkExtensionLifecycleAdminPerm(res, checkContext);
-        if (!res.hasPermission(new AllPermission())) {
-          throw new SecurityException();
+                if (!res.hasPermission(new AllPermission())) {
+                    throw new SecurityException();
+                }
+            }
+
+            /* Commit bundle to storage */
+            ba.setLastModified(System.currentTimeMillis());
+
+            bundles.put(location, res);
+
+            return res;
+        } catch (Exception e) {
+            if (ba != null) {
+                ba.purge();
+            }
+            throw new BundleException("Failed to install bundle: " + e, e);
         }
-      }
-
-      /* Commit bundle to storage */
-      ba.setLastModified(System.currentTimeMillis());
-
-      bundles.put(location, res);
-              
-      return res;
-    } catch (Exception e) {
-      if (ba != null) {
-        ba.purge();
-      }
-      throw new BundleException("Failed to install bundle: " + e, e);
     }
-  }
-    
 
-  /**
-   * Remove bundle registration.
-   *
+    /**
+     * Remove bundle registration.
+     *
    * @param location The location to be removed
-   */
-  void remove(String location) {
-    bundles.remove(location);
-  }
+     */
+    void remove(String location) {
+        bundles.remove(location);
+    }
 
-
-  /**
-   * Get bundle that has specified bundle identifier.
-   *
+    /**
+     * Get bundle that has specified bundle identifier.
+     *
    * @param id The identifier of bundle to get.
    * @return BundleImpl representing bundle or null
    *         if bundle was not found.
-   */
-  public Bundle getBundle(long id) {
-    synchronized (bundles) {
-      for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
-        BundleImpl b = (BundleImpl)e.nextElement();
-        if (b.id == id) {
-          return b;
+     */
+    public Bundle getBundle(long id) {
+        synchronized (bundles) {
+            for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
+                BundleImpl b = (BundleImpl) e.nextElement();
+                if (b.id == id) {
+                    return b;
+                }
+            }
         }
-      }
+        return null;
     }
-    return null;
-  }
 
-
-  /**
-   * Get bundle that has specified bundle location.
-   *
+    /**
+     * Get bundle that has specified bundle location.
+     *
    * @param location The location of bundle to get.
    * @return BundleImpl representing bundle or null
    *         if bundle was not found.
-   */
-  public Bundle getBundle(String location) {
-    return (Bundle) bundles.get(location);
-  }
+     */
+    public Bundle getBundle(String location) {
+        return (Bundle) bundles.get(location);
+    }
 
-
-  /**
-   * Get bundle that has specified bundle symbolic name and version.
-   *
+    /**
+     * Get bundle that has specified bundle symbolic name and version.
+     *
    * @param name The symbolic name of bundle to get.
    * @param version The bundle version of bundle to get.
-   * @return BundleImpl for bundle or null.
-   */
-  BundleImpl getBundle(String name, Version version) {
-    synchronized (bundles) {
-      for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
-        BundleImpl b = (BundleImpl)e.nextElement();
-        if (name.equals(b.symbolicName) && version.equals(b.version)) {
-          return b;
+     * @return BundleImpl for bundle or null.
+     */
+    BundleImpl getBundle(String name, Version version) {
+        synchronized (bundles) {
+            for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
+                BundleImpl b = (BundleImpl) e.nextElement();
+                if (name.equals(b.symbolicName) && version.equals(b.version)) {
+                    return b;
+                }
+            }
         }
-      }
+        return null;
     }
-    return null;
-  }
 
-
-  /**
-   * Get all installed bundles.
-   *
-   * @return A Bundle array with bundles.
-   */
-  List getBundles() {
-    ArrayList res = new ArrayList(bundles.size());
-    synchronized (bundles) {
-      res.addAll(bundles.values());
+    /**
+     * Get all installed bundles.
+     *
+     * @return A Bundle array with bundles.
+     */
+    List getBundles() {
+        ArrayList res = new ArrayList(bundles.size());
+        synchronized (bundles) {
+            res.addAll(bundles.values());
+        }
+        return res;
     }
-    return res;
-  }
 
-
-  /**
-   * Get all bundles that has specified bundle symbolic name.
-   *
+    /**
+     * Get all bundles that has specified bundle symbolic name.
+     *
    * @param name The symbolic name of bundles to get.
-   * @return A List of BundleImpl.
-   */
-  List getBundles(String name) {
+     * @return A List of BundleImpl.
+     */
+    List getBundles(String name) {
     ArrayList res = new ArrayList();
     synchronized (bundles) {
+      boolean toolkitHackUsed = false;
+
       for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
         BundleImpl b = (BundleImpl)e.nextElement();
         if (name.equals(b.symbolicName)) {
           res.add(b);
+        } else if (name.startsWith(TC_TOOLKIT_PREFIX) && b.symbolicName.startsWith(TC_TOOLKIT_PREFIX)) {
+          // XXX: This is quite an enormous hack. We make sure to only ever load one version of the toolkit in this OSGi runtime at a higher level
+          if (toolkitHackUsed) {
+            throw new AssertionError("Multile toolkit like bundles detected");
+          }
+          res.add(b);
+          toolkitHackUsed = true;
         }
       }
     }
     return res;
   }
 
-
-  /**
+    /**
    * Get all bundles that has specified bundle symbolic name and
    * version range. Result is sorted in decreasing version order.
-   *
+     *
    * @param name The symbolic name of bundles to get.
    * @param range Version range of bundles to get.
-   * @return A List of BundleImpl.
-   */
-  List getBundles(String name, VersionRange range) {
-    List res = getBundles(name);
-    for (int i = 0; i < res.size(); ) {
-      BundleImpl b = (BundleImpl)res.remove(i);
-      if (range.withinRange(b.version)) {
-        int j = i;
-        while (--j >= 0) {
-          if (b.version.compareTo(((BundleImpl)res.get(j)).version) <= 0) {
-            break;
-          }
+     * @return A List of BundleImpl.
+     */
+    List getBundles(String name, VersionRange range) {
+        List res = getBundles(name);
+        for (int i = 0; i < res.size();) {
+            BundleImpl b = (BundleImpl) res.remove(i);
+            if (range.withinRange(b.version)) {
+                int j = i;
+                while (--j >= 0) {
+                    if (b.version.compareTo(((BundleImpl) res.get(j)).version) <= 0) {
+                        break;
+                    }
+                }
+                res.add(j + 1, b);
+                i++;
+            }
         }
-        res.add(j + 1, b);
-        i++;
-      }
+        return res;
     }
-    return res;
-  }
 
-
-  /**
-   * Get all bundles currently in bundle state ACTIVE.
-   *
-   * @return A List of BundleImpl.
-   */
-  List getActiveBundles() {
-    ArrayList slist = new ArrayList();
-    synchronized (bundles) {
-      for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
-        BundleImpl b = (BundleImpl)e.nextElement();
-        int s = b.getState();
-        if (s == Bundle.ACTIVE || s == Bundle.STARTING) {
-          slist.add(b);
+    /**
+     * Get all bundles currently in bundle state ACTIVE.
+     *
+     * @return A List of BundleImpl.
+     */
+    List getActiveBundles() {
+        ArrayList slist = new ArrayList();
+        synchronized (bundles) {
+            for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
+                BundleImpl b = (BundleImpl) e.nextElement();
+                int s = b.getState();
+                if (s == Bundle.ACTIVE || s == Bundle.STARTING) {
+                    slist.add(b);
+                }
+            }
         }
-      }
+        return slist;
     }
-    return slist;
-  }
 
-
-  /**
+    /**
    * Try to load any saved framework state.
    * This is done by installing all saved bundles from the local archive
    * copy, and restoring the saved state for each bundle. This is only
    * intended to be executed during the start of the framework.
-   *
-   */
-  synchronized void load() {
-    BundleArchive [] bas = framework.storage.getAllBundleArchives();
+     *
+     */
+    synchronized void load() {
+        BundleArchive[] bas = framework.storage.getAllBundleArchives();
     for (int i = 0; i < bas.length; i++) {
       BundleImpl b = new BundleImpl(framework, bas[i]);
-      bundles.put(b.location, b);
-    }
-  }
-
-
-  /**
-   * Start a list of bundles in order
-   *
-   * @param slist Bundles to start.
-   */
-  void startBundles(List slist) {
-    // Sort in start order
-    for (Iterator i = slist.iterator(); i.hasNext();) {
-      BundleImpl rb = (BundleImpl)i.next();
-      if (rb.getUpdatedState() == Bundle.RESOLVED) {
-        try {
-          rb.start();
-        } catch (BundleException be) {
-          rb.framework.listeners.frameworkError(rb, be);
+            bundles.put(b.location, b);
         }
-      } 
-    }    
-  }
+    }
 
+    /**
+     * Start a list of bundles in order
+     *
+   * @param slist Bundles to start.
+     */
+    void startBundles(List slist) {
+        // Sort in start order
+        for (Iterator i = slist.iterator(); i.hasNext();) {
+            BundleImpl rb = (BundleImpl) i.next();
+            if (rb.getUpdatedState() == Bundle.RESOLVED) {
+                try {
+                    rb.start();
+                } catch (BundleException be) {
+                    rb.framework.listeners.frameworkError(rb, be);
+                }
+            }
+        }
+    }
 
-  /**
+    /**
    * Returns all fragment bundles that is 
    * already attached and targets given bundle.
-   *
+     *
    * @param target the targetted bundle
-   * @return a list of all matching fragment bundles.
-   */
-  List getFragmentBundles(BundleImpl target) {
-    ArrayList retval = new ArrayList();
-    for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
-      BundleImpl b = (BundleImpl)e.nextElement();
+     * @return a list of all matching fragment bundles.
+     */
+    List getFragmentBundles(BundleImpl target) {
+        ArrayList retval = new ArrayList();
+        for (Enumeration e = bundles.elements(); e.hasMoreElements();) {
+            BundleImpl b = (BundleImpl) e.nextElement();
       if (b.isFragment() &&
           b.state != Bundle.UNINSTALLED &&
           b.getFragmentHost() == target) {
-        retval.add(b);
-      }
+                retval.add(b);
+            }
+        }
+        return retval;
     }
-    return retval;
-  }
 }
