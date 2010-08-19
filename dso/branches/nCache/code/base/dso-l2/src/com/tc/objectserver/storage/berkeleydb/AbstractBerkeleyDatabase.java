41edd05c254b70845ce343a0ca8ded5d812c6b99
mgovinda <mgovinda@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-08-19 01:34:35 +0000

    private branch: avoiding explicit casting to BDB while getting transaction

--- dso/branches/nCache/code/base/dso-l2/src/com/tc/objectserver/storage/berkeleydb/AbstractBerkeleyDatabase.java
+++ dso/branches/nCache/code/base/dso-l2/src/com/tc/objectserver/storage/berkeleydb/AbstractBerkeleyDatabase.java
@@ -6,6 +6,7 @@ package com.tc.objectserver.storage.berkeleydb;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.Transaction;
 import com.tc.objectserver.storage.api.PersistenceTransaction;
+import com.tc.util.Assert;
 
 public abstract class AbstractBerkeleyDatabase {
   protected final Database db;
@@ -15,8 +16,13 @@ public abstract class AbstractBerkeleyDatabase {
   }
 
   protected Transaction pt2nt(PersistenceTransaction tx) {
-    return (tx instanceof BerkeleyDBPersistenceTransaction) ? ((BerkeleyDBPersistenceTransaction) tx).getTransaction()
-        : null;
+    Object o = tx.getTransaction();
+    if (o != null) {
+      Assert.eval(o instanceof Transaction);
+      return (Transaction) o;
+    } else {
+      return null;
+    }
   }
 
   public final Database getDatabase() {
