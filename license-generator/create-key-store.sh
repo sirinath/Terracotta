e4cecec35bdb8d9e441fcb6d859253ced009c934
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2010-11-04 23:50:02 +0000

    moved to internal

--- license-generator/create-key-store.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-keytool -genkey -keyalg RSA -alias terracotta -keystore resources/keystore.jks -keypass terracotta -storepass terracotta -validity 36500
-
--- license-generator/signjars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`
-
-for i in lib/*.jar; do
-  jarsigner -keystore resources/keystore.jks -storepass terracotta $i terracotta -keypass terracotta
-done
