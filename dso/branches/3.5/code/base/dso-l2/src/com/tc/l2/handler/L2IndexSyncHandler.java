7f7469858abdd4d99ca4d3d9e48fedbdf438a06a
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2011-03-15 18:28:32 +0000

    break up index data files into chunks during passive sync (DEV-5435, DEV-5559, DEV-5560)

--- dso/branches/3.5/code/base/dso-l2/src/com/tc/l2/handler/L2IndexSyncHandler.java
+++ dso/branches/3.5/code/base/dso-l2/src/com/tc/l2/handler/L2IndexSyncHandler.java
@@ -7,7 +7,8 @@ package com.tc.l2.handler;
 import com.tc.async.api.AbstractEventHandler;
 import com.tc.async.api.ConfigurationContext;
 import com.tc.async.api.EventContext;
-import com.tc.async.api.Sink;
+import com.tc.l2.ha.L2HAZapNodeRequestProcessor;
+import com.tc.l2.msg.IndexSyncAckMessage;
 import com.tc.l2.msg.IndexSyncCompleteAckMessage;
 import com.tc.l2.msg.IndexSyncCompleteMessage;
 import com.tc.l2.msg.IndexSyncMessage;
@@ -16,6 +17,8 @@ import com.tc.l2.msg.IndexSyncStartMessage;
 import com.tc.l2.state.StateSyncManager;
 import com.tc.logging.TCLogger;
 import com.tc.logging.TCLogging;
+import com.tc.net.groups.GroupException;
+import com.tc.net.groups.GroupManager;
 import com.tc.objectserver.core.api.ServerConfigurationContext;
 import com.tc.objectserver.search.IndexHACoordinator;
 
@@ -25,7 +28,7 @@ public class L2IndexSyncHandler extends AbstractEventHandler {
   private final IndexHACoordinator indexHACoordinator;
 
   private StateSyncManager         stateSyncManager;
-  private Sink                     sendSink;
+  private GroupManager             groupManager;
 
   public L2IndexSyncHandler(final IndexHACoordinator indexHACoordinator) {
     this.indexHACoordinator = indexHACoordinator;
@@ -51,7 +54,16 @@ public class L2IndexSyncHandler extends AbstractEventHandler {
     stateSyncManager.indexSyncComplete();
     IndexSyncCompleteAckMessage ackMessage = IndexSyncMessageFactory.createIndexSyncCompleteAckMessage(msg
         .messageFrom());
-    this.sendSink.add(ackMessage);
+
+    try {
+      this.groupManager.sendTo(ackMessage.getDestinatioinNodeID(), ackMessage);
+    } catch (final GroupException e) {
+      final String error = "Error sending IndexSyncCompleteAckMessage to " + ackMessage.getDestinatioinNodeID()
+                           + ": Caught exception while sending message to ACTIVE";
+      logger.error(error, e);
+      this.groupManager.zapNode(ackMessage.getDestinatioinNodeID(), L2HAZapNodeRequestProcessor.COMMUNICATION_ERROR,
+                                "Error sending objects." + L2HAZapNodeRequestProcessor.getErrorString(e));
+    }
   }
 
   private void doSyncPrepare() {
@@ -59,7 +71,17 @@ public class L2IndexSyncHandler extends AbstractEventHandler {
   }
 
   private void doSyncIndex(final IndexSyncMessage syncMsg) {
-    this.indexHACoordinator.applyIndexSync(syncMsg.getCacheName(), syncMsg.getFileName(), syncMsg.getData());
+    byte[] data = syncMsg.getData();
+    this.indexHACoordinator.applyIndexSync(syncMsg.getCacheName(), syncMsg.getFileName(), data);
+
+    IndexSyncAckMessage ack = IndexSyncMessageFactory.createIndexSyncAckMessage(syncMsg.getMessageID(), data.length);
+    try {
+      this.groupManager.sendTo(syncMsg.messageFrom(), ack);
+    } catch (GroupException e) {
+      logger.error(e);
+      this.groupManager.zapNode(syncMsg.messageFrom(), L2HAZapNodeRequestProcessor.COMMUNICATION_ERROR,
+                                "Error sending index ack." + L2HAZapNodeRequestProcessor.getErrorString(e));
+    }
   }
 
   @Override
@@ -67,7 +89,6 @@ public class L2IndexSyncHandler extends AbstractEventHandler {
     super.initialize(context);
     final ServerConfigurationContext oscc = (ServerConfigurationContext) context;
     this.stateSyncManager = oscc.getL2Coordinator().getStateSyncManager();
-    this.sendSink = oscc.getStage(ServerConfigurationContext.INDEXES_SYNC_SEND_STAGE).getSink();
+    this.groupManager = oscc.getL2Coordinator().getGroupManager();
   }
-
 }
