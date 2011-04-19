61545f273b45c4c17bcf412eea0628c757d0d7d3
hhuynh <hhuynh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2011-04-19 21:28:36 +0000

    mvn

--- dso/branches/mvn2/dso-unit-tests/src/test/java/com/tctest/YoungGCTestAndActivePassiveTestBase.java
+++ dso/branches/mvn2/dso-unit-tests/src/test/java/com/tctest/YoungGCTestAndActivePassiveTestBase.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public abstract class YoungGCTestAndActivePassiveTestBase extends GCAndActivePassiveTestBase {
 
-  YoungGCTestAndActivePassiveTestBase() {
+  public YoungGCTestAndActivePassiveTestBase() {
     gcConfigHelper = new YoungGCConfigurationHelper();
   }
 
