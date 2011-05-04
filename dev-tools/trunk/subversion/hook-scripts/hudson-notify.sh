c67bf0b64f4cc2a007b4f800e509b9c3db2eccda
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2011-05-04 16:01:04 +0000

    Update hook script to notify both Jenkins master nodes of changes.

--- dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
+++ dev-tools/trunk/subversion/hook-scripts/hudson-notify.sh
@@ -6,18 +6,22 @@ repos="$1"
 rev="$2"
 repo_name=`basename $repos`
 hudson_master_host="rh5fm0.terracotta.lan:9000"
+dso_hudson_master_host="su10mo9.terracotta.lan:9000"
 
 uuid=`svnlook uuid $repos`
 notify_url=http://$hudson_master_host/subversion/${uuid}/notifyCommit?rev=$rev
+dso_notify_url=http://$dso_hudson_master_host/subversion/${uuid}/notifyCommit?rev=$rev
 timeout=10
 tries=3
 
-/usr/bin/wget \
-  --header "Content-Type:text/plain;charset=UTF-8" \
-  --timeout $timeout --tries $tries \
-  --post-data "`svnlook changed --revision $rev $repos`" \
-  --output-document "-" \
-  $notify_url
+for url in $notify_url $dso_notify_url; do
+  /usr/bin/wget \
+    --header "Content-Type:text/plain;charset=UTF-8" \
+    --timeout $timeout --tries $tries \
+    --post-data "`svnlook changed --revision $rev $repos`" \
+    --output-document "-" \
+    $url
+done
 
 timestamp=`/bin/date`
 log=/export1/svn-mirror/logs/$repo_name-post-commit.log
