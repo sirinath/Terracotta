9c737e4eaf6cdd5f09a7727c50cd2ef15c850253
npurwar <npurwar@7fc7bbf3-cf45-46d4-be06-341739edd864>
2011-05-18 17:47:59 +0000

    merge 17764

--- dso/branches/3.5/code/base/dso-l2/src/com/tc/objectserver/tx/ServerTransactionManagerImpl.java
+++ dso/branches/3.5/code/base/dso-l2/src/com/tc/objectserver/tx/ServerTransactionManagerImpl.java
@@ -51,7 +51,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
-import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
