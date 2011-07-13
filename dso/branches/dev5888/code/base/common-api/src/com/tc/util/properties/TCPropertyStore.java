e65de596708f60b62dcba95b3c25476b9138921a
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2011-07-13 19:21:26 +0000

    tc prop order retained

--- dso/branches/dev5888/code/base/common-api/src/com/tc/util/properties/TCPropertyStore.java
+++ dso/branches/dev5888/code/base/common-api/src/com/tc/util/properties/TCPropertyStore.java
@@ -3,105 +3,83 @@
  */
 package com.tc.util.properties;
 
-import com.tc.util.Assert;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
-public class TCPropertyStore extends Properties {
-  private final Map<String, String> propertyNameMap = new HashMap<String, String>();
+public class TCPropertyStore {
+  private final Map<Key, String> props = new LinkedHashMap<Key, String>();
 
-  @Override
-  public synchronized Object setProperty(String key, String value) {
-    propertyNameMap.put(key.toLowerCase(), key);
-    return super.setProperty(key, value);
+  public synchronized boolean containsKey(String key) {
+    return props.containsKey(new Key(key));
   }
 
-  @Override
-  public synchronized Object put(Object key, Object value) {
-    if (!(key instanceof String)) { throw new AssertionError("Only String keys allowed : " + key); }
-    propertyNameMap.put(((String) key).toLowerCase(), (String) key);
-    return super.put(key, value);
+  public synchronized void setProperty(String key, String value) {
+    Key k = new Key(key);
+
+    // do put and remove so that a replaced mapping will retain the new Key object
+    props.remove(k);
+    props.put(k, value.trim());
   }
 
-  @Override
-  public synchronized void load(InputStream inStream) throws IOException {
-    super.load(inStream);
-    Enumeration<?> keys = propertyNames();
-    while (keys.hasMoreElements()) {
-      String propertyName = (String) keys.nextElement();
-      propertyNameMap.put(propertyName.toLowerCase(), propertyName);
+  public synchronized String getProperty(String key) {
+    return props.get(new Key(key));
+  }
+
+  public synchronized int size() {
+    return props.size();
+  }
+
+  public synchronized String[] keysArray() {
+    String[] keys = new String[props.size()];
+    int index = 0;
+    for (Key key : props.keySet()) {
+      keys[index++] = key.getKey();
+    }
+    return keys;
+  }
+
+  public synchronized void load(InputStream in) throws IOException {
+    new Properties() {
+      @Override
+      public synchronized Object put(Object key, Object value) {
+        TCPropertyStore.this.setProperty((String) key, (String) value);
+        return null;
+      }
+    }.load(in);
+  }
+
+  public synchronized void putAll(TCPropertyStore other) {
+    for (String key : other.keysArray()) {
+      setProperty(key, other.getProperty(key));
     }
   }
 
-  @Override
-  public synchronized Object get(Object key) {
-    if (!(key instanceof String)) { throw new AssertionError("Only String keys allowed : " + key); }
-    String propertyName = propertyNameMap.get(((String) key).toLowerCase());
-    return propertyName != null ? super.get(propertyName) : null;
-  }
+  private static class Key {
+    private final String key;
 
-  @Override
-  public String getProperty(String key) {
-    String propertyName = propertyNameMap.get(key.toLowerCase());
-    return propertyName != null ? super.getProperty(propertyName) : null;
-  }
-
-  @Override
-  public String getProperty(String key, String defaultValue) {
-    String propertyName = propertyNameMap.get(key.toLowerCase());
-    return propertyName != null ? super.getProperty(propertyName, defaultValue) : defaultValue;
-  }
-
-  @Override
-  public synchronized boolean containsKey(Object key) {
-    if (!(key instanceof String)) { throw new AssertionError("Only String keys allowed : " + key); }
-    String propertyName = propertyNameMap.get(((String) key).toLowerCase());
-    return propertyName != null ? super.containsKey(propertyName) : false;
-  }
-
-  @Override
-  public synchronized Object remove(Object key) {
-    if (!(key instanceof String)) { throw new AssertionError("Only String keys allowed : " + key); }
-    String propertyName = propertyNameMap.get(((String) key).toLowerCase());
-    return propertyName != null ? super.remove(propertyName) : super.remove(key);
-  }
-
-  @Override
-  public synchronized void clear() {
-    super.clear();
-    propertyNameMap.clear();
-  }
-
-  @Override
-  public synchronized Object clone() {
-    throw new UnsupportedOperationException();
-  }
-
-  public synchronized void putAll(TCPropertyStore propStore) {
-    Set<Object> keySet = propStore.keySet();
-    for (Iterator iter = keySet.iterator(); iter.hasNext();) {
-      String propertyName = (String) iter.next();
-      propertyNameMap.put(propertyName.toLowerCase(), propertyName);
+    Key(String key) {
+      this.key = key;
     }
-    super.putAll(propStore);
-  }
 
-  @Override
-  public synchronized void putAll(Map<? extends Object, ? extends Object> t) {
-    Assert.assertTrue(t instanceof TCPropertyStore);
-    putAll(t);
-  }
+    String getKey() {
+      return key;
+    }
 
-  // used in test
-  public int keySize() {
-    return propertyNameMap.size();
+    @Override
+    public int hashCode() {
+      return key.toLowerCase().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof Key)) { return false; }
+
+      Key other = (Key) obj;
+      return key.equalsIgnoreCase(other.key);
+    }
   }
 
 }
