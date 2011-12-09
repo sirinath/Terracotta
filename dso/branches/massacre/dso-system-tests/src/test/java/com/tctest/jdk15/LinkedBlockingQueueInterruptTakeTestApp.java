ebf1215d41733f6ca777de2485ee25ede3fc82fa
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2011-12-09 00:06:01 +0000

    fix test

--- dso/branches/massacre/dso-system-tests/src/test/java/com/tctest/jdk15/LinkedBlockingQueueInterruptTakeTestApp.java
+++ dso/branches/massacre/dso-system-tests/src/test/java/com/tctest/jdk15/LinkedBlockingQueueInterruptTakeTestApp.java
@@ -4,7 +4,6 @@
  */
 package com.tctest.jdk15;
 
-
 import com.tc.object.config.ConfigVisitor;
 import com.tc.object.config.DSOClientConfigHelper;
 import com.tc.object.config.TransparencyClassSpec;
@@ -18,21 +17,21 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class LinkedBlockingQueueInterruptTakeTestApp extends AbstractTransparentApp {
-  private static final int    DEFAULT_NUM_OF_LOOPS = 1000;
+  private static final int          DEFAULT_NUM_OF_LOOPS = 1000;
 
-  private static final int    CAPACITY             = 100;
+  private static final int          CAPACITY             = 100;
 
-  private final int           numOfLoops;
+  private final int                 numOfLoops;
 
-  private final LinkedBlockingQueue   queue = new LinkedBlockingQueue(CAPACITY);
-  private int                         count;
-  private final CyclicBarrier         barrier;
+  private final LinkedBlockingQueue queue                = new LinkedBlockingQueue(CAPACITY);
+  private int                       count;
+  private final CyclicBarrier       barrier;
 
-  private int                 localCount;
+  private int                       localCount;
 
-  private transient Thread1 thread1;
+  private transient Thread1         thread1;
 
-  private volatile int node = -1;
+  private volatile int              node                 = -1;
 
   public LinkedBlockingQueueInterruptTakeTestApp(String appId, ApplicationConfig cfg, ListenerProvider listenerProvider) {
     super(appId, cfg, listenerProvider);
@@ -44,12 +43,12 @@ public class LinkedBlockingQueueInterruptTakeTestApp extends AbstractTransparent
     try {
       node = barrier.await();
 
-      thread1 = new Thread1("Node "+node+" thread 1");
+      thread1 = new Thread1("Node " + node + " thread 1");
       thread1.start();
-      Thread2 thread2 = new Thread2("Node "+node+" thread 2");
+      Thread2 thread2 = new Thread2("Node " + node + " thread 2");
       thread2.start();
 
-      System.out.println("Node "+node+" : waiting for thread 2");
+      System.out.println("Node " + node + " : waiting for thread 2");
 
       thread2.join();
 
@@ -63,24 +62,24 @@ public class LinkedBlockingQueueInterruptTakeTestApp extends AbstractTransparent
         }
       }
 
-      System.out.println("Node "+node+" : stopping thread 1");
+      System.out.println("Node " + node + " : stopping thread 1");
       thread1.stopLoop();
       thread1.interrupt();
 
-      System.out.println("Node "+node+" : waiting for thread 1");
+      System.out.println("Node " + node + " : waiting for thread 1");
       thread1.join();
 
       // wait for all thread 1 to be finished
       node = barrier.await();
 
-      System.out.println(">>> Node "+node+" : Local count : "+localCount);
+      System.out.println(">>> Node " + node + " : Local count : " + localCount);
 
       if (0 == node) {
-        System.out.println(">>> Total count : "+count);
+        System.out.println(">>> Total count : " + count);
 
         // check how many items have been taken from the queue
         int expected = numOfLoops * getParticipantCount();
-        System.out.println("Took "+count+" from queue, expected "+expected);
+        System.out.println("Took " + count + " from queue, expected " + expected);
         Assert.assertEquals(count, expected);
       }
     } catch (Throwable t) {
@@ -99,26 +98,28 @@ public class LinkedBlockingQueueInterruptTakeTestApp extends AbstractTransparent
       this.stop = true;
     }
 
+    @Override
     public void run() {
       while (!stop) {
         Object taken = null;
         try {
-//          System.out.println("Node "+node+" - thread 1 : "+queue.size()+" - taking");
+          // System.out.println("Node "+node+" - thread 1 : "+queue.size()+" - taking");
           taken = queue.take();
-          System.out.println("Node "+node+" - thread 1 : "+queue.size()+" - took : " + taken);
+          System.out.println("Node " + node + " - thread 1 : " + queue.size() + " - took : " + taken);
         } catch (InterruptedException e) {
-          System.out.println("Node "+node+" - thread 1 : InterruptedException");
+          System.out.println("Node " + node + " - thread 1 : InterruptedException");
         } finally {
-//          System.out.println("Node "+node+" - thread 1 : checking taken state");
+          // System.out.println("Node "+node+" - thread 1 : checking taken state");
           if (taken != null) {
             synchronized (queue) {
               queue.notifyAll();
-              System.out.println("Node "+node+" - thread 1 : updating counts - "+(++localCount)+", "+(++count));
+              System.out.println("Node " + node + " - thread 1 : updating counts - " + (++localCount) + ", "
+                                 + (++count));
             }
           }
         }
       }
-      System.out.println("Node "+node+" - thread 1 : finished");
+      System.out.println("Node " + node + " - thread 1 : finished");
     }
   }
 
@@ -127,6 +128,7 @@ public class LinkedBlockingQueueInterruptTakeTestApp extends AbstractTransparent
       super(name);
     }
 
+    @Override
     public void run() {
       for (int i = 0; i < numOfLoops; i++) {
         try {
@@ -143,23 +145,23 @@ public class LinkedBlockingQueueInterruptTakeTestApp extends AbstractTransparent
               // do nothing
             }
           }
-          Object o = new Date();
-//          System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - putting : "+o);
+          Object o = new Date().getTime();
+          // System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - putting : "+o);
           queue.put(o);
-          System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - put : "+o);
+          System.out.println("Node " + node + " - thread 2 : " + queue.size() + " - put : " + o);
           synchronized (queue) {
-//            System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - checking queue size");
+            // System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - checking queue size");
             if (0 == queue.size()) {
-              System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - interrupting thread 1");
+              System.out.println("Node " + node + " - thread 2 : " + queue.size() + " - interrupting thread 1");
               thread1.interrupt();
-//              System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - interrupted thread 1");
+              // System.out.println("Node "+node+" - thread 2 : "+queue.size()+" - interrupted thread 1");
             }
           }
         } catch (InterruptedException e) {
-          System.out.println("Node "+node+" - thread 2 : InterruptedException");
+          System.out.println("Node " + node + " - thread 2 : InterruptedException");
         }
       }
-      System.out.println("Node "+node+" - thread 2 : finished");
+      System.out.println("Node " + node + " - thread 2 : finished");
     }
   }
 
