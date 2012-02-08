6beedd791c9eea1f554fbe8fcef6c3c4471b2411
rsingh <rsingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-02-08 20:24:01 +0000

    making the test time based instead of count based Issue MNK-3361

--- dso/branches/3.5/code/base/dso-crash-tests/tests.base/com/tctest/CreateLotsOfGarbageGCTestApp.java
+++ dso/branches/3.5/code/base/dso-crash-tests/tests.base/com/tctest/CreateLotsOfGarbageGCTestApp.java
@@ -6,7 +6,6 @@ import com.tc.object.config.TransparencyClassSpec;
 import com.tc.simulator.app.ApplicationConfig;
 import com.tc.simulator.listener.ListenerProvider;
 import com.tc.util.concurrent.ThreadUtil;
-import com.tc.util.runtime.Os;
 import com.tctest.runner.AbstractTransparentApp;
 
 /**
@@ -14,18 +13,13 @@ import com.tctest.runner.AbstractTransparentApp;
  */
 public class CreateLotsOfGarbageGCTestApp extends AbstractTransparentApp {
 
-  private static final int SIZE  = 50;
-  private static int       LOOP_COUNT;
+  private static final long TIME_TO_RUN = 5 * 60 * 1000;
+  private static final int  SIZE        = 50;
 
-  private final Object[]   array = new Object[SIZE];
+  private final Object[]    array       = new Object[SIZE];
 
   public CreateLotsOfGarbageGCTestApp(String appId, ApplicationConfig cfg, ListenerProvider listenerProvider) {
     super(appId, cfg, listenerProvider);
-    if (Os.isSolaris()) {
-      LOOP_COUNT = 2000;
-    } else {
-      LOOP_COUNT = 3000;
-    }
   }
 
   public static void visitL1DSOConfig(ConfigVisitor visitor, DSOClientConfigHelper config) {
@@ -39,7 +33,9 @@ public class CreateLotsOfGarbageGCTestApp extends AbstractTransparentApp {
   }
 
   public void run() {
-    for (int i = 0; i < LOOP_COUNT; i++) {
+    long start = System.currentTimeMillis();
+    int i = 0;
+    while (System.currentTimeMillis() - start < TIME_TO_RUN) {
       synchronized (array) {
         for (int j = 0; j < SIZE; j++) {
           array[j] = new Object();
@@ -49,6 +45,7 @@ public class CreateLotsOfGarbageGCTestApp extends AbstractTransparentApp {
         System.out.println("Loop count : " + i);
         ThreadUtil.reallySleep(1000);
       }
+      i++;
     }
   }
 }
