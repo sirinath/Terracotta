34a9151d25371dcc234fe0eadab1d02713b0230f
npurwar <npurwar@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-02-13 12:29:49 +0000

    toolkit2_dev: clustered notifier

--- dso/branches/toolkit2_dev/dso-common/src/main/java/com/tc/object/SerializationUtil.java
+++ dso/branches/toolkit2_dev/dso-common/src/main/java/com/tc/object/SerializationUtil.java
@@ -47,6 +47,7 @@ public class SerializationUtil {
   public final static int         SET_TARGET_MAX_TOTAL_COUNT           = 31;
   public final static int         SET_MAX_TTI                          = 32;
   public final static int         SET_MAX_TTL                          = 33;
+  public final static int         CLUSTERED_NOTIFIER                   = 34;
 
   public final static String      PUSH_SIGNATURE                       = "push(Ljava/lang/Object;)java/lang/Object;";
   public final static String      POP_SIGNATURE                        = "pop()java/lang/Object;";
@@ -126,6 +127,7 @@ public class SerializationUtil {
   public final static String      SIGNAL_ALL_SIGNATURE                 = "signalAll()V";
   public final static String      TRANSFORM_VALUES_SIGNATURE           = "transformValues(Lgnu/trove/TObjectFunction;)V";
   public final static String      URL_SET_SIGNATURE                    = "set(Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V";
+  public final static String      CLUSTERED_NOTIFIER_SIGNATURE         = "clusteredNotifier()V";
 
   private final TObjectIntHashMap mappings                             = new TObjectIntHashMap();
 
@@ -172,6 +174,7 @@ public class SerializationUtil {
     mappings.put(SET_TARGET_MAX_TOTAL_COUNT_SIGNATURE, SET_TARGET_MAX_TOTAL_COUNT);
     mappings.put(SET_MAX_TTI_SIGNATURE, SET_MAX_TTI);
     mappings.put(SET_MAX_TTL_SIGNATURE, SET_MAX_TTL);
+    mappings.put(CLUSTERED_NOTIFIER_SIGNATURE, CLUSTERED_NOTIFIER);
   }
 
   public String[] getSignatures() {
