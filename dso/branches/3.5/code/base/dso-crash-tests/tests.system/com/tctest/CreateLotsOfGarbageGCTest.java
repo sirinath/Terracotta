3ad3b13208f37b3fdd6dd91e7325dbfdce516d98
rsingh <rsingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-02-15 00:13:33 +0000

    MNK-3412

--- dso/branches/3.5/code/base/dso-crash-tests/tests.system/com/tctest/CreateLotsOfGarbageGCTest.java
+++ dso/branches/3.5/code/base/dso-crash-tests/tests.system/com/tctest/CreateLotsOfGarbageGCTest.java
@@ -10,12 +10,19 @@ import com.tc.test.MultipleServersSharedDataMode;
 import com.tc.test.activepassive.ActivePassiveTestSetupManager;
 import com.tc.util.runtime.Os;
 
+import java.util.ArrayList;
+
 public class CreateLotsOfGarbageGCTest extends GCAndActivePassiveTestBase implements TestConfigurator {
 
   public CreateLotsOfGarbageGCTest() {
     if (Os.isSolaris()) {
       disableTest();
     }
+  }
+
+  @Override
+  protected void setExtraJvmArgs(ArrayList jvmArgs) {
+    jvmArgs.add("-Xmx512m");
   }
 
   @Override
