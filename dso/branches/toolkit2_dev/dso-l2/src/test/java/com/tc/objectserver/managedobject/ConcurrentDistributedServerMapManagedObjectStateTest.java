0cce602d1241836138fb892f0bcc787caac442cd
asingh <asingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-02-27 12:42:42 +0000

    toolkit2_dev: update cdsmDso state field names

--- dso/branches/toolkit2_dev/dso-l2/src/test/java/com/tc/objectserver/managedobject/ConcurrentDistributedServerMapManagedObjectStateTest.java
+++ dso/branches/toolkit2_dev/dso-l2/src/test/java/com/tc/objectserver/managedobject/ConcurrentDistributedServerMapManagedObjectStateTest.java
@@ -46,15 +46,15 @@ public class ConcurrentDistributedServerMapManagedObjectStateTest extends Abstra
 
   private TestDNACursor createDNACursor() {
     final TestDNACursor cursor = new TestDNACursor();
-    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.DSO_LOCK_TYPE_FIELDNAME, new Integer(1),
+    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.LOCK_TYPE_FIELDNAME, new Integer(1),
                              false);
     cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.MAX_TTI_SECONDS_FIELDNAME,
                              Integer.valueOf(0), false);
     cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.MAX_TTL_SECONDS_FIELDNAME,
                              Integer.valueOf(0), false);
-    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.TARGET_MAX_TOTAL_COUNT_FIELDNAME,
+    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.MAX_COUNT_IN_CLUSTER_FIELDNAME,
                              Integer.valueOf(0), false);
-    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.INVALIDATE_ON_CHANGE,
+    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.INVALIDATE_ON_CHANGE_FIELDNAME,
                              Boolean.valueOf(false), false);
     cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.CACHE_NAME_FIELDNAME, "bob", false);
     cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.LOCAL_CACHE_ENABLED_FIELDNAME,
