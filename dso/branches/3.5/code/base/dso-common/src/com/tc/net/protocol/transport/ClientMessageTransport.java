341fee2ccc74fc750999bd1810f448cb2fd22ff0
twu <twu@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-03-07 07:55:51 +0000

    MNK-3511: Fix synchronization for wasOpen

--- dso/branches/3.5/code/base/dso-common/src/com/tc/net/protocol/transport/ClientMessageTransport.java
+++ dso/branches/3.5/code/base/dso-common/src/com/tc/net/protocol/transport/ClientMessageTransport.java
@@ -147,7 +147,9 @@ public class ClientMessageTransport extends MessageTransportBase implements Reco
    * Returns true if the MessageTransport was ever in an open state.
    */
   public boolean wasOpened() {
-    return this.wasOpened;
+    synchronized (isOpen) {
+      return this.wasOpened;
+    }
   }
 
   public boolean isNotOpen() {
@@ -312,7 +314,7 @@ public class ClientMessageTransport extends MessageTransportBase implements Reco
   void reconnect(TCConnection connection) throws Exception {
 
     // don't do reconnect if open is still going on
-    if (!this.wasOpened) {
+    if (!wasOpened()) {
       this.logger.warn("Transport was opened already. Skip reconnect " + connection);
       return;
     }
