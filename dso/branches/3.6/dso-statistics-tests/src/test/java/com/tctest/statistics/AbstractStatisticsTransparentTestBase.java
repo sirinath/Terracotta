406c7b26e1b4c4c58796a336e541d7bb388fcd7c
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-03-12 22:30:24 +0000

    disable SVT rest interface by default (DEV-6975)

--- dso/branches/3.6/dso-statistics-tests/src/test/java/com/tctest/statistics/AbstractStatisticsTransparentTestBase.java
+++ dso/branches/3.6/dso-statistics-tests/src/test/java/com/tctest/statistics/AbstractStatisticsTransparentTestBase.java
@@ -4,18 +4,29 @@
  */
 package com.tctest.statistics;
 
+import com.tc.properties.TCPropertiesConsts;
+import com.tc.properties.TCPropertiesImpl;
 import com.tc.statistics.beans.StatisticsGatewayMBean;
 import com.tc.statistics.beans.StatisticsMBeanNames;
 import com.tc.test.JMXUtils;
 import com.tctest.TransparentTestBase;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.management.MBeanServerConnection;
 import javax.management.MBeanServerInvocationHandler;
 import javax.management.remote.JMXConnector;
 
 public abstract class AbstractStatisticsTransparentTestBase extends TransparentTestBase {
+
+  @Override
+  protected void setExtraJvmArgs(final ArrayList jvmArgs) {
+    // REST interface is disabled out the box now (see DEV-6875)
+    TCPropertiesImpl.getProperties().setProperty(TCPropertiesConsts.CVT_REST_INTERFACE_ENABLED, "true");
+    System.setProperty(TCPropertiesImpl.tcSysProp(TCPropertiesConsts.CVT_REST_INTERFACE_ENABLED), "true");
+    jvmArgs.add("-D" + TCPropertiesImpl.tcSysProp(TCPropertiesConsts.CVT_REST_INTERFACE_ENABLED) + "=true");
+  }
 
   protected void waitForAllNodesToConnectToGateway(final int nodeCount) throws IOException, InterruptedException {
     final JMXConnector jmxc = JMXUtils.getJMXConnector("localhost", getAdminPort());
