fa8b536786c2e3676fe528242f6359ce5655d229
rsingh <rsingh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-03-13 21:35:17 +0000

    MNK-3517, disable test for windows + jdk1.5

--- dso/branches/3.6/dso-system-tests/src/test/java/com/tctest/jdk15/LinkedBlockingQueueYoungGenGCTest.java
+++ dso/branches/3.6/dso-system-tests/src/test/java/com/tctest/jdk15/LinkedBlockingQueueYoungGenGCTest.java
@@ -4,17 +4,27 @@
  */
 package com.tctest.jdk15;
 
+import com.tc.util.runtime.Os;
+import com.tc.util.runtime.Vm;
 import com.tctest.TestConfigurator;
 import com.tctest.TransparentTestIface;
 import com.tctest.YoungGCTestBase;
 
 public class LinkedBlockingQueueYoungGenGCTest extends YoungGCTestBase implements TestConfigurator {
 
+  public LinkedBlockingQueueYoungGenGCTest() {
+    if (Os.isWindows() && Vm.isJDK15()) {
+      disableTest();
+    }
+  }
+
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setAttribute(LinkedBlockingQueueTestApp.GC_TEST_KEY, "true");
     super.doSetUp(t);
   }
 
+  @Override
   protected Class getApplicationClass() {
     return LinkedBlockingQueueTestApp.class;
   }
