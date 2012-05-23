d6aae49e1a420a66e8aaee69ed78e8f332ba8fbd
nbangarw <nbangarw@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-05-23 20:33:16 +0000

    DEV-6737 Merging from trunk

--- dso/branches/3.7/dso-l2/src/main/java/com/tc/objectserver/impl/DistributedObjectServer.java
+++ dso/branches/3.7/dso-l2/src/main/java/com/tc/objectserver/impl/DistributedObjectServer.java
@@ -1051,7 +1051,7 @@ public class DistributedObjectServer implements TCDumper, LockInfoDumpHandler, S
 
     this.serverMapRequestManager = this.serverBuilder
         .createServerMapRequestManager(this.objectManager, channelManager, respondToServerTCMapStage.getSink(),
-                                       objectRequestStage.getSink(), this.clientStateManager);
+                                       objectRequestStage.getSink(), this.clientStateManager, channelStats);
     this.dumpHandler.registerForDump(new CallbackDumpAdapter(this.serverMapRequestManager));
 
     final ServerTransactionFactory serverTransactionFactory = new ServerTransactionFactory();
