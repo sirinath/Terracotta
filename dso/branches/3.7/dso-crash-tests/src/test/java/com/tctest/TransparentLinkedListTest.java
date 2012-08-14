ed24b40cb93da16981e975fcefba2659e07a5744
twu <twu@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-08-14 22:05:27 +0000

    MNK-3950: Reduced the number of clients a bit

--- dso/branches/3.7/dso-crash-tests/src/test/java/com/tctest/TransparentLinkedListTest.java
+++ dso/branches/3.7/dso-crash-tests/src/test/java/com/tctest/TransparentLinkedListTest.java
@@ -8,25 +8,29 @@ import com.tc.config.schema.setup.TestConfigurationSetupManagerFactory;
 import com.terracottatech.config.PersistenceMode;
 
 public class TransparentLinkedListTest extends TransparentTestBase implements TestConfigurator {
-  private static final int NODE_COUNT           = 3;
-  private static final int EXECUTION_COUNT      = 3;
+  private static final int NODE_COUNT           = 2;
+  private static final int EXECUTION_COUNT      = 2;
   private static final int LOOP_ITERATION_COUNT = 3;
 
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setApplicationInstancePerClientCount(EXECUTION_COUNT)
         .setIntensity(LOOP_ITERATION_COUNT);
     t.initializeTestRunner();
   }
 
+  @Override
   protected void setupConfig(TestConfigurationSetupManagerFactory configFactory) {
     configFactory.setGCEnabled(true);
     configFactory.setPersistenceMode(PersistenceMode.TEMPORARY_SWAP_ONLY);
   }
 
+  @Override
   protected Class getApplicationClass() {
     return TransparentLinkedListTestApp.class;
   }
 
+  @Override
   protected boolean canRunCrash() {
     return true;
   }
