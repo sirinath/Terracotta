85f03d7d50ee9a5320fb35963e64d89bb15e38bf
amaheshw <amaheshw@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-08-29 06:40:26 +0000

    dev-8059, merge 20598 in 3.7.x

--- dso/branches/3.7/dso-l1/src/main/java/com/tc/object/bytecode/hook/impl/DSOContextImpl.java
+++ dso/branches/3.7/dso-l1/src/main/java/com/tc/object/bytecode/hook/impl/DSOContextImpl.java
@@ -239,6 +239,7 @@ public class DSOContextImpl implements DSOContext {
       }
     }
     getClassResource("non.existent.Class", getClass().getClassLoader(), true);
+    logger.info("DSOContext created with expressRejoinClient=" + expressRejoinClient);
   }
 
   private void resolveClasses() {
