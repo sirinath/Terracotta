e6835fedb63c61a018086dd93669203bdffb618d
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-09-06 20:26:29 +0000

    3.6.4-rc

---
+++ dso/branches/3.6.4-rc/dso-l1/src/main/java/com/tc/object/TCObjectSelfStore.java
@@ -0,0 +1,44 @@
+/*
+ */
+package com.tc.object;
+
+import com.tc.invalidation.Invalidations;
+import com.tc.net.NodeID;
+import com.tc.object.servermap.localcache.AbstractLocalCacheStoreValue;
+import com.tc.object.servermap.localcache.L1ServerMapLocalCacheStore;
+
+import java.util.Set;
+
+public interface TCObjectSelfStore {
+  void initializeTCObjectSelfStore(TCObjectSelfCallback callback);
+
+  Object getById(ObjectID oid);
+
+  boolean addTCObjectSelf(L1ServerMapLocalCacheStore store, AbstractLocalCacheStoreValue localStoreValue,
+                          Object tcoself, boolean isNew);
+
+  int size();
+
+  void addAllObjectIDs(Set oids);
+
+  boolean contains(ObjectID objectID);
+
+  void addTCObjectSelfTemp(TCObjectSelf obj);
+
+  void removeTCObjectSelfTemp(TCObjectSelf value, boolean notifyServer);
+
+  public void removeTCObjectSelf(AbstractLocalCacheStoreValue localStoreValue);
+
+  void initializeTCObjectSelfIfRequired(TCObjectSelf tcoSelf);
+
+  public void removeObjectById(ObjectID oid);
+
+  /**
+   * Handshake manager tries to get hold of all the objects present in the local caches
+   */
+  public void addAllObjectIDsToValidate(Invalidations invalidations, NodeID remoteID);
+
+  void shutdown();
+
+  void removeTCObjectSelf(TCObjectSelf value);
+}
