a2279feb3c98ba8cfdbc066a1170ef5525766a1a
apurv <apurv@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-09-26 09:51:58 +0000

    Initial commit of license-server

---
+++ dso/branches/community-license-server/license-app/src/main/java/org/terracotta/license/LicenseUsageManagerImpl.java
@@ -0,0 +1,11 @@
+package org.terracotta.license;
+
+/**
+ * Class to maintain the current state of the cluster
+ * in terms of allocated resources and resources available.
+ *
+ */
+public class LicenseUsageManagerImpl {
+
+
+}
