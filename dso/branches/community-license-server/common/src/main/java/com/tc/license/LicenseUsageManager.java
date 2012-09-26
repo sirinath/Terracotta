30d487aa3dcc946aedc0fc8bec0e78f427640c42
nbangarw <nbangarw@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-09-26 19:11:00 +0000

    dev-week license manager rest api

---
+++ dso/branches/community-license-server/common/src/main/java/com/tc/license/LicenseUsageManager.java
@@ -0,0 +1,32 @@
+package com.tc.license;
+
+import org.terracotta.license.License;
+
+public interface LicenseUsageManager {
+
+  public enum LicenseServerState {
+    UNINITIALIZED, ACTIVE, PASSIVE
+  }
+
+  public boolean allocateL1BigMemory(String clientUUID, String fullyQualifiedCacheName, long memoryInBytes);
+
+  public void releaseL1BigMemory(String clientUUID, String fullyQualifiedCacheName);
+
+  public void allocateL2BigMemory(String serverUUID, long memoryInBytes);
+
+  public void releaseL2BigMemory(String serverUUID);
+
+  public void freeUpAllResources(String UUID);
+
+  public void l1Joined(String clientUUID);
+
+  public void l1Removed(String clientUUID);
+
+  public boolean verifyCapability(String capability);
+
+  public boolean reloadLicense(License license);
+
+  public License getLicense();
+
+  public LicenseServerState getState();
+}
---
+++ dso/branches/community-license-server/deploy/src/main/java/com/tc/server/LicenseServlet.java
@@ -0,0 +1,93 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.server;
+
+import org.mortbay.util.ajax.JSON;
+import org.terracotta.license.License;
+import org.terracotta.license.LicenseException;
+
+import com.tc.license.LicenseUsageManager;
+import com.tc.license.LicenseUsageManager.LicenseServerState;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LicenseServlet extends RestfulServlet {
+
+  public static String        LICENSE_USAGE_MANAGER_ATTRIBUTE = "LICENSE_USAGE_MANAGER_ATTRIBUTE";
+  private LicenseUsageManager licenseUsageManager;
+
+  @Override
+  public void init() {
+    this.licenseUsageManager = (LicenseUsageManager) getServletContext().getAttribute(LICENSE_USAGE_MANAGER_ATTRIBUTE);
+  }
+
+  public void methodFetchLicense(final HttpServletRequest request, final HttpServletResponse response) throws Throwable {
+    License license = licenseUsageManager.getLicense();
+    Map<String, String> responseMap = new HashMap<String, String>();
+    responseMap.put(LicenseServerConstants.LICENSE, license.toString());
+    sendResponse(response, responseMap, true);
+  }
+
+  public void methodState(final HttpServletRequest request, final HttpServletResponse response) throws Throwable {
+    LicenseServerState state = licenseUsageManager.getState();
+    Map<String, String> responseMap = new HashMap<String, String>();
+    responseMap.put(LicenseServerConstants.STATE, state.toString());
+    sendResponse(response, responseMap, true);
+  }
+
+  public void methodAllocateL1BigMemory(final HttpServletRequest request, final HttpServletResponse response)
+      throws Throwable {
+    try {
+      String uuid = request.getParameter(LicenseServerConstants.JVM_UUID);
+      String cacheName = request.getParameter(LicenseServerConstants.FULLY_QUALIFIED_CACHE_NAME);
+      Long memory = Long.parseLong(request.getParameter(LicenseServerConstants.MEMORY));
+      licenseUsageManager.allocateL1BigMemory(uuid, cacheName, memory);
+      sendResponse(response, null, true);
+    } catch (Exception e) {
+      sendResponse(response, null, false);
+    }
+  }
+
+  public void methodReleaseL1BigMemory(final HttpServletRequest request, final HttpServletResponse response)
+      throws Throwable {
+    try {
+      String uuid = request.getParameter(LicenseServerConstants.JVM_UUID);
+      String cacheName = request.getParameter(LicenseServerConstants.FULLY_QUALIFIED_CACHE_NAME);
+      licenseUsageManager.releaseL1BigMemory(uuid, cacheName);
+      sendResponse(response, null, true);
+    } catch (Exception e) {
+      sendResponse(response, null, false);
+    }
+
+  }
+
+  private void sendResponse(final HttpServletResponse response, Map<String, String> attributes, boolean success)
+      throws Exception {
+    final Map<String, String> responseMap = new HashMap<String, String>();
+
+    responseMap.put(LicenseServerConstants.RESPONSE_CODE, success ? LicenseServerConstants.SUCCESS_CODE
+        : LicenseServerConstants.FAILURE_CODE);
+    if (attributes != null) {
+      responseMap.putAll(attributes);
+    }
+    print(response, JSON.toString(responseMap));
+  }
+
+  public void methodTest(final HttpServletRequest request, final HttpServletResponse response) throws Throwable {
+    printOk(response);
+  }
+
+  public void methodHello(final HttpServletRequest request, final HttpServletResponse response) throws Throwable {
+    print(response, "Hello");
+  }
+
+  public void methodException(final HttpServletRequest request, final HttpServletResponse response) throws Throwable {
+    throw new LicenseException("Something Worng with your License");
+  }
+
+}
