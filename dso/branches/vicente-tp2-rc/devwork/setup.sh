61b8a64a20a9b06db8d718588cadcf28895badc1
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-12-03 23:56:51 +0000

    updated for vicente-tp2-rc

--- dso/branches/vicente-tp2-rc/devwork/setup.sh
+++ dso/branches/vicente-tp2-rc/devwork/setup.sh
@@ -1,12 +1,11 @@
 # trunk: https://svn.terracotta.org/repo/internal/enterprise/trunk
-svn co https://svn.terracotta.org/repo/internal/search/trunk search
-svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/trunk/ terracotta-license
-svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/trunk management-apps
-svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/trunk fast-restartable-store
-svn co https://svn.terracotta.org/repo/internal/offheap-store/trunk offheap-store
-svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
-svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
-svn co https://svn.terracotta.org/repo/forge/projects/management-core/trunk management-core
-svn co https://svn.terracotta.org/repo/quartz/trunk quartz
-svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
-svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/trunk terracotta-toolkit-api
+svn co https://svn.terracotta.org/repo/internal/search/branches/vicente-tp2-rc search
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/branches/vicente-tp2-rc terracotta-license
+svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/vicente-tp2-rc management-apps
+svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/branches/vicente-tp2-rc fast-restartable-store
+svn co https://svn.terracotta.org/repo/internal/offheap-store/branches/vicente-tp2-rc offheap-store
+svn co https://svn.terracotta.org/repo/ehcache/branches/vicente-tp2-rc/ehcache ehcache
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/vicente-tp2-rc ehcache-ee
+svn co https://svn.terracotta.org/repo/forge/projects/management-core/branches/vicente-tp2-rc management-core
+svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/branches/vicente-tp2-rc terracotta-toolkit-api
+
