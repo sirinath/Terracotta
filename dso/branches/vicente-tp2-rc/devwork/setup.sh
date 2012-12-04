f7394b9f54b37525c96ecb898f862b9e35c10f2c
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-12-04 02:05:25 +0000

    fix bad repo locations

--- dso/branches/vicente-tp2-rc/devwork/setup.sh
+++ dso/branches/vicente-tp2-rc/devwork/setup.sh
@@ -4,7 +4,7 @@ svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/branc
 svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/vicente-tp2-rc management-apps
 svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/branches/vicente-tp2-rc fast-restartable-store
 svn co https://svn.terracotta.org/repo/internal/offheap-store/branches/vicente-tp2-rc offheap-store
-svn co https://svn.terracotta.org/repo/ehcache/branches/vicente-tp2-rc/ehcache ehcache
+svn co https://svn.terracotta.org/repo/ehcache/branches/vicente-tp2-rc ehcache
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/vicente-tp2-rc ehcache-ee
 svn co https://svn.terracotta.org/repo/forge/projects/management-core/branches/vicente-tp2-rc management-core
 svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/branches/vicente-tp2-rc terracotta-toolkit-api
