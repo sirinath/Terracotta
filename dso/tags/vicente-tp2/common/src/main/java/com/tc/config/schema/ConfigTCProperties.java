f5c54d2de0aa01e7e8a110f67fd420b2774967e6
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-12-04 20:24:19 +0000

    Tag vicente-tp2 from vicente-tp2-rc branch

---
+++ dso/tags/vicente-tp2/common/src/main/java/com/tc/config/schema/ConfigTCProperties.java
@@ -0,0 +1,11 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.config.schema;
+
+import com.tc.config.TcProperty;
+
+public interface ConfigTCProperties {
+  
+  public TcProperty[] getTcPropertiesArray();
+}
