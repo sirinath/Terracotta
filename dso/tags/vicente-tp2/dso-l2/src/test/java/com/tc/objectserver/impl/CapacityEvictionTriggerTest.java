4857b10fbf95bd725634f86dad28cea90b21b1e0
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2012-12-11 01:59:57 +0000

    Tag vicente-tp2 from vicente-tp2-rc branch

---
+++ dso/tags/vicente-tp2/dso-l2/src/test/java/com/tc/objectserver/impl/CapacityEvictionTriggerTest.java
@@ -0,0 +1,49 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.objectserver.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import com.tc.object.ObjectID;
+import com.tc.objectserver.api.ServerMapEvictionManager;
+import com.tc.objectserver.l1.impl.ClientObjectReferenceSetChangedListener;
+
+/**
+ *
+ * @author mscott
+ */
+public class CapacityEvictionTriggerTest extends AbstractEvictionTriggerTest {
+    
+    public CapacityEvictionTriggerTest() {
+    }
+
+    @Override
+    public AbstractEvictionTrigger createTrigger() {
+        return new CapacityEvictionTrigger(Mockito.mock(ServerMapEvictionManager.class), ObjectID.NULL_ID);
+    }
+    
+    @Test
+    public void testCapacityEvictionChaining() throws Exception {
+        //  ten million elements in map
+        Mockito.when(getEvictableMap().getSize()).thenReturn(10000000);
+        //  set max to 250k
+        Mockito.when(getEvictableMap().getMaxTotalCount()).thenReturn(250000);
+        checkCycle(250000);
+    Mockito.verify(this.getClientSet())
+        .addReferenceSetChangeListener(Matchers.<ClientObjectReferenceSetChangedListener> any());
+    }
+
+    @Override @Before
+    public void setUp() {
+        getEvictableMap().startEviction();
+        Mockito.when(getEvictableMap().isEvicting()).thenReturn(Boolean.TRUE);
+        Mockito.when(getEvictableMap().getSize()).thenReturn(250);
+        super.setUp();
+    }
+    
+    
+}
---
+++ dso/tags/vicente-tp2/management-agent/management-tsa-impl/src/main/java/com/terracotta/management/service/TsaManagementClientService.java
@@ -0,0 +1,58 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.management.service;
+
+import net.sf.ehcache.management.service.impl.DfltSamplerRepositoryServiceMBean;
+import org.terracotta.management.ServiceExecutionException;
+
+import com.terracotta.management.resource.ClientEntity;
+import com.terracotta.management.resource.ConfigEntity;
+import com.terracotta.management.resource.ServerGroupEntity;
+import com.terracotta.management.resource.StatisticsEntity;
+import com.terracotta.management.resource.ThreadDumpEntity;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * An interface for service implementations providing an abstraction of the TSA capabilities.
+ *
+ * @author Ludovic Orban
+ */
+public interface TsaManagementClientService {
+
+  Collection<ThreadDumpEntity> clientsThreadDump(Set<String> clientIds) throws ServiceExecutionException;
+
+  Collection<ThreadDumpEntity> serversThreadDump(Set<String> serverNames) throws ServiceExecutionException;
+
+  Collection<ClientEntity> getClientEntities() throws ServiceExecutionException;
+
+  Collection<ServerGroupEntity> getTopology() throws ServiceExecutionException;
+
+  StatisticsEntity getClientStatistics(String clientId, Set<String> attributes) throws ServiceExecutionException;
+
+  StatisticsEntity getServerStatistics(String serverName, Set<String> attributes) throws ServiceExecutionException;
+
+  Set<String> getAllClientIds() throws ServiceExecutionException;
+
+  Set<String> getAllServerNames() throws ServiceExecutionException;
+
+  boolean runDgc() throws ServiceExecutionException;
+
+  Collection<StatisticsEntity> getDgcStatistics(int maxDgcStatsEntries) throws ServiceExecutionException;
+
+  Set<String> getL2Urls() throws ServiceExecutionException;
+
+  Set<String> getL1Nodes() throws ServiceExecutionException;
+
+  boolean isEnterpriseEdition() throws ServiceExecutionException;
+
+  byte[] invokeMethod(String validatedNode, Class<DfltSamplerRepositoryServiceMBean> clazz, String ticket, String token,
+                      String securityCallbackUrl, String methodName, Class<?>[] paramClasses, Object[] params) throws ServiceExecutionException;
+
+  Collection<ConfigEntity> getServerConfigs(Set<String> serverNames) throws ServiceExecutionException;
+
+  Collection<ConfigEntity> getClientConfigs(Set<String> clientIds) throws ServiceExecutionException;
+
+}
