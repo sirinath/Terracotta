4763835ba5829e415331345dce5971533d06803e
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-01-12 12:22:58 +0000

    Tag 3.6.6 from 3.6.6-rc branch

---
+++ dso/tags/3.6.6/devwork/setup.sh
@@ -0,0 +1,14 @@
+# tc-3.6: https://svn.terracotta.org/repo/internal/enterprise/branches/3.6
+svn co https://svn.terracotta.org/repo/ehcache/branches/ehcache-core-2.5.x ehcache-core
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/ehcache-core-ee-2.5.x ehcache-core-ee
+svn co https://svn.terracotta.org/repo/quartz/branches/quartz-2.1.x/quartz quartz
+svn co https://svn.terracotta.org/repo/forge/projects/terracotta-ehcache/branches/tc-3.6 terracotta-ehcache
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-ehcache-ee/branches/tc-3.6 terracotta-ehcache-ee
+svn co https://svn.terracotta.org/repo/forge/projects/terracotta-quartz/branches/tc-3.6 terracotta-quartz
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-quartz-ee/branches/tc-3.6 terracotta-quartz-ee
+svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit/branches/tc-3.6.3 terracotta-toolkit
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-toolkit-ee/branches/tc-3.6.3 terracotta-toolkit-ee
+svn co https://svn.terracotta.org/repo/forge/projects/tim-ehcache/branches/tc-3.6 tim-ehcache
+svn co https://svn.terracotta.org/repo/forge/enterprise/tim-ehcache-ee/branches/tc-3.6 tim-ehcache-ee
+svn co https://svn.terracotta.org/repo/forge/projects/tim-quartz/branches/tc-3.6 tim-quartz
+svn co https://svn.terracotta.org/repo/forge/enterprise/tim-quartz-ee/branches/tc-3.6 tim-quartz-ee
