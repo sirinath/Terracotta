5711a14ba9eb32d4575f2cf20d8806cdcc6dd13b
twu <twu@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-02-05 22:59:25 +0000

    DEV-9048: Ignore transitions from missing object to missing object state for client side object lookups.

--- dso/branches/inline-dgc-rework/dso-l1/src/main/java/com/tc/object/LookupState.java
+++ dso/branches/inline-dgc-rework/dso-l1/src/main/java/com/tc/object/LookupState.java
@@ -93,6 +93,11 @@ enum LookupState implements LookupStateTransition {
 
   MISSING_OBJECT_ID {
     @Override
+    public LookupState makeMissingObject() {
+      return this;
+    }
+
+    @Override
     public boolean isMissing() {
       return true;
     }
