881c39d0fbec31ae4867ac12f20b3fb3c529c05e
jvoegele <jvoegele@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-02-05 19:17:09 +0000

    Branch for Vicente FCS.

---
+++ dso/branches/4.0/toolkit-impl/src/main/java/com/terracotta/toolkit/concurrent/locks/ToolkitLockingApi.java
@@ -0,0 +1,189 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.toolkit.concurrent.locks;
+
+import org.terracotta.toolkit.ToolkitObjectType;
+import org.terracotta.toolkit.internal.concurrent.locks.ToolkitLockTypeInternal;
+
+import com.tc.abortable.AbortedOperationException;
+import com.tc.object.ObjectID;
+import com.tc.platform.PlatformService;
+import com.terracotta.toolkit.abortable.ToolkitAbortableOperationException;
+
+import java.util.concurrent.TimeUnit;
+
+public class ToolkitLockingApi {
+
+  private static final String STRING_LOCK_ID_PREFIX      = "string-lock-prefix";
+  private static final String TOOLKIT_OBJECT_LOCK_PREFIX = "toolkit-object-lock-prefix";
+  private static final String DELIMITER                  = "|";
+
+  private ToolkitLockingApi() {
+    // private
+  }
+
+  // used just for txn boundaries
+  public static UnnamedToolkitLock createConcurrentTransactionLock(String name, PlatformService service) {
+    return new UnnamedToolkitLock(service, name, ToolkitLockTypeInternal.CONCURRENT);
+  }
+
+  // locks created for name based toolkitObjectTypes
+  public static UnnamedToolkitLock createUnnamedLocked(ToolkitObjectType toolkitObjectType, String stringLockId,
+                                                       ToolkitLockTypeInternal lockType, PlatformService service) {
+    return new UnnamedToolkitLock(service, generateStringLockId(toolkitObjectType, stringLockId), lockType);
+  }
+
+  // RWLsocks created for toolkitLockedObjects
+  public static UnnamedToolkitReadWriteLock createUnnamedReadWriteLock(ToolkitObjectType type, ObjectID oid,
+                                                                       PlatformService service) {
+    return new UnnamedToolkitReadWriteLock(service, generateStringLockId(type, String.valueOf(oid.toLong())));
+  }
+
+  // RWLsocks created for toolkitObject - used for ToolkitReadWriteLock
+  public static UnnamedToolkitReadWriteLock createUnnamedReadWriteLock(ToolkitObjectType toolkitObjectType,
+                                                                       String name, PlatformService service) {
+    return new UnnamedToolkitReadWriteLock(service, generateStringLockId(toolkitObjectType, name));
+  }
+
+  // used for servermap keys
+  public static UnnamedToolkitReadWriteLock createUnnamedReadWriteLock(long longLockId, PlatformService service) {
+    return new UnnamedToolkitReadWriteLock(service, longLockId);
+  }
+
+  public static void lock(ToolkitLockDetail lockDetail, PlatformService service) {
+    try {
+      service.beginLock(lockDetail.getLockId(), lockDetail.getLockLevel());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void unlock(ToolkitLockDetail lockDetail, PlatformService service) {
+    try {
+      service.commitLock(lockDetail.getLockId(), lockDetail.getLockLevel());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void lockInterruptibly(ToolkitLockDetail lockDetail, PlatformService service)
+      throws InterruptedException {
+    try {
+      service.beginLockInterruptibly(lockDetail.getLockId(), lockDetail.getLockLevel());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static boolean tryLock(ToolkitLockDetail lockDetail, PlatformService service) {
+    try {
+      return service.tryBeginLock(lockDetail.getLockId(), lockDetail.getLockLevel());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static boolean tryLock(ToolkitLockDetail lockDetail, long time, TimeUnit unit, PlatformService service)
+      throws InterruptedException {
+    try {
+      return service.tryBeginLock(lockDetail.getLockId(), lockDetail.getLockLevel(), time, unit);
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static boolean isHeldByCurrentThread(ToolkitLockDetail lockDetail, PlatformService service) {
+    try {
+      return service.isHeldByCurrentThread(lockDetail.getLockId(), lockDetail.getLockLevel());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void lockIdWait(ToolkitLockDetail lockDetail, PlatformService service) throws InterruptedException {
+    // 0 timeout means infinite wait
+    try {
+      service.lockIDWait(lockDetail.getLockId(), 0, TimeUnit.MILLISECONDS);
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void lockIdWait(ToolkitLockDetail lockDetail, long time, TimeUnit unit, PlatformService service)
+      throws InterruptedException {
+    // 0 timeout means infinite wait
+    try {
+      service.lockIDWait(lockDetail.getLockId(), time, unit);
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void lockIdNotify(ToolkitLockDetail lockDetail, PlatformService service) {
+    try {
+      service.lockIDNotify(lockDetail.getLockId());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void lockIdNotifyAll(ToolkitLockDetail lockDetail, PlatformService service) {
+    try {
+      service.lockIDNotifyAll(lockDetail.getLockId());
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  public static void lock(long longLockId, ToolkitLockTypeInternal lockType, PlatformService service) {
+    doBeginLock(longLockId, lockType, service);
+  }
+
+  public static void unlock(long longLockId, ToolkitLockTypeInternal lockType, PlatformService service) {
+    doCommitLock(longLockId, lockType, service);
+  }
+
+  public static void lock(String stringLockName, ToolkitLockTypeInternal lockType, PlatformService service) {
+    doBeginLock(generateStringLockId(stringLockName), lockType, service);
+  }
+
+  public static void unlock(String stringLockName, ToolkitLockTypeInternal lockType, PlatformService service) {
+    doCommitLock(generateStringLockId(stringLockName), lockType, service);
+  }
+
+  public static void lock(ToolkitObjectType toolkitObjectType, String toolkitObjectName,
+                          ToolkitLockTypeInternal lockType, PlatformService service) {
+    doBeginLock(generateStringLockId(toolkitObjectType, toolkitObjectName), lockType, service);
+  }
+
+  public static void unlock(ToolkitObjectType toolkitObjectType, String toolkitObjectName,
+                            ToolkitLockTypeInternal lockType, PlatformService service) {
+    doCommitLock(generateStringLockId(toolkitObjectType, toolkitObjectName), lockType, service);
+  }
+
+  private static void doBeginLock(Object lockId, ToolkitLockTypeInternal lockType, PlatformService service) {
+    try {
+      service.beginLock(lockId, LockingUtils.translate(lockType));
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  private static void doCommitLock(Object lockId, ToolkitLockTypeInternal lockType, PlatformService service) {
+    try {
+      service.commitLock(lockId, LockingUtils.translate(lockType));
+    } catch (AbortedOperationException e) {
+      throw new ToolkitAbortableOperationException(e);
+    }
+  }
+
+  private static String generateStringLockId(String stringLockName) {
+    return STRING_LOCK_ID_PREFIX + DELIMITER + stringLockName;
+  }
+
+  private static String generateStringLockId(ToolkitObjectType toolkitObjectType, String toolkitObjectName) {
+    return TOOLKIT_OBJECT_LOCK_PREFIX + DELIMITER + toolkitObjectType.name() + DELIMITER + toolkitObjectName;
+  }
+
+}
---
+++ dso/branches/4.0/toolkit-impl/src/main/java/com/terracotta/toolkit/nonstop/NonStopSubTypeInvocationHandler.java
@@ -0,0 +1,89 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.toolkit.nonstop;
+
+import org.terracotta.toolkit.ToolkitRuntimeException;
+import org.terracotta.toolkit.nonstop.NonStopConfiguration;
+import org.terracotta.toolkit.rejoin.RejoinException;
+
+import com.terracotta.toolkit.abortable.ToolkitAbortableOperationException;
+import com.terracotta.toolkit.util.ToolkitInstanceProxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class NonStopSubTypeInvocationHandler<T> implements InvocationHandler {
+  private final NonStopContext             context;
+  private final NonStopConfigurationLookup nonStopConfigurationLookup;
+  private final T                          delegate;
+  private final Class                      klazz;
+
+  public NonStopSubTypeInvocationHandler(NonStopContext context, NonStopConfigurationLookup nonStopConfigurationLookup,
+                                         T delegate, Class<T> klazz) {
+    this.context = context;
+    this.nonStopConfigurationLookup = nonStopConfigurationLookup;
+    this.delegate = delegate;
+    this.klazz = klazz;
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    NonStopConfiguration nonStopConfiguration = nonStopConfigurationLookup.getNonStopConfiguration();
+
+    if (!nonStopConfiguration.isEnabled()) { return invokeMethod(method, args, delegate); }
+
+    if (nonStopConfiguration.isImmediateTimeoutEnabled() && !context.getNonStopClusterListener().areOperationsEnabled()) { return invokeMethod(method,
+                                                                                                                                               args,
+                                                                                                                                               resolveTimeoutBehavior(nonStopConfiguration)); }
+    boolean started = context.getNonStopManager().tryBegin(getTimeout(nonStopConfiguration));
+    try {
+      context.getNonStopClusterListener().waitUntilOperationsEnabled();
+      Object returnValue = invokeMethod(method, args, delegate);
+      return createNonStopSubtypeIfNecessary(returnValue, method.getReturnType());
+    } catch (ToolkitAbortableOperationException e) {
+      return invokeMethod(method, args, resolveTimeoutBehavior(nonStopConfiguration));
+    } catch (RejoinException e) {
+      // TODO: Review this.. Is this the right place to handle this...
+      return invokeMethod(method, args, resolveTimeoutBehavior(nonStopConfiguration));
+    } finally {
+      if (started) {
+        context.getNonStopManager().finish();
+      }
+    }
+  }
+
+  private Object resolveTimeoutBehavior(NonStopConfiguration nonStopConfiguration) {
+    return context.getNonstopTimeoutBehaviorResolver()
+        .resolveTimeoutBehaviorForSubType(nonStopConfigurationLookup.getObjectType(), nonStopConfiguration, klazz);
+  }
+
+  protected Object createNonStopSubtypeIfNecessary(Object returnValue, Class klazzParam) {
+    if (NonStopSubTypeUtil.isNonStopSubtype(klazzParam)) {
+      return ToolkitInstanceProxy.newNonStopSubTypeProxy(nonStopConfigurationLookup, context, returnValue, klazzParam);
+    } else {
+      return returnValue;
+    }
+  }
+
+  private long getTimeout(NonStopConfiguration nonStopConfiguration) {
+    if (nonStopConfiguration.isEnabled()) {
+      return nonStopConfiguration.getTimeoutMillis();
+    } else {
+      return -1;
+    }
+  }
+
+  private Object invokeMethod(Method method, Object[] args, Object object) throws Throwable {
+    try {
+      return method.invoke(object, args);
+    } catch (InvocationTargetException t) {
+      throw t.getTargetException();
+    } catch (IllegalArgumentException e) {
+      throw new ToolkitRuntimeException(e);
+    } catch (IllegalAccessException e) {
+      throw new ToolkitRuntimeException(e);
+    }
+  }
+}
