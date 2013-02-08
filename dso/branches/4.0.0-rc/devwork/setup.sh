8fe860da92991e0054189e936d0b520dc353c8e2
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-02-08 23:31:04 +0000

    updated for 4.0.0-rc branch

--- dso/branches/4.0.0-rc/devwork/setup.sh
+++ dso/branches/4.0.0-rc/devwork/setup.sh
@@ -1,16 +1,17 @@
 # trunk: https://svn.terracotta.org/repo/internal/enterprise/trunk
-svn co https://svn.terracotta.org/repo/internal/search/trunk search
-svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/trunk/ terracotta-license
-svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/trunk management-apps
-svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/trunk fast-restartable-store
-svn co https://svn.terracotta.org/repo/internal/offheap-store/trunk offheap-store
-svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
+svn co https://svn.terracotta.org/repo/internal/search/branches/4.0.0-rc search
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/branches/4.0.0-rc terracotta-license
+svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/4.0.0-rc management-apps
+svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/branches/4.0.0-rc fast-restartable-store
+svn co https://svn.terracotta.org/repo/internal/offheap-store/branches/4.0.0-rc offheap-store
+svn co https://svn.terracotta.org/repo/ehcache/branches/4.0.0-rc ehcache
+# Why is there no 4.0.0-rc branch for statistics?
 svn co https://svn.terracotta.org/repo/forge/projects/statistics/trunk statistics
-svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
-svn co https://svn.terracotta.org/repo/forge/projects/management-core/trunk management-core
-svn co https://svn.terracotta.org/repo/quartz/trunk quartz
-svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
-svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/trunk terracotta-toolkit-api
-svn co https://svn.terracotta.org/repo/forge/projects/core-storage-api/trunk core-storage-api
-svn co https://svn.terracotta.org/repo/internal/bigmemory-core-storage/trunk bigmemory-core-storage
-svn co https://svn.terracotta.org/repo/forge/projects/heap-core-storage/trunk heap-core-storage
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/4.0.0-rc ehcache-ee
+svn co https://svn.terracotta.org/repo/forge/projects/management-core/branches/4.0.0-rc management-core
+# svn co https://svn.terracotta.org/repo/quartz/trunk quartz
+# svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
+svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/branches/4.0.0-rc terracotta-toolkit-api
+svn co https://svn.terracotta.org/repo/forge/projects/core-storage-api/branches/4.0.0-rc core-storage-api
+svn co https://svn.terracotta.org/repo/internal/bigmemory-core-storage/branches/4.0.0-rc bigmemory-core-storage
+svn co https://svn.terracotta.org/repo/forge/projects/heap-core-storage/branches/4.0.0-rc heap-core-storage
