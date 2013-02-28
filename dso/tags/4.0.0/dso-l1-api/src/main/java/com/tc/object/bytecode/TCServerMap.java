9038ea5a7664e15be3644d9452f5057b3154742b
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-02-28 21:09:35 +0000

    Delete 4.0.0 tag to retag.

--- dso/tags/4.0.0/dso-l1-api/src/main/java/com/tc/object/bytecode/TCServerMap.java
+++
@@ -1,9 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tc.object.bytecode;
-
-public interface TCServerMap extends Manageable {
-
-  public void evictedInServer(boolean notifyEvicted, Object key);
-}
