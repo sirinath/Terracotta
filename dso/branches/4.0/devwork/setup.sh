db4988b1a935a15165e7deb5a377b66b06c503d3
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-03-19 23:08:36 +0000

    merge of change 22463 from trunk

--- dso/branches/4.0/devwork/setup.sh
+++ dso/branches/4.0/devwork/setup.sh
@@ -1,16 +1,6 @@
 # trunk: https://svn.terracotta.org/repo/internal/enterprise/branches/4.0
-svn co https://svn.terracotta.org/repo/internal/search/trunk search
-svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/trunk terracotta-license
 svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/tc-4.0 management-apps
-svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/trunk fast-restartable-store
-svn co https://svn.terracotta.org/repo/internal/offheap-store/trunk offheap-store
 svn co https://svn.terracotta.org/repo/ehcache/branches/ehcache-2.7.x ehcache
-svn co https://svn.terracotta.org/repo/forge/projects/statistics/trunk statistics
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/ehcache-core-ee-2.7.x ehcache-ee
-svn co https://svn.terracotta.org/repo/forge/projects/management-core/branches/tc-4.0 management-core
 #svn co https://svn.terracotta.org/repo/quartz/trunk quartz
 #svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
-svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/trunk terracotta-toolkit-api
-svn co https://svn.terracotta.org/repo/forge/projects/core-storage-api/trunk core-storage-api
-svn co https://svn.terracotta.org/repo/internal/bigmemory-core-storage/trunk bigmemory-core-storage
-svn co https://svn.terracotta.org/repo/forge/projects/heap-core-storage/trunk heap-core-storage
