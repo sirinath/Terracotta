8366983d6b296f735179e5098bce9d368a9728a9
amaheshw <amaheshw@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-04-26 17:01:52 +0000

    cdv-1506, cdv-1507, merge 22729 in 4.0.x

--- dso/branches/4.0/common/src/test/java/com/tc/lang/ThrowableHandlerTest.java
+++ dso/branches/4.0/common/src/test/java/com/tc/lang/ThrowableHandlerTest.java
@@ -8,6 +8,8 @@ import com.tc.logging.CallbackOnExitHandler;
 import com.tc.logging.CallbackOnExitState;
 import com.tc.logging.TCLogging;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import junit.framework.TestCase;
 
 public class ThrowableHandlerTest extends TestCase {
