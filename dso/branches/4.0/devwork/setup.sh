fd2dace303f6cda8e4ce3320dc858d97f51a34a1
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-04-29 22:24:40 +0000

    updated versions

--- dso/branches/4.0/devwork/setup.sh
+++ dso/branches/4.0/devwork/setup.sh
@@ -1,7 +1,6 @@
-# trunk: https://svn.terracotta.org/repo/internal/enterprise/trunk
-svn co https://svn.terracotta.org/repo/internal/search/trunk search
-svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/trunk management-apps
-svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
-svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
-svn co https://svn.terracotta.org/repo/quartz/trunk quartz
-svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
+# 4.0: https://svn.terracotta.org/repo/internal/enterprise/branches/4.0
+svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/tc-4.0 management-apps
+svn co https://svn.terracotta.org/repo/ehcache/branches/ehcache-2.7.x ehcache
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/ehcache-core-ee-2.7.x ehcache-ee
+# svn co https://svn.terracotta.org/repo/quartz/trunk quartz
+# svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
