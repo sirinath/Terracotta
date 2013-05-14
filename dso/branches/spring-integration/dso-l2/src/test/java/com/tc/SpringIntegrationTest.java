5c5d4e4e0ba54d6453b7294dd93fe4dac121ec7e
eshelest <eshelest@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-05-14 21:43:45 +0000

    First Spring integration test added.

--- dso/branches/spring-integration/dso-l2/src/test/java/com/tc/SpringIntegrationTest.java
+++ dso/branches/spring-integration/dso-l2/src/test/java/com/tc/SpringIntegrationTest.java
@@ -1,23 +1,40 @@
 package com.tc;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.tc.objectserver.l1.api.ClientStateManager;
+
+import static com.tc.util.Assert.assertNotNull;
 
 /**
  * @author Eugene Shelestovich
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:context-common.xml")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class SpringIntegrationTest {
+
+  private ClientStateManager clientStateManager;
 
   @Autowired
   public void setClientStateManager(final ClientStateManager clientStateManager) {
+    this.clientStateManager = clientStateManager;
+  }
+
+  @Before
+  public void setUp() {
 
   }
 
+  @Test
+  public void testFoo() {
+    assertNotNull(clientStateManager);
+  }
 
 }
