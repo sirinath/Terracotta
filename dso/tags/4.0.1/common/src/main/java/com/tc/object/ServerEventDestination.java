dfbfcf357a0a67e0d78cb8bf7e7d8acdbf9f8b0f
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-05-23 23:29:22 +0000

    Delete 4.0.1 tag to retag.

--- dso/tags/4.0.1/common/src/main/java/com/tc/object/ServerEventDestination.java
+++
@@ -1,9 +0,0 @@
-package com.tc.object;
-
-/**
- * @author Eugene Shelestovich
- */
-public interface ServerEventDestination {
-  String getDestinationName();
-  void handleServerEvent(ServerEventType type, Object key);
-}
