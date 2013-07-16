239eefeca094b1ae6e6c36c2ec9cced5e08b3c11
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-07-16 17:27:29 +0000

    4.0.3-rc

---
+++ dso/branches/4.0.3-rc/common/src/main/java/com/tc/handler/CallbackZapServerNodeExceptionAdapter.java
@@ -0,0 +1,30 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.handler;
+
+import com.tc.logging.CallbackOnExitState;
+import com.tc.logging.TCLogger;
+import com.tc.object.persistence.api.PersistentMapStore;
+
+public class CallbackZapServerNodeExceptionAdapter extends CallbackDirtyDatabaseCleanUpAdapter {
+
+  private final TCLogger consoleLogger;
+  private String         consoleMessage = "This Terracotta server instance shut down because of a "
+                                          + "conflict or communication failure with another Terracotta "
+                                          + "server instance. The database must be manually wiped before "
+                                          + "it can be started and allowed to rejoin the cluster.";
+
+  public CallbackZapServerNodeExceptionAdapter(TCLogger logger, TCLogger consoleLogger,
+                                               PersistentMapStore clusterStateStore) {
+    super(logger, clusterStateStore);
+    this.consoleLogger = consoleLogger;
+  }
+
+  @Override
+  public void callbackOnExit(CallbackOnExitState state) {
+    super.callbackOnExit(state);
+    consoleLogger.error(consoleMessage + "\n");
+  }
+
+}
