24be23f4f52ac772ca4cb63ff6384fa35fb0cb73
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-08-23 13:54:01 +0000

    Tag 4.0.4 from 4.0.4-rc branch

---
+++ dso/tags/4.0.4/dso-l2/src/main/java/com/tc/objectserver/search/NullSearchRequestManager.java
@@ -0,0 +1,21 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.objectserver.search;
+
+import com.tc.async.api.ConfigurationContext;
+import com.tc.object.msg.SearchQueryRequestMessage;
+
+public class NullSearchRequestManager implements SearchRequestManager {
+
+  @Override
+  public void queryRequest(SearchQueryRequestMessage request) {
+    //
+  }
+
+  @Override
+  public void initializeContext(ConfigurationContext context) {
+    //
+  }
+
+}
