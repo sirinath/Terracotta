4e4bb2df7a6b046a29ee90065a5ab983fc47d3ae
amaheshw <amaheshw@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-09-06 12:07:02 +0000

    internal api removal branch

---
+++ dso/branches/internal-api-remove/dso-l2/src/test/java/com/tc/object/TestRequestManagedObjectMessage.java
@@ -0,0 +1,98 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object;
+
+import com.tc.net.ClientID;
+import com.tc.net.NodeID;
+import com.tc.net.protocol.tcm.MessageChannel;
+import com.tc.object.msg.RequestManagedObjectMessage;
+import com.tc.util.ObjectIDSet;
+
+import java.util.Set;
+
+public class TestRequestManagedObjectMessage implements RequestManagedObjectMessage {
+
+  private ObjectIDSet removed;
+  private ObjectIDSet objectIDs;
+
+  public TestRequestManagedObjectMessage() {
+    super();
+  }
+
+  @Override
+  public ObjectRequestID getRequestID() {
+    return null;
+  }
+
+  @Override
+  public ObjectIDSet getRequestedObjectIDs() {
+    return this.objectIDs;
+  }
+
+  public void setObjectIDs(ObjectIDSet IDs) {
+    this.objectIDs = IDs;
+  }
+
+  @Override
+  public ObjectIDSet getRemoved() {
+    return this.removed;
+  }
+
+  public void setRemoved(ObjectIDSet rm) {
+    this.removed = rm;
+  }
+
+  @Override
+  public void initialize(ObjectRequestID rID, Set<ObjectID> requestedObjectIDs, int requestDepth,
+                         ObjectIDSet removeObjects) {
+    //
+  }
+
+  @Override
+  public void send() {
+    //
+  }
+
+  @Override
+  public MessageChannel getChannel() {
+    return null;
+  }
+
+  @Override
+  public NodeID getSourceNodeID() {
+    return new ClientID(0);
+  }
+
+  @Override
+  public int getRequestDepth() {
+    return 400;
+  }
+
+  @Override
+  public void recycle() {
+    return;
+  }
+
+  @Override
+  public String getRequestingThreadName() {
+    return "TestThreadDummy";
+  }
+
+  @Override
+  public LOOKUP_STATE getLookupState() {
+    return LOOKUP_STATE.CLIENT;
+  }
+
+  @Override
+  public ClientID getClientID() {
+    return new ClientID(0);
+  }
+
+  @Override
+  public Object getKey() {
+    return null;
+  }
+
+}
