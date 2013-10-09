b4c1845a54590db511e6cc40c0af3f9a174f79dd
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-10-09 02:49:29 +0000

    INT-2715: creating RC branches

---
+++ dso/branches/4.0.5-rc/dso-l1/src/main/java/com/tc/object/tx/ClientTransactionImpl.java
@@ -0,0 +1,178 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object.tx;
+
+import com.tc.object.ObjectID;
+import com.tc.object.TCObject;
+import com.tc.object.change.TCChangeBuffer;
+import com.tc.object.change.TCChangeBufferImpl;
+import com.tc.object.dmi.DmiDescriptor;
+import com.tc.object.locks.Notify;
+import com.tc.object.metadata.MetaDataDescriptorInternal;
+import com.tc.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Client side transaction : Collects all changes by a single thread under a lock
+ */
+public class ClientTransactionImpl extends AbstractClientTransaction {
+  private final Map<ObjectID, TCChangeBuffer> objectChanges = new LinkedHashMap<ObjectID, TCChangeBuffer>();
+
+  private Map                                 newRoots;
+  private List                                notifies;
+  private List                                dmis;
+
+  // used to keep things referenced until the transaction is completely ACKED
+  private final Map                           referenced    = new IdentityHashMap();
+  private final int                           session;
+
+  public ClientTransactionImpl(int session) {
+    super();
+    this.session = session;
+  }
+
+  @Override
+  public boolean isConcurrent() {
+    return this.getLockType().isConcurrent();
+  }
+
+  @Override
+  public boolean hasChangesOrNotifies() {
+    return !(objectChanges.isEmpty() && getNewRoots().isEmpty() && getNotifies().isEmpty());
+  }
+
+  @Override
+  public boolean hasChanges() {
+    return !(objectChanges.isEmpty() && getNewRoots().isEmpty());
+  }
+
+  @Override
+  public Map getNewRoots() {
+    return newRoots == null ? Collections.EMPTY_MAP : newRoots;
+  }
+
+  @Override
+  public List getNotifies() {
+    return notifies == null ? Collections.EMPTY_LIST : notifies;
+  }
+
+  @Override
+  public Map getChangeBuffers() {
+    return this.objectChanges;
+  }
+
+  @Override
+  protected void basicLiteralValueChanged(TCObject source, Object newValue, Object oldValue) {
+    getOrCreateChangeBuffer(source).literalValueChanged(newValue);
+    // To prevent it gcing on us.
+    addReferenced(oldValue);
+    addReferenced(newValue);
+  }
+
+  @Override
+  protected void basicFieldChanged(TCObject source, String classname, String fieldname, Object newValue, int index) {
+    getOrCreateChangeBuffer(source).fieldChanged(classname, fieldname, newValue, index);
+  }
+
+  @Override
+  protected void basicArrayChanged(TCObject source, int startPos, Object array, int length) {
+    getOrCreateChangeBuffer(source).arrayChanged(startPos, array, length);
+  }
+
+  @Override
+  protected void basicLogicalInvoke(TCObject source, int method, Object[] parameters) {
+    getOrCreateChangeBuffer(source).logicalInvoke(method, parameters);
+  }
+
+  @Override
+  protected void basicCreate(TCObject object) {
+    getOrCreateChangeBuffer(object);
+  }
+
+  @Override
+  protected void basicCreateRoot(String name, ObjectID root) {
+    if (newRoots == null) {
+      newRoots = new HashMap();
+    }
+    newRoots.put(name, root);
+  }
+
+  private TCChangeBuffer getOrCreateChangeBuffer(TCObject object) {
+    addReferenced(object.getPeerObject());
+
+    ObjectID oid = object.getObjectID();
+
+    TCChangeBuffer cb = objectChanges.get(oid);
+    if (cb == null) {
+      cb = new TCChangeBufferImpl(object);
+      objectChanges.put(oid, cb);
+    }
+
+    return cb;
+  }
+
+  private void addReferenced(Object pojo) {
+    Assert.assertNotNull("pojo", pojo);
+    referenced.put(pojo, null);
+  }
+
+  @Override
+  public Collection getReferencesOfObjectsInTxn() {
+    return Collections.unmodifiableCollection(referenced.keySet());
+  }
+
+  @Override
+  public void addNotify(Notify notify) {
+    if (!notify.isNull()) {
+      if (notifies == null) {
+        notifies = new ArrayList();
+      }
+
+      notifies.add(notify);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "ClientTransactionImpl@" + System.identityHashCode(this) + " [ " + getTransactionID() + " ]";
+  }
+
+  @Override
+  public int getNotifiesCount() {
+    return getNotifies().size();
+  }
+
+  @Override
+  public void addDmiDescriptor(DmiDescriptor dd) {
+    if (dmis == null) {
+      dmis = new ArrayList();
+    }
+    dmis.add(dd);
+  }
+
+  @Override
+  protected void basicAddMetaDataDescriptor(TCObject tco, MetaDataDescriptorInternal md) {
+    getOrCreateChangeBuffer(tco).addMetaDataDescriptor(md);
+  }
+
+  @Override
+  public List getDmiDescriptors() {
+    return dmis == null ? Collections.EMPTY_LIST : dmis;
+  }
+
+  @Override
+  public int getSession() {
+    return session;
+  }
+
+}
