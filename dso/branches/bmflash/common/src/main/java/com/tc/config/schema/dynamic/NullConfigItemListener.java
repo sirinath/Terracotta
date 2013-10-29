24fe9a369774864e144a6ef5411e4f39c0928352
twu <twu@7fc7bbf3-cf45-46d4-be06-341739edd864>
2013-10-29 00:20:51 +0000

    Branch off for bigmemory flash

---
+++ dso/branches/bmflash/common/src/main/java/com/tc/config/schema/dynamic/NullConfigItemListener.java
@@ -0,0 +1,26 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.config.schema.dynamic;
+
+/**
+ * A {@link ConfigItemListener} that does precisely nothing.
+ */
+public class NullConfigItemListener implements ConfigItemListener {
+
+  private static final NullConfigItemListener INSTANCE = new NullConfigItemListener();
+  
+  public static NullConfigItemListener getInstance() {
+    return INSTANCE;
+  }
+  
+  private NullConfigItemListener() {
+    // Nothing here.
+  }
+  
+  @Override
+  public void valueChanged(Object oldValue, Object newValue) {
+    // Nothing here.
+  }
+
+}
