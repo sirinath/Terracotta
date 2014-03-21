b3b4a789392327dd8ec6cd600b1e8b9598938208
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-03-21 04:20:32 +0000

    Tag 4.1.2 from 4.1.2-rc branch

---
+++ dso/tags/4.1.2/dso-common/src/main/java/com/tc/object/msg/SearchRequestMessageFactory.java
@@ -0,0 +1,13 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.object.msg;
+
+import com.tc.net.NodeID;
+
+public interface SearchRequestMessageFactory {
+
+  SearchQueryRequestMessage newSearchQueryRequestMessage(final NodeID nodeID);
+  
+  SearchResultsRequestMessage newSearchResultsRequestMessage(final NodeID nodeID);
+}
