6c49763d2d0a6f858c767fb956571f39ef268edf
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-03-26 05:41:52 +0000

    Tag 3.7.8 from 3.7.8-rc branch

---
+++ dso/tags/3.7.8/dso-l2/src/test/java/com/tc/objectserver/tx/TestServerTransactionManager.java
@@ -0,0 +1,160 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.objectserver.tx;
+
+import com.tc.exception.ImplementMe;
+import com.tc.net.NodeID;
+import com.tc.object.ObjectID;
+import com.tc.object.tx.ServerTransactionID;
+import com.tc.object.tx.TransactionID;
+import com.tc.objectserver.api.ObjectInstanceMonitor;
+import com.tc.objectserver.core.api.ManagedObject;
+import com.tc.objectserver.managedobject.ApplyTransactionInfo;
+import com.tc.objectserver.storage.api.PersistenceTransactionProvider;
+import com.tc.util.concurrent.NoExceptionLinkedQueue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class TestServerTransactionManager implements ServerTransactionManager {
+
+  public final NoExceptionLinkedQueue skipCalls = new NoExceptionLinkedQueue();
+
+  public TestServerTransactionManager() {
+    //
+  }
+
+  public final NoExceptionLinkedQueue shutdownClientCalls = new NoExceptionLinkedQueue();
+  public final ArrayList              incomingTxnContexts = new ArrayList();
+  public final List                   incomingTxns        = new ArrayList();
+
+  @Override
+  public void shutdownNode(NodeID deadClient) {
+    this.shutdownClientCalls.put(deadClient);
+  }
+
+  @Override
+  public void addWaitingForAcknowledgement(NodeID waiter, TransactionID requestID, NodeID waitee) {
+    throw new ImplementMe();
+
+  }
+
+  @Override
+  public boolean isWaiting(NodeID waiter, TransactionID requestID) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void acknowledgement(NodeID waiter, TransactionID requestID, NodeID waitee) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void broadcasted(NodeID waiter, TransactionID requestID) {
+    // NOP
+  }
+
+  @Override
+  public void skipApplyAndCommit(ServerTransaction txn) {
+    this.skipCalls.put(txn);
+  }
+
+  @Override
+  public void addTransactionListener(ServerTransactionListener listener) {
+    // NOP
+  }
+
+  @Override
+  public void removeTransactionListener(ServerTransactionListener listener) {
+    // NOP
+  }
+
+  @Override
+  public void apply(ServerTransaction txn, Map objects, ApplyTransactionInfo includeIDs,
+                    ObjectInstanceMonitor instanceMonitor) {
+    // NOP
+  }
+
+  @Override
+  public void incomingTransactions(NodeID nodeID, Map<ServerTransactionID, ServerTransaction> txns, boolean relayed) {
+    this.incomingTxnContexts.add(new Object[] { nodeID, txns, Boolean.valueOf(relayed) });
+    this.incomingTxns.addAll(txns.values());
+  }
+
+  @Override
+  public void transactionsRelayed(NodeID node, Set serverTxnIDs) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void commit(PersistenceTransactionProvider ptxp, Collection<ManagedObject> objects,
+                     Map<String, ObjectID> newRoots, Collection<ServerTransactionID> appliedServerTransactionIDs,
+                     SortedSet<ObjectID> deletedObjects) {
+    // NOP
+  }
+
+  @Override
+  public void setResentTransactionIDs(NodeID source, Collection transactionIDs) {
+    // NOP
+  }
+
+  @Override
+  public void start(Set cids) {
+    // NOP
+  }
+
+  @Override
+  public void goToActiveMode() {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void callBackOnTxnsInSystemCompletion(TxnsInSystemCompletionListener l) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void nodeConnected(NodeID nodeID) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public int getTotalPendingTransactionsCount() {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void objectsSynched(NodeID node, ServerTransactionID tid) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void callBackOnResentTxnsInSystemCompletion(TxnsInSystemCompletionListener l) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public long getTotalNumOfActiveTransactions() {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void processingMetaDataCompleted(NodeID sourceID, TransactionID txnID) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void processMetaData(ServerTransaction txn, ApplyTransactionInfo applyInfo) {
+    //
+  }
+
+  public void callbackOnLowWaterMarkInSystemCompletion(Runnable r) {
+    //
+  }
+}
