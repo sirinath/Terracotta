bd05120c662606811354117a3b2117809dca565a
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-04-04 18:20:11 +0000

    Tag 4.0.6 from 4.0.6-rc branch

---
+++ dso/tags/4.0.6/dso-common/src/main/java/com/tc/net/protocol/transport/DefaultConnectionIdFactory.java
@@ -0,0 +1,56 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.net.protocol.transport;
+
+import com.tc.exception.ImplementMe;
+import com.tc.util.UUID;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class DefaultConnectionIdFactory implements ConnectionIDFactory {
+
+  private long         sequence;
+
+  private final String serverID = UUID.getUUID().toString();
+
+  @Override
+  public synchronized ConnectionID nextConnectionId(String clientJvmID) {
+    return new ConnectionID(clientJvmID, sequence++, serverID);
+  }
+
+  @Override
+  public ConnectionID makeConnectionId(String clientJvmID, long channelID) {
+    return new ConnectionID(clientJvmID, channelID, serverID);
+  }
+
+  @Override
+  public Set<ConnectionID> loadConnectionIDs() {
+    return Collections.EMPTY_SET;
+  }
+
+  @Override
+  public void init(String clusterID, long nextAvailChannelID, Set<ConnectionID> connections) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void registerForConnectionIDEvents(ConnectionIDFactoryListener listener) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void restoreConnectionId(ConnectionID rv) {
+    //
+  }
+
+  @Override
+  public long getCurrentConnectionID() {
+    return sequence;
+  }
+
+  public String getServerID() {
+    return serverID;
+  }
+}
