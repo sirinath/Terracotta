cbb3f77444ebdd4de4e258816018e35ac0be7c77
adahanne <adahanne@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-04-24 16:40:04 +0000

    TABQA-7344 : we had to upgrade management-core in management-ehcache, and as such, exception mappers are only present in mangement-core 1.2.x - so upgrading management-core 1.2.3 and re using those mappers in ApplicationTsa

--- dso/branches/4.0/management-agent/management-tsa-impl/src/main/java/com/terracotta/management/ApplicationTsa.java
+++ dso/branches/4.0/management-agent/management-tsa-impl/src/main/java/com/terracotta/management/ApplicationTsa.java
@@ -1,15 +1,27 @@
 package com.terracotta.management;
 
-import com.terracotta.management.resource.services.*;
-import com.terracotta.management.web.resource.services.IdentityAssertionResourceService;
 import net.sf.ehcache.management.resource.services.ElementsResourceServiceImpl;
+
+import org.terracotta.management.application.DefaultApplication;
+
+import com.terracotta.management.resource.services.BackupResourceServiceImpl;
+import com.terracotta.management.resource.services.ConfigurationResourceServiceImpl;
+import com.terracotta.management.resource.services.DiagnosticsResourceServiceImpl;
+import com.terracotta.management.resource.services.JmxResourceServiceImpl;
+import com.terracotta.management.resource.services.LogsResourceServiceImpl;
+import com.terracotta.management.resource.services.MonitoringResourceServiceImpl;
+import com.terracotta.management.resource.services.OperatorEventsResourceServiceImpl;
+import com.terracotta.management.resource.services.ShutdownResourceServiceImpl;
+import com.terracotta.management.resource.services.TopologyResourceServiceImpl;
+import com.terracotta.management.web.resource.services.IdentityAssertionResourceService;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class ApplicationTsa extends javax.ws.rs.core.Application {
+public class ApplicationTsa extends DefaultApplication {
 
 
+  @Override
   public Set<Class<?>> getClasses() {
     Set<Class<?>> s = new HashSet<Class<?>>();
     s.add(ElementsResourceServiceImpl.class);
@@ -30,9 +42,6 @@ public class ApplicationTsa extends javax.ws.rs.core.Application {
     s.add(net.sf.ehcache.management.resource.services.CacheManagerConfigsResourceServiceImpl.class);
     s.add(net.sf.ehcache.management.resource.services.CacheConfigsResourceServiceImpl.class);
     s.add(net.sf.ehcache.management.resource.services.AgentsResourceServiceImpl.class);
-    s.add(net.sf.ehcache.management.resource.exceptions.WebApplicationExceptionMapper.class);
-    s.add(net.sf.ehcache.management.resource.exceptions.DefaultExceptionMapper.class);
-    s.add(net.sf.ehcache.management.resource.exceptions.ResourceRuntimeExceptionMapper.class);
     return s;
   }
 
