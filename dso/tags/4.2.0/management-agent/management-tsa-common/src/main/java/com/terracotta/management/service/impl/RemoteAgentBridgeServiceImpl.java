2fac4d1ec10d65bfeb01b2199e0c0d88a34f07da
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-07-08 16:51:26 +0000

    Tag 4.2.0 from 4.2.0-rc branch

---
+++ dso/tags/4.2.0/management-agent/management-tsa-common/src/main/java/com/terracotta/management/service/impl/RemoteAgentBridgeServiceImpl.java
@@ -0,0 +1,105 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.management.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terracotta.management.ServiceExecutionException;
+import org.terracotta.management.l1bridge.RemoteAgentEndpoint;
+import org.terracotta.management.l1bridge.RemoteCallDescriptor;
+
+import com.terracotta.management.service.RemoteAgentBridgeService;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.management.JMX;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+/**
+ * @author Ludovic Orban
+ */
+public class RemoteAgentBridgeServiceImpl implements RemoteAgentBridgeService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(RemoteAgentBridgeServiceImpl.class);
+
+  private final MBeanServerConnection mBeanServerConnection = ManagementFactory.getPlatformMBeanServer();
+
+  public RemoteAgentBridgeServiceImpl() {
+  }
+
+  @Override
+  public Set<String> getRemoteAgentNodeNames() throws ServiceExecutionException {
+    try {
+      Set<String> nodeNames = new HashSet<String>();
+      Set<ObjectName> objectNames = mBeanServerConnection.queryNames(new ObjectName("*:type=" + RemoteAgentEndpoint.IDENTIFIER + ",*"), null);
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("local server contains {} RemoteAgentEndpoint MBeans", objectNames.size());
+        Set<ObjectName> remoteAgentEndpointObjectNames = mBeanServerConnection.queryNames(new ObjectName("*:*"), null);
+        LOG.debug("server found {} RemoteAgentEndpoint MBeans", remoteAgentEndpointObjectNames.size());
+        for (ObjectName remoteAgentEndpointObjectName : remoteAgentEndpointObjectNames) {
+          LOG.debug("  {}", remoteAgentEndpointObjectName);
+        }
+      }
+      for (ObjectName objectName : objectNames) {
+        String node = objectName.getKeyProperty("node");
+        LOG.debug("RemoteAgentEndpoint node name: {}", node);
+        nodeNames.add(node);
+      }
+      return nodeNames;
+    } catch (Exception e) {
+      throw new ServiceExecutionException("error making JMX call", e);
+    }
+  }
+
+  @Override
+  public Map<String, String> getRemoteAgentNodeDetails(String nodeName) throws ServiceExecutionException {
+    try {
+      ObjectName objectName = findRemoteAgentEndpoint(nodeName);
+
+      Map<String, String> attributes = new HashMap<String, String>();
+      String version = (String)mBeanServerConnection.getAttribute(objectName, "Version");
+      String agency = (String)mBeanServerConnection.getAttribute(objectName, "Agency");
+      attributes.put("Version", version);
+      attributes.put("Agency", agency);
+      return attributes;
+    } catch (ServiceExecutionException see) {
+      throw see;
+    } catch (Exception e) {
+      throw new ServiceExecutionException("error making JMX call", e);
+    }
+  }
+
+  @Override
+  public byte[] invokeRemoteMethod(String nodeName, final RemoteCallDescriptor remoteCallDescriptor) throws ServiceExecutionException {
+    try {
+      ObjectName objectName = findRemoteAgentEndpoint(nodeName);
+
+      RemoteAgentEndpoint proxy = JMX.newMBeanProxy(mBeanServerConnection, objectName, RemoteAgentEndpoint.class);
+      return proxy.invoke(remoteCallDescriptor);
+    } catch (ServiceExecutionException see) {
+      throw see;
+    } catch (Exception e) {
+      throw new ServiceExecutionException("Error making remote L1 call", e);
+    }
+  }
+
+  private ObjectName findRemoteAgentEndpoint(String nodeName) throws IOException, MalformedObjectNameException, ServiceExecutionException {
+    Set<ObjectName> objectNames = mBeanServerConnection.queryNames(new ObjectName("*:type=" + RemoteAgentEndpoint.IDENTIFIER + ",*"), null);
+    for (ObjectName objectName : objectNames) {
+      String node = objectName.getKeyProperty("node");
+      if (nodeName.equals(node)) {
+        return objectName;
+      }
+    }
+    throw new ServiceExecutionException("Cannot find node : " + nodeName);
+  }
+
+}
