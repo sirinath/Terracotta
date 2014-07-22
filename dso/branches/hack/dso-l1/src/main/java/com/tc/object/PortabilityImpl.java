b903b45bfbf110342477dad64aab48ae298e917c
teck <teck@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-07-22 06:40:15 +0000

    allow Object to be portable to satisfy tests

--- dso/branches/hack/dso-l1/src/main/java/com/tc/object/PortabilityImpl.java
+++ dso/branches/hack/dso-l1/src/main/java/com/tc/object/PortabilityImpl.java
@@ -29,7 +29,7 @@ public class PortabilityImpl implements Portability {
 
     String clazzName = clazz.getName();
 
-    boolean bool = LiteralValues.isLiteral(clazzName) || (config.getSpec(clazzName) != null);
+    boolean bool = LiteralValues.isLiteral(clazzName) || (config.getSpec(clazzName) != null) || clazz == Object.class;
     portableCache.put(clazz, Boolean.valueOf(bool));
     return bool;
   }
