b0c350fbd2de68da0f178708715ad236e839d0ce
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-08-12 13:54:57 +0000

    Tag 4.1.4 from 4.1.4-rc branch

---
+++ dso/tags/4.1.4/common/src/main/java/com/tc/text/ParagraphFormatter.java
@@ -0,0 +1,10 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.text;
+
+public interface ParagraphFormatter {
+
+  public String format(String in);
+
+}
