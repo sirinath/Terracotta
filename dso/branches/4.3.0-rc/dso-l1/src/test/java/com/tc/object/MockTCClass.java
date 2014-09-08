53f203e1207fe23440101bb9dc4d9e5e00c21771
ljacomet <ljacomet@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-09-08 09:37:52 +0000

    TABINF-3974: re-create branch for 4.3.0-rc build

---
+++ dso/branches/4.3.0-rc/dso-l1/src/test/java/com/tc/object/MockTCClass.java
@@ -0,0 +1,71 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.object;
+
+import com.tc.exception.ImplementMe;
+import com.tc.object.dna.api.DNA;
+import com.tc.object.dna.api.DNAWriter;
+import com.tc.platform.PlatformService;
+
+import java.lang.reflect.Constructor;
+
+public class MockTCClass implements TCClass {
+  private final String  name = MockTCClass.class.getName();
+
+  public MockTCClass() {
+    //
+  }
+
+  @Override
+  public TraversedReferences getPortableObjects(final Object pojo, final TraversedReferences addTo) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public Constructor getConstructor() {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public void hydrate(final TCObject tcObject, final DNA dna, final Object pojo, final boolean force) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public void dehydrate(final TCObject tcObject, final DNAWriter writer, final Object pojo) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public TCObject createTCObject(final ObjectID id, final Object peer, final boolean isNew) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public boolean isUseNonDefaultConstructor() {
+    return false;
+  }
+
+  @Override
+  public Object getNewInstanceFromNonDefaultConstructor(final DNA dna, PlatformService platformService) {
+    throw new ImplementMe();
+  }
+
+  @Override
+  public Class getPeerClass() {
+    return getClass();
+  }
+
+  @Override
+  public ClientObjectManager getObjectManager() {
+    throw new ImplementMe();
+  }
+
+}
