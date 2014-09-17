56150554bffec5f921e9eb6a722800e90682273e
speddir <speddir@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-09-17 13:36:07 +0000

    TABINF-3984: create branch for 4.0.6_fix1-rc build

---
+++ dso/branches/4.0.6_fix1-rc/dso-l1/src/main/java/com/tc/object/locks/RemoteLockManager.java
@@ -0,0 +1,39 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.object.locks;
+
+import com.tc.abortable.AbortedOperationException;
+import com.tc.net.ClientID;
+import com.tc.object.ClearableCallback;
+
+import java.util.Collection;
+
+public interface RemoteLockManager extends ClearableCallback {
+  public ClientID getClientID();
+
+  public void lock(LockID lock, ThreadID thread, ServerLockLevel level);
+
+  public void tryLock(LockID lock, ThreadID thread, ServerLockLevel level, long timeout);
+
+  public void unlock(LockID lock, ThreadID thread, ServerLockLevel level);
+
+  public void wait(LockID lock, ThreadID thread, long waitTime);
+
+  public void interrupt(LockID lock, ThreadID thread);
+
+  public void recallCommit(LockID lock, Collection<ClientServerExchangeLockContext> lockState, boolean batch);
+
+  public void flush(LockID lock, boolean noLocksLeftOnClient) throws AbortedOperationException;
+
+  public boolean asyncFlush(LockID lock, LockFlushCallback callback, boolean noLocksLeftOnClient);
+
+  public void query(LockID lock, ThreadID thread);
+
+  public void waitForServerToReceiveTxnsForThisLock(LockID lock) throws AbortedOperationException;
+
+  public void shutdown();
+
+  public boolean isShutdown();
+
+}
