4fe010e1d619ad9c84471a3c107e26548c108b55
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-09-23 20:37:57 +0000

    Tag 4.2.0 from 4.2.0-rc branch

---
+++ dso/tags/4.2.0/management-agent/management-tsa-v2/src/main/java/com/terracotta/management/resource/AbstractTsaEntityV2.java
@@ -0,0 +1,17 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.management.resource;
+
+import org.terracotta.management.resource.AbstractEntityV2;
+
+/**
+ * A {@link org.terracotta.management.resource.AbstractEntityV2} abstract subclass from which topology entities
+ * are derived from.
+ *
+ * @author Ludovic Orban
+ */
+public abstract class AbstractTsaEntityV2 extends AbstractEntityV2 {
+
+
+}
