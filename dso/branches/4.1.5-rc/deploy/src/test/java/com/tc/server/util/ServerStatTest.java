6e4818cbeae5bd8bc3f1422959b21d872508f6d6
asuresh <asuresh@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-10-06 10:46:46 +0000

    TAB-5084 merge rev 25695 from branches/4.0.6_fix1-rc

--- dso/branches/4.1.5-rc/deploy/src/test/java/com/tc/server/util/ServerStatTest.java
+++ dso/branches/4.1.5-rc/deploy/src/test/java/com/tc/server/util/ServerStatTest.java
@@ -68,4 +68,88 @@ public class ServerStatTest {
     assertThat(jmxPortResult, is(ServerStat.DEFAULT_TSA_PORT + L2DSOConfigObject.DEFAULT_JMXPORT_OFFSET_FROM_TSAPORT));
   }
 
+  @Test
+  public void testComputeTargetHostWithoutJmxPort() {
+    String expectedHost = "mock.tc.org";
+
+    Server server = mock(Server.class);
+
+    when(server.getHost()).thenReturn(expectedHost);
+
+    String host = ServerStat.computeTargetHost(server);
+    assertThat(host, is(expectedHost));
+  }
+
+  @Test
+  public void testComputeTargetHostWithoutJmxPortBinding() {
+    String expectedHost = "mock.tc.org";
+
+    Server server = mock(Server.class);
+    BindPort bindPort = mock(BindPort.class);
+
+    when(server.getHost()).thenReturn(expectedHost);
+    when(server.getJmxPort()).thenReturn(bindPort);
+
+    String host = ServerStat.computeTargetHost(server);
+    assertThat(host, is(expectedHost));
+  }
+
+  @Test
+  public void testComputeTargetHostWithJmxPortBinding() {
+    String expectedHost = "mock.tc.org";
+
+    Server server = mock(Server.class);
+    BindPort bindPort = mock(BindPort.class);
+
+    when(server.getJmxPort()).thenReturn(bindPort);
+    when(bindPort.getBind()).thenReturn(expectedHost);
+
+    String host = ServerStat.computeTargetHost(server);
+    assertThat(host, is(expectedHost));
+  }
+
+  @Test
+  public void testComputeTargetHostWithInvalidIPv4JmxPortBinding() {
+    String expectedHost = "mock.tc.org";
+    String jmxHostBinding = "0.0.0.0";
+
+    Server server = mock(Server.class);
+    BindPort bindPort = mock(BindPort.class);
+
+    when(server.isSetJmxPort()).thenReturn(true);
+    when(server.getHost()).thenReturn(expectedHost);
+    when(server.getJmxPort()).thenReturn(bindPort);
+    when(bindPort.getBind()).thenReturn(jmxHostBinding);
+
+    String host = ServerStat.computeTargetHost(server);
+    assertThat(host, is(expectedHost));
+  }
+
+  @Test
+  public void testComputeTargetHostWithInvalidIPv6JmxPortBinding() {
+    String expectedHost = "mock.tc.org";
+    String jmxHostBinding = "::";
+
+    Server server = mock(Server.class);
+    BindPort bindPort = mock(BindPort.class);
+
+    when(server.isSetJmxPort()).thenReturn(true);
+    when(server.getHost()).thenReturn(expectedHost);
+    when(server.getJmxPort()).thenReturn(bindPort);
+    when(bindPort.getBind()).thenReturn(jmxHostBinding);
+
+    String host = ServerStat.computeTargetHost(server);
+    assertThat(host, is(expectedHost));
+  }
+
+  @Test
+  public void testComputeTargetHostWithoutJmxPortBindingAndServerHost() {
+    String expectedHost = "localhost";
+
+    Server server = mock(Server.class);
+
+    String host = ServerStat.computeTargetHost(server);
+    assertThat(host, is(expectedHost));
+  }
+
 }
