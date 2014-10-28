2f6809d67524dacc38b81e49c1a61ebcafafe867
speddir <speddir@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-10-28 15:38:06 +0000

    TABINF-3974: re-create branch for 4.3.0-rc build

---
+++ dso/branches/4.3.0-rc/common/src/main/java/com/tc/config/schema/builder/RootConfigBuilder.java
@@ -0,0 +1,12 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.config.schema.builder;
+
+public interface RootConfigBuilder {
+
+  public void setFieldName(String name);
+
+  public void setRootName(String name);
+
+}
