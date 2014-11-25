8f7a7da93fcaeab339f80e549ac3febcdd8250ca
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2014-11-25 00:07:56 +0000

    Tag 4.0.7 from 4.0.7-rc branch

---
+++ dso/tags/4.0.7/devwork/setup.sh
@@ -0,0 +1,6 @@
+# 4.0: https://svn.terracotta.org/repo/internal/enterprise/branches/4.0
+svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/tc-4.0 management-apps
+svn co https://svn.terracotta.org/repo/ehcache/branches/ehcache-2.7.x ehcache
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/ehcache-core-ee-2.7.x ehcache-ee
+# svn co https://svn.terracotta.org/repo/quartz/trunk quartz
+# svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
