163ca3dfb4649010ce083085f9ea02f3b2fc1819
gkeim <gkeim@7fc7bbf3-cf45-46d4-be06-341739edd864>
2015-01-14 03:31:57 +0000

    Update devwork stuff

--- dso/branches/4.2.1-rc/devwork/setup.sh
+++ dso/branches/4.2.1-rc/devwork/setup.sh
@@ -3,4 +3,5 @@
 svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/4.2.1-rc management-apps
 svn co https://svn.terracotta.org/repo/ehcache/branches/4.2.1-rc ehcache
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/4.2.1-rc ehcache-ee
-
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-session/branches/4.2.1-rc terracotta-sessions
+svn co https://svn.terracotta.org/repo/internal/wan40/branches/4.2.1-rc wan40
