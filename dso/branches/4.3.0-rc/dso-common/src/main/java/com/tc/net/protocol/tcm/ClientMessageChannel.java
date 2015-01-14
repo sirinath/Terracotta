dc5afddc7d59ae9f430d070d43ed35ca1708fddf
speddir <speddir@7fc7bbf3-cf45-46d4-be06-341739edd864>
2015-01-14 17:58:57 +0000

    TABINF-3974: re-create branch for 4.3.0-rc build

---
+++ dso/branches/4.3.0-rc/dso-common/src/main/java/com/tc/net/protocol/tcm/ClientMessageChannel.java
@@ -0,0 +1,16 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.net.protocol.tcm;
+
+
+public interface ClientMessageChannel extends MessageChannel {
+
+  public int getConnectCount();
+
+  public int getConnectAttemptCount();
+
+  public ChannelIDProvider getChannelIDProvider();
+
+  public void reopen() throws Exception;
+}
