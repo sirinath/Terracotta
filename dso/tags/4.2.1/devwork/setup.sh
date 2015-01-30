fb2669f9494be643b025f98b4a85ab2e97fa1df9
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2015-01-30 00:11:09 +0000

    Delete 4.2.1 tag to retag.

--- dso/tags/4.2.1/devwork/setup.sh
+++
@@ -1,6 +0,0 @@
-# 4.2: https://svn.terracotta.org/repo/internal/enterprise/branches/4.2.1-rc
-
-svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/4.2.1-rc management-apps
-svn co https://svn.terracotta.org/repo/ehcache/branches/4.2.1-rc ehcache
-svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/4.2.1-rc ehcache-ee
-
