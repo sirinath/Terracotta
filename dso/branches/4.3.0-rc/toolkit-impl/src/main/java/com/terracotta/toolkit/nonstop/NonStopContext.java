6d52f6352649a95720cfd62dde99083d9ec1651e
speddir <speddir@7fc7bbf3-cf45-46d4-be06-341739edd864>
2015-02-06 20:14:24 +0000

    TABINF-3974: re-create branch for 4.3.0-rc build

---
+++ dso/branches/4.3.0-rc/toolkit-impl/src/main/java/com/terracotta/toolkit/nonstop/NonStopContext.java
@@ -0,0 +1,27 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.toolkit.nonstop;
+
+import org.terracotta.toolkit.nonstop.NonStopConfigurationRegistry;
+
+import com.tc.abortable.AbortableOperationManager;
+import com.terracotta.toolkit.ToolkitInitializer;
+
+public interface NonStopContext {
+
+  NonStopManager getNonStopManager();
+
+  NonStopConfigurationRegistry getNonStopConfigurationRegistry();
+
+  AbortableOperationManager getAbortableOperationManager();
+
+  NonstopTimeoutBehaviorResolver getNonstopTimeoutBehaviorResolver();
+
+  ToolkitInitializer getToolkitInitializer();
+
+  NonStopClusterListener getNonStopClusterListener();
+
+  boolean isEnabledForCurrentThread();
+
+}
