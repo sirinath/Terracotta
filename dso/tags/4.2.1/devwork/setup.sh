863c7bb9512bf07c92b093690f03d18e4fbeb5a9
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2015-02-19 21:28:52 +0000

    Tag 4.2.1 from 4.2.1-rc branch

---
+++ dso/tags/4.2.1/devwork/setup.sh
@@ -0,0 +1,7 @@
+# 4.2: https://svn.terracotta.org/repo/internal/enterprise/branches/4.2.1-rc
+
+svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/4.2.1-rc management-apps
+svn co https://svn.terracotta.org/repo/ehcache/branches/4.2.1-rc ehcache
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/4.2.1-rc ehcache-ee
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-session/branches/4.2.1-rc terracotta-sessions
+svn co https://svn.terracotta.org/repo/internal/wan40/branches/4.2.1-rc wan40
