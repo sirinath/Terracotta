85b1c524e274e42e148c54d9bb5983cf5b88de20
speddir <speddir@7fc7bbf3-cf45-46d4-be06-341739edd864>
2015-02-25 16:15:14 +0000

    TABINF-4025 branch for 3.7.10-rc build

---
+++ dso/branches/3.7.10-rc/aspectwerkz/src/main/java/com/tc/aspectwerkz/transform/inlining/weaver/LabelToLineNumberVisitor.java
@@ -0,0 +1,32 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.aspectwerkz.transform.inlining.weaver;
+
+import com.tc.asm.ClassVisitor;
+import com.tc.asm.MethodVisitor;
+import com.tc.asm.Label;
+import com.tc.asm.Opcodes;
+import com.tc.aspectwerkz.transform.InstrumentationContext;
+
+/**
+ * @author <a href="mailto:alex AT gnilux DOT com">Alexandre Vasseur</a>
+ */
+public class LabelToLineNumberVisitor extends ClassVisitor {
+
+  private InstrumentationContext m_ctx;
+
+  public LabelToLineNumberVisitor(ClassVisitor cv, InstrumentationContext ctx) {
+    super(Opcodes.ASM4, cv);
+    m_ctx = ctx;
+  }
+
+  public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+    return new MethodVisitor(Opcodes.ASM4, super.visitMethod(access, name, desc, signature, exceptions)) {
+      public void visitLineNumber(int i, Label label) {
+        super.visitLineNumber(i, label);
+        m_ctx.addLineNumberInfo(label, i);
+      }
+    };
+  }
+}
---
+++ dso/branches/3.7.10-rc/dso-l1/src/main/java/com/tc/object/bytecode/MulticastAnnotationVisitor.java
@@ -0,0 +1,62 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.object.bytecode;
+
+import com.tc.asm.AnnotationVisitor;
+import com.tc.asm.Opcodes;
+
+/**
+ * AnnotationVisitor that is able to delegate all the calls to an array of
+ * other annotation visitors.
+ */
+public class MulticastAnnotationVisitor extends AnnotationVisitor {
+  
+  private final AnnotationVisitor[] visitors;
+  
+  public MulticastAnnotationVisitor(AnnotationVisitor[] visitors) {
+    super(Opcodes.ASM4);
+    this.visitors = visitors;
+  }
+
+  @Override
+  public void visit(String name, Object value) {
+    for (AnnotationVisitor visitor : visitors) {
+      visitor.visit(name, value);
+    }
+  }
+
+  @Override
+  public AnnotationVisitor visitAnnotation(String name, String desc) {
+    AnnotationVisitor[] annotationVisitors = new AnnotationVisitor[visitors.length];
+    for (int i = 0; i < visitors.length; i++) {
+      annotationVisitors[i] = visitors[i].visitAnnotation(name, desc);
+    }
+    
+    return new MulticastAnnotationVisitor(annotationVisitors);
+  }
+
+  @Override
+  public AnnotationVisitor visitArray(String name) {
+    AnnotationVisitor[] annotationVisitors = new AnnotationVisitor[visitors.length];
+    for (int i = 0; i < visitors.length; i++) {
+      annotationVisitors[i] = visitors[i].visitArray(name);
+    }
+    
+    return new MulticastAnnotationVisitor(annotationVisitors);
+  }
+
+  @Override
+  public void visitEnd() {
+    for (AnnotationVisitor visitor : visitors) {
+      visitor.visitEnd();
+    }
+  }
+
+  @Override
+  public void visitEnum(String name, String desc, String value) {
+    for (AnnotationVisitor visitor : visitors) {
+      visitor.visitEnum(name, desc, value);
+    }
+  }
+}
